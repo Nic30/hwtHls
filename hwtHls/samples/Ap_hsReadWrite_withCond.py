d8e140ec835ebe026505a80c4c501a3fdd88a437
Michal Orsak <xorsak01@fit.vutbr.cz>
2016-12-22 19:53:45 +0100

    extract from HWToolkit

---
+++ hwtHls/samples/Ap_hsReadWrite_withCond.py
@@ -0,0 +1,45 @@
+from hwt.interfaces.std import Handshaked
+from hwt.intfLvl import Unit
+from hwtHls.baseSynthesizer import hls
+from hwtHls.codeObjs import FsmNode
+from hwt.synthesizer.shortcuts import toRtl
+
+
+class HlsHs(Handshaked):
+    def __init__(self, *args, **kwargs):
+        super(HlsHs, self).__init__(*args, **kwargs)
+        self._hlsNodes = []
+        
+    def read(self):
+        rNode = FsmNode()
+        rNode.lReady = self.rd
+        rNode.lValid = self.vld
+        rNode.ldata = self.data
+        return rNode
+        
+    def write(self, fsmReadNode):
+        assert fsmReadNode.rValid is None
+        assert fsmReadNode.rReady is None
+        
+        fsmReadNode.rValid = self.vld
+        fsmReadNode.rReady = self.rd
+        fsmReadNode.rData = self.data
+        self._hlsNodes.append(fsmReadNode)
+        
+
+
+class TestHlsUnit(Unit):
+    a = HlsHs()
+    b = HlsHs()
+    
+    @hls
+    def readAndWrite(self):
+        c = self.a.read()
+        self.b.write(c) 
+    
+
+
+if __name__ == "__main__":
+    u = TestHlsUnit()
+    print(toRtl(u))
+
---
+++ hwtHls/samples/vivadoHLSUnit.py
@@ -0,0 +1,12 @@
+from hwt.synthesizer.shortcuts import toRtl
+from hwtHls.vivadoHLS.unit import VivadoHLSUnit
+
+
+class ExactMatcher(VivadoHLSUnit):
+    _top = "exactMatch"
+    _project = "exactMatcherVivadoHls"
+    
+    
+if __name__ == "__main__":
+    print(toRtl(ExactMatcher))
+    print(ExactMatcher._entity)
