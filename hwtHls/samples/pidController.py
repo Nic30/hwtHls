5d06a9a2a35be1f0b6cbdd2b5603dd8d27f2213b
Nic30 <nic30@seznam.cz>
2017-11-05 22:12:23 +0100

    HLS PID controller

--- hwtHls/samples/pidController.py
+++ hwtHls/samples/pidController.py
@@ -1,10 +1,11 @@
-from hwt.synthesizer.unit import Unit
-from hwt.synthesizer.param import Param
+from hwt.code import Add
 from hwt.interfaces.std import VectSignal
 from hwt.interfaces.utils import addClkRstn
-from hwt.code import Add
+from hwt.synthesizer.param import Param
+from hwt.synthesizer.unit import Unit
 from hwt.synthesizer.utils import toRtl
-from hwt.hdl.types.bits import Bits
+from hwtHls.platform.virtual import VirtualHlsPlatform
+from hwtHls.hls import Hls
 
 
 class PidController(Unit):
@@ -35,6 +36,7 @@ class PidController(Unit):
         self.DATAIN_WIDTH = Param(16)
         self.DATAOUT_WIDTH = Param(16)
         self.COEF_WIDTH = Param(16)
+        self.CLK_FREQ = Param(int(100e6))
 
     def _declr(self):
         addClkRstn(self)
@@ -50,6 +52,7 @@ class PidController(Unit):
         err = self._sig("err", dtype=self.input._dtype)
         err(self.input - self.target)
 
+        # create y-pipeline
         y = [self.input, ]
         for i in range(2):
             _y = self._reg("y%d" % i, dtype=self.input._dtype, defVal=0)
@@ -65,6 +68,34 @@ class PidController(Unit):
         self.output(u)
 
 
+class PidControllerHls(PidController):
+    def _impl(self):
+        u = self._reg("u", dtype=self.output._dtype, defVal=0)
+        # create y-pipeline
+        y = [self.input, ]
+        for i in range(2):
+            _y = self._reg("y%d" % i, dtype=self.input._dtype, defVal=0)
+            # feed data from last register
+            _y(y[-1])
+            y.append(_y)
+
+        def trim(signal):
+            return signal._reinterpret_cast(self.output._dtype)
+
+        with Hls(self, freq=self.CLK_FREQ) as hls:
+            r = hls.read
+            err = r(self.input) - r(self.target)
+            a = [r(c) for c in self.coefs]
+            y = [r(_y) for _y in y]
+
+            _u = Add(r(u), a[0] * err, a[1] * y[0],
+                     a[2] * y[1], a[3] * y[2], key=trim)
+            hls.write(_u, u)
+        self.output(u)
+
+
 if __name__ == "__main__":
     u = PidController()
     print(toRtl(u))
+    u = PidControllerHls()
+    print(toRtl(u, targetPlatform=VirtualHlsPlatform()))
