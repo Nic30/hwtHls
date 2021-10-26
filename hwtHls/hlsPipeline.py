0d73679b03ae2896184fcf3fc5f7d6d37fc20703
Nic30 <nic30@seznam.cz>
2021-10-26 13:37:09 +0200

    HlsPipeline: support for coherency check of IO

--- hwtHls/hlsPipeline.py
+++ hwtHls/hlsPipeline.py
@@ -1,9 +1,10 @@
-from typing import List, Dict
+from typing import List, Dict, Union, Optional
 
 from hwt.hdl.statements.assignmentContainer import HdlAssignmentContainer
 from hwt.hdl.types.defs import BIT
 from hwt.interfaces.hsStructIntf import HsStructIntf
 from hwt.interfaces.std import Signal
+from hwt.pyUtils.uniqList import UniqList
 from hwt.synthesizer.interface import Interface
 from hwt.synthesizer.interfaceLevel.unitImplHelpers import getSignalName
 from hwt.synthesizer.rtlLevel.extract_part_drivers import extract_part_drivers
@@ -12,7 +13,7 @@ from hwt.synthesizer.rtlLevel.netlist import RtlNetlist
 from hwt.synthesizer.rtlLevel.remove_unconnected_signals import removeUnconnectedSignals
 from hwt.synthesizer.rtlLevel.rtlSignal import RtlSignal
 from hwt.synthesizer.unit import Unit
-from hwtHls.codeOps import HlsRead, HlsWrite, HlsIO, AbstractHlsOp
+from hwtHls.netlist.codeOps import HlsRead, HlsWrite, HlsIO, AbstractHlsOp
 from hwtHls.errors import HlsSyntaxError
 from hwtHls.hwtNetlistToHwtHlsNetlist import HwtNetlistToHwtHlsNetlist
 from ipCorePackager.constants import DIRECTION
@@ -26,27 +27,46 @@ class HlsPipeline():
     :ivar parentUnit: parent unit where RTL should be instantiated
     :ivar platform: platform with configuration of this HLS context
     :ivar freq: target frequency for RTL (in Hz)
-    :ivar resources: optional resource constrains
+    :ivar resource_constrain: optional resource constrains
+    :ivar inputs: list of HlsRead operations in this pipeline
+    :ivar outputs: list of HlsWrite operations in this pipeline
+    :ivar io_by_interface: dictionary which agregates all io operations by its interface
+    :ivar nodes: list of all schedulization nodes present in this pipeline (except inputs/outputs)
     :ivar ctx: RtlNetlist (container of RTL signals for this HLS context)
         the purpose of objects in this ctx is only to store the input code
         these objecs are not present in output circuit and are only form of code
         themplate which must be translated
+    :ivar allow_io_aggregation: If true the io_by_interface property is used to store
+        the meta of how chunks of code access same interfaces and how the arbitration should be done.
+        If False the assertion error is raised if some IO is accessed on multiple code locations.
     """
 
     def __init__(self, parentUnit: Unit,
-                 freq, resource_constrain=None):
+                 freq: Union[float, int],
+                 resource_constrain=None,
+                 allow_io_aggregation=False,
+                 coherency_checked_io:Optional[UniqList[Interface]]=None):
+        """
+        :see: For parameter meaning see doc of this class.
+        """
         self.parentUnit = parentUnit
         self.platform = parentUnit._target_platform
         if self.platform is None:
-            raise Exception("HLS requires platform to be specified")
+            raise ValueError("HLS requires platform to be specified")
 
         self.clk_period = 1 / int(freq)
         self.resource_constrain = resource_constrain
         self.inputs: List[HlsRead] = []
         self.outputs: List[HlsWrite] = []
+        self.io_by_interface: Dict[Interface, List[Union[HlsRead, HlsWrite]]] = {}
         self.nodes: List[AbstractHlsOp] = []
         self._io: Dict[HlsIO, Interface] = {}
         self.ctx = RtlNetlist()
+        self.allow_io_aggregation = allow_io_aggregation
+        if coherency_checked_io is None:
+            coherency_checked_io = UniqList()
+
+        self.coherency_checked_io: UniqList[Interface] = coherency_checked_io
 
         self.scheduler = self.platform.scheduler(self)
         self.allocator = self.platform.allocator(self)
@@ -69,18 +89,18 @@ class HlsPipeline():
                 w = HlsWrite(self, a.src, a.dst)
                 w.indexes = a.indexes
                 reconnect_endpoint_list(w.indexes, a, w)
+                self.outputs.append(w)
+                self.ctx.statements.add(w)
 
         for a in to_destroy:
             statements.remove(a)
 
-    def _build_data_flow_graph(self, nodes: List[AbstractHlsOp]):
+    def _build_data_flow_graph(self):
         """
         Walk signals and extract operations as data flow graph composed of AbstractHlsOp
 
         (convert from representation with signals
          to directed graph of operations)
-
-        :param nodes: output list for result
         """
         extract_part_drivers(self.ctx)
         self._convert_indexed_io_assignments_to_HlsWrite()
@@ -100,6 +120,7 @@ class HlsPipeline():
                 # ReadOnly
                 r = HlsRead(self, ioIntf, io)
                 self.ctx.interfaces[r] = DIRECTION.IN
+                self.inputs.append(r)
 
             else:
                 raise HlsSyntaxError("Unused IO", io, ioIntf)
@@ -124,7 +145,7 @@ class HlsPipeline():
             # nodeToHlsNode[out] = out
 
         # list of discovered nodes
-        nodes.extend(nodeToHlsNode.values())
+        self.nodes.extend(nodeToHlsNode.values())
 
         for i in self.inputs:
             if i.dependsOn[0] is None:
@@ -134,6 +155,22 @@ class HlsPipeline():
         """
         Convert code template to circuit (netlist of Hdl objects)
         """
+        assert not self.io_by_interface
+        io_by_interface = self.io_by_interface
+        for op in self.inputs:
+            op: HlsRead
+            op_list = io_by_interface.get(op.src, None)
+            if op_list  is None:
+                op_list = io_by_interface[op.src] = []
+            op_list.append(op)
+
+        for op in self.outputs:
+            op: HlsWrite
+            op_list = io_by_interface.get(op.dst, None)
+            if op_list  is None:
+                op_list = io_by_interface[op.dst] = []
+            op_list.append(op)
+
         self.scheduler.schedule(self.resource_constrain)
         self.allocator.allocate()
 
