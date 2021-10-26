b5bf1bc8a81ff377021103826f9740b988d5feb2
Nic30 <nic30@seznam.cz>
2021-10-26 13:36:06 +0200

    asap scheduler: support for arbitrary io nodes

--- hwtHls/scheduler/asap.py
+++ hwtHls/scheduler/asap.py
@@ -1,14 +1,76 @@
-from typing import List, Set, Tuple
-
-from hwt.hdl.operator import isConst
-from hwtHls.codeOps import HlsConst, AbstractHlsOp, HlsRead, HlsWrite, \
-    HlsOperation
-from hwtHls.scheduler.errors import UnresolvedChild, TimeConstraintError
-from math import inf
+from typing import List
+from hwtHls.netlist.codeOps import HlsConst, HlsWrite, HlsOperation
+from hwtHls.scheduler.errors import TimeConstraintError
 from hwtHls.clk_math import start_of_next_clk_period
 
 
-#def child_asap_time(_ch: Tuple[AbstractHlsOp, int]):
+def _asap(node: HlsOperation, clk_period: float) -> float:
+    """
+    The recursive function of ASAP scheduling
+    """
+    if node.asap_end is None:
+        if node.dependsOn:
+            #print(node)
+            input_times = [_asap(d.obj, clk_period)[d.out_i] for d in node.dependsOn if d is not None]
+            # now we have times when the value is available on input
+            # and we must resolve the minimal time so each input timing constraints are satisfied
+            time_when_all_inputs_present = 0.0
+            latest_input_i = None
+            if not hasattr(node, "latency_pre"):
+                raise NotImplementedError(node.usedBy, node)
+            for in_i, (available_in_time, in_delay, in_cycles) in enumerate(
+                zip(input_times, node.latency_pre, node.in_cycles_offset)):
+                next_clk_time = start_of_next_clk_period(available_in_time, clk_period)
+                time_budget = next_clk_time - available_in_time
+                if in_delay >= time_budget:
+                    available_in_time = next_clk_time
+                    if in_delay >= clk_period:
+                        raise TimeConstraintError(
+                            "Impossible scheduling, clk_period too low for ",
+                            node.latency_pre, node.latency_post, node)
+                normalized_time = (available_in_time
+                                   +in_delay
+                                   +in_cycles * clk_period)
+                if normalized_time >= time_when_all_inputs_present:
+                    latest_input_i = in_i
+                    time_when_all_inputs_present = normalized_time
+            if latest_input_i is None:
+                # no input
+                node_zero_time = 0.0
+            else:
+                node_zero_time = (time_when_all_inputs_present
+                                  -node.in_cycles_offset[latest_input_i] * clk_period
+                                  -node.latency_pre[latest_input_i])
+            asap_start = node.asap_start = []
+            asap_end = node.asap_end = []
+
+            for (in_delay, in_cycles) in zip(node.latency_pre, node.in_cycles_offset):
+                asap_start.append(node_zero_time + in_delay + in_cycles * clk_period)
+            for (out_delay, out_cycles) in zip(node.latency_post, node.cycles_latency):
+                asap_end.append(time_when_all_inputs_present + out_delay + out_cycles * clk_period)
+
+        elif isinstance(node, HlsConst):
+            return [0.0, ]
+        else:
+            node.asap_start = [0.0, ]
+            node.asap_end = [0.0, ]
+            if isinstance(node, HlsWrite):
+                node.src.asap_start = [0.0, ]
+                node.src.asap_end = [0.0, ]
+
+    return node.asap_end
+
+
+def asap(outputs: List[HlsWrite], clk_period: float):
+    """
+    As Soon As Possible scheduler
+    * The graph must not contain cycles.
+    * DFS from outputs, decorate nodes with asap_start,asap_end time.
+    """
+    for o in outputs:
+        _asap(o, clk_period)
+
+# def child_asap_time(_ch: Tuple[AbstractHlsOp, int]):
 #    ch, i = _ch
 #    if ch.asap_end is None:
 #        if isinstance(ch, HlsConst):
@@ -45,68 +107,6 @@ from hwtHls.clk_math import start_of_next_clk_period
 #
 #    if newlyDiscoveredInputs:
 #        asap_filter_inputs(newlyDiscoveredInputs, realInputs)
-
-
-def _asap(node: HlsOperation, clk_period: float) -> float:
-    """
-    The recursive function of ASAP scheduling
-    """
-    if node.asap_end is None:
-        if node.dependsOn:
-            input_times = [_asap(d.obj, clk_period)[d.out_i] for d in node.dependsOn if d is not None]
-            # now we have times when the value is available on input
-            # and we must resolve the minimal time so each input timing constraints are satisfied
-            time_when_all_inputs_present = 0.0
-            latest_input_i = None
-            for in_i, (available_in_time, in_delay, in_cycles) in enumerate(
-                zip(input_times, node.latency_pre, node.in_cycles_offset)):
-                next_clk_time = start_of_next_clk_period(available_in_time, clk_period)
-                time_budget = next_clk_time - available_in_time
-                if in_delay >= time_budget:
-                    available_in_time = next_clk_time
-                    if in_delay >= clk_period:
-                        raise TimeConstraintError(
-                            "Impossible scheduling, clk_period too low for ",
-                            node.latency_pre, node.latency_post, node)
-                normalized_time = available_in_time + in_delay + in_cycles * clk_period
-                if normalized_time >= time_when_all_inputs_present:
-                    latest_input_i = in_i
-                    time_when_all_inputs_present = normalized_time
-            if latest_input_i is None:
-                # no input
-                node_zero_time = 0.0
-            else:
-                node_zero_time = time_when_all_inputs_present - node.in_cycles_offset[latest_input_i] * clk_period - node.latency_pre[latest_input_i]
-            asap_start = node.asap_start = []
-            asap_end = node.asap_end = []
-
-            for (in_delay, in_cycles) in zip(node.latency_pre, node.in_cycles_offset):
-                asap_start.append(node_zero_time + in_delay + in_cycles * clk_period)
-            for (out_delay, out_cycles) in zip(node.latency_post, node.cycles_latency):
-                asap_end.append(time_when_all_inputs_present + out_delay + out_cycles * clk_period)
-
-        elif isinstance(node, HlsConst):
-            return [0.0, ]
-        else:
-            node.asap_start = [0.0, ]
-            node.asap_end = [0.0, ]
-            if isinstance(node, HlsWrite):
-                node.src.asap_start = [0.0, ]
-                node.src.asap_end = [0.0, ]
-
-    return node.asap_end
-
-
-def asap(outputs: List[HlsWrite], clk_period: float):
-    """
-    As Soon As Possible scheduler
-
-    DFS from outputs, decorate nodes with asap_start,asap_end time
-
-    :return: maximum schedueled timme
-    """
-    for o in outputs:
-        _asap(o, clk_period)
 
 # def asap(inputs: List[HlsRead], outputs: List[HlsWrite], clk_period: float):
 #    """
