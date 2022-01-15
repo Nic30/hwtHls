b2899318f3a9942eada08caf1816c12230686899
Nic30 <nic30@seznam.cz>
2022-01-15 18:04:45 +0100

    additional tests for AxiStream

---
+++ tests/io/axiStream/axisParseEth.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from hwt.interfaces.hsStructIntf import HsStructIntf
+from hwt.interfaces.utils import addClkRstn
+from hwt.synthesizer.param import Param
+from hwt.synthesizer.unit import Unit
+from hwtHls.hlsStreamProc.streamProc import HlsStreamProc
+from hwtLib.amba.axis import AxiStream
+from hwtLib.types.net.ethernet import Eth2Header_t, eth_mac_t
+
+
+class AxiSParseEth(Unit):
+
+    def _config(self) -> None:
+        self.DATA_WIDTH = Param(512)
+        self.CLK_FREQ = Param(int(100e6))
+
+    def _declr(self):
+        addClkRstn(self)
+        self.clk.FREQ = self.CLK_FREQ
+        with self._paramsShared():
+            self.i = AxiStream()
+            self.dst_mac: HsStructIntf[eth_mac_t] = HsStructIntf()._m()
+            self.dst_mac.T = eth_mac_t
+
+    def _impl(self) -> None:
+        hls = HlsStreamProc(self)
+
+        # :note: the read has to be put somewhere in the code later
+        # because it needs to have a code location where it happens
+        # we declare it as python variable so we do not need to use tmp
+        # variable in hls
+        eth = hls.read(self.i, Eth2Header_t)
+
+        hls.thread(
+            hls.While(True,
+                eth,
+                hls.write(eth.dst, self.dst_mac)
+            )
+        )
+
+
+if __name__ == "__main__":
+    from hwtHls.platform.virtual import VirtualHlsPlatform, makeDebugPasses
+    from hwt.synthesizer.utils import to_rtl_str
+
+    u = AxiSParseEth()
+    p = VirtualHlsPlatform(**makeDebugPasses("tmp"))
+    print(to_rtl_str(u, target_platform=p))
---
+++ tests/io/axiStream/axisParseEth_test.py
@@ -0,0 +1,63 @@
+
+from math import ceil
+import unittest
+
+from hwt.hdl.types.bits import Bits
+from hwt.simulator.simTestCase import SimTestCase
+from hwtHls.platform.virtual import VirtualHlsPlatform
+from hwtLib.amba.axis import axis_send_bytes
+from hwtLib.types.net.ethernet import Eth2Header_t, ETHER_TYPE
+from hwtSimApi.constants import CLK_PERIOD
+from pyMathBitPrecise.bit_utils import  int_to_int_list
+from tests.io.axiStream.axisParseEth import AxiSParseEth
+
+
+class AxiSParseEthTC(SimTestCase):
+
+    def _test_parse(self, DATA_WIDTH):
+        u = AxiSParseEth()
+        u.DATA_WIDTH = DATA_WIDTH
+        self.compileSimAndStart(u, target_platform=VirtualHlsPlatform())
+        dsts = [0x010203040506, 0x111213141516, 0x212223242526, ]
+        for dst in dsts:
+            v = Eth2Header_t.from_py({
+                "dst": dst,
+                "src": 0x778899101112,
+                "type": ETHER_TYPE.IPv4,
+                
+            })
+            v = int(v._reinterpret_cast(Bits(v._dtype.bit_length())))
+            data = int_to_int_list(v, 8, ceil(Eth2Header_t.bit_length() / 8))
+            axis_send_bytes(u.i, data)
+
+        t = CLK_PERIOD * (len(u.i._ag.data) + 5) 
+        self.runSim(t)
+
+        self.assertValSequenceEqual(u.dst_mac._ag.data, dsts, "[%s] != [%s]" % (
+            ", ".join("0x%x" % int(i) for i in u.dst_mac._ag.data),
+            ", ".join("0x%x" % int(i) for i in dsts)
+        ))
+
+    def test_parse_8b(self):
+        self._test_parse(8)
+
+    def test_parse_16b(self):
+        self._test_parse(16)
+
+    def test_parse_24b(self):
+        self._test_parse(24)
+
+    def test_parse_48b(self):
+        self._test_parse(48)
+
+    def test_parse_512b(self):
+        self._test_parse(512)
+
+
+if __name__ == '__main__':
+
+    suite = unittest.TestSuite()
+    # suite.addTest(AxiSParseEthTC('test_parse_24b'))
+    suite.addTest(unittest.makeSuite(AxiSParseEthTC))
+    runner = unittest.TextTestRunner(verbosity=3)
+    runner.run(suite)
