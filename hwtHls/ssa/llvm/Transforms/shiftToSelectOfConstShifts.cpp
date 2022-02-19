ba1b0a1a6f2946af72edf2da435825d34e2911b5
Nic30 <nic30@seznam.cz>
2022-02-19 21:07:13 +0100

    placeholder for ShiftToSelectOfConstShiftPass in cpp

---
+++ hwtHls/ssa/llvm/Transforms/shiftToSelectOfConstShifts.cpp
@@ -0,0 +1,12 @@
+#include "shiftToSelectOfConstShifts.h"
+
+using namespace llvm;
+
+namespace hwtHls {
+
+llvm::PreservedAnalyses ShiftToSelectOfConstShiftPass::run(llvm::Function &F,
+		llvm::FunctionAnalysisManager &AM) {
+	throw std::runtime_error("NotImplemented");
+}
+
+}
