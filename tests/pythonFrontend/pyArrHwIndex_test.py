cbde7c13b5871d2c411c9f61e051c30800fd163f
nic30 <nic30@nic30-Precision-5530>
2022-03-24 17:43:53 +0100

    add test PyArrHwIndex_TC.test_CntrArray_cfgDot

--- tests/pythonFrontend/pyArrHwIndex_test.py
+++ tests/pythonFrontend/pyArrHwIndex_test.py
@@ -1,8 +1,26 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from tests.baseSsaTest import BaseSsaTC
+from tests.baseSsaTest import BaseSsaTC, TestFinishedSuccessfuly
 from tests.pythonFrontend.pyArrHwIndex import Rom, CntrArray
+from hwtHls.hlsStreamProc.streamProc import HlsStreamProc
+from hwtHls.ssa.translation.fromPython.fromPython import PythonBytecodeToSsa
+import os
+from _io import StringIO
+from hwtHls.ssa.transformation.runFn import SsaPassRunFn
+
+
+class CntrArrayWithCfgDotDump(CntrArray):
+
+    def _config(self) -> None:
+        CntrArray._config(self)
+        self.CFG_FILE = None
+
+    def _impl(self):
+        hls = HlsStreamProc(self, freq=int(100e6))
+        c = PythonBytecodeToSsa(hls, self.mainThread)
+        hls._thread(*c.translateFunction(hls))
+        self.blockTracker.dumpCfgToDot(self.CFG_FILE)
 
 
 class PyArrHwIndex_TC(BaseSsaTC):
@@ -13,6 +31,16 @@ class PyArrHwIndex_TC(BaseSsaTC):
         
     def test_CntrArray_ll(self):
         self._test_ll(CntrArray)
+
+    def test_CntrArray_cfgDot(self):
+        buff = StringIO()
+        ssa_passes = [
+            SsaPassRunFn(TestFinishedSuccessfuly.raise_)
+        ]
+        u = CntrArray()
+        u.CFG_FILE = buff
+        self._runTranslation(u, ssa_passes)
+        self.assert_same_as_file(buff.getvalue(), os.path.join("data", "CntrArray_cfg.dot"))
 
 
 if __name__ == "__main__":
