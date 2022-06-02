cbac3e8648ea8e5582235a5565f333fb40c8a3b4
Nic30 <nic30@seznam.cz>
2022-06-02 11:23:18 +0200

    HlsStreamProcPyThread inherit constructor

--- hwtHls/ssa/translation/fromPython/thread.py
+++ hwtHls/ssa/translation/fromPython/thread.py
@@ -6,19 +6,16 @@ from hwt.synthesizer.rtlLevel.rtlSignal import RtlSignal
 from hwtHls.hlsStreamProc.streamProc import HlsStreamProcThread, HlsStreamProc
 from hwtHls.ssa.translation.fromAst.astToSsa import AstToSsa
 from hwtHls.ssa.translation.fromPython.fromPython import PythonBytecodeToSsa
-from hwtHls.ssa.translation.toHwtHlsNetlist.pipelineMaterialization import SsaSegmentToHwPipeline
 from ipCorePackager.constants import DIRECTION
 
 
 class HlsStreamProcPyThread(HlsStreamProcThread):
 
     def __init__(self, hls: HlsStreamProc, fn: FunctionType, *fnArgs, **fnKwargs):
-        self.hls = hls
+        super(HlsStreamProcPyThread, self).__init__(hls)
         self.bytecodeToAst = PythonBytecodeToSsa(hls, fn)
         self.fnArgs = fnArgs
         self.fnKwargs = fnKwargs
-        self.toSsa: Optional[AstToSsa] = None
-        self.toHw: Optional[SsaSegmentToHwPipeline] = None
         self.code = None
         self._imports: List[Tuple[Union[RtlSignal, Interface], DIRECTION.IN]] = [] 
         self._exports: List[Tuple[Union[RtlSignal, Interface], DIRECTION.IN]] = [] 
