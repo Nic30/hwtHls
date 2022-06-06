daae4727daa53a227d90f81571f38aeb58303bbf
Nic30 <nic30@seznam.cz>
2022-06-06 02:41:18 +0200

    doc

--- hwtHls/netlist/analysis/hlsNetlistAnalysisPass.py
+++ hwtHls/netlist/analysis/hlsNetlistAnalysisPass.py
@@ -13,5 +13,9 @@ class HlsNetlistAnalysisPass():
         raise NotImplementedError("Implement this in implementation of this abstract class")
 
     def invalidate(self):
-        "Remove any modification outside of this class when this analysis is invalidated"
+        """
+        Remove any modification outside of this class when this analysis is invalidated
+        :note: to invalidate pass use HlsNetlistCtx.invalidateAnalysis, this function is callback for mentioned function
+        which should be used by the pass to implement additional actions
+        """
         pass
