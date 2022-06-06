65301687d66663ee1e350fae6629a4d55ac1a413
Nic30 <nic30@seznam.cz>
2022-06-06 02:41:35 +0200

    HlsNetlistAnalysisPassBlockSyncType

--- hwtHls/netlist/analysis/blockSyncType.py
+++ hwtHls/netlist/analysis/blockSyncType.py
@@ -1,50 +1,18 @@
-from typing import Set, Tuple, Dict
+from typing import Set, List
 
-from hwt.pyUtils.uniqList import UniqList
-from hwtHls.ssa.analysis.liveness import EdgeLivenessDict
-from hwtHls.ssa.analysis.threadMining import SsaPassThreadMining
+from hwtHls.llvm.llvmIr import MachineBasicBlock, MachineLoopInfo, MachineLoop
+from hwtHls.netlist.analysis.hlsNetlistAnalysisPass import HlsNetlistAnalysisPass
+from hwtHls.netlist.nodes.node import HlsNetNode
 from hwtHls.ssa.basicBlock import SsaBasicBlock
-from hwtHls.ssa.transformation.utils.blockAnalysis import collect_all_blocks
-from hwtHls.ssa.value import SsaValue
+from hwtHls.ssa.translation.llvmToMirAndMirToHlsNetlist.utils import MachineBasicBlockSyncContainer
+from hwtHls.netlist.analysis.dataThreads import HlsNetlistAnalysisPassDataThreads
 
 
-class BlockMeta():
-    """
-    :ivar isCycleEntryPoint: This block is a header of the cycle.
-    :ivar needsControl: This block needs the control channel on the input for its functionality.
-    :ivar requiresStarter: This block requires a provider of initial sync token for its functionality.
-    :ivar isInitialization: This block is run just once on the beginning of the program.
-    :ivar phiCyclicArgs: Set of variables which are written in this block but also read by some PHI.
-    :ivar inLiveVarsWithMultipleSrcBlocks: values which may come from multiple predecessor blocks
-        and the MUX needs to be generated if this should be in pipeline
-    """
-
-    def __init__(self,
-                 isCycleEntryPoint: bool,
-                 needsControl: bool,
-                 requiresStarter: bool,
-                 isInitialization:bool,
-                 phiCyclicArgs: Set[SsaValue],
-                 inLiveVarsWithMultipleSrcBlocks: UniqList[SsaValue]):
-        self.isCycleEntryPoint = isCycleEntryPoint
-        self.needsControl = needsControl
-        self.requiresStarter = requiresStarter
-        self.isInitialization = isInitialization
-        self.phiCyclicArgs = phiCyclicArgs
-        self.inLiveVarsWithMultipleSrcBlocks = inLiveVarsWithMultipleSrcBlocks
-
-    def __repr__(self):
-        flags = []
-        for f in ('isCycleEntryPoint', 'needsControl', 'requiresStarter', 'isInitialization'):
-            if getattr(self, f):
-                flags.append(f)
-        if self.phiCyclicArgs:
-            flags.append(f"phiCyclicArgs={self.phiCyclicArgs}")
-        return f"<{self.__class__.__name__:s} {', '.join(flags)}>"
-
-
-class SaaGetBlockSyncType():
+class HlsNetlistAnalysisPassBlockSyncType(HlsNetlistAnalysisPass):
     '''
+    This pass updates blockSync dictionary in :class:`HlsNetlistAnalysisPassMirToNetlist` with
+    flags which are describing what type of synchronization for block should be used.
+    :note: This is thread level synchronization of control flow in blocks not RTL type of synchronization.
 
     .. code-block:: llvm
 
@@ -71,177 +39,104 @@ class SaaGetBlockSyncType():
             %1 = read()
             br %1 label while0, label while1
 
-    In this case phi can select just based on value of %1 if the channel with its value is initializaed to while0 braching value.
-    However this is not implemented yet and controll channels are generated instead.
-
+    In this case PHI can select just based on value of %1 if the channel with its value is initialized to while0 branching value.
+    However this is not implemented yet and control channels are generated instead.
     '''
 
-    def __init__(self, start: SsaBasicBlock,
-                 out_of_pipeline_edges:Set[Tuple[SsaBasicBlock, SsaBasicBlock]],
-                 edge_var_live:EdgeLivenessDict):
-        self._blockMeta = {}
-        self.start_block = start
-        self.edge_var_live = edge_var_live
-        self.out_of_pipeline_edges = out_of_pipeline_edges
-
-    def _get_SsaBasicBlock_meta(self, block: SsaBasicBlock) -> BlockMeta:
+    def _getBlockMeta(self, mb: MachineBasicBlock):
         """
         The code needs a synchronization if it starts a new thread without data dependencies and has predecessor thread.
         :note: They synchronization is always marked for the start of the thread.
         """
-        m: BlockMeta = self._blockMeta[block]
+        # resolve control enable flag for a block
+        mbSync: MachineBasicBlockSyncContainer = self.blockSync[mb]
+        mbThreads: List[Set[HlsNetNode]] = self.threadsPerBlock[mb]
+        loops: MachineLoopInfo = self.loops
 
-        threads = self.threadsPerBlock[block]
-        predThreadIds = set()
-        for pred in block.predecessors:
+        predThreadIds: Set[int] = set()
+        for pred in mb.predecessors():
+            pred: MachineBasicBlock
             predThreadIds.update(id(t) for t in self.threadsPerBlock[pred])
-        threadsStartingThere = [t for t in self.threadsPerBlock[block] if id(t) not in predThreadIds]
+        threadsStartingThere = [t for t in mbThreads if id(t) not in predThreadIds]
 
-        if m.isCycleEntryPoint:
+        if mb.pred_size() == 0:
+            mbSync.needsStarter = True
+
+        if self.loops.isLoopHeader(mb):
+            loop: MachineLoop = loops.getLoopFor(mb)
             # The synchronization is not required if it could be only by the data itself.
             # It can be done by data itself if there is an single output/write which has all
             # input as transitive dependencies (unconditionally.) And if this is an infinite cycle.
             # So we do not need to check the number of executions.
-            needsControlOld = m.needsControl
-            if not m.needsControl:
-                if len(threads) > 1 or len(block.predecessors) > 2 or bool(block.phis):
+            needsControlOld = mbSync.needsControl
+            if not mbSync.needsControl:
+                
+                if len(mbThreads) > 1 or mb.pred_size() > 1:
                     # multiple independent threads in body or more entry points to a loop
-                    m.needsControl = True
+                    loopBodySelfSynchronized = True
+                    for pred in mb.predecessors():
+                        pred: MachineBasicBlock
+                        if not loop.containsBlock(pred) and not self.blockSync[pred].needsStarter:
+                            loopBodySelfSynchronized = False
+                            break
+                            
+                    if loopBodySelfSynchronized and mb.pred_size() == 2:
+                        pass
+                    else:
+                        mbSync.needsControl = True
 
                 else:
-                    for cond, suc in block.successors.targets:
-                        if cond is not None:
-                            m.needsControl = True
-                            break
+                    if mb.succ_size() > 1:
+                        mbSync.needsControl = True
 
-                    if not m.needsControl:
+                    if not mbSync.needsControl:
                         sucThreads = sum((len(self.threadsPerBlock[suc])
-                                          for suc in block.successors.iterBlocks()))
+                                          for suc in mb.successors.iterBlocks()))
                         if sucThreads > 1:
-                            m.needsControl = True
+                            mbSync.needsControl = True
 
-                        # and not any(bool(suc.phis) for suc in block.successors.iterBlocks()):
-                        # pred = block.predecessors[0]
-                        # if (pred, block) in self.out_of_pipeline_edges:
-                        #    pred = block.predecessors[1]
-                        #
-                        # assert (pred, block) not in self.out_of_pipeline_edges
-                        # for cond, suc in pred.successors.targets:
-                        #    if suc is block and cond is None:
-                        #        m.needsControl = len(self.threadsPerBlock[pred]) > 1
-                        #        if m.needsControl:
-                        #            break
+            if not needsControlOld and mbSync.needsControl:
+                self._onBlockNeedsControl(mb)
 
-            if m.needsControl:
-                m.requiresStarter = len(block.predecessors) == 1
-            if not needsControlOld and m.needsControl:
-                self._onBlockNeedsControl(block)
-        else:
-            m.requiresStarter = (
-                not block.predecessors and
-                (
-                    len(threadsStartingThere) >= 1 or
-                    len(block.successors) != 1
-                )
-            )
-            # sucThreadCnt = sum((len(self.threadsPerBlock[suc]) for suc in block.successors.iterBlocks()))
-            # predThreadCnt = sum((len(self.threadsPerBlock[suc]) for suc in block.predecessors))
-            if not m.needsControl:
-                m.needsControl = (
-                    m.requiresStarter or
-                    # (len(threads) >= 1 and not self.edge_var_live) or
-                    # (self._inputVariableCnt(block) == 0 and bool(threads)) or
-                    # ( predThreadCnt > 1) or
-                    # sucThreadCnt > 1 or
-                    len(threadsStartingThere) > 1 or
-                    (bool(threads) and
-                        (
-                            any(self._blockMeta[pred].needsControl for pred in block.predecessors) or
-                            any(self._blockMeta[suc].needsControl for suc in block.successors.iterBlocks())
-                        )
+        elif not mbSync.needsControl:
+            mbSync.needsControl = (
+                len(threadsStartingThere) > 1 or
+                (bool(mbThreads) and
+                    (
+                        any(self.blockSync[pred].needsControl for pred in mb.predecessors()) or
+                        any(self.blockSync[suc].needsControl for suc in mb.successors())
                     )
                 )
-                if m.needsControl:
-                    self._onBlockNeedsControl(block)
+            )
+            if mbSync.needsControl:
+                self._onBlockNeedsControl(mb)
 
-    def _onBlockNeedsControl(self, block: SsaBasicBlock):
-        for pred in block.predecessors:
-            m: BlockMeta = self._blockMeta[pred]
-            if not m.needsControl:
-                m.needsControl = True
-                if not m.requiresStarter and not pred.predecessors:
-                    m.requiresStarter = True
+    def _onBlockNeedsControl(self, mb: SsaBasicBlock):
+        for pred in mb.predecessors():
+            mbSync: MachineBasicBlockSyncContainer = self.blockSync[pred]
+            if not mbSync.needsControl:
+                mbSync.needsControl = True
+                if not mbSync.needsStarter and not pred.pred_size():
+                    mbSync.needsStarter = True
 
                 self._onBlockNeedsControl(pred)
 
-        for suc in block.successors.iterBlocks():
-            m: BlockMeta = self._blockMeta[suc]
-            if not m.needsControl:
-                m.needsControl = True
+        for suc in mb.successors():
+            mbSync: MachineBasicBlockSyncContainer = self.blockSync[suc]
+            if not mbSync.needsControl:
+                mbSync.needsControl = True
                 self._onBlockNeedsControl(suc)
 
-    def _inputVariableCnt(self, block: SsaBasicBlock):
-        return sum(
-            len(self.edge_var_live.get(pred, {}).get(block, ()))
-            for pred in block.predecessors
-        )
 
-    def collectPhiCyclicArgs(self, block: SsaBasicBlock, m: BlockMeta):
-        liveIn = set()
-        liveOut = set()
-        for pred in block.predecessors:
-            liveIn.update(self.edge_var_live[pred][block])
-        for suc in block.successors.iterBlocks():
-            liveOut.update(self.edge_var_live[block][suc])
-        for v in liveIn.intersection(liveOut):
-            if v.block is block:
-                m.phiCyclicArgs.add(v)
+    def run(self):
+        from hwtHls.ssa.translation.llvmToMirAndMirToHlsNetlist.mirToNetlist import HlsNetlistAnalysisPassMirToNetlist
+        originalMir: HlsNetlistAnalysisPassMirToNetlist = self.netlist.requestAnalysis(HlsNetlistAnalysisPassMirToNetlist)
+        threads: HlsNetlistAnalysisPassDataThreads = self.netlist.requestAnalysis(HlsNetlistAnalysisPassDataThreads)
+        self.threadsPerBlock = threads.threadsPerBlock
+        self.blockSync = originalMir.blockSync
+        self.loops: MachineLoopInfo = originalMir.loops
 
-    def collectCycles(self, block: SsaBasicBlock, m: BlockMeta):
-        for pred in block.predecessors:
-            if (pred, block) in self.out_of_pipeline_edges:
-                m.isCycleEntryPoint = True
-                break
+        for mb in originalMir.mf:
+            mb: MachineBasicBlock
+            self._getBlockMeta(mb)
 
-        if m.isCycleEntryPoint:
-            self.collectPhiCyclicArgs(block, m)
-
-    def markInitializations(self):
-        block = self.start_block
-        while len(block.predecessors) <= 1:
-            m: BlockMeta = self._blockMeta[block]
-            m.isInitialization = True
-            if len(block.successors.targets) == 1:
-                block = block.successors.targets[0][1]
-            else:
-                break
-
-    def collectLiveInVarsWithMultipleSrcBlocks(self, block: SsaBasicBlock):
-        seenInLiveVars: UniqList[SsaValue] = UniqList()
-        inLiveVarsWithMultipleSrcBlocks: UniqList[SsaValue] = UniqList()
-        for pred in block.predecessors:
-            block_var_live = self.edge_var_live.get(pred, {})
-            for v in block_var_live.get(block, ()):
-                if v in seenInLiveVars:
-                    inLiveVarsWithMultipleSrcBlocks.append(v)
-                else:
-                    seenInLiveVars.append(v)
-        return inLiveVarsWithMultipleSrcBlocks
-        
-    def apply(self):
-        threadMining = SsaPassThreadMining(self.out_of_pipeline_edges)
-        threadMining.apply(self.start_block)
-        self.threadsPerBlock = threadMining.threadsPerBlock
-        self._blockMeta: Dict[SsaBasicBlock, BlockMeta] = {}
-        blocks = list(collect_all_blocks(self.start_block, set()))
-
-        for block in blocks:
-            m = self._blockMeta[block] = BlockMeta(False, False, False, False,
-                                                   set(), self.collectLiveInVarsWithMultipleSrcBlocks(block))
-            self.collectCycles(block, m)
-        self.markInitializations()
-
-        for b in blocks:
-            self._get_SsaBasicBlock_meta(b)
-
-        return self._blockMeta
