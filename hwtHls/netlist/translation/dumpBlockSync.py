c07f44dec0ddab8d30affbdabd21560e88e17f11
Nic30 <nic30@seznam.cz>
2022-06-06 02:44:09 +0200

    HlsNetlistPassDumpBlockSync

---
+++ hwtHls/netlist/translation/dumpBlockSync.py
@@ -0,0 +1,31 @@
+from io import StringIO
+from typing import Dict
+
+from hwtHls.llvm.llvmIr import MachineBasicBlock, MachineFunction
+from hwtHls.netlist.context import HlsNetlistCtx
+from hwtHls.netlist.transformation.hlsNetlistPass import HlsNetlistPass
+from hwtHls.platform.fileUtils import OutputStreamGetter
+from hwtHls.ssa.translation.llvmToMirAndMirToHlsNetlist.utils import MachineBasicBlockSyncContainer
+
+
+class HlsNetlistPassDumpBlockSync(HlsNetlistPass):
+
+    def __init__(self, outStreamGetter: OutputStreamGetter):
+        self.outStreamGetter = outStreamGetter
+
+    def _printBlockSync(self, mf: MachineFunction, blockSync: Dict[MachineBasicBlock, MachineBasicBlockSyncContainer], out: StringIO):
+        for mb in mf:
+            mb: MachineBasicBlock
+            mbSync: MachineBasicBlockSyncContainer = blockSync[mb]
+            out.write(f"{mb.getName().str():s}: {mbSync}\n")
+        
+    def apply(self, hls: "HlsStreamProc", netlist: HlsNetlistCtx):
+        from hwtHls.ssa.translation.llvmToMirAndMirToHlsNetlist.mirToNetlist import HlsNetlistAnalysisPassMirToNetlist
+        toNetlist: HlsNetlistAnalysisPassMirToNetlist = netlist.requestAnalysis(HlsNetlistAnalysisPassMirToNetlist)
+        out, doClose = self.outStreamGetter(netlist.parentUnit._getDefaultName())
+        try:
+            self._printBlockSync(toNetlist.mf, toNetlist.blockSync, out)
+        finally:
+            if doClose:
+                out.close()
+
