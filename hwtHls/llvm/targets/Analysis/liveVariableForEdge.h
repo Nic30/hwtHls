5f921cab2b9a252dad56e36681685563182acb2f
Nic30 <nic30@seznam.cz>
2022-06-13 22:48:23 +0200

    doc

--- hwtHls/llvm/targets/Analysis/liveVariableForEdge.h
+++ hwtHls/llvm/targets/Analysis/liveVariableForEdge.h
@@ -6,5 +6,11 @@
 namespace hwtHls {
 using EdgeLivenessDict = std::map<llvm::MachineBasicBlock*, std::map<llvm::MachineBasicBlock*, std::set<llvm::Register>>>;
 
+/*
+ * Get set of registers which are live on edge between two blocks.
+ * Live register means that it is used by some successor block.
+ * When resolving register live the input PHIs are taken in account and if the value is not selected by PHI on this path it is not live.
+ * This means that this does not correspond to live variables of the block.
+ * */
 EdgeLivenessDict getLiveVariablesForBlockEdge(llvm::MachineFunction &MF);
 }
