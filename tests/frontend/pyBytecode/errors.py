f86dfc29f48849aede7de4d2fadbc2d4f731d90b
Nic30 <nic30@seznam.cz>
2022-06-22 13:56:15 +0200

    PyBytecodeErrors_TC

---
+++ tests/frontend/pyBytecode/errors.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from hwt.hdl.types.defs import BIT
+from hwt.interfaces.std import VectSignal
+from hwt.synthesizer.unit import Unit
+from hwtHls.frontend.pyBytecode.thread import HlsThreadFromPy
+from hwtHls.scope import HlsScope
+
+
+class ErrorUseOfUnitialized0(Unit):
+
+    def _declr(self):
+        self.i = VectSignal(8, signed=False)
+        self.o = VectSignal(8, signed=False)._m()
+
+    def mainThread(self, hls: HlsScope):
+        res = a < 1 
+        while BIT.from_py(1):
+            hls.write(hls.read(self.i), self.o)
+
+    def _impl(self):
+        hls = HlsScope(self, freq=int(100e6))
+        hls.addThread(HlsThreadFromPy(hls, self.mainThread, hls))
+        hls.compile()
+
+
+class ErrorUseOfUnitialized1(ErrorUseOfUnitialized0):
+
+    def mainThread(self, hls: HlsScope):
+        res = a < 1 
+        a = 1
+        while BIT.from_py(1):
+            hls.write(res, self.o)
+
+
+class UseOfNone(ErrorUseOfUnitialized0):
+
+    def mainThread(self, hls: HlsScope):
+        a = None
+        while BIT.from_py(1):
+            hls.write(a is None, self.o)
+
+
+if __name__ == "__main__":
+    from hwt.synthesizer.utils import to_rtl_str
+    from hwtHls.platform.virtual import VirtualHlsPlatform
+    u = UseOfNone()
+    print(to_rtl_str(u, target_platform=VirtualHlsPlatform(debugDir="tmp")))
---
+++ tests/frontend/pyBytecode/errors_test.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from hwt.simulator.simTestCase import SimTestCase
+from hwtHls.errors import HlsSyntaxError
+from hwtHls.platform.virtual import VirtualHlsPlatform
+from hwtSimApi.utils import freq_to_period
+from tests.frontend.pyBytecode.errors import ErrorUseOfUnitialized0, ErrorUseOfUnitialized1, UseOfNone
+
+
+class PyBytecodeErrors_TC(SimTestCase):
+    __FILE__ = __file__
+
+    def _testEndsWithErr(self, unitCls, errCls):
+        with self.assertRaises(errCls):
+            self.compileSimAndStart(unitCls(), target_platform=VirtualHlsPlatform())
+        
+    def test_ErrorUseOfUnitialized0(self):
+        self._testEndsWithErr(ErrorUseOfUnitialized0, HlsSyntaxError)
+
+    def test_ErrorUseOfUnitialized1(self):
+        self._testEndsWithErr(ErrorUseOfUnitialized1, HlsSyntaxError)
+    
+    def test_UseOfNone(self):
+        u = UseOfNone()
+        self.compileSimAndStart(u, target_platform=VirtualHlsPlatform())
+        CLK_PERIOD = freq_to_period(int(100e6))
+        self.runSim(3 * int(CLK_PERIOD))
+
+        self.assertValSequenceEqual(u.o._ag.data, [1, 1, 1])
+
+
+if __name__ == "__main__":
+    import unittest
+
+    suite = unittest.TestSuite()
+    # suite.addTest(PyBytecodeErrors_TC('test_ErrorUseOfUnitialized0'))
+    suite.addTest(unittest.makeSuite(PyBytecodeErrors_TC))
+    runner = unittest.TextTestRunner(verbosity=3)
+    runner.run(suite)
