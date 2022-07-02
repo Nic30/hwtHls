f6d53847c823cf8169c24cc71ab6b420c86502b1
Nic30 <nic30@seznam.cz>
2022-07-02 12:57:03 +0200

    doc

--- hwtHls/ssa/transformation/ssaPass.py
+++ hwtHls/ssa/transformation/ssaPass.py
@@ -3,4 +3,4 @@
 class SsaPass():
 
     def apply(self, hls: "HlsScope", to_ssa: "HlsAstToSsa"):
-        raise NotImplementedError("Should be implemented in inheriting class", self)
+        raise NotImplementedError("Should be implemented in child class", self)
