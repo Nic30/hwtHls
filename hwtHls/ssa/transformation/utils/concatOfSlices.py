8a1479034e9e9be9536800e4b177831a77f3caf3
Nic30 <nic30@seznam.cz>
2022-08-09 19:00:00 +0200

    ConcatOfSlices: aggregate immediately

--- hwtHls/ssa/transformation/utils/concatOfSlices.py
+++ hwtHls/ssa/transformation/utils/concatOfSlices.py
@@ -1,8 +1,9 @@
+from copy import copy
+from itertools import islice
 from typing import List, Union, Tuple, Optional
 
 from hwt.hdl.value import HValue
 from hwtHls.ssa.value import SsaValue
-from copy import copy
 
 
 class ConcatOfSlices():
@@ -15,13 +16,16 @@ class ConcatOfSlices():
     def __init__(self, slices: List[Union[Tuple[Union[HValue, SsaValue], int, int], Union[HValue, SsaValue]]]):
         # normalize slices to a format List[Tuple[Union[HValue, SsaValue], int, int]]
         w = 0
-        _slices:Optional[List] = None
+        _slices:Optional[List[Tuple[Union[HValue, SsaValue], int, int]]] = None
         for i, s in enumerate(slices):
             if isinstance(s, ConcatOfSlices):
                 if _slices is None:
-                    _slices = []
-
-                _slices.extend(s.slices)
+                    _slices = [x for x in s.slices]
+                else:
+                    assert s.slices, s
+                    self._appendToSlices(_slices, s.slices[0])
+                    _slices.extend(islice(s.slices, 0, None))
+                
                 w += s.bit_length
                 continue
 
@@ -38,7 +42,7 @@ class ConcatOfSlices():
                 w += width
 
             if _slices is not None:
-                _slices.append(s)
+                self._appendToSlices(_slices, s)
 
         if _slices is not None:
             slices = _slices
@@ -46,9 +50,28 @@ class ConcatOfSlices():
         self.slices: List[Tuple[Union[HValue, SsaValue], int, int]] = slices
         self.bit_length = w
 
+    @staticmethod
+    def _appendToSlices(res: List[Tuple[Union[HValue, SsaValue], int, int]], item: Tuple[Union[HValue, SsaValue], int, int]):
+        if res:
+            last = res[-1]
+            if last[0] is item[0]:
+                # consecutive slices on same variable
+                # high to low
+                prevLow = last[2]
+                itemHigh = item[1]
+                if prevLow == itemHigh:
+                    res[-1] = (last[0], last[1], item[0])
+                    return
+
+        res.append(item)
+
     def concat(self, other: 'ConcatOfSlices'):
         res = ConcatOfSlices(())
-        res.slices = self.slices + other.slices
+        res.slices = [x for x in self.slices]
+        assert other.slices, other
+        self._appendToSlices(res.slices, other.slices[0])
+        res.slices.extend(islice(other.slices, 1, None))
+
         res.bit_length = self.bit_length + other.bit_length
         return res
 
