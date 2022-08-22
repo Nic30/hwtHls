6f0a5db7e9f5cb9ec0a83e30a6f39c8bd7f92a6c
Nic30 <nic30@seznam.cz>
2022-08-22 14:46:20 +0200

    doc

--- hwtHls/architecture/transformation/controlChannelSimplify.py
+++ hwtHls/architecture/transformation/controlChannelSimplify.py
@@ -2,9 +2,9 @@
 
 class RtlAllocatorPassControlChannelSymplify():
     """
-    * reduce parallel sync channels (if multiple channels lead from same source to same destion they are dudundat 1 is enough)
-    * reduce hanshake always satisfier cycles
+    * reduce parallel sync channels (if multiple channels lead from same source to same destination they are redundant 1 is enough)
+    * reduce handshake always satisfier cycles
        * from same to same -> no sync required
-       * a cycle with multiple IO which does not genereate additional transactions
+       * a cycle with multiple IO which does not generate additional transactions
           *
     """
