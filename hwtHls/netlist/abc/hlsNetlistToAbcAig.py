db2bb12e328345d8069a84fb5ccc1eedd5e30ced
Nic30 <nic30@seznam.cz>
2022-08-28 12:54:15 +0200

    HlsNetlistToAbcAig: support TERNARY with arbitrary operand cnt

--- hwtHls/netlist/abc/hlsNetlistToAbcAig.py
+++ hwtHls/netlist/abc/hlsNetlistToAbcAig.py
@@ -1,14 +1,15 @@
-from typing import Dict, Tuple, Optional
+from typing import Dict, Tuple
 
-from hwt.hdl.operator import Operator
 from hwt.hdl.operatorDefs import AllOps
+from hwt.pyUtils.arrayQuery import grouper
 from hwt.pyUtils.uniqList import UniqList
 from hwt.synthesizer.rtlLevel.rtlSignal import RtlSignal
-from hwtHls.netlist.abc.abcCpp import Abc_Ntk_t, Abc_Aig_t, Abc_NtkType_t, Abc_NtkFunc_t, Abc_Frame_t, Abc_Obj_t, Abc_ObjType_t
-from hwtHls.netlist.nodes.ports import HlsNetNodeOut
-from hwtHls.netlist.nodes.ops import HlsNetNodeOperator
+from hwtHls.netlist.abc.abcCpp import Abc_Ntk_t, Abc_Aig_t, Abc_Frame_t, Abc_Obj_t
 from hwtHls.netlist.abc.rtlNetlistToAbcAig import RtlNetlistToAbcAig
 from hwtHls.netlist.nodes.const import HlsNetNodeConst
+from hwtHls.netlist.nodes.ops import HlsNetNodeOperator
+from hwtHls.netlist.nodes.ports import HlsNetNodeOut
+from pyMathBitPrecise.bit_utils import ValidityError
 
 
 class HlsNetlistToAbcAig(RtlNetlistToAbcAig):
@@ -24,7 +25,10 @@ class HlsNetlistToAbcAig(RtlNetlistToAbcAig):
 
         d = o.obj
         if isinstance(d, HlsNetNodeConst):
-            v = int(d.val)
+            try:
+                v = int(d.val)
+            except ValidityError:
+                v = 0
             if v == 1:
                 res = self.c1
             else:
@@ -34,11 +38,12 @@ class HlsNetlistToAbcAig(RtlNetlistToAbcAig):
             assert isinstance(d, HlsNetNodeOperator), d
             d: HlsNetNodeOperator
             op = d.operator
-            if len(d._inputs) == 1:
+            inCnt = len(d._inputs)
+            if inCnt == 1:
                 assert d.operator == AllOps.NOT, d
                 res = aig.Not(self._translate(aig, d.dependsOn[0]))
     
-            elif len(d._inputs) == 2:
+            elif inCnt == 2:
                 lhs, rhs = (self._translate(aig, i) for i in d.dependsOn)
                 if op == AllOps.AND:
                     res = aig.And(lhs, rhs)
@@ -49,11 +54,28 @@ class HlsNetlistToAbcAig(RtlNetlistToAbcAig):
                 else:
                     raise NotImplementedError(d)
     
-            elif len(d._inputs) == 3:
+            elif inCnt >= 3:
                 assert d.operator == AllOps.TERNARY
-                o0, c, o1 = (self._translate(aig, i) for i in d.dependsOn)
-                res = aig.Mux(c, o1, o0) # ABC notation is in in this order
-    
+                if inCnt == 3:
+                    o0, c, o1 = (self._translate(aig, i) for i in d.dependsOn)
+                    res = aig.Mux(c, o1, o0)  # ABC notation is in in this order
+                else:
+                    assert inCnt % 2 == 1, d
+                    prevCond = None
+                    prevVal = None
+                    # mux must be build from end so first condition ends up at the top
+                    for v, c in grouper(2, reversed(d.dependsOn), padvalue=None):
+                        v = self._translate(aig, v)
+                        if c is not None:
+                            c = self._translate(aig, c)
+                        if prevVal is None:
+                            assert prevCond is None
+                            prevVal = v
+                        else:
+                            prevVal = aig.Mux(prevCond, v, prevVal)
+                        prevCond = c
+                    assert prevCond is None, (prevCond, d)
+                    res = prevVal
             else:
                 raise NotImplementedError(d)
             
