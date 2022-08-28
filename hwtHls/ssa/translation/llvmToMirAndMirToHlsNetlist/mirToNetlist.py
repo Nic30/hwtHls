21706fbad99a1696b3e53e0bf15ad7077d9fcc73
Nic30 <nic30@seznam.cz>
2022-08-28 13:00:47 +0200

    HlsNetlistAnalysisPassMirToNetlist._injectVldMaskToExpr:result caching

--- hwtHls/ssa/translation/llvmToMirAndMirToHlsNetlist/mirToNetlist.py
+++ hwtHls/ssa/translation/llvmToMirAndMirToHlsNetlist/mirToNetlist.py
@@ -521,34 +521,45 @@ class HlsNetlistAnalysisPassMirToNetlist(HlsNetlistAnalysisPassMirToNetlistDatap
 
         self._injectVldMaskToSkipWhenConditions()
 
-    def _injectVldMaskToExpr(self, out: HlsNetNodeOut) -> HlsNetNodeOut:
+    def _injectVldMaskToExpr(self, out: HlsNetNodeOut, maskAppliedTo: Set[HlsNetNodeOut], maskForOut: Dict[HlsNetNodeOut, Tuple[HlsNetNodeOut, HlsNetNodeOut]]) -> HlsNetNodeOut:
         """
         For channels which are read optionally we may have to mask incoming data if the data is used directly in this clock cycle
         to decide if some IO channel should be enabled.
         """
         assert isinstance(out, HlsNetNodeOut), (out, "When this function is called every output should be already resolved")
         outObj = out.obj
+        m = maskForOut.get(outObj, None)
+        if m is not None:
+            return m
+        
         builder = self.builder
+        
+        maskToApply = None
         if isinstance(outObj, HlsNetNodeReadSync):
             return out, None
 
         elif isinstance(outObj, (HlsNetNodeRead, HlsNetNodeWrite)):
             maskToApply = builder.buildReadSync(out)
+            maskAppliedTo.add(maskToApply)
 
         elif isinstance(outObj, HlsNetNodeOperator):
             ops = []
             needsRebuild = False
-            maskToApply = None
             for o in outObj.dependsOn:
-                _o, _maskToApply = self._injectVldMaskToExpr(o)
+                if o in maskAppliedTo:
+                    continue
+
+                _o, _maskToApply = self._injectVldMaskToExpr(o, maskAppliedTo, maskForOut)
                 ops.append(_o)
                 if _o is not o:
                     needsRebuild = True
+
                 if _maskToApply is not None:
                     if maskToApply is None:
                         maskToApply = _maskToApply
                     elif maskToApply is not _maskToApply:
                         maskToApply = builder.buildAnd(maskToApply, _maskToApply)
+                        maskAppliedTo.add(maskToApply)
                     else:
                         pass
 
@@ -563,15 +574,16 @@ class HlsNetlistAnalysisPassMirToNetlist(HlsNetlistAnalysisPassMirToNetlistDatap
         elif isinstance(outObj, HlsNetNodeExplicitSync):
             # inject mask to expression on other side of this node
             oldI = outObj.dependsOn[0]
-            newI, maskToApply = self._injectVldMaskToExpr(outObj.dependsOn[0])
-            if newI is not oldI:
-                builder.replaceInputDriver(outObj._inputs[0], newI)
-            # return original out because we did not modify the node itself
-        else:
-            maskToApply = None
+            if oldI not in maskAppliedTo:
+                newI, maskToApply = self._injectVldMaskToExpr(outObj.dependsOn[0], maskAppliedTo, maskForOut)
+                if newI is not oldI:
+                    builder.replaceInputDriver(outObj._inputs[0], newI)
+                # return original out because we did not modify the node itself
 
         if maskToApply is not None and out._dtype.bit_length() == 1:
-            return builder.buildAnd(out, maskToApply), None
+            res = builder.buildAnd(out, maskToApply)
+            maskAppliedTo.add(res)
+            return res, None
         else:
             return out, maskToApply
         
@@ -583,12 +595,20 @@ class HlsNetlistAnalysisPassMirToNetlist(HlsNetlistAnalysisPassMirToNetlistDatap
         To have expression as simple as possible we add this "and" to top most 1b signal generated from the input.
         (There must be some because the original condition is 1b wide.)
         """
+        i = 0
+        maskAppliedTo: Set[HlsNetNodeOut] = set()
+        maskForOut: Dict[HlsNetNodeOut, Tuple[HlsNetNodeOut, HlsNetNodeOut]] = {}
         for n in self.netlist.iterAllNodes():
             if isinstance(n, HlsNetNodeExplicitSync):
+                # print("injecting", i)
+                i += 1
                 n: HlsNetNodeExplicitSync
                 if n.skipWhen is not None:
                     o = n.dependsOn[n.skipWhen.in_i]
-                    _o, maskToApply = self._injectVldMaskToExpr(o)
+                    if o in maskAppliedTo:
+                        continue
+
+                    _o, maskToApply = self._injectVldMaskToExpr(o, maskAppliedTo, maskForOut)
                     assert maskToApply is None, "Should be already applied, because this should be 1b signal"
                     if o is not _o:
                         self.builder.replaceInputDriver(n.skipWhen, _o)
