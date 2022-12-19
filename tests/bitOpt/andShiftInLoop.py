85797eef84633bda371e99eae8f06d78ea782f32
Nic30 <nic30@seznam.cz>
2022-12-19 20:18:02 +0100

    tests: add more AndShiftInLoop variants

--- tests/bitOpt/andShiftInLoop.py
+++ tests/bitOpt/andShiftInLoop.py
@@ -9,6 +9,7 @@ from hwt.simulator.simTestCase import SimTestCase
 from hwt.synthesizer.param import Param
 from hwt.synthesizer.unit import Unit
 from hwtHls.frontend.pyBytecode.thread import HlsThreadFromPy
+from hwtHls.platform.platform import HlsDebugBundle
 from hwtHls.platform.virtual import VirtualHlsPlatform
 from hwtHls.scope import HlsScope
 from hwtLib.examples.errors.combLoops import freeze_set_of_sets
@@ -17,7 +18,38 @@ from hwtSimApi.utils import freq_to_period
 from pyMathBitPrecise.bit_utils import mask
 
 
-class AndShiftInLoop(Unit):
+class AndShiftInLoop2(Unit):
+
+    def _config(self) -> None:
+        self.FREQ = Param(int(100e6))
+
+    def _declr(self):
+        addClkRstn(self)
+        self.clk.FREQ = self.FREQ
+        self.i = VectSignal(8, signed=False)
+        self.o = VectSignal(8, signed=False)._m()
+
+    def mainThread(self, hls: HlsScope):
+        t = self.i._dtype
+        i0 = hls.var("i0", t)
+        i1 = hls.var("i0", t)
+        i0 = mask(8)
+        i1 = mask(8)
+        while BIT.from_py(1):
+            i = hls.read(self.i).data
+            i0 &= i
+            i1 &= (i0 << 1)
+            hls.write(i1, self.o)
+
+    def _impl(self):
+        hls = HlsScope(self)
+        mainThread = HlsThreadFromPy(hls, self.mainThread, hls)
+        # mainThread.bytecodeToSsa.debug = True
+        hls.addThread(mainThread)
+        hls.compile()
+
+
+class AndShiftInLoop3(Unit):
 
     def _config(self) -> None:
         self.FREQ = Param(int(100e6))
@@ -42,7 +74,7 @@ class AndShiftInLoop(Unit):
             hls.write(i2, self.o)
 
     def _impl(self):
-        hls = HlsScope(self, freq=int(100e6))
+        hls = HlsScope(self)
         mainThread = HlsThreadFromPy(hls, self.mainThread, hls)
         # mainThread.bytecodeToSsa.debug = True
         hls.addThread(mainThread)
@@ -63,8 +95,39 @@ class AndShiftInLoop_TC(SimTestCase):
 
         self.assertEqual(comb_loops, frozenset(), msg="\n".join(msg_buff))
 
-    def test_AndShiftInLoop(self):
-        u = AndShiftInLoop()
+    def test_AndShiftInLoop2(self):
+        u = AndShiftInLoop2()
+        self.compileSimAndStart(u, target_platform=VirtualHlsPlatform())
+        CLK_PERIOD = int(freq_to_period(u.FREQ))
+        CLK = 7
+
+        expected = []
+        all1 = mask(8)
+        up1 = mask(7) << 1
+        inputs = [all1, all1, all1, up1, up1, up1, up1, up1, up1]
+        inIt = iter(inputs)
+
+        def model():
+            i0 = uint8_t.from_py(all1)
+            i1 = uint8_t.from_py(all1)
+            while True:
+                i = next(inIt)
+                i0 = i0 & i
+                i1 = i1 & (i0 << 1)
+                yield int(i1)
+
+        m = model()
+        for _ in range(CLK):
+            expected.append(next(m))
+        
+        u.i._ag.data.extend(inputs)
+        self.runSim((CLK + 1) * CLK_PERIOD)
+        self._test_no_comb_loops()
+
+        self.assertValSequenceEqual(u.o._ag.data, expected)
+
+    def test_AndShiftInLoop3(self):
+        u = AndShiftInLoop3()
         self.compileSimAndStart(u, target_platform=VirtualHlsPlatform())
         CLK_PERIOD = int(freq_to_period(u.FREQ))
         CLK = 7
@@ -99,8 +162,8 @@ class AndShiftInLoop_TC(SimTestCase):
 
 if __name__ == "__main__":
     from hwt.synthesizer.utils import to_rtl_str
-    u = AndShiftInLoop()
-    print(to_rtl_str(u, target_platform=VirtualHlsPlatform(debugDir="tmp")))
+    u = AndShiftInLoop3()
+    print(to_rtl_str(u, target_platform=VirtualHlsPlatform(debugDir="tmp", debugFilter=HlsDebugBundle.DBG_ARCH_SYNC)))
 
     import unittest
     suite = unittest.TestSuite()
