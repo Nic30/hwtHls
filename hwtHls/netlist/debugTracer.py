e815f613b8c30c8d6273715e2e145c1c386f3e56
Nic30 <nic30@seznam.cz>
2023-01-10 21:29:08 +0100

    DebugTracer

---
+++ hwtHls/netlist/debugTracer.py
@@ -0,0 +1,66 @@
+from io import StringIO
+from types import FunctionType
+from typing import Optional, Union
+
+from hwtHls.netlist.nodes.node import HlsNetNode
+
+
+class DebugTracer():
+    INDENT = "  "
+
+    def __init__(self, out: Optional[StringIO]):
+        self._out = out
+        self._scope = []
+        self._labelPrinted = []
+        self._curIndent = ""
+
+    def scoped(self, nameOrObj: Union[str, FunctionType], node: Optional[HlsNetNode]):
+        self._scope.append((nameOrObj, node))
+        self._labelPrinted.append(False)
+        return self
+
+    def _writeScopeLabel(self):
+        out = self._out
+        for i, ((obj, node), printed) in enumerate(zip(self._scope, self._labelPrinted)):
+            if printed:
+                continue
+
+            if isinstance(obj, str):
+                objStr = obj
+            elif isinstance(obj, (FunctionType, type)):  # :note: type is a base type of class objects
+                objStr = getattr(obj, "__qualname__", obj.__name__)
+            else:
+                objStr = repr(obj)
+
+            for _ in range(i):
+                out.write(self.INDENT)
+
+            out.write(objStr)
+            if node is not None:
+                out.write(f"<{node._id}>")
+            out.write(":\n")
+            self._labelPrinted[i] = True
+            
+    def __enter__(self):
+        out = self._out
+        if out is not None:
+            self._curIndent = self._curIndent + self.INDENT
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self._out is not None:
+            self._curIndent = self._curIndent[0:-len(self.INDENT)]
+        self._scope.pop()
+        self._labelPrinted.pop()
+
+    def log(self, msg, formater=lambda x: x, ending="\n"):
+        out = self._out
+        if out is not None:
+            _msg = formater(msg)
+            if not isinstance(_msg, str):
+                _msg = repr(_msg)
+            if not self._labelPrinted[-1]:
+                self._writeScopeLabel()
+            out.write(self._curIndent)
+            out.write(_msg)
+            out.write(ending)
