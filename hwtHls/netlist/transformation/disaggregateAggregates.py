9f6d56914374b6f17e5e93bd70e66ef5198c6a32
Nic30 <nic30@seznam.cz>
2023-01-10 21:28:12 +0100

    HlsNetlistPassDisaggregateAggregates: use ObservableList

--- hwtHls/netlist/transformation/disaggregateAggregates.py
+++ hwtHls/netlist/transformation/disaggregateAggregates.py
@@ -1,11 +1,12 @@
 from collections import deque
-from typing import Set, List, Deque
+from typing import Set, Deque
 
 from hwtHls.netlist.context import HlsNetlistCtx
 from hwtHls.netlist.nodes.aggregate import HlsNetNodeAggregate
 from hwtHls.netlist.nodes.node import HlsNetNode
 from hwtHls.netlist.nodes.read import HlsNetNodeRead
 from hwtHls.netlist.nodes.write import HlsNetNodeWrite
+from hwtHls.netlist.observableList import ObservableList
 from hwtHls.netlist.transformation.hlsNetlistPass import HlsNetlistPass
 
 
@@ -16,7 +17,7 @@ class HlsNetlistPassDisaggregateAggregates(HlsNetlistPass):
 
     def apply(self, hls: "HlsScope", netlist: HlsNetlistCtx):
         removedNodes: Set[HlsNetNodeAggregate] = set()
-        addedNodes: List[HlsNetNode] = []
+        addedNodes: ObservableList[HlsNetNode] = ObservableList()
         # discover clusters of bitwise operators
         toExpand: Deque[HlsNetNodeAggregate] = deque(n for n in netlist.nodes if isinstance(n, HlsNetNodeAggregate))
         while toExpand:
@@ -35,7 +36,7 @@ class HlsNetlistPassDisaggregateAggregates(HlsNetlistPass):
             n.destroy()
             removedNodes.add(n)
 
-        addedNodes.extend([n for n in netlist.nodes if n not in removedNodes])
+        addedNodes.extend(n for n in netlist.nodes if n not in removedNodes)
         netlist.nodes = addedNodes
         # drop builder.operatorCache because we removed most of bitwise operator from the circuit
         netlist.builder.operatorCache.clear()
