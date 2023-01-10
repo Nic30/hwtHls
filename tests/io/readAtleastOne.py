069d28a33b7c18111683461e3cd2789c45e2da60
Nic30 <nic30@seznam.cz>
2023-01-10 22:04:51 +0100

    ReadAtleastOne_TC

---
+++ tests/io/readAtleastOne.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from hwt.hdl.types.defs import BIT
+from hwt.interfaces.std import Handshaked
+from hwt.interfaces.utils import addClkRstn
+from hwt.synthesizer.param import Param
+from hwt.synthesizer.unit import Unit
+from hwtHls.frontend.pyBytecode.thread import HlsThreadFromPy
+from hwtHls.scope import HlsScope
+from hwtHls.frontend.pyBytecode.markers import PyBytecodeInPreproc
+
+
+class ReadAtleastOneOf2(Unit):
+
+    def _config(self) -> None:
+        self.DATA_WIDTH = Param(8)
+        self.CLK_FREQ = Param(int(100e6))
+
+    def _declr(self):
+        addClkRstn(self)
+        self.clk.FREQ = self.CLK_FREQ
+        with self._paramsShared():
+            self.i0 = Handshaked()
+            self.i1 = Handshaked()
+        
+            self.o: Handshaked = Handshaked()._m()
+
+    def mainThread(self, hls: HlsScope):
+        
+        pre = PyBytecodeInPreproc
+        while BIT.from_py(1):
+            o = self.o.data._dtype.from_py(None)
+            
+            i0 = pre(hls.read(self.i0, blocking=False))
+            if i0.valid:
+                o = i0.data
+            else:
+                o = hls.read(self.i1)
+
+            hls.write(o, self.o)
+
+    def _impl(self):
+        hls = HlsScope(self)
+        mainThread = HlsThreadFromPy(hls, self.mainThread, hls)
+        # mainThread.bytecodeToSsa.debug = True
+        hls.addThread(mainThread)
+        hls.compile()
+
+
+class ReadAtleastOneOf3(ReadAtleastOneOf2):
+
+    def _declr(self):
+        ReadAtleastOneOf2._declr(self)
+        with self._paramsShared():
+            self.i2 = Handshaked()
+
+    def mainThread(self, hls: HlsScope):
+        
+        pre = PyBytecodeInPreproc
+        while BIT.from_py(1):
+            o = self.o.data._dtype.from_py(None)
+            
+            i0 = pre(hls.read(self.i0, blocking=False))
+            if i0.valid:
+                o = i0.data
+            else:
+                i1 = pre(hls.read(self.i1, blocking=False))
+                if i1.valid:
+                    o = i1.data
+                else:
+                    o = hls.read(self.i2)
+
+            hls.write(o, self.o)
+
+
+if __name__ == "__main__":
+    from hwt.synthesizer.utils import to_rtl_str
+    from hwtHls.platform.virtual import VirtualHlsPlatform
+    u = ReadAtleastOneOf3()
+    # u.CLK_FREQ = int(100e6)
+    print(to_rtl_str(u, target_platform=VirtualHlsPlatform(debugDir="tmp")))
+    
---
+++ tests/io/readAtleastOne_test.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from typing import Type
+
+from hwt.simulator.simTestCase import SimTestCase
+from hwt.simulator.utils import valToInt
+from hwt.synthesizer.unit import Unit
+from hwtHls.platform.virtual import VirtualHlsPlatform
+from hwtSimApi.constants import CLK_PERIOD
+from tests.frontend.ast.trivial_test import HlsAstTrivial_TC
+from tests.io.readAtleastOne import ReadAtleastOneOf2, ReadAtleastOneOf3
+
+
+class ReadAtleastOne_TC(SimTestCase):
+
+    def _test_no_comb_loops(self):
+        HlsAstTrivial_TC._test_no_comb_loops(self)
+
+    def _test_ReadAtleastOne(self, cls: Type[Unit], N: int, inputCnt:int):
+        assert N % inputCnt == 0, (N, inputCnt)
+        u = cls()
+        self.compileSimAndStart(u, target_platform=VirtualHlsPlatform())
+        for i in range(inputCnt):
+            inp = getattr(u, f"i{i:d}")
+            inp._ag.data.extend(range(i * (N // inputCnt), (i + 1) * (N // inputCnt)))
+            self.randomize(inp)
+        self.randomize(u.o)
+        self.runSim(4 * N * CLK_PERIOD)
+        self._test_no_comb_loops()
+        d = [valToInt(v) for v in u.o._ag.data]
+        d.sort(key=lambda x:-1 if x is None else x)
+        self.assertListEqual(d, list(range(N)))
+
+    def test_ReadAtleastOneOf2(self, cls=ReadAtleastOneOf2):
+        self._test_ReadAtleastOne(cls, 16, 2)
+
+    def test_ReadAtleastOneOf3(self, cls=ReadAtleastOneOf3):
+        self._test_ReadAtleastOne(cls, 24, 3)
+
+
+if __name__ == "__main__":
+    from hwt.synthesizer.utils import to_rtl_str
+    u = ReadAtleastOneOf2()
+    print(to_rtl_str(u, target_platform=VirtualHlsPlatform(debugDir="tmp")))
+    
+    import unittest
+    suite = unittest.TestSuite()
+    # suite.addTest(ReadAtleastOne_TC('test_ReadAtleastOneOf3'))
+    suite.addTest(unittest.makeSuite(ReadAtleastOne_TC))
+    runner = unittest.TextTestRunner(verbosity=3)
+    runner.run(suite)
+ 
