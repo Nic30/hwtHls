82b55642df251dfcb2c1042d1f778e3ad510b06b
Nic30 <nic30@seznam.cz>
2023-01-10 21:21:38 +0100

    HlsNetlistAnalysisPassDiscoverIoSyncSccs -> HlsNetlistAnalysisPassSyncDomains

--- hwtHls/netlist/analysis/syncDomains.py
+++ hwtHls/netlist/analysis/syncDomains.py
@@ -1,96 +1,276 @@
-from networkx.algorithms.components.strongly_connected import strongly_connected_components
-from networkx.classes.digraph import DiGraph
-from typing import List, Dict, Set
+from itertools import chain
+from typing import List, Dict, Set, Tuple
 
 from hwt.pyUtils.uniqList import UniqList
 from hwtHls.netlist.analysis.hlsNetlistAnalysisPass import HlsNetlistAnalysisPass
+from hwtHls.netlist.analysis.syncGroupClusterContext import SyncGroupLabel, \
+    SyncGroupClusterContext
+from hwtHls.netlist.nodes.delay import HlsNetNodeDelayClkTick
 from hwtHls.netlist.nodes.explicitSync import HlsNetNodeExplicitSync
 from hwtHls.netlist.nodes.node import HlsNetNode
 from hwtHls.netlist.nodes.orderable import HOrderingVoidT, HExternalDataDepT
-from hwtHls.netlist.nodes.read import HlsNetNodeRead
-from hwtHls.netlist.nodes.write import HlsNetNodeWrite
-from hwtHls.netlist.nodes.readSync import HlsNetNodeReadSync
+from hwtHls.netlist.nodes.ports import HlsNetNodeIn
 
 
-class HlsNetlistAnalysisPassDiscoverIoSyncSccs(HlsNetlistAnalysisPass):
+class HlsNetlistAnalysisPassSyncDomains(HlsNetlistAnalysisPass):
     """
-    Discover Strongly Connected Components of IO operations in netlist.
-
+    Discover Strongly Connected Components (SCCs) of IO and sync operations in netlist.
+    SCCs do represent the scheduling and architectural constraint related to not to data but to synchronization of IO channel functionality.
     :note: Found subgraphs do have special scheduling requirements. By default all nodes with such a component
         must be scheduled in a single clock cycle in order to assert that the read is not performed speculatively
         when it is optional. A speculative read of data in circuits which explicitly specify that the read should
         not be performed may lead to a deadlock.
-    :note: The IO SCC is not related to data but to synchronization of IO channel functionality.
+    
+    The reasons why HlsNetNodeExplicitSync appears in the circuit:
+    * Implementation of optional/non-blocking read.
+      * Because of this individual circuit pats may run independently and do require independent control.
+      * HlsNetNodeExplicitSync is never associated with a write, if this was the case the sync should be already
+        merged to write.
+    * Selection of inputs for loops.
+      * Because of this it is likely that there are multiple sync nodes along the data path.
+        Not everything needs to be scheduled in a single clock and data can potentially stall or be dropped anywhere along the path.
+        This implies that if this is a case data has to have some validity flag.
+    
+    
+    Rules for extraction of sync for pipeline
+    * In every pipeline which is not fully initialized by reset there must be set of validity flags
+      for each stage in order to resolve if the the stage contains data or not.
+    * HlsNetlistAnalysisPassIoDiscoverSyncSccs specifies which nodes are affect by each sync node.
+    * From flags of the sync it can be resolved if the data may be dropped or read non-blocking (= has skipWhen flag).
+      If there is sync a sync all predecessors must be in separate pipeline so it runs asynchronously from rest of the pipeline
+      and this sync must be an inter-element channel. However this is only required if predecessors of the sync can not fit in the same clock tick.
+      If they can the sync can be realized combinationally and there is no need for another asynchronous pipeline. 
+
+    Rules for extraction of sync for FSM
+    * Note that if some part of FSM should run asynchronously  it means that it must be a separate FSM.
+    * Rules are similar as for pipeline however FSMs are always in a single state and communication between FSMs may be in multiple states.
+      This alone creates a possibility for deadlock but there is yet another problem. If the FSM is cut it cancels register sharing inside FSM.
+      And all IO nodes specific to some IO channels needs to stay in a single FSM. This further complicates architecture. 
+
+    Explicit sync flag combinations (both flags are optional)
+    ---------------------------------------------|
+    | extra cond | skip when | meaning           |
+    ==============================================
+    | 0          | 0         | block             |
+    | 1          | 0         | accept            |
+    | 0          | 1         | drop              |
+    | 1          | 1         | read non blocking |
+    ----------------------------------------------
+    
+    Other notes:
+    * There is no real difference between explicit sync generated for loops or IO.
+    * The sync is often tied with the data and often multiple IO channels are interacting with each other sync.
+    * It is highly desired to schedule all sync into the same clock tick as an IO to reduce control complexity.
+    * Pipeline flushing is a specific case of this problematic. It is a functionality described using sync nodes.
+    * What is an exact query for the sync nodes which must be scheduled in same clock cycle?
+       * There is no such rule, instead sync should be aggregated as much as possible.
+    * Due to optional read there may be the situation where pipeline have multiple potential starts
+      or FSM may contain multiple sub-states based on which data were available.
+      This is a problem because there may be a situation where it is resolved if the read should be performed or not
+      after read was done. Depending on how sync is implemented this may potentially result in deadlock
+      if read waits on resolution of sync or data loose if the data was read before it was resolved that data should be read.
     """
 
     def __init__(self, netlist: "HlsNetlistCtx"):
         HlsNetlistAnalysisPass.__init__(self, netlist)
         self.ioSccs: List[UniqList[HlsNetNode]] = []
-        self.syncUses: Dict[HlsNetNodeRead, UniqList[HlsNetNodeExplicitSync]] = {}
+        # self.syncUses: Dict[HlsNetNodeRead, UniqList[HlsNetNodeExplicitSync]] = {}
+        self.syncOfNode: Dict[HlsNetNode, Set[HlsNetNodeExplicitSync]] = {}
+        # self.interSyncDomainConnections: Set[Tuple[HlsNetNodeExplicitSync, HlsNetNodeExplicitSync]] = set()
+        self.syncDomains: List[Tuple[SyncGroupLabel, UniqList[HlsNetNode]]] = []
+        
+    @staticmethod
+    def _discoverSyncUsers(syncNode:HlsNetNodeExplicitSync, syncOfNode: Dict[HlsNetNode, Set[HlsNetNodeExplicitSync]]):
+        """
+        Discover all nodes which are directly data dependent on this sync, but are not a sync node
+        This is a first step in discovering the reach of the sync node.
+        """
+        seen: Set[HlsNetNode] = set()
+        toSearch: List[HlsNetNode] = [syncNode, ]
+        
+        while toSearch:
+            n0: HlsNetNode = toSearch.pop()
+            if n0 in seen:
+                continue
+            else:
+                seen.add(n0)
+                if n0 is not syncNode:
+                    curSync = syncOfNode[n0]
+                    if syncNode in curSync:
+                        # because this node and all its uses already have this sync
+                        continue
+                    curSync.add(syncNode)
+
+                if n0 is not syncNode and isinstance(n0, HlsNetNodeExplicitSync):
+                    # this is end of the search
+                    # interSyncDomainConnections.add((syncNode, n0))
+                    continue
+
+                else:
+                    # follow the connection to uses
+                    for o, uses in zip(n0._outputs, n0.usedBy):
+                        if  o._dtype is HOrderingVoidT or o._dtype is HExternalDataDepT:
+                            # skip ordering because it is not a real data dependency
+                            continue
+
+                        for u in uses:
+                            u: HlsNetNodeIn
+                            n1 = u.obj  
+                            if n1 not in seen:
+                                toSearch.append(n1)
+
+    def _discoverSyncSinkUsers(self, syncOfNode: Dict[HlsNetNode, Set[HlsNetNodeExplicitSync]]):
+        """
+        For every dependency object which is not yet in any group assign it to a group of its sink
+        """
+        nodesWithSyncedSinkOnly: Set[HlsNetNode] = set(
+            n
+            for n in self.netlist.iterAllNodes()
+            if not syncOfNode[n] and not isinstance(n, HlsNetNodeExplicitSync))
+        # nodes must be detected in advance because this information is required when
+        # some node is synchronized by multiple sinks
+        for n in self.netlist.iterAllNodes():
+            sync = syncOfNode[n]
+            if sync:
+                toSearch: List[HlsNetNode] = [n, ]
+                seen = set()
+                while toSearch:
+                    n0: HlsNetNode = toSearch.pop()
+                    if n0 in seen:
+                        continue
+    
+                    if n0 is n:
+                        updatePropagete = True
+                    else:
+                        # assert n0 in nodesWithSyncedSinkOnly, "Should be trivially satisfied because we are searching only such nodes"
+                        assert not isinstance(n0, HlsNetNodeExplicitSync), (n0, "This node should have been discovered previously and should not appear in nodes without sync")
+                        n0sync = syncOfNode[n0]
+                        prevSyncLen = len(n0sync)
+                        n0sync.update(sync)
+                        updatePropagete = len(n0sync) != prevSyncLen
+
+                    seen.add(n0)
+                    if updatePropagete:
+                        for dep in n0.dependsOn:
+                            t = dep._dtype 
+                            if t is HOrderingVoidT or t is HExternalDataDepT:
+                                continue
+                            depObj = dep.obj 
+                            if depObj not in nodesWithSyncedSinkOnly or depObj in seen:
+                                continue
+                            toSearch.append(dep.obj)
+
+    def _discoverSyncCutsByClkOffset(self, allSyncs: List[HlsNetNodeExplicitSync],
+                                     allDelays: List[HlsNetNodeDelayClkTick],
+                                     syncOfNode: Dict[HlsNetNode, Set[HlsNetNodeExplicitSync]]):
+        """
+        for every sync which takes more than 1 clock to complete cut off the group part after this node from rest of the group
+        where this sync is
+        """
+        for syncNode in chain(allSyncs, allDelays):
+            if syncNode.realization is None:
+                syncNode.resolveRealization()
+            if not any(syncNode.outputClkTickOffset):
+                continue
+
+            # for all successors cancel all dependencies which are shared with this node
+            # because output of this node shifts successors to a next clock and thus they can not
+            # be in IO SCC because they must be scheduled in a different clock cycle
+            n0Deps = syncOfNode.get(syncNode, None)
+            if n0Deps is not None:
+                # remove syncNode, None and all predecessors of syncNode from successors of syncNode
+                seen: Set[HlsNetNode] = set()
+                # walk all nodes which are directly data dependent on this sync, but are a sync node
+                toSearch: List[HlsNetNode] = [syncNode, ]
+                
+                while toSearch:
+                    n0: HlsNetNode = toSearch.pop()
+                    if n0 in seen:
+                        continue
+                    else:
+                        seen.add(n0)
+                        if n0 is not syncNode:
+                            sync = syncOfNode[n0]
+                            if sync:
+                                for discardedDepNode in n0Deps:
+                                    sync.discard(discardedDepNode)
+                                sync.discard(syncNode)
+            
+                        if n0 is not syncNode and isinstance(n0, HlsNetNodeExplicitSync):
+                            # this is end of the search
+                            # interSyncDomainConnections.add((syncNode, n0))
+                            continue
+
+                        else:
+                            # follow the connection to uses
+                            for uses0 in n0.usedBy:
+                                for u in uses0:
+                                    u: HlsNetNodeIn
+                                    n1 = u.obj  
+                                    if n1 not in seen:
+                                        toSearch.append(n1)
+            else:
+                raise NotImplementedError("Remove just self from all successors")       
+
+    def _addSelfToSyncOfSelf(self, allSyncs: List[HlsNetNodeExplicitSync], syncOfNode: Dict[HlsNetNode, Set[HlsNetNodeExplicitSync]]):
+        """
+        Add each sync node to a sync of self.
+        """
+        for syncNode in allSyncs:
+            # add sync node to its cluster
+            sync = syncOfNode.get(syncNode, None)
+            if sync is None:
+                sync = syncOfNode[syncNode] = set()
+            sync.add(syncNode)
+    
+    def _discoverSyncAssociationsDefToUse(self):
+        """
+        Collect all associations of sync nodes to all nodes.
+        Start search on HlsNetNodeExplicitSync instances and walk circuit in def to use direction
+        and stop on each HlsNetNodeExplicitSync instance.
+        
+        If something with delay > 1 clk is discovered it is required to move all successors from this IO SCC.
+        This must be also done transitively. If node with > 1 clk delay has some predecessors the nodes which does have bout this node and its predecessor
+        
+        """
+
+        syncOfNode = self.syncOfNode = {n: set() for n in self.netlist.iterAllNodes()}
+        allSyncs: List[HlsNetNodeExplicitSync] = []
+        allDelays: List[HlsNetNodeDelayClkTick] = []
+        # from every sync walk down (def->use) and discover which nodes are affected
+        for syncNode in self.netlist.iterAllNodes():
+            if not isinstance(syncNode, HlsNetNodeExplicitSync):
+                if isinstance(syncNode, HlsNetNodeDelayClkTick):
+                    allDelays.append(syncNode)
+                continue
+            syncNode: HlsNetNodeExplicitSync
+            allSyncs.append(syncNode)
+            self._discoverSyncUsers(syncNode, syncOfNode)
+
+        self._discoverSyncCutsByClkOffset(allSyncs, allDelays, syncOfNode)
+        self._discoverSyncSinkUsers(syncOfNode)
+        self._addSelfToSyncOfSelf(allSyncs, syncOfNode)
+
+    def _discoverSyncDomains(self):
+        """
+        After reach of the sync to every node resolve the SCCs in synchronization
+        node to sync set map -> sync group label to node list map -> sync group cluster to node list map -> IO SCC to node list map
+        """
+        sgcc = SyncGroupClusterContext(self.syncOfNode)
+        syncGroups, syncGroupOfNode = sgcc.resolveSyncGroups()
+        self.syncDomains = sorted(syncGroups.items(), key=lambda x: x[0][0]._id)
+        ioSccs = self.ioSccs
+        for scc in sgcc.mergeSyncGroupsToClusters(syncGroups, self.syncDomains, syncGroupOfNode):
+            ioSccs.append(scc)
 
     def run(self):
-        assert not self.ioSccs and not self.syncUses, "Must be run only once."
+        assert not self.ioSccs  # and not self.syncUses, "Must be run only once."
         # The naive discovery algorithm for the sync associated to IO operation mapping has O(|nodes|*|edges|) time complexity.
         # Commonly the |nodes| > 10k and |edges| > 30K, that said the time complexity is prohibitively large.
         # The |HlsNetNodeExplicitSync| << |HlsNetNode| from this reason we start search at HlsNetNodeExplicitSync
         # and walk only in the direction to a read (defs). 
         
         # :note: If the value for read is not in the dict it means that there is no extra sync for this read.
-        syncUses = self.syncUses
-        usersOfSync: Dict[HlsNetNodeReadSync, UniqList[HlsNetNodeReadSync]] = {}
-        g = DiGraph()
-        for n in self.netlist.iterAllNodes():
-            n: HlsNetNode
-            g.add_node(n)
-            for dep in n.dependsOn:
-                if dep._dtype is not HOrderingVoidT and dep._dtype is not HExternalDataDepT:
-                    g.add_edge(dep.obj, n)
-
-            if n.__class__ is HlsNetNodeExplicitSync or isinstance(n, HlsNetNodeReadSync):
-                toSearch = [dep.obj for dep in n.dependsOn if dep._dtype is not HOrderingVoidT and dep._dtype is not HExternalDataDepT]
-                # anyDepFound = False
-                seen = set()
-                while toSearch:
-                    depObj: HlsNetNode = toSearch.pop()
-                    if depObj in seen:
-                        continue
-                    seen.add(depObj)
-                    if isinstance(depObj, (HlsNetNodeRead, HlsNetNodeWrite)):
-                        depSyncUses = syncUses.get(depObj, None)
-                        if depSyncUses is None:
-                            depSyncUses = syncUses[depObj] = UniqList()
-                        depSyncUses.append(n)
-                        users = usersOfSync.get(n, None)
-                        if users is None:
-                            users = usersOfSync[n] = UniqList()
-                        users.append(depObj)
-                        # anyDepFound = True
-                    else:
-                        toSearch.extend(dep.obj for dep in depObj.dependsOn
-                                        if dep._dtype is not HOrderingVoidT and dep._dtype is not HExternalDataDepT and dep.obj not in seen)
-                # assert anyDepFound, (n, "Must be associated with some IO otherwise it should already be removed")
-
-        # add edge from sync back to read/write to mark they are in same SCC
-        for op, extraSyncList in syncUses.items():
-            for extraSync in extraSyncList:
-                g.add_edge(extraSync, op)
-            if op._associatedReadSync is not None:
-                g.add_edge(op._associatedReadSync, op)
-
-        # build a cycle on all IO which is using same sync
-        for users in usersOfSync.values():
-            if len(users) > 1:
-                userIt = iter(users)
-                firstUser = next(userIt)
-                lastUser = firstUser
-                for u in users:
-                    g.add_edge(lastUser, u)
-                    lastUser = u
-
-                g.add_edge(lastUser, firstUser)
-            
-        ioSccs = self.ioSccs
-        for ioScc in strongly_connected_components(g):
-            ioScc: Set[HlsNetNode]
-            if len(ioScc) > 1:
-                ioSccs.append(UniqList(sorted(ioScc, key=lambda n: n._id)))
+        self._discoverSyncAssociationsDefToUse()
+        self._discoverSyncDomains()
+        
---
+++ hwtHls/netlist/analysis/syncGroupClusterContext.py
@@ -0,0 +1,152 @@
+from networkx.algorithms.components.strongly_connected import strongly_connected_components
+from networkx.classes.digraph import DiGraph
+from typing import List, Dict, Set, Tuple, Generator
+
+from hwt.pyUtils.uniqList import UniqList
+from hwtHls.netlist.nodes.const import HlsNetNodeConst
+from hwtHls.netlist.nodes.explicitSync import HlsNetNodeExplicitSync
+from hwtHls.netlist.nodes.node import HlsNetNode
+from hwtHls.netlist.nodes.ports import HlsNetNodeIn
+
+SyncGroupLabel = Tuple[HlsNetNodeExplicitSync, ...]
+
+
+class SyncGroupClusterContext():
+
+    def __init__(self, syncOfNode: Dict[HlsNetNode, Set[HlsNetNodeExplicitSync]]):
+        self.syncOfNode = syncOfNode
+
+    def resolveSyncGroups(self):
+        # :note: every input/output should have some sync group because it must be connected
+        # to some IO (transitively)
+
+        syncGroups: Dict[SyncGroupLabel, UniqList[HlsNetNode]] = {}
+        syncGroupOfNode: Dict[HlsNetNode, SyncGroupLabel] = {}
+        for n, syncGroupSet in self.syncOfNode.items():
+            if not syncGroupSet:
+                continue
+
+            assert syncGroupSet, (n, syncGroupSet,)
+            syncGroupLabel = tuple(sorted(syncGroupSet, key=lambda n: n._id))
+            groupNodes = syncGroups.get(syncGroupLabel, None)
+            if groupNodes is None:
+                groupNodes = syncGroups[syncGroupLabel] = UniqList()
+            groupNodes.append(n)
+            syncGroupOfNode[n] = syncGroupLabel
+        return syncGroups, syncGroupOfNode
+
+    def _copySyncGroupToDiGraph(self, g:DiGraph, nodes: UniqList[HlsNetNode]) -> Generator[HlsNetNode, None, None]:
+        g.add_nodes_from(nodes)
+        allNodes = g.nodes
+        for n0 in nodes:
+            for dep in n0.dependsOn:
+                n1 = dep.obj 
+                if n1 in allNodes:
+                    g.add_edge(n1, n0)
+                else:
+                    yield n1
+            
+            for uses0 in n0.usedBy:
+                for u in uses0:
+                    u: HlsNetNodeIn
+                    n1 = u.obj  
+                    if n1 not in allNodes:
+                        # found something out of this cluster, yield for external decision if copy should continue there
+                        yield n1
+                    else:
+                        g.add_edge(n0, n1)
+    
+    def _copySyncGroupToDiGraphFlodding(self,
+                                        syncGroupLabel: SyncGroupLabel,
+                                        syncGroupNodes: UniqList[HlsNetNode],
+                                        syncGroups: Dict[SyncGroupLabel, UniqList[HlsNetNode]],
+                                        syncGroupOfNode: Dict[HlsNetNode, SyncGroupLabel],
+                                        resolvedGroups: Set[SyncGroupLabel]):
+        """
+        :param resolvedGroups: groups which were discovered previously and are definitely not tied to current cluster
+            because if this was the case this group would already have been in seen as well
+        """
+        g = DiGraph()
+        tiedGroups: List[SyncGroupLabel] = []
+        
+        groupsToCollect = [(syncGroupLabel, syncGroupNodes)]
+        while groupsToCollect:
+            label, nodes = groupsToCollect.pop()
+            resolvedGroups.add(label)
+            labelAsSet = set(label)
+            seenGroups: Set[SyncGroupLabel] = set()
+            tiedGroups.append(label)
+            # syncGroup may be tied with its neighbors
+            # search only in def->use direction
+            for externNode in self._copySyncGroupToDiGraph(g, nodes):
+                conectedGroupLabel = syncGroupOfNode.get(externNode, None)
+                if conectedGroupLabel is None or conectedGroupLabel in seenGroups or conectedGroupLabel in resolvedGroups:
+                    continue
+                seenGroups.add(conectedGroupLabel)
+                # if group intersect in at least a single item it means that this group is tied trough the sync
+                if labelAsSet.isdisjoint(set(conectedGroupLabel)):
+                    continue
+                groupsToCollect.append((conectedGroupLabel, syncGroups[conectedGroupLabel]))
+        return g, tiedGroups
+
+    def mergeSyncGroupsToClusters(self,
+                                  syncGroups: Dict[SyncGroupLabel, UniqList[HlsNetNode]],
+                                  syncGroupsSorted: List[Tuple[SyncGroupLabel, UniqList[HlsNetNode]]],
+                                  syncGroupOfNode: Dict[HlsNetNode, SyncGroupLabel]):
+        # sync groups are scope of unique reach of sync nodes, some of them may be tied in the IO SCC
+        # this dictionary is used to keep track of this association, key is the label of the group and value (if present)
+        # is some tied group label which was picked as a representator of this cluster because it was found first
+        resolvedSyncGroups: Set[SyncGroupLabel] = set()
+        for syncGroupLabel, syncGroupNodes in syncGroupsSorted:
+            syncGroupLabel: SyncGroupLabel
+            syncGroupNodes: UniqList[HlsNetNode]
+            if syncGroupLabel in resolvedSyncGroups:
+                continue
+            g, tiedGroups = self._copySyncGroupToDiGraphFlodding(syncGroupLabel, syncGroupNodes, syncGroups, syncGroupOfNode, resolvedSyncGroups)
+            resolvedSyncGroups.update(tiedGroups)
+            # once all sync group clusters are resolved we connect all explicit sync with all reachable
+            # reads so we create a cycle in the graph  whenever there is a read connected to sync transitively
+        
+            # add edge from sync back to read/write to mark they are in same SCC
+            for op in tuple(g.nodes):
+                if isinstance(op, HlsNetNodeExplicitSync):
+                    op: HlsNetNodeExplicitSync
+        
+                    for extraSync in syncGroupOfNode[op]:
+                        if extraSync is op:
+                            # skip because this edge is not necessary because 1 node is SCC by def.
+                            continue
+
+                        g.add_edge(extraSync, op)
+            
+                    if op._associatedReadSync is not None:
+                        g.add_edge(op._associatedReadSync, op)
+        
+            # build a cycle on all IO which is using same sync
+            # for users in usersOfSync.values():
+            #    if len(users) > 1:
+            #        userIt = iter(users)
+            #        firstUser = next(userIt)
+            #        lastUser = firstUser
+            #        for u in users:
+            #            g.add_edge(lastUser, u)
+            #            lastUser = u
+            #
+            #        g.add_edge(lastUser, firstUser)
+                
+            for ioScc in strongly_connected_components(g):
+                ioScc: Set[HlsNetNode]
+                # if it is worth extracting
+                if len(ioScc) > 1:
+                    _ioScc = UniqList(sorted(ioScc, key=lambda n: n._id))
+                    # privatize constants to reduce number of ports
+                    usedConstants = []
+                    for n in _ioScc:
+                        for dep in n.dependsOn:
+                            if isinstance(dep.obj, HlsNetNodeConst) and len(dep.obj.usedBy[0]) == 1:
+                                # assert len(dep.obj.usedBy[0]) == 1, dep.obj.usedBy
+                                usedConstants.append(dep.obj)
+    
+                    _ioScc.extend(usedConstants)
+                    yield _ioScc
+
