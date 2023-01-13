4800d4845024181c7223730e0317bd887cd5f23a
Nic30 <nic30@seznam.cz>
2023-01-13 13:48:15 +0100

    codestyle

--- tests/io/bram/bramWrite.py
+++ tests/io/bram/bramWrite.py
@@ -4,7 +4,7 @@
 
 from hwt.hdl.types.bits import Bits
 from hwt.hdl.types.defs import BIT
-from hwt.interfaces.std import BramPort_withoutClk, Handshaked
+from hwt.interfaces.std import BramPort_withoutClk
 from hwt.interfaces.utils import addClkRstn
 from hwt.synthesizer.param import Param
 from hwt.synthesizer.unit import Unit
