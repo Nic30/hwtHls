959e5602aadea8d14b46c927054a406ea469123b
Nic30 <nic30@seznam.cz>
2023-02-16 11:19:42 +0100

    SyncReachToGraphwiz->BetweenSyncIslandsToGraphwiz

--- hwtHls/netlist/translation/betweenSyncIslandsToGraphwiz.py
+++ hwtHls/netlist/translation/betweenSyncIslandsToGraphwiz.py
@@ -3,7 +3,7 @@ from itertools import chain
 import pydot
 from typing import List, Dict
 
-from hwtHls.netlist.analysis.syncReach import HlsNetlistAnalysisPassSyncReach
+from hwtHls.netlist.analysis.betweenSyncIslands import HlsNetlistAnalysisPassBetweenSyncIslands
 from hwtHls.netlist.context import HlsNetlistCtx
 from hwtHls.netlist.nodes.node import HlsNetNode
 from hwtHls.netlist.transformation.hlsNetlistPass import HlsNetlistPass
@@ -11,47 +11,47 @@ from hwtHls.netlist.translation.toGraphwiz import HwtHlsNetlistToGraphwiz
 from hwtHls.platform.fileUtils import OutputStreamGetter
 
 
-class HwtHlsNetlistSyncReachToGraphwiz(HwtHlsNetlistToGraphwiz):
+class HwtHlsNetlistBetweenSyncIslandsToGraphwiz(HwtHlsNetlistToGraphwiz):
     """
     Generate a Graphwiz (dot) diagram of sync domains extracted from the netlist.
     """
 
     def __init__(self, name:str, nodes:List[HlsNetNode]):
         HwtHlsNetlistToGraphwiz.__init__(self, name, nodes)
-        self.syncReachGroupOfSyncNode: Dict[HlsNetNode, pydot.Cluster] = {}
+        self.syncIslandsGroupOfSyncNode: Dict[HlsNetNode, pydot.Cluster] = {}
     
     def _getGraph(self, n:HlsNetNode):
         try:
-            return self.syncReachGroupOfSyncNode[n]
+            return self.syncIslandsGroupOfSyncNode[n]
         except KeyError:
             return self.graph
 
-    def construct(self, syncReach: HlsNetlistAnalysisPassSyncReach):
+    def construct(self, syncIslands: HlsNetlistAnalysisPassBetweenSyncIslands):
         g = self.graph
-        syncReachGroupOfSyncNode = self.syncReachGroupOfSyncNode
+        syncIslandsGroupOfSyncNode = self.syncIslandsGroupOfSyncNode
         clusterNodes = []
-        for island in syncReach.syncIslands:
+        for island in syncIslands.syncIslands:
             clusterNode = pydot.Cluster(f"n{self._getNewNodeId()}", label=f'"{html.escape(repr(island)):s}"')
             clusterNodes.append(clusterNode)
             g.add_subgraph(clusterNode)
             for n in chain(island.inputs, island.nodes):  # 
-                assert n not in syncReachGroupOfSyncNode, (
-                    n, clusterNode.get("label"), syncReachGroupOfSyncNode[n].get("label"))
-                syncReachGroupOfSyncNode[n] = clusterNode
+                assert n not in syncIslandsGroupOfSyncNode, (
+                    n, clusterNode.get("label"), syncIslandsGroupOfSyncNode[n].get("label"))
+                syncIslandsGroupOfSyncNode[n] = clusterNode
 
-        for clusterNode, island in zip(clusterNodes, syncReach.syncIslands):
-            for n in chain(island.controlOutputs, island.dataOutputs):
+        for clusterNode, island in zip(clusterNodes, syncIslands.syncIslands):
+            for n in island.outputs:
                 n: HlsNetNode
-                if n not in syncReachGroupOfSyncNode:
-                    syncReachGroupOfSyncNode[n] = clusterNode
+                if n not in syncIslandsGroupOfSyncNode:
+                    syncIslandsGroupOfSyncNode[n] = clusterNode
             
-        super(HwtHlsNetlistSyncReachToGraphwiz, self).construct()
+        super(HwtHlsNetlistBetweenSyncIslandsToGraphwiz, self).construct()
     
     def dumps(self):
         return self.graph.to_string()
 
 
-class HlsNetlistPassSyncReachToGraphwiz(HlsNetlistPass):
+class HlsNetlistPassBetweenSyncIslandsToGraphwiz(HlsNetlistPass):
 
     def __init__(self, outStreamGetter: OutputStreamGetter):
         self.outStreamGetter = outStreamGetter
@@ -60,9 +60,9 @@ class HlsNetlistPassSyncReachToGraphwiz(HlsNetlistPass):
         name = netlist.label
         out, doClose = self.outStreamGetter(name)
         try:
-            toGraphwiz = HwtHlsNetlistSyncReachToGraphwiz(name, netlist.iterAllNodes())
-            syncReach = netlist.getAnalysis(HlsNetlistAnalysisPassSyncReach)
-            toGraphwiz.construct(syncReach)
+            toGraphwiz = HwtHlsNetlistBetweenSyncIslandsToGraphwiz(name, netlist.iterAllNodes())
+            syncIslands = netlist.getAnalysis(HlsNetlistAnalysisPassBetweenSyncIslands)
+            toGraphwiz.construct(syncIslands)
             out.write(toGraphwiz.dumps())
         finally:
             if doClose:
