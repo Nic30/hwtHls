dfa5f01705b091a96a9fe278b9029d75086575e7
Nic30 <nic30@seznam.cz>
2023-02-16 11:15:09 +0100

    HlsNetlistPassTrivialSimplifyExplicitSync

---
+++ hwtHls/netlist/transformation/simplifyExpr/trivialSimplifyExplicitSync.py
@@ -0,0 +1,23 @@
+from typing import Set
+
+from hwtHls.netlist.context import HlsNetlistCtx
+from hwtHls.netlist.debugTracer import DebugTracer
+from hwtHls.netlist.nodes.node import HlsNetNode
+from hwtHls.netlist.transformation.hlsNetlistPass import HlsNetlistPass
+from hwtHls.netlist.transformation.simplifySync.simplifyNonBlockingIo import netlistReduceExplicitSyncConditions
+from hwtHls.netlist.nodes.explicitSync import HlsNetNodeExplicitSync
+
+
+class HlsNetlistPassTrivialSimplifyExplicitSync(HlsNetlistPass):
+
+    def __init__(self, dbgTracer: DebugTracer):
+        self.dbgTracer = dbgTracer
+        
+    def apply(self, hls:"HlsScope", netlist:HlsNetlistCtx):
+        dbgTracer = self.dbgTracer
+        removed: Set[HlsNetNode] = set()
+        for n in netlist.iterAllNodes():
+            if isinstance(n, HlsNetNodeExplicitSync):
+                netlistReduceExplicitSyncConditions(dbgTracer, n, None, removed)
+        if removed:
+            netlist.filterNodesUsingSet(removed)
