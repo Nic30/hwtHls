cb7835a6309bc33eb5170b57b7ca1bc686eaada0
Nic30 <nic30@seznam.cz>
2023-02-20 18:08:18 +0100

    netlistReduceExplicitSyncMergeSuccessorIsland: use IoClusterCores

--- hwtHls/netlist/transformation/simplifySync/simplifySyncIsland.py
+++ hwtHls/netlist/transformation/simplifySync/simplifySyncIsland.py
@@ -8,19 +8,19 @@ the gate we have to analyze all possible ways which leads to the gate.
 from itertools import chain
 from typing import Set
 
-from hdlConvertorAst.to.hdlUtils import iter_with_last
 from hwt.pyUtils.uniqList import UniqList
+from hwtHls.netlist.analysis.consystencyCheck import HlsNetlistPassConsystencyCheck
 from hwtHls.netlist.analysis.dataThreads import HlsNetlistAnalysisPassDataThreads
 from hwtHls.netlist.analysis.reachability import HlsNetlistAnalysisPassReachabilility
 from hwtHls.netlist.debugTracer import DebugTracer
 from hwtHls.netlist.nodes.IoClusterCore import HlsNetNodeIoClusterCore
 from hwtHls.netlist.nodes.explicitSync import HlsNetNodeExplicitSync
 from hwtHls.netlist.nodes.node import HlsNetNode
-from hwtHls.netlist.transformation.simplifySync.simplifyExplicitSync import netlistHoistExplicitSync, \
-    extendSyncFlagsFromMultipleParallel, _getPortDrive, extendSyncFlagsFrom
-from hwtHls.netlist.transformation.simplifySync.simplifySyncUtils import trasferHlsNetNodeExplicitSyncFlagsSeriallyConnected, removeExplicitSync
-from hwtHls.netlist.analysis.consystencyCheck import HlsNetlistPassConsystencyCheck
+from hwtHls.netlist.nodes.ports import HlsNetNodeOut, unlink_hls_node_input_if_exists_with_worklist
+from hwtHls.netlist.transformation.simplifySync.simplifyExplicitSync import netlistReduceExplicitSyncWithoutInput, \
+    extendSyncFlagsFromMultipleParallel, _getPortDrive
 from hwtHls.netlist.transformation.simplifySync.simplifyNonBlockingIo import netlistReduceExplicitSyncTryExtractNonBlockingReadOrWrite
+from hwtHls.netlist.transformation.simplifySync.simplifySyncUtils import removeExplicitSync
 
 
 def netlistReduceExplicitSyncMergeSuccessorIsland(
@@ -38,18 +38,20 @@ def netlistReduceExplicitSyncMergeSuccessorIsland(
     assert isinstance(coreNode, HlsNetNodeIoClusterCore), coreNode
     inputs = [dep.obj for dep in coreNode.usedBy[coreNode.inputNodePort.out_i]]
     outputs = [dep.obj for dep in coreNode.usedBy[coreNode.outputNodePort.out_i]]
-    iCnt = len(inputs)
-    oCnt = len(outputs)
     modified = False
+    
+    anySync = False
     for n in chain(inputs, outputs):
         if n.__class__ is HlsNetNodeExplicitSync:
-            if n not in removed:
+            if n in removed:
                 continue
+            
+            anySync = True
             if netlistReduceExplicitSyncTryExtractNonBlockingReadOrWrite(dbgTracer, n, worklist, removed, reachDb):
                 HlsNetlistPassConsystencyCheck._checkCycleFree(n.netlist, removed)
                 # HlsNetlistPassConsystencyCheck().apply(None, _n.netlist, removed=removed)
                 modified = True
-            elif netlistHoistExplicitSync(dbgTracer, n, worklist, removed, reachDb):
+            elif netlistReduceExplicitSyncWithoutInput(dbgTracer, n, worklist, removed, reachDb):
                 dbgTracer.log(("hoist", n._id))
                 modified = True
 
@@ -58,217 +60,79 @@ def netlistReduceExplicitSyncMergeSuccessorIsland(
         worklist.extend(outputs)
         worklist.append(coreNode)
         return modified
+    elif not anySync:
+        return modified
 
     with dbgTracer.scoped(netlistReduceExplicitSyncMergeSuccessorIsland, coreNode):
         # print("netlistReduceExplicitSyncMergeSuccessorIsland", n, iCnt, oCnt, sorted([n._id for n in inputs]),
         #                    sorted([n._id for n in outputs]))
-        if iCnt == 0 and oCnt == 0:
-            removed.add(coreNode)
-            return True
-        elif iCnt == 1 and oCnt == 0:
-            return False  # nothing to optimize
-        elif iCnt == 0 and oCnt == 1:
-            return False  # nothing to optimize
-        elif iCnt == 1 and oCnt == 1:
-            # a sequence of sync
-            src = inputs[0]
-            dst = outputs[0]
-            if src is dst:
-                return False  # case where this node is input and output together
+        # for each output check if it is possible to hoist it
+        # for each input check if all its inputs are hoistable
+        # the goal is to remove as much of HlsNetNodeExplicitSync instances as possible
+        # care must be taken for input/outut which have multiple dependencies
+        # because paralell paths do specify a split or join of synchronization graph
+        
+        # If the HlsNetNodeExplicitSync is removed its dependencies can not be simply removed
+        # instead each input must be connected to each output of the HlsNetNodeExplicitSync instance.
+        
+        # There is also a specific case of where the flag can be hoisted to some input but no
+        # to others this leads to a case where HlsNetNodeExplicitSync isntance is disconneced
+        # from input/output but must remain there for other IO.
+        
+        # This leads to a need to modify the HlsNetNodeIoCore node. Depending on combination of removing
+        # and keeping of IO and links / may spot or merge HlsNetNodeIoCore instances.
+        
+        # 
+        seenInputs: UniqList[HlsNetNodeExplicitSync] = UniqList()
+        hoistableTo = {}
+        for io in chain(inputs, outputs):
+            assert io not in removed, io
+            if io.__class__ is not HlsNetNodeExplicitSync:
+                continue
+            
+            syncFlags = tuple(_getPortDrive(p) for p in (io.skipWhen, io.extraCond))
+            for src in reachDb.getDirectDataPredecessors(io):
+                seenInputs.append(src)
 
-            # if threads.threadPerNode[src] is not threads.threadPerNode[dst]:
-            #    # can not sink or hoist because there is no data dependency which would 
-            #    # assert that the circuit would behave the same once the sync flag is moved
-            #    # up or down in the circuit
-            #    return False
-    
-            # dst control or ordering depends as well on src
-            controlDependsOnSrc = False
-            for i, dep in zip(dst._inputs, dst.dependsOn):
-                dst: HlsNetNodeExplicitSync
-                if i.in_i != 0:
-                    depObj = dep.obj
-                    controlDependsOnSrc |= depObj is src or\
-                        reachDb.doesReachTo(src, depObj)
-    
-                    if controlDependsOnSrc:
+                isHoistable = True
+                for sf in syncFlags:
+                    if sf is None:
+                        continue
+                    sf: HlsNetNodeOut
+                    if reachDb.doesReachTo(src, sf):
+                        isHoistable = False
                         break
+
+                _hoistableTo = hoistableTo.get(io, None)
+                if _hoistableTo is None:
+                    _hoistableTo = hoistableTo[io] = []
+                if isHoistable:
+                    _hoistableTo.append(src)
+    
+        for i in seenInputs:
+            assert i not in removed, i
+            allSuccHoistable = True
+            anySyncToRm = False
+            sucs = tuple(reachDb.getDirectDataSuccessors(i))
+            for o in sucs:
+                if o.__class__ is HlsNetNodeExplicitSync:
+                    anySyncToRm = True
+                assert o in hoistableTo, (o, "must have ben seen because it is how ", i, "was discovered")
+                if i not in hoistableTo[o]:
+                    allSuccHoistable = False
             
-            # If we hoist sync flags from successor to predecessor we may cancel the move of the successor
-            # if predecessor data is not always valid.
-            hasNoSkipWhen = src.skipWhen is None and dst.skipWhen is None
-            if controlDependsOnSrc:
-                # sink control
-                if (src.__class__ is HlsNetNodeExplicitSync or hasNoSkipWhen) and\
-                        (src.extraCond is not None or src.skipWhen is not None):
-                    dbgTracer.log(("sink control to", dst._id))
-                    # transfer from successor to this to optimize final extraCond and skipWhen flags
-                    # which are controlling the channel
-                    trasferHlsNetNodeExplicitSyncFlagsSeriallyConnected(src, dst)
-                    # if src.__class__ is HlsNetNodeExplicitSync:
-                    #    netlistReduceExplicitSyncUseless(src, worklist, removed, reachDb)
-                    # else:
-                    worklist.append(src)
-                    worklist.append(dst)
-                    worklist.append(coreNode)
-                    return True
-            else:
-                # hoist control
-                if (dst.__class__ is HlsNetNodeExplicitSync or hasNoSkipWhen) and (dst.extraCond is not None or dst.skipWhen is not None):
-                    # transfer from successor to this to optimize final extraCond and skipWhen flags
-                    # which are controlling the channel
-                    dbgTracer.log(("hoist control from", dst._id))
-                    trasferHlsNetNodeExplicitSyncFlagsSeriallyConnected(dst, src)
-    
-                    worklist.append(src)
-                    worklist.append(dst)
-                    worklist.append(coreNode)
-                    return True
-    
-        elif iCnt == 1 and oCnt > 1:
-            # parallel output sync with a single input
-            src = inputs[0]
-            outputs = sorted(outputs, key=lambda o: o._id)
-            everySucIsSync = all(o.__class__ is HlsNetNodeExplicitSync for o in outputs)
-            if everySucIsSync:
-                # it may be possible to hoist all outputs to this
-                everyFlagHoistable = True
-                for o in outputs:
-                    for p in (o.skipWhen, o.extraCond):
-                        d = _getPortDrive(p)
-                        if p is not None and reachDb.doesReachTo(src, d):
-                            everyFlagHoistable = False
-                            break
-                        
-                if everyFlagHoistable:
-                    dbgTracer.log(("hoist control from many and remove sucs", outputs), lambda x: f"{x[0]} {[o._id for o in x[1]]}")
-                    extendSyncFlagsFromMultipleParallel(outputs, src, worklist)
-                    for o in outputs:
-                        removeExplicitSync(o, worklist, removed)
-                    worklist.append(src)
-                    worklist.append(coreNode)
-                    return True
+            if anySyncToRm and allSuccHoistable:
+                dbgTracer.log((sucs, i), lambda x: f"hoist control from many {[o._id for o in x[0]]} to {x[1]._id}")
+                extendSyncFlagsFromMultipleParallel(sucs, i, worklist)
+                for o in sucs:
+                    if o.__class__ is HlsNetNodeExplicitSync:
+                        # [todo] only if hoisted to every predecessor
+                        unlink_hls_node_input_if_exists_with_worklist(o.skipWhen, worklist, True)
+                        unlink_hls_node_input_if_exists_with_worklist(o.extraCond, worklist, True)
+                modified = True
+                # for o in outputs:
+                #    removeExplicitSync(o, worklist, removed)
+                # worklist.append(src)
+                # worklist.append(coreNode)
                 
-            if src.__class__ is HlsNetNodeExplicitSync:
-                # :note: can sink because there is no risk that we require some input which may be optional
-                if src.skipWhen is None and src.extraCond is None:
-                    # nothing to sink
-                    dbgTracer.log(("rm", src._id))
-                    pass
-                else:
-                    # sink n flags to every o
-                    dbgTracer.log(("sink control to many and remove self", outputs), lambda x: f"{x[0]} {[o._id for o in x[1]]}")
-                    for o in outputs:
-                        extendSyncFlagsFrom(src, o)
-    
-                removeExplicitSync(src, worklist, removed)
-                worklist.extend(outputs)
-                worklist.append(src)
-                worklist.append(coreNode)
-
-                return True
-    
-            return False
-    
-        elif iCnt > 1 and oCnt == 1:
-            # parallel input sync with a single output
-            
-            # :note: For each node a sync from every transitively connected input must be connected at once
-            #   or it must not be sinked at all because change in order would change functionality of sync flags.
-            
-            # :note: there is a risk that some input is optional, for this we have to probe the island
-            # to discover if here is some mux which cuts the dependency on input under some condition
-            # and there are some sync flags which allow for input skip we must not sink behind this mux
-            # because it would make input always required
-            if outputs[0].__class__ is HlsNetNodeExplicitSync:
-                raise NotImplementedError("Try hoist flags from output", inputs, outputs)
-                
-            return False
-    
-        elif iCnt > 1 and oCnt > 1:
-            # n inputs to m outputs
-            # try merge sync with the same sync flags to reduce total number of sync
-            # For every output try hoist sync flags as much as possible
-            # if any other opt. is not possible, try to hoist this node if it is HlsNetNodeExplicitSync
-            for n in inputs:
-                if n in removed:
-                    continue
-                _modified = False
-                # :note: inputs/outputs do not need to be disjunct that is why set is used
-                if n.__class__ is HlsNetNodeExplicitSync:
-                    # it is not possible to hoist any sync, the remaining option is to sink flags from this sync to
-                    # connected nodes sync nodes of this island.
-                    
-                    # All sync flags from every input HlsNetNodeExplicitSync can be moved to every direct data successor of each HlsNetNodeExplicitSync
-                    # The move is impossible if some sync flags can not be moved.
-                    sucs = tuple(reachDb.getDirectDataSuccessors(n))
-                    dbgTracer.log(("sink control to successors and remove self", n._id, sucs),
-                                  lambda x: f"{x[0]} {x[1]:d}-> {[o._id for o in x[2]]}")
-    
-                    for isLast, suc in iter_with_last(sucs):
-                        preds = tuple(pred for pred in reachDb.getDirectDataPredecessors(suc)
-                                      if pred.__class__ is HlsNetNodeExplicitSync)
-                        everyPredIsSync = True
-                        for pred in preds:
-                            if pred.__class__ is not HlsNetNodeExplicitSync:
-                                everyPredIsSync = False
-                                break
-
-                        if not everyPredIsSync:
-                            raise NotImplementedError()
-                        
-                        toRm = []
-                        if len(preds) > 1:
-                            extendSyncFlagsFromMultipleParallel(preds, n, worklist)
-                            for pred in preds:
-                                predSuccs = tuple(reachDb.getDirectDataSuccessors(suc))
-                                if len(predSuccs) == 1:
-                                    # [todo] ther may be the case where predecessors are somehov dependent on each other
-                                    # and there is some partial ordering betwen them, in this case predSuccs may contain other pred
-                                    toRm.append(pred)  
-                            
-                        else:
-                            trasferHlsNetNodeExplicitSyncFlagsSeriallyConnected(n, suc, removeFromSrc=isLast)
-
-                        if isLast:
-                            toRm.append(n)
-                        worklist.append(suc)
-    
-                        for n1 in toRm:
-                            if n1 in removed:
-                                continue
-                            removeExplicitSync(dbgTracer, n1, worklist, removed)
-    
-                    _modified = True
-                    worklist.extend(inputs)
-    
-                if _modified:
-                    worklist.append(n)
-                    worklist.append(coreNode)
-                    modified = True
-                    break
-    
-            return modified
-    
-        elif iCnt == 1 and oCnt == 0:
-            # output of some other sync without any successor sync
-            src = tuple(inputs)[0]
-            if src.__class__ is HlsNetNodeExplicitSync:
-                # if not reachDb.hasControlPredecessor(n):
-                #    # this does not synchronize anything and it can be removed
-                #    raise NotImplementedError(n)
-    
-                if netlistHoistExplicitSync(dbgTracer, src, worklist, removed, reachDb):
-                    dbgTracer.log("hoist tailing self")
-                    worklist.append(src)
-                    worklist.append(coreNode)
-                    return True
-                
-            return False
-
-        elif iCnt != 0 and oCnt == 0:
-            # [todo] converto to outputs if beneficial
-            return False
-
-        else:
-            raise AssertionError("Unsupported combination of input/output count in IO cluster", iCnt, oCnt)
+        return modified 
