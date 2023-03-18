e51ae098fdb84b97aa617185efceaf07df3b9ae6
Nic30 <nic30@seznam.cz>
2023-03-18 15:51:06 +0100

    AxiSParseIfTC: additional tests

--- tests/io/amba/axiStream/axisParseIf.py
+++ tests/io/amba/axiStream/axisParseIf.py
@@ -12,9 +12,89 @@ from hwtHls.io.amba.axiStream.stmRead import HlsStmReadAxiStream
 from hwtHls.scope import HlsScope
 from hwtLib.amba.axis import AxiStream
 from tests.io.amba.axiStream.axisParseLinear import AxiSParse2fields
+from hwt.synthesizer.param import Param
+
+
+class AxiSParse2If2B(AxiSParse2fields):
+    """
+    Optionally read second byte from input stream
+    """
+
+    def _declr(self):
+        addClkRstn(self)
+        self.clk.FREQ = self.CLK_FREQ
+        with self._paramsShared():
+            self.i = AxiStream()
+        self.o = HsStructIntf()._m()
+        self.o.T = Bits(8)
+
+    def _impl(self) -> None:
+        hls = HlsScope(self)
+        # add register to prevent zero time data exchange ins sim (to see nicely transaction nicely in wave)
+        i = self.i
+        o = self.o
+        v0 = HlsStmReadAxiStream(hls, i, Bits(8), True)
+        v1 = HlsStmReadAxiStream(hls, i, Bits(8), True)
+
+        ast = HlsAstBuilder(hls)
+        hls.addThread(HlsThreadFromAst(hls,
+            ast.While(True,
+                HlsStmReadStartOfFrame(hls, i),
+                v0,
+                ast.If(v0.data._eq(2),
+                    v1,  # read 2B, output
+                    hls.write(v1.data, o),
+                ).Else(
+                    hls.write(v0.data._reinterpret_cast(o._dtype), o),
+                ),
+                HlsStmReadEndOfFrame(hls, i),
+            ),
+            self._name)
+        )
+        hls.compile()
+
+
+class AxiSParse2IfLess(AxiSParse2fields):
+    """
+    Read packet in following fomat: 1B header, 2 or 0B footer
+    """
+
+    def _declr(self):
+        addClkRstn(self)
+        self.clk.FREQ = self.CLK_FREQ
+        with self._paramsShared():
+            self.i = AxiStream()
+        self.o = HsStructIntf()._m()
+        self.o.T = Bits(32)
+
+    def _impl(self) -> None:
+        hls = HlsScope(self)
+        # add register to prevent zero time data exchange ins sim (to see nicely transaction nicely in wave)
+        i = self.i
+        o = self.o
+        v0 = HlsStmReadAxiStream(hls, i, Bits(8), True)
+        v1a = HlsStmReadAxiStream(hls, i, Bits(16), True)
+
+        ast = HlsAstBuilder(hls)
+        hls.addThread(HlsThreadFromAst(hls,
+            ast.While(True,
+                HlsStmReadStartOfFrame(hls, i),
+                v0,
+                ast.If(v0.data < 128,
+                    v1a,  # read 2B, output
+                    hls.write(v1a.data._reinterpret_cast(o._dtype), o),
+                ),
+                HlsStmReadEndOfFrame(hls, i),
+            ),
+            self._name)
+        )
+        hls.compile()
 
 
 class AxiSParse2If(AxiSParse2fields):
+    """
+    Read packet in following fomat: 2B header, 2 or 4 or 1B footer
+    """
 
     def _declr(self):
         addClkRstn(self)
@@ -56,6 +136,12 @@ class AxiSParse2If(AxiSParse2fields):
 
 
 class AxiSParse2IfAndSequel(AxiSParse2fields):
+    """
+    Read packet in following format: 2B header, 3 or 4 or 0 bytes, 1B footer 
+    """
+    def _config(self)->None:
+        AxiSParse2fields._config(self)
+        self.WRITE_FOOTER = Param(True)
 
     def _declr(self):
         addClkRstn(self)
@@ -79,15 +165,15 @@ class AxiSParse2IfAndSequel(AxiSParse2fields):
             ast.While(True,
                 HlsStmReadStartOfFrame(hls, i),
                 v0,
-                ast.If(v0._eq(3),
+                ast.If(v0.data._eq(3),
                        v1a,
-                       hls.write(v1a._reinterpret_cast(o._dtype), o),
-                ).Elif(v0._eq(4),
+                       hls.write(v1a.data._reinterpret_cast(o._dtype), o),
+                ).Elif(v0.data._eq(4),
                        v1b,
-                       hls.write(v1b._reinterpret_cast(o._dtype), o),
+                       hls.write(v1b.data._reinterpret_cast(o._dtype), o),
                 ),
                 v2,
-                hls.write(v2._reinterpret_cast(o._dtype), o),
+                *([hls.write(v2.data._reinterpret_cast(o._dtype), o)] if self.WRITE_FOOTER else ()),
                 HlsStmReadEndOfFrame(hls, i),
             ),
             self._name)
@@ -100,8 +186,8 @@ if __name__ == "__main__":
     from hwt.synthesizer.utils import to_rtl_str
     from hwtHls.platform.platform import HlsDebugBundle
 
-    u = AxiSParse2If()
-    u.DATA_WIDTH = 24
+    u = AxiSParse2IfAndSequel()
+    u.DATA_WIDTH = 16
     u.CLK_FREQ = int(40e6)
     p = VirtualHlsPlatform(debugFilter=HlsDebugBundle.ALL_RELIABLE)
     p._debugExpandCompositeNodes = True
--- tests/io/amba/axiStream/axisParseIf_test.py
+++ tests/io/amba/axiStream/axisParseIf_test.py
@@ -12,16 +12,63 @@ from hwtHls.platform.virtual import VirtualHlsPlatform
 from hwtLib.amba.axis import axis_send_bytes
 from hwtSimApi.utils import freq_to_period
 from pyMathBitPrecise.bit_utils import  int_to_int_list, mask
-from tests.io.amba.axiStream.axisParseIf import AxiSParse2If
+from tests.io.amba.axiStream.axisParseIf import AxiSParse2If2B, AxiSParse2IfLess, AxiSParse2If, AxiSParse2IfAndSequel
 
 
 class AxiSParseIfTC(SimTestCase):
 
+    def _test_AxiSParse2If2B(self, DATA_WIDTH:int, freq=int(1e6), N=16):
+        u = AxiSParse2If2B()
+        u.DATA_WIDTH = DATA_WIDTH
+        u.CLK_FREQ = freq
+        self.compileSimAndStart(u, target_platform=VirtualHlsPlatform())
+        
+        T1 = HStruct(
+            (Bits(8), "v0"),
+        )
+        T2 = HStruct(
+            (Bits(8), "v0"),
+            (Bits(8), "v1"),
+        )
+        
+        ref = []
+        for _ in range(N):
+            T = self._rand.choice((T1, T2))
+            if T is T1:
+                d = {"v0": 1}
+                ref.append(1)
+            else:
+                v1_t = T.field_by_name["v1"].dtype
+                v1 = self._rand.getrandbits(v1_t.bit_length())
+                d = {
+                    "v0": 2,
+                    "v1": v1
+                }
+                ref.append(v1)
+
+            v = T.from_py(d)
+            w = v._dtype.bit_length()
+            v = v._reinterpret_cast(Bits(w))
+            v.vld_mask = mask(w)
+            v = int(v)
+            data = int_to_int_list(v, 8, ceil(T.bit_length() / 8))
+            axis_send_bytes(u.i, data)
+
+        t = int(freq_to_period(freq)) * (len(u.i._ag.data) + 10) * 2
+        self.runSim(t)
+
+        self.assertValSequenceEqual(u.o._ag.data, ref, "%r [%s] != [%s]" % (
+            u.o,
+            ", ".join("0x%x" % int(i) if i._is_full_valid() else repr(i) for i in u.o._ag.data),
+            ", ".join("0x%x" % i for i in ref)
+        ))
+
+    # AxiSParse2IfLess 
     def _test_AxiSParse2If(self, DATA_WIDTH:int, freq=int(1e6), N=16):
         u = AxiSParse2If()
         u.DATA_WIDTH = DATA_WIDTH
         u.CLK_FREQ = freq
-        self.compileSimAndStart(u, target_platform=VirtualHlsPlatform(debugFilter=HlsDebugBundle.DBG_ARCH_SYNC))
+        self.compileSimAndStart(u, target_platform=VirtualHlsPlatform())
         T1 = HStruct(
             (Bits(16), "v0"),
             (Bits(8), "v1"),
@@ -65,6 +112,96 @@ class AxiSParseIfTC(SimTestCase):
             ", ".join("0x%x" % i for i in ref)
         ))
 
+    def _test_AxiSParse2IfAndSequel(self, DATA_WIDTH:int, freq=int(1e6), N=16, WRITE_FOOTER=True):
+        u = AxiSParse2IfAndSequel()
+        u.WRITE_FOOTER = WRITE_FOOTER
+        u.DATA_WIDTH = DATA_WIDTH
+        u.CLK_FREQ = freq
+        self.compileSimAndStart(u, target_platform=VirtualHlsPlatform(
+            debugFilter=HlsDebugBundle.ALL_RELIABLE.union({
+                        HlsDebugBundle.DBG_20_addSyncSigNames}
+            )))
+        T0 = HStruct(
+            (Bits(16), "v0"),
+            (Bits(8), "v2"),
+        )
+        T2 = HStruct(
+            (Bits(16), "v0"),
+            (Bits(24), "v1"),
+            (Bits(8), "v2"),
+        )
+        T4 = HStruct(
+            (Bits(16), "v0"),
+            (Bits(32), "v1"),
+            (Bits(8), "v2"),
+        )
+        
+        ref = []
+        ALL_Ts = [T0, T2, T4]
+        for _ in range(N):
+            T = self._rand.choice(ALL_Ts)
+            v2 = self._rand.getrandbits(8)
+            d = {
+                "v0": {T0: 10, T2: 3, T4:4}[T],
+                "v2": v2
+            }
+            if T is not T0:
+                v1_width = T.field_by_name["v1"].dtype.bit_length()
+                v1 = self._rand.getrandbits(v1_width)
+                d["v1"] = v1
+                ref.append(v1)
+
+            if WRITE_FOOTER:
+                ref.append(v2)
+
+            v = T.from_py(d)
+            w = v._dtype.bit_length()
+            v = v._reinterpret_cast(Bits(w))
+            v.vld_mask = mask(w)
+            v = int(v)
+            data = int_to_int_list(v, 8, ceil(T.bit_length() / 8))
+            axis_send_bytes(u.i, data)
+
+        t = int(freq_to_period(freq)) * (len(u.i._ag.data) + 10) * 2
+        if WRITE_FOOTER:
+            t *= 2
+        self.runSim(t)
+
+        self.assertValSequenceEqual(u.o._ag.data, ref, "%r [%s] != [%s]" % (
+            u.o,
+            ", ".join("0x%x" % int(i) if i._is_full_valid() else repr(i) for i in u.o._ag.data),
+            ", ".join("0x%x" % i for i in ref)
+        ))
+
+    # AxiSParse2If2B
+    def test_AxiSParse2If2B_8b_1MHz(self):
+        self._test_AxiSParse2If2B(8)
+
+    def test_AxiSParse2If2B_16b_1MHz(self):
+        self._test_AxiSParse2If2B(16)
+
+    def test_AxiSParse2If2B_24b_1MHz(self):
+        self._test_AxiSParse2If2B(24)
+
+    def test_AxiSParse2If2B_8b_40MHz(self):
+        self._test_AxiSParse2If2B(8, freq=int(40e6))
+
+    def test_AxiSParse2If2B_16b_40MHz(self):
+        self._test_AxiSParse2If2B(16, freq=int(40e6))
+
+    def test_AxiSParse2If2B_24b_40MHz(self):
+        self._test_AxiSParse2If2B(24, freq=int(40e6))
+    
+    def test_AxiSParse2If2B_8b_100MHz(self):
+        self._test_AxiSParse2If2B(8, freq=int(100e6))
+
+    def test_AxiSParse2If2B_16b_100MHz(self):
+        self._test_AxiSParse2If2B(16, freq=int(100e6))
+
+    def test_AxiSParse2If2B_24b_100MHz(self):
+        self._test_AxiSParse2If2B(24, freq=int(100e6))
+    
+    # AxiSParse2If
     def test_AxiSParse2If_8b_1MHz(self):
         self._test_AxiSParse2If(8)
 
@@ -110,16 +247,109 @@ class AxiSParseIfTC(SimTestCase):
     def test_AxiSParse2If_512b_100MHz(self):
         self._test_AxiSParse2If(512, freq=int(100e6))
 
+    # AxiSParse2IfAndSequel
+    def test_AxiSParse2IfAndSequel_8b_1MHz(self):
+        self._test_AxiSParse2IfAndSequel(8)
+
+    def test_AxiSParse2IfAndSequel_16b_1MHz(self):
+        self._test_AxiSParse2IfAndSequel(16)
+
+    def test_AxiSParse2IfAndSequel_24b_1MHz(self):
+        self._test_AxiSParse2IfAndSequel(24)
+
+    def test_AxiSParse2IfAndSequel_48b_1MHz(self):
+        self._test_AxiSParse2IfAndSequel(48)
+
+    def test_AxiSParse2IfAndSequel_512b_1MHz(self):
+        self._test_AxiSParse2IfAndSequel(512)
+
+    def test_AxiSParse2IfAndSequel_8b_40MHz(self):
+        self._test_AxiSParse2IfAndSequel(8, freq=int(40e6))
+
+    def test_AxiSParse2IfAndSequel_16b_40MHz(self):
+        self._test_AxiSParse2IfAndSequel(16, freq=int(40e6))
+
+    def test_AxiSParse2IfAndSequel_24b_40MHz(self):
+        self._test_AxiSParse2IfAndSequel(24, freq=int(40e6))
+
+    def test_AxiSParse2IfAndSequel_48b_40MHz(self):
+        self._test_AxiSParse2IfAndSequel(48, freq=int(40e6))
+
+    def test_AxiSParse2IfAndSequel_512b_40MHz(self):
+        self._test_AxiSParse2IfAndSequel(512, freq=int(40e6))
+
+    def test_AxiSParse2IfAndSequel_8b_100MHz(self):
+        self._test_AxiSParse2IfAndSequel(8, freq=int(100e6))
+
+    def test_AxiSParse2IfAndSequel_16b_100MHz(self):
+        self._test_AxiSParse2IfAndSequel(16, freq=int(100e6))
+
+    def test_AxiSParse2IfAndSequel_24b_100MHz(self):
+        self._test_AxiSParse2IfAndSequel(24, freq=int(100e6))
+
+    def test_AxiSParse2IfAndSequel_48b_100MHz(self):
+        self._test_AxiSParse2IfAndSequel(48, freq=int(100e6))
+
+    def test_AxiSParse2IfAndSequel_512b_100MHz(self):
+        self._test_AxiSParse2IfAndSequel(512, freq=int(100e6))
+
+    def test_AxiSParse2IfAndSequel_NO_FOOTER_8b_1MHz(self):
+        self._test_AxiSParse2IfAndSequel(8, WRITE_FOOTER=False)
+
+    def test_AxiSParse2IfAndSequel_NO_FOOTER_16b_1MHz(self):
+        self._test_AxiSParse2IfAndSequel(16, WRITE_FOOTER=False)
+
+    def test_AxiSParse2IfAndSequel_NO_FOOTER_24b_1MHz(self):
+        self._test_AxiSParse2IfAndSequel(24, WRITE_FOOTER=False)
+
+    def test_AxiSParse2IfAndSequel_NO_FOOTER_48b_1MHz(self):
+        self._test_AxiSParse2IfAndSequel(48, WRITE_FOOTER=False)
+
+    def test_AxiSParse2IfAndSequel_NO_FOOTER_512b_1MHz(self):
+        self._test_AxiSParse2IfAndSequel(512, WRITE_FOOTER=False)
+
+    def test_AxiSParse2IfAndSequel_NO_FOOTER_8b_40MHz(self):
+        self._test_AxiSParse2IfAndSequel(8, freq=int(40e6), WRITE_FOOTER=False)
+
+    def test_AxiSParse2IfAndSequel_NO_FOOTER_16b_40MHz(self):
+        self._test_AxiSParse2IfAndSequel(16, freq=int(40e6), WRITE_FOOTER=False)
+
+    def test_AxiSParse2IfAndSequel_NO_FOOTER_24b_40MHz(self):
+        self._test_AxiSParse2IfAndSequel(24, freq=int(40e6), WRITE_FOOTER=False)
+
+    def test_AxiSParse2IfAndSequel_NO_FOOTER_48b_40MHz(self):
+        self._test_AxiSParse2IfAndSequel(48, freq=int(40e6), WRITE_FOOTER=False)
+
+    def test_AxiSParse2IfAndSequel_NO_FOOTER_512b_40MHz(self):
+        self._test_AxiSParse2IfAndSequel(512, freq=int(40e6), WRITE_FOOTER=False)
+
+    def test_AxiSParse2IfAndSequel_NO_FOOTER_8b_100MHz(self):
+        self._test_AxiSParse2IfAndSequel(8, freq=int(100e6), WRITE_FOOTER=False)
+
+    def test_AxiSParse2IfAndSequel_NO_FOOTER_16b_100MHz(self):
+        self._test_AxiSParse2IfAndSequel(16, freq=int(100e6), WRITE_FOOTER=False)
+
+    def test_AxiSParse2IfAndSequel_NO_FOOTER_24b_100MHz(self):
+        self._test_AxiSParse2IfAndSequel(24, freq=int(100e6), WRITE_FOOTER=False)
+
+    def test_AxiSParse2IfAndSequel_NO_FOOTER_48b_100MHz(self):
+        self._test_AxiSParse2IfAndSequel(48, freq=int(100e6), WRITE_FOOTER=False)
+
+    def test_AxiSParse2IfAndSequel_NO_FOOTER_512b_100MHz(self):
+        self._test_AxiSParse2IfAndSequel(512, freq=int(100e6), WRITE_FOOTER=False)
+
 
 if __name__ == '__main__':
-    from hwt.synthesizer.utils import to_rtl_str
-    u = AxiSParse2If()
-    u.DATA_WIDTH = 512
-    u.CLK_FREQ = int(100e6)
-    print(to_rtl_str(u, target_platform=VirtualHlsPlatform(debugFilter=HlsDebugBundle.ALL_RELIABLE)))
+    #from hwt.synthesizer.utils import to_rtl_str
+    #u = AxiSParse2If()
+    #u.WRITE_FOOTER = False
+    #u.DATA_WIDTH = 16
+    #u.CLK_FREQ = int(1e6)
+    #print(to_rtl_str(u, target_platform=VirtualHlsPlatform(debugFilter=HlsDebugBundle.ALL_RELIABLE.union({
+    #   HlsDebugBundle.DBG_20_addSyncSigNames}))))
 
     suite = unittest.TestSuite()
-    suite.addTest(AxiSParseIfTC('test_AxiSParse2If_16b_100MHz'))
-    #suite.addTest(unittest.makeSuite(AxiSParseIfTC))
+    # suite.addTest(AxiSParseIfTC('test_AxiSParse2IfAndSequel_NO_FOOTER_48b_100MHz'))
+    suite.addTest(unittest.makeSuite(AxiSParseIfTC))
     runner = unittest.TextTestRunner(verbosity=3)
     runner.run(suite)
