6ce817a367d1403f6a5da5c76d2239467e1d05a8
Nic30 <nic30@seznam.cz>
2023-03-18 15:26:35 +0100

    llvm: DumpAndExitPass add options to print/throw conditionally

--- hwtHls/llvm/Transforms/dumpAndExitPass.h
+++ hwtHls/llvm/Transforms/dumpAndExitPass.h
@@ -1,18 +1,32 @@
 #include <llvm/IR/Function.h>
 #include <llvm/IR/PassManager.h>
+#include <llvm/Analysis/AliasAnalysis.h>
+#include <llvm/Analysis/BasicAliasAnalysis.h>
+#include <llvm/Analysis/GlobalsModRef.h>
 
 namespace hwtHls {
 
 class IntentionalCompilationInterupt: std::exception {};
 class DumpAndExitPass: public llvm::PassInfoMixin<DumpAndExitPass> {
+	bool dumpFn;
+	bool throwErrAndExit;
 public:
-	explicit DumpAndExitPass() {
+	explicit DumpAndExitPass(bool dumpFn, bool throwErrAndExit): dumpFn(dumpFn), throwErrAndExit(throwErrAndExit) {
 	}
 
 	llvm::PreservedAnalyses run(llvm::Function &F,
 			llvm::FunctionAnalysisManager &AM) {
-		F.dump();
-		throw IntentionalCompilationInterupt();
+		if (dumpFn)
+			F.dump();
+		if (throwErrAndExit)
+			throw IntentionalCompilationInterupt();
+		// Mark all the analyses that instcombine updates as preserved.
+		llvm::PreservedAnalyses PA;
+		PA.preserveSet<llvm::CFGAnalyses>();
+		PA.preserve<llvm::AAManager>();
+		PA.preserve<llvm::BasicAA>();
+		PA.preserve<llvm::GlobalsAA>();
+		return PA;
 	}
 };
 
