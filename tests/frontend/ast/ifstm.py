2d0c72e4799d003942bb5257e0537beda98caea6
Nic30 <nic30@seznam.cz>
2023-03-20 18:33:07 +0100

    HlsSimpleIfStatement_TC: fix imports when not __main__

--- tests/frontend/ast/ifstm.py
+++ tests/frontend/ast/ifstm.py
@@ -5,6 +5,7 @@ from hwt.interfaces.utils import addClkRstn
 from hwt.synthesizer.param import Param
 from hwtHls.frontend.ast.builder import HlsAstBuilder
 from hwtHls.frontend.ast.thread import HlsThreadFromAst
+from hwtHls.platform.virtual import VirtualHlsPlatform
 from hwtHls.scope import HlsScope
 from hwtLib.examples.statements.ifStm import SimpleIfStatement
 from hwtSimApi.utils import freq_to_period
@@ -57,7 +58,7 @@ class HlsSimpleIfStatement_TC(BaseSsaTC):
             u.a._ag.data.append(get_bit(i, 0))
             u.b._ag.data.append(get_bit(i, 1))
             u.c._ag.data.append(get_bit(i, 2))
-        
+
         def model(a, b, c):
             if a:
                 return b
@@ -74,14 +75,13 @@ class HlsSimpleIfStatement_TC(BaseSsaTC):
 
 
 if __name__ == "__main__":
-    from hwtHls.platform.virtual import VirtualHlsPlatform
     from hwt.synthesizer.utils import to_rtl_str
     from hwtHls.platform.platform import HlsDebugBundle
 
     u = HlsSimpleIfStatement()
     p = VirtualHlsPlatform(debugFilter=HlsDebugBundle.ALL_RELIABLE)
     print(to_rtl_str(u, target_platform=p))
-    
+
     import unittest
     suite = unittest.TestSuite()
     # suite.addTest(HlsAstExprTree3_example_TC('test_simple'))
