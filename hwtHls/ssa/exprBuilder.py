5abefa87d977ef32fe0f6288fc208fb98afb5b1e
Nic30 <nic30@seznam.cz>
2023-05-05 21:49:06 +0200

    rm unused SsaExprBuilderProxy

--- hwtHls/ssa/exprBuilder.py
+++ hwtHls/ssa/exprBuilder.py
@@ -13,66 +13,6 @@ from hwtHls.ssa.phi import SsaPhi
 from hwtHls.ssa.value import SsaValue
 
 
-class SsaExprBuilderProxy():
-
-    def __init__(self, parent:"SsaExprBuilder", var: SsaValue):
-        self.parent = parent
-        self.var = var
-
-    def _normalize(self, other):
-        if not isinstance(other, SsaValue):
-            raise NotImplementedError()
-        return other
-
-    def __add__(self, other):
-        return self.parent.binaryOp(self, AllOps.ADD, self._normalize(other))
-
-    def __sub__(self, other):
-        return self.parent.binaryOp(self, AllOps.SUB, self._normalize(other))
-
-    def __mul__(self, other):
-        return self.parent.binaryOp(self, AllOps.MUL, self._normalize(other))
-
-    def __floordiv__(self, other):
-        return self.parent.binaryOp(self, AllOps.DIV, self._normalize(other))
-
-    def __and__(self, other):
-        return self.parent.binaryOp(self, AllOps.AND, self._normalize(other))
-
-    def __or__(self, other):
-        return self.parent.binaryOp(self, AllOps.OR, self._normalize(other))
-
-    def __xor__(self, other):
-        return self.parent.binaryOp(self, AllOps.XOR, self._normalize(other))
-
-    def __eq__(self, other):
-        return self.parent.binaryOp(self, AllOps.EQ, self._normalize(other))
-
-    def __ne__(self, other):
-        return self.parent.binaryOp(self, AllOps.NE, self._normalize(other))
-
-    def __gt__(self, other):
-        return self.parent.binaryOp(self, AllOps.GT, self._normalize(other))
-
-    def __ge__(self, other):
-        return self.parent.binaryOp(self, AllOps.GE, self._normalize(other))
-
-    def __lt__(self, other):
-        return self.parent.binaryOp(self, AllOps.LT, self._normalize(other))
-
-    def __le__(self, other):
-        return self.parent.binaryOp(self, AllOps.LE, self._normalize(other))
-
-    def __invert__(self):
-        return self.parent.unaryOp(self, AllOps.NOT)
-
-    def __neg__(self):
-        return self.parent.binaryOp(self, AllOps.MINUS_UNARY)
-
-    def __getitem__(self, key):
-        return self.parent.binaryOp(self, AllOps.INDEX, self._normalize(key))
-
-
 class SsaExprBuilder():
 
     def __init__(self, block:SsaBasicBlock, position: Optional[int]=None):
@@ -126,9 +66,6 @@ class SsaExprBuilder():
             b.phis.insert(pos, instr)
             self.position += 1
         
-    def unaryOp(self, o: Union[SsaValue, HValue], operator: OpDefinition) -> SsaExprBuilderProxy:
-        return self.var(self._unaryOp(o.var, operator))
-
     def _normalizeOperandForOperatorResTypeEval(self,
                                                 o: Union[SsaValue, HValue, RtlSignal, Signal]
                                                 ) -> Tuple[Union[SsaValue, HValue], Union[SsaValue, HValue]]:
@@ -214,17 +151,9 @@ class SsaExprBuilder():
         return instr
         
         
-    def binaryOp(self, o0: Union[SsaValue, HValue, RtlSignal, Signal],
-                 operator: OpDefinition,
-                 o1: Union[SsaValue, HValue, RtlSignal, Signal]) -> SsaExprBuilderProxy:
-        return self.var(self._binaryOp(o0.var, operator, o1.var))
-
     def buildSliceConst(self, v: SsaValue, highBitNo: int, lowBitNo: int):
         i = SLICE.from_py(slice(highBitNo, lowBitNo, -1))
         return self._binaryOp(v, AllOps.INDEX, i)
-
-    def var(self, v: SsaValue):
-        return SsaExprBuilderProxy(self, v)
 
     def concat(self, *args) -> SsaValue:
         """
