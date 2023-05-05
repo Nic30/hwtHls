cda2723c14a55a368222acef2d24e155a007785b
Nic30 <nic30@seznam.cz>
2023-05-05 22:22:24 +0200

    codestyle

--- hwtHls/netlist/transformation/createIoClusters.py
+++ hwtHls/netlist/transformation/createIoClusters.py
@@ -15,8 +15,8 @@ class HlsNetlistPassCreateIoClusters(HlsNetlistPass):
     """
     Discover all HlsNetNodeExplicitSync node relations and extract HlsNetNodeIoClusterCore nodes which are conecting
     tied HlsNetNodeExplicitSync instances.
-    To guarantee that the ordering remains the same after circuit optimization. 
-    
+    To guarantee that the ordering remains the same after circuit optimization.
+
     :attention: Expects HlsNetlistPassMoveExplicitSyncOutOfDataAndAddVoidDataLinks to be applied before.
     """
 
@@ -37,7 +37,7 @@ class HlsNetlistPassCreateIoClusters(HlsNetlistPass):
             for o in outputs:
                 o: HlsNetNodeExplicitSync
                 link_hls_nodes(oPort, o.getOutputOfClusterPort())
-        
+
         minT = None
         for io in chain(inputs, outputs):
             io: HlsNetNodeExplicitSync
@@ -52,7 +52,7 @@ class HlsNetlistPassCreateIoClusters(HlsNetlistPass):
 
         assert minT is not None
         cc.setScheduling({cc: (minT, tuple(minT for _ in cc._inputs), tuple(minT for _ in cc._outputs))})
-        
+
         return cc
 
     def createIoClusterCores(self, netlist: HlsNetlistCtx, reachDb: HlsNetlistAnalysisPassReachabilility):
@@ -72,18 +72,18 @@ class HlsNetlistPassCreateIoClusters(HlsNetlistPass):
                 inputs = UniqList((i for i in inputs if i not in outputs))
                 if n in inputs or n.dependsOn[n._outputOfCluster.in_i] is None:
                     self.createIoClusterCore(netlist, inputs, outputs)
-                    
+
             if n.dependsOn[n._outputOfCluster.in_i] is None:
                 inputs, outputs, _ = HlsNetlistAnalysisPassBetweenSyncIslands.discoverSyncIsland(n, DIRECTION.OUT, reachDb)
                 inputs = UniqList((i for i in inputs if i not in outputs))
-                
+
                 self.createIoClusterCore(netlist, inputs, outputs)
-                assert n.dependsOn[n._outputOfCluster.in_i] is not None, n 
-        
+                assert n.dependsOn[n._outputOfCluster.in_i] is not None, n
+
         for n in allSync:
             if n.dependsOn[n._inputOfCluster.in_i] is None:
                 self.createIoClusterCore(netlist, UniqList((n,)), UniqList())
-        
+
     def apply(self, hls:"HlsScope", netlist:HlsNetlistCtx):
         try:
             reachDb = netlist.getAnalysis(HlsNetlistAnalysisPassReachabilility)
