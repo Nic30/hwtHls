ff107c98c183a8e1bced56f580cfe7229da575a6
Nic30 <nic30@seznam.cz>
2023-05-05 21:42:35 +0200

    RtlNetlistPassDumpStreamNodes: dump nodes per stage

--- hwtHls/architecture/translation/dumpStreamNodes.py
+++ hwtHls/architecture/translation/dumpStreamNodes.py
@@ -1,7 +1,7 @@
 
-from hwtHls.architecture.connectionsOfStage import ConnectionsOfStage
 from hwtHls.architecture.archElementFsm import ArchElementFsm
 from hwtHls.architecture.archElementPipeline import ArchElementPipeline
+from hwtHls.architecture.connectionsOfStage import ConnectionsOfStage
 from hwtHls.architecture.transformation.rtlNetlistPass import RtlNetlistPass
 from hwtHls.netlist.context import HlsNetlistCtx
 from hwtHls.platform.fileUtils import OutputStreamGetter
@@ -17,19 +17,24 @@ class RtlNetlistPassDumpStreamNodes(RtlNetlistPass):
         try:
             for elem_i, elm in enumerate(netlist.allocator._archElements):
                 out.write(f"########## {elm.__class__.__name__:s} {elem_i:d} ##########\n")
-                nodes = [n._id for n in elm.allNodes]
-                nodes.sort()
-                out.write(f" nodes={nodes}\n")
+                #nodes = [n._id for n in elm.allNodes]
+                #nodes.sort()
+                #out.write(f" nodes={nodes}\n")
                 if isinstance(elm, (ArchElementFsm, ArchElementPipeline)):
                     elm: ArchElementPipeline
-                    for st_i, st in enumerate(elm.connections):
-                        st: ConnectionsOfStage
+                    stages = elm.stages if isinstance(elm, ArchElementPipeline) else elm.fsm.states
+                    for st_i, (stCon, stNodes) in enumerate(zip(elm.connections, stages)):
+                        stCon: ConnectionsOfStage
                         out.write(f" ########## st {st_i:d} ##########\n")
-                        if st.sync_node is not None:
-                            out.write(repr(st.sync_node))
+                        nodes = [n._id for n in stNodes]
+                        nodes.sort()
+                        out.write(f"   nodes={nodes}\n")
+
+                        if stCon.sync_node is not None:
+                            out.write(repr(stCon.sync_node))
                             out.write("\n")
                 else:
                     raise NotImplementedError(elm)
-        finally: 
+        finally:
             if doClose:
                 out.close()
