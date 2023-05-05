49ff67737fa6f676ff93c7dc802ca8eb08e90788
Nic30 <nic30@seznam.cz>
2023-05-05 22:25:38 +0200

    HlsNetlistPassInjectVldMaskToSkipWhenConditions: support for opt. io
    * the value from some IO may not be required even if it is
      input to an expression tree, for example there may be a MUX
      which select a different value. In this case the value of IO is not
      required and this is now implemented in io condition discovery alg.

--- hwtHls/netlist/transformation/injectVldMaskToSkipWhenConditions.py
+++ hwtHls/netlist/transformation/injectVldMaskToSkipWhenConditions.py
@@ -1,19 +1,20 @@
-from typing import Set, Dict, Tuple
+from typing import Set, List, Union
 
 from hwt.hdl.operatorDefs import AllOps
+from hwt.hdl.types.defs import BIT
 from hwtHls.netlist.builder import HlsNetlistBuilder
 from hwtHls.netlist.context import HlsNetlistCtx
+from hwtHls.netlist.nodes.const import HlsNetNodeConst
 from hwtHls.netlist.nodes.explicitSync import HlsNetNodeExplicitSync
+from hwtHls.netlist.nodes.loopControl import HlsNetNodeLoopStatus
 from hwtHls.netlist.nodes.mux import HlsNetNodeMux
 from hwtHls.netlist.nodes.ops import HlsNetNodeOperator
 from hwtHls.netlist.nodes.ports import HlsNetNodeOut
+from hwtHls.netlist.nodes.programStarter import HlsProgramStarter
 from hwtHls.netlist.nodes.read import HlsNetNodeRead
 from hwtHls.netlist.nodes.readSync import HlsNetNodeReadSync
 from hwtHls.netlist.nodes.write import HlsNetNodeWrite
 from hwtHls.netlist.transformation.hlsNetlistPass import HlsNetlistPass
-from hwtHls.netlist.nodes.const import HlsNetNodeConst
-from hwtHls.netlist.nodes.loopGate import HlsLoopGateStatus
-from hwtHls.netlist.nodes.programStarter import HlsProgramStarter
 
 
 class HlsNetlistPassInjectVldMaskToSkipWhenConditions(HlsNetlistPass):
@@ -26,12 +27,40 @@ class HlsNetlistPassInjectVldMaskToSkipWhenConditions(HlsNetlistPass):
     First we walk expression from use to definition and collect which which validity flags are already anded to expression.
     Then for first 1b signal generated from each input we add "and" with the valid of IO input.
     (There must be some because the original condition is 1b wide.)
+
+    :note: It is not possible to have ands with validity flags from previous steps of compilation.
+        This is because the inputs may be added during the compilation once the variable
+        life is passed trough some buffer. This happens among other cases also in headers of loops for induction variable.
+        For each variable a MUX is constructed which is selecting the value from various predecessors
+        and the output value from this mux is driving the write to backedge buffer for that variable.
+        This means that the compare must be ANDed with a currently selected source read for this induction
+        variable.
+
+    .. code-block:: Python3
+    
+        i = def
+        while i != 0:
+            i += 1
+        
+    
+    .. code-block:: Python3
+    
+        i_pred = [def, ]
+        i_buff = []
+        
+        while True:
+            i = i_buff.readNB()
+            if not i.valid:
+                i = i_pred.read()
+            if i != 0: # there valid i is required
+                i_buff.append(i + 1)
+            else:
+                break
     """
 
     def apply(self, hls: "HlsScope", netlist: HlsNetlistCtx):
         builder = netlist.builder
         maskAppliedTo: Set[HlsNetNodeOut] = set()
-        maskForOut: Dict[HlsNetNodeOut, Tuple[HlsNetNodeOut, HlsNetNodeOut]] = {}
 
         for n in netlist.iterAllNodes():
             if isinstance(n, HlsNetNodeExplicitSync):
@@ -39,41 +68,52 @@ class HlsNetlistPassInjectVldMaskToSkipWhenConditions(HlsNetlistPass):
                 for inp in (n.skipWhen, n.extraCond):
                     if inp is not None:
                         o = n.dependsOn[inp.in_i]
-                        self._collectAndedVlds(o, set(), set(), maskAppliedTo)
+                        assert o is not None, inp
+                        if o in maskAppliedTo:
+                            continue
+                        self._detectAndedVlds(o, set(), set(), maskAppliedTo)
                         if o in maskAppliedTo:
                             continue
 
-                        _o, maskToApply = self._injectVldMaskToExpr(builder, o, maskAppliedTo, maskForOut)
+                        _o, maskToApply = self._injectAndVldIntoExpr(builder, o, maskAppliedTo)
                         assert maskToApply is None, "Should be already applied, because this should be 1b signal"
                         if o is not _o:
                             builder.replaceInputDriver(inp, _o)
 
-    def _collectAndedVlds(self, o: HlsNetNodeOut, ioWithAndedVld: Set[HlsNetNodeExplicitSync], ioUsed: Set[HlsNetNodeExplicitSync],
+    def _detectAndedVlds(self, o: HlsNetNodeOut,
+                          ioWithAndedVld: Set[Union[HlsNetNodeExplicitSync, HlsNetNodeMux]],
+                          ioUsed: Set[Union[HlsNetNodeExplicitSync, HlsNetNodeMux]],
                           maskAppliedTo: Set[HlsNetNodeOut]):
+        """
+        :param ioWithAndedVld: Set of IO and mux nodes which have all required masks already applied to this expression.
+        :param maskAppliedTo: Set of outputs which have all required masks already applied and thus
+            it is not required to modyfy its use nor it is required to search it over and over.
+        """
         if o in maskAppliedTo:
             return
 
         outObj = o.obj
-        if isinstance(outObj, (HlsNetNodeConst, HlsLoopGateStatus, HlsProgramStarter)):
+        if isinstance(outObj, (HlsNetNodeConst, HlsNetNodeLoopStatus, HlsProgramStarter)):
             pass
+
         elif isinstance(outObj, HlsNetNodeOperator):
             op = outObj.operator
             if op == AllOps.AND or len(outObj._inputs) == 1:
                 for operand in outObj.dependsOn:
-                    self._collectAndedVlds(operand, ioWithAndedVld, ioUsed, maskAppliedTo)
+                    self._detectAndedVlds(operand, ioWithAndedVld, ioUsed, maskAppliedTo)
             else:
                 _ioWithAndedVld0: Set[HlsNetNodeExplicitSync] = None
                 _ioUsed0: Set[HlsNetNodeExplicitSync] = None
                 for operand in outObj.dependsOn:
                     _ioWithAndedVld1: Set[HlsNetNodeExplicitSync] = set()
                     _ioUsed1: Set[HlsNetNodeExplicitSync] = set()
-                    self._collectAndedVlds(operand, _ioWithAndedVld1, _ioUsed1, maskAppliedTo)
+                    self._detectAndedVlds(operand, _ioWithAndedVld1, _ioUsed1, maskAppliedTo)
                     if _ioWithAndedVld0 is None:
                         _ioWithAndedVld0 = _ioWithAndedVld1
                         _ioUsed0 = _ioUsed1
                     else:
                         _ioWithAndedVld0.intersection_update(_ioWithAndedVld1)
-                        _ioUsed0.intersection_update(_ioUsed1)
+                        _ioUsed0.update(_ioUsed1)
 
                 if len(_ioWithAndedVld0) == len(_ioUsed0):
                     maskAppliedTo.add(o)
@@ -81,53 +121,97 @@ class HlsNetlistPassInjectVldMaskToSkipWhenConditions(HlsNetlistPass):
                 ioWithAndedVld.update(_ioWithAndedVld0)
                 ioUsed.update(_ioUsed0)
                 return
+
         elif isinstance(outObj, HlsNetNodeReadSync):
             return
+
         elif isinstance(outObj, (HlsNetNodeRead, HlsNetNodeWrite)):
             if o is outObj._validNB or o is outObj._valid:
                 ioWithAndedVld.add(outObj)
             ioUsed.add(outObj)
 
         elif outObj.__class__ is HlsNetNodeExplicitSync:
-            self._collectAndedVlds(outObj.dependsOn[0], ioWithAndedVld, ioUsed, maskAppliedTo)
+            self._detectAndedVlds(outObj.dependsOn[0], ioWithAndedVld, ioUsed, maskAppliedTo)
+
         else:
             raise NotImplementedError(o)
 
         if len(ioWithAndedVld) == len(ioUsed):
             maskAppliedTo.add(o)
 
-    def _injectVldMaskToExpr(self, builder: HlsNetlistBuilder,
+    def _injectAndVldIntoExpr(self, builder: HlsNetlistBuilder,
                              out: HlsNetNodeOut,
-                             maskAppliedTo: Set[HlsNetNodeOut],
-                             maskForOut: Dict[HlsNetNodeOut,
-                                              Tuple[HlsNetNodeOut, HlsNetNodeOut]]) -> HlsNetNodeOut:
+                             maskAppliedTo: Set[HlsNetNodeOut]) -> HlsNetNodeOut:
         """
         For channels which are read optionally we may have to mask incoming data if the data is used directly in this clock cycle
         to decide if some IO channel should be enabled.
+        
+        :param out: an output port where the mask with vld of source IO should be applied if required.
+        :param maskAppliedTo: :see: HlsNetlistPassInjectVldMaskToSkipWhenConditions._detectAndedVlds
         """
         assert isinstance(out, HlsNetNodeOut), (out, "When this function is called every output should be already resolved")
         outObj = out.obj
-        m = maskForOut.get(outObj, None)
-        if m is not None:
-            return m
-
         maskToApply = None
         if isinstance(outObj, HlsNetNodeReadSync):
             return out, None
 
         elif isinstance(outObj, (HlsNetNodeRead, HlsNetNodeWrite)):
             maskToApply = builder.buildReadSync(out)
-            maskAppliedTo.add(maskToApply)
+            maskAppliedTo.add(maskToApply)  # add to prevent apply of the mask on the vld itself
 
+        elif isinstance(outObj, HlsNetNodeMux):
+            # for mux conditions are driving which input is required
+            outObj: HlsNetNodeMux
+            # if there is a MUX it is required to resolve masks for every value and condition
+            # New mux is then constructed for masks which select mask of original mux value.
+            # This new mux output is then used as a new mask.
+            needsRebuild = False
+            maskMuxOps: List[HlsNetNodeOut] = []
+            ops: List[HlsNetNodeOut] = []
+            anyMaskRequred = False
+            for v, c in outObj._iterValueConditionDriverPairs():
+                if v in maskAppliedTo:
+                    _v, _maskToApply = self._injectAndVldIntoExpr(builder, v, maskAppliedTo)
+                    if _v is not v:
+                        needsRebuild = True
+                else:
+                    _v = v
+                    _maskToApply = None
+                ops.append(_v)
+                if _maskToApply is None:
+                    _maskToApply = builder.buildConstBit(1)  # 1 because the value is already anded with mask
+                else:
+                    anyMaskRequred = True
+                maskMuxOps.append(_maskToApply)
+                    
+                if c is not None:
+                    if c in maskAppliedTo:
+                        ops.append(c)
+                        maskMuxOps.append(c)
+                    else:
+                        _c, _maskToApply = self._injectAndVldIntoExpr(builder, c, maskAppliedTo)
+                        if _maskToApply is not None:
+                            _c = builder.buildAnd(c, _maskToApply)
+                            anyMaskRequred = True
+                        ops.append(_c)
+                        maskMuxOps.append(_c)
+
+            if anyMaskRequred:
+                maskToApply = builder.buildMux(BIT, tuple(maskMuxOps), name=f"n{outObj._id}_vld")
+                maskAppliedTo.add(maskToApply)
+                
+            if needsRebuild:
+                out = builder.buildMux(out._dtype, tuple(ops))
+        
         elif isinstance(outObj, HlsNetNodeOperator):
-            ops = []
+            ops: List[HlsNetNodeOut] = []
             needsRebuild = False
             for o in outObj.dependsOn:
                 if o in maskAppliedTo:
                     ops.append(o)
                     continue
 
-                _o, _maskToApply = self._injectVldMaskToExpr(builder, o, maskAppliedTo, maskForOut)
+                _o, _maskToApply = self._injectAndVldIntoExpr(builder, o, maskAppliedTo)
                 ops.append(_o)
                 if _o is not o:
                     needsRebuild = True
@@ -146,8 +230,6 @@ class HlsNetlistPassInjectVldMaskToSkipWhenConditions(HlsNetlistPass):
                     out = builder.buildAnd(*ops)
                 elif outObj.operator == AllOps.OR:
                     out = builder.buildOr(*ops)
-                elif isinstance(outObj, HlsNetNodeMux):
-                    out = builder.buildMux(out._dtype, tuple(ops))
                 else:
                     out = builder.buildOp(outObj.operator, out._dtype, *ops)
 
@@ -155,7 +237,7 @@ class HlsNetlistPassInjectVldMaskToSkipWhenConditions(HlsNetlistPass):
             # inject mask to expression on other side of this node
             oldI = outObj.dependsOn[0]
             if oldI not in maskAppliedTo:
-                newI, maskToApply = self._injectVldMaskToExpr(builder, outObj.dependsOn[0], maskAppliedTo, maskForOut)
+                newI, maskToApply = self._injectAndVldIntoExpr(builder, outObj.dependsOn[0], maskAppliedTo)
                 if newI is not oldI:
                     builder.replaceInputDriver(outObj._inputs[0], newI)
                 # return original out because we did not modify the node itself
