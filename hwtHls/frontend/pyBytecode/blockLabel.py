70037976cc44cf907a05b948948039e33cf2b3e4
Nic30 <nic30@seznam.cz>
2023-05-05 21:29:45 +0200

    pyFrontend: BlockLabel pretty name for nested functions

--- hwtHls/frontend/pyBytecode/blockLabel.py
+++ hwtHls/frontend/pyBytecode/blockLabel.py
@@ -1,8 +1,23 @@
+from types import FunctionType, MethodWrapperType, MethodType
 from typing import Tuple, Union, List
 
 
-BlockLabel = Tuple[Union[int, "PreprocLoopScope"], ...]
+class BlockLabel(Tuple[Union[int, "PreprocLoopScope", FunctionType, MethodType, MethodWrapperType, "BlockLabel"], ...]):
+
+    def __new__(cls, *args:Union[int, "PreprocLoopScope", FunctionType, MethodType, MethodWrapperType, "BlockLabel"]):
+        return tuple.__new__(cls, args)
+
+    def __repr__(self):
+        nameParts = []
+        for item in self:
+            if isinstance(item, (FunctionType, MethodType, MethodWrapperType,)):
+                fnName = getattr(item, "__qualname__", item.__name__)
+                nameParts.append(fnName)
+            else:
+                nameParts.append(repr(item))
+        res = f'({", ".join(nameParts)})'
+        return res
 
 
 def generateBlockLabel(preprocLoopScope: List["PreprocLoopScope"], blockOffset:int):
-    return (*preprocLoopScope, blockOffset)
+    return BlockLabel(*preprocLoopScope, blockOffset)
