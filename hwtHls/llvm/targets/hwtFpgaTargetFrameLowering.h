9972a9920855f2bcdd3294ef67bb363a964a9fba
Nic30 <nic30@seznam.cz>
2023-05-06 22:53:32 +0200

    HwtFpgaTargetFrameLowering: checks to prevent stack obj. misuse

--- hwtHls/llvm/targets/hwtFpgaTargetFrameLowering.h
+++ hwtHls/llvm/targets/hwtFpgaTargetFrameLowering.h
@@ -19,6 +19,64 @@ public:
 	bool hasFP(const llvm::MachineFunction &MF) const override {
 		return false;
 	}
+	virtual unsigned getStackAlignmentSkew(const MachineFunction &MF) const
+			override {
+		llvm_unreachable("HwtFpgaTarget does not use stack");
+	}
+	virtual bool isStackIdSafeForLocalArea(unsigned StackId) const override {
+		llvm_unreachable("HwtFpgaTarget does not use stack");
+	}
+	virtual bool allocateScavengingFrameIndexesNearIncomingSP(
+			const MachineFunction &MF) const override {
+		llvm_unreachable("HwtFpgaTarget does not use stack");
+	}
+
+	virtual bool assignCalleeSavedSpillSlots(MachineFunction &MF,
+			const TargetRegisterInfo *TRI, std::vector<CalleeSavedInfo> &CSI,
+			unsigned &MinCSFrameIndex, unsigned &MaxCSFrameIndex) const
+					override {
+		llvm_unreachable("HwtFpgaTarget does not use stack");
+	}
+
+	virtual bool assignCalleeSavedSpillSlots(MachineFunction &MF,
+			const TargetRegisterInfo *TRI,
+			std::vector<CalleeSavedInfo> &CSI) const override {
+		llvm_unreachable("HwtFpgaTarget does not use stack");
+	}
+
+	virtual bool enableCalleeSaveSkip(const MachineFunction &MF) const
+			override {
+		return true;
+	}
+
+	virtual bool hasReservedCallFrame(const MachineFunction &MF) const
+			override {
+		return true;
+	}
+
+	virtual bool needsFrameIndexResolution(const MachineFunction &MF) const
+			override {
+		return false;
+	}
+
+	virtual StackOffset getFrameIndexReference(const MachineFunction &MF,
+			int FI, Register &FrameReg) const override {
+		llvm_unreachable("HwtFpgaTarget does not use stack");
+	}
+
+	virtual void determineCalleeSaves(MachineFunction &MF, BitVector &SavedRegs,
+			RegScavenger *RS = nullptr) const override {
+		llvm_unreachable("HwtFpgaTarget does not use stack");
+	}
+
+	virtual TargetStackID::Value getStackIDForScalableVectors() const override {
+		llvm_unreachable("HwtFpgaTarget does not use stack");
+	}
+
+	virtual bool isSupportedStackID(TargetStackID::Value ID) const override {
+		llvm_unreachable("HwtFpgaTarget does not use stack");
+
+	}
 };
 
 }
