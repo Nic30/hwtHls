03fa2981e19827c64f02ef2fe94411128e6b06e0
Nic30 <nic30@seznam.cz>
2023-09-07 17:12:11 +0200

    add _runLlvmIrFunction

--- hwtHls/ssa/analysis/llvmMirInterpret.py
+++ hwtHls/ssa/analysis/llvmMirInterpret.py
@@ -1,39 +1,42 @@
+from itertools import islice
+from typing import Tuple, List, Generator, Union
 
-from typing import Tuple, List, Optional, Generator, Dict, Union
-from hwtHls.llvm.llvmIr import parseMIR, LlvmCompilationBundle, MachineFunction, MachineBasicBlock, MachineInstr, TargetOpcode, MachineOperand, \
-    Register, CmpInst, TypeToIntegerType
-from hwtHls.ssa.translation.llvmMirToNetlist.lowLevel import HlsNetlistAnalysisPassMirToNetlistLowLevel
-from hwt.hdl.value import HValue
+from hwt.code import Concat
+from hwt.hdl.operatorDefs import AllOps
 from hwt.hdl.types.bits import Bits
 from hwt.hdl.types.defs import INT, SLICE
-from hwt.code import Concat
-from hwt.synthesizer.rtlLevel.constants import NOT_SPECIFIED
+from hwt.hdl.value import HValue
 from hwt.pyUtils.arrayQuery import grouper
-from itertools import islice
+from hwt.synthesizer.rtlLevel.constants import NOT_SPECIFIED
+from hwtHls.llvm.llvmIr import parseMIR, LlvmCompilationBundle, MachineFunction, MachineBasicBlock, MachineInstr, TargetOpcode, MachineOperand, \
+    CmpInst, TypeToIntegerType, Register
+from hwtHls.ssa.translation.llvmMirToNetlist.lowLevel import HlsNetlistAnalysisPassMirToNetlistLowLevel
 
 
-class IoUnerflowErr(Exception):
+class SimIoUnerflowErr(Exception):
     """
     This exception is raised when there is not enough data on some IO, it may mean that
     the simulation did finish or simulated function is missing some data
     """
 
 
-def runMachineFunction(mf: MachineFunction, args: Tuple[Generator[Union[int, HValue], None, None], List[HValue], ...]):
+def runLlvmMachineFunction(mf: MachineFunction, args: Tuple[Generator[Union[int, HValue], None, None], List[HValue], ...]):
     """
     :param args: arguments for executed function, generator is used for inputs, list is for RAM/ROMs and outputs streams 
     """
     MRI = mf.getRegInfo()
     regs: List[Union[HValue, List[Union[int, HValue]], None]] = [None for _ in range(MRI.getNumVirtRegs())]
     mb: MachineBasicBlock = mf.getBlockNumbered(0)
-    globalValues: Dict[Register, HValue] = {}
+    # globalValues: Dict[Register, HValue] = {}
     assert mb is not None
     while True:
         nextMb = None
+        ops: List[Union[Register, MachineBasicBlock, int, HValue]] = []
         for mi in mb:
             mi: MachineInstr
+            # print(mi)
             opc = mi.getOpcode()
-            ops = []
+            ops.clear()
             for mo in mi.operands():
                 mo: MachineOperand
                 if mo.isReg():
@@ -64,7 +67,7 @@ def runMachineFunction(mf: MachineFunction, args: Tuple[Generator[Union[int, HVa
                     ops.append(mo.getGlobal())
                 else:
                     raise NotImplementedError(mi, mo)
-            
+
             if opc == TargetOpcode.HWTFPGA_ARG_GET:
                 dst, i = ops
                 assert dst.virtRegIndex() == i
@@ -73,14 +76,21 @@ def runMachineFunction(mf: MachineFunction, args: Tuple[Generator[Union[int, HVa
 
             elif opc == TargetOpcode.HWTFPGA_CLOAD or opc == TargetOpcode.HWTFPGA_CSTORE:
                 val, io, index, cond = ops
-                if isinstance(cond, int):
+                isBlocking = isinstance(cond, int)
+                if isBlocking:
                     if not cond:
-                        continue
+                        raise AssertionError("Always disabled load or store, this instruction should not exits", mi)
                 else:
                     assert cond._is_full_valid(), mi
                     if not cond:
+                        if opc == TargetOpcode.HWTFPGA_CLOAD:
+                            llt = MRI.getType(val)
+                            assert llt.isValid()
+                            width = llt.getSizeInBits()
+                            t = Bits(width)
+                            regs[val.virtRegIndex()] = t.from_py(0, vld_mask=1 << width - 1)
                         continue
-          
+
                 if not isinstance(index, int) or index != 0:
                     raise NotImplementedError(mi)
 
@@ -88,12 +98,18 @@ def runMachineFunction(mf: MachineFunction, args: Tuple[Generator[Union[int, HVa
                     try:
                         v = next(io)
                     except StopIteration:
-                        raise IoUnerflowErr("undeflow on io argument", mi)
-    
+                        raise SimIoUnerflowErr("undeflow on io argument", mi)
+
                     llt = MRI.getType(val)
                     assert llt.isValid()
                     t = Bits(llt.getSizeInBits())
-                    regs[val.virtRegIndex()] = t.from_py(v)
+                    if isinstance(v, HValue):
+                        if v._dtype != t:
+                            assert v._dtype.bit_length() == t.bit_length(), (mi, v._dtype, t, v)
+                            v = v._reinterpret_cast(t)
+                    else:
+                        v = t.from_py(v)
+                    regs[val.virtRegIndex()] = v
                 else:
                     assert opc == TargetOpcode.HWTFPGA_CSTORE, mi
                     io.append(val)
@@ -120,7 +136,7 @@ def runMachineFunction(mf: MachineFunction, args: Tuple[Generator[Union[int, HVa
                 half = (len(ops) - 1) // 2
                 res = Concat(*reversed(ops[1:half + 1]))
                 regs[dst.virtRegIndex()] = res
-            
+
             elif opc == TargetOpcode.HWTFPGA_MUX:
                 dst = ops[0]
                 res = NOT_SPECIFIED
@@ -146,48 +162,56 @@ def runMachineFunction(mf: MachineFunction, args: Tuple[Generator[Union[int, HVa
                 res = op._evalFn(src0, src1)
                 regs[dst.virtRegIndex()] = res
             elif opc == TargetOpcode.HWTFPGA_BR:
-                nextMb = ops[0]  
+                nextMb = ops[0]
             elif opc == TargetOpcode.HWTFPGA_BRCOND:
                 cond, _mb = ops
-                assert cond._is_full_valid(), mi
+                assert cond._is_full_valid(), (mi, "Branch condition must be valid")
                 if cond:
                     nextMb = _mb
                     break
             else:
-                binOp = HlsNetlistAnalysisPassMirToNetlistLowLevel.OPC_TO_OP.get(opc)
-                if binOp is not None:
+                op = HlsNetlistAnalysisPassMirToNetlistLowLevel.OPC_TO_OP.get(opc)
+                if op  is AllOps.NOT:
+                    dst, src0 = ops
+                    if src0._dtype.signed is not None:
+                        src0 = src0.cast_sign(None)
+                    res = op._evalFn(src0)
+                    regs[dst.virtRegIndex()] = res
+                    continue
+
+                if op is not None:
                     dst, src0, src1 = ops
                     if src0._dtype.signed is not None:
                         src0 = src0.cast_sign(None)
                     if src1._dtype.signed is not None:
                         src1 = src1.cast_sign(None)
-                    res = binOp._evalFn(src0, src1)
+                    res = op._evalFn(src0, src1)
                     regs[dst.virtRegIndex()] = res
                     continue
 
                 if opc == TargetOpcode.PseudoRET:
                     return
-                
+
                 raise NotImplementedError(mi)
-            
+
         if nextMb is not None:
             mb = nextMb
         else:
             mb = mb.getFallThrough(False)
 
-    
+
 def runMirStr(mirStr: str, nameOfMain: str, args: list):
     ctx = LlvmCompilationBundle(nameOfMain)
+    m = parseMIR(mirStr, nameOfMain, ctx)
     MMI = ctx.getMachineModuleInfo()
-    m = parseMIR(mirStr, nameOfMain, ctx.ctx, MMI)
     assert m is not None
     f = m.getFunction(ctx.strCtx.addStringRef(nameOfMain))
     assert f is not None
     mf = MMI.getMachineFunction(f)
     assert mf is not None
     try:
-        runMachineFunction(mf, args)
-    except IoUnerflowErr:
+        runLlvmMachineFunction(mf, args)
+    except SimIoUnerflowErr:
         pass
 
     return ctx, MMI, m, mf
