ea6d351bc829196e2b27c9a6c374d1089b2496ca
Nic30 <nic30@seznam.cz>
2023-09-07 17:40:50 +0200

    tests: PyBytecodeInline_TC: additional tests

--- tests/frontend/pyBytecode/pragmaInline.py
+++ tests/frontend/pyBytecode/pragmaInline.py
@@ -2,11 +2,12 @@
 # -*- coding: utf-8 -*-
 
 from hwt.hdl.types.defs import BIT
-from hwt.interfaces.std import VectSignal
+from hwt.interfaces.std import VectSignal, Signal, Handshaked
 from hwt.interfaces.utils import addClkRstn
 from hwt.synthesizer.unit import Unit
 from hwtHls.frontend.pyBytecode import hlsBytecode
-from hwtHls.frontend.pyBytecode.markers import PyBytecodeInline
+from hwtHls.frontend.pyBytecode.markers import PyBytecodeInline, \
+    PyBytecodePreprocHwCopy, PyBytecodeBlockLabel
 from hwtHls.frontend.pyBytecode.thread import HlsThreadFromPy
 from hwtHls.scope import HlsScope
 from hwtLib.types.ctypes import uint8_t
@@ -39,6 +40,7 @@ class PragmaInline_NestedLoop(PragmaInline_singleBlock):
     @hlsBytecode
     def mainThread(self, hls: HlsScope):
         b1 = BIT.from_py(1)
+
         @hlsBytecode
         def fn():
             while b1:
@@ -155,9 +157,102 @@ class PragmaInline_writeCntr3(PragmaInline_writeCntr1):
             cntr = PyBytecodeInline(self.writeAndIncrement)(hls, cntr)
 
 
+class PragmaInline_writeSaturatedCntr4(PragmaInline_writeCntr1):
+
+    @hlsBytecode
+    def writeAndIncrement(self, hls, cntr, limit=4):
+        cntrTmp = PyBytecodePreprocHwCopy(cntr)
+        if limit > 0:
+            limitBit = cntr[limit]
+            if ~limitBit:
+                cntr += 1
+        # else:
+        #    hls.write(cntr, self.o)
+
+        return cntrTmp
+
+    @hlsBytecode
+    def mainThread(self, hls: HlsScope):
+        cntr = uint8_t.from_py(0)
+
+        while BIT.from_py(1):
+            cntrTmp = PyBytecodeInline(self.writeAndIncrement)(hls, cntr)
+            hls.write(cntrTmp, self.o)
+
+
+class PragmaInline_SequenceCounter(PragmaInline_singleBlock):
+
+    def _declr(self):
+        addClkRstn(self)
+        self.o = Handshaked()._m()
+        self.o.DATA_WIDTH = 8
+        self.i = Signal()
+
+    @hlsBytecode
+    @PyBytecodeInline
+    def oneSeqLen(self, hls: HlsScope):
+        cntr = uint8_t.from_py(0)
+        PyBytecodeBlockLabel("oneSeqLen")
+        while hls.read(self.i).data:
+            PyBytecodeBlockLabel("whileRead")
+            cntr += 1
+        PyBytecodeBlockLabel("oneSeqLenRet")
+        return cntr
+
+    @hlsBytecode
+    def mainThread(self, hls: HlsScope):
+        PyBytecodeBlockLabel("entry")
+        while BIT.from_py(1):
+            PyBytecodeBlockLabel("mainLoop")
+            oneLen = self.oneSeqLen(self, hls)
+            hls.write(oneLen, self.o)
+
+    def _impl(self):
+        PragmaInline_singleBlock._impl(self)
+
+
+class PragmaInline_FilterZeros(PragmaInline_SequenceCounter):
+
+    def _declr(self):
+        addClkRstn(self)
+        self.o = Handshaked()._m()
+        self.o.DATA_WIDTH = 8
+        self.i = Handshaked()
+        self.i.DATA_WIDTH = 8
+
+    @hlsBytecode
+    @PyBytecodeInline
+    def oneSeqLen(self, hls: HlsScope):
+        PyBytecodeBlockLabel("oneSeqLen")
+        v = hls.read(self.i).data
+        while v._eq(0):
+            PyBytecodeBlockLabel("whileRead")
+            v = hls.read(self.i).data
+        PyBytecodeBlockLabel("oneSeqLenRet")
+        return v
+
+
+class PragmaInline_TwoInLoopLiveVars(PragmaInline_FilterZeros):
+
+    @hlsBytecode
+    @PyBytecodeInline
+    def oneSeqLen(self, hls: HlsScope):
+        PyBytecodeBlockLabel("oneSeqLen")
+        cntr = uint8_t.from_py(0)
+        v = hls.read(self.i).data
+        while v < 128:
+            PyBytecodeBlockLabel("whileRead")
+            cntr += v
+            v = hls.read(self.i).data
+        if v < 4:
+            cntr += 1
+        PyBytecodeBlockLabel("oneSeqLenRet")
+        return cntr
+
+
 if __name__ == "__main__":
     from hwt.synthesizer.utils import to_rtl_str
     from hwtHls.platform.virtual import VirtualHlsPlatform
     from hwtHls.platform.platform import HlsDebugBundle
-    u = PragmaInline_writeCntr2()
+    u = PragmaInline_SequenceCounter()
     print(to_rtl_str(u, target_platform=VirtualHlsPlatform(debugFilter=HlsDebugBundle.ALL_RELIABLE)))
--- tests/frontend/pyBytecode/pragmaInline_test.py
+++ tests/frontend/pyBytecode/pragmaInline_test.py
@@ -7,7 +7,9 @@ from hwtSimApi.utils import freq_to_period
 from tests.frontend.pyBytecode.pragmaInline import PragmaInline_singleBlock, \
     PragmaInline_NestedLoop, PragmaInline_return1_0, PragmaInline_return1_1, \
     PragmaInline_return1_1hw, PragmaInline_writeCntr0, PragmaInline_writeCntr1, \
-    PragmaInline_writeCntr2, PragmaInline_writeCntr3
+    PragmaInline_writeCntr2, PragmaInline_writeCntr3, \
+    PragmaInline_writeSaturatedCntr4, PragmaInline_SequenceCounter, \
+    PragmaInline_FilterZeros, PragmaInline_TwoInLoopLiveVars
 
 
 class PyBytecodeInline_TC(SimTestCase):
@@ -55,16 +57,80 @@ class PyBytecodeInline_TC(SimTestCase):
     def test_PragmaInline_writeCntr3(self):
         self._test_writesCntr(PragmaInline_writeCntr3)
 
+    def test_PragmaInline_writeSaturatedCntr4(self):
+        self._test_writesCntr(PragmaInline_writeSaturatedCntr4)
+
+    def test_PragmaInline_SequenceCounter(self):
+        u = PragmaInline_SequenceCounter()
+        self.compileSimAndStart(u, target_platform=VirtualHlsPlatform())
+        CLK_PERIOD = freq_to_period(u.clk.FREQ)
+        u.i._ag.data.extend([0, 0, 1, 0, 1, 1, 1, 0, 0, 0])
+
+        def model(data):
+            cntr = 0
+            for d in data:
+                if d == 0:
+                    yield cntr
+                    cntr = 0
+                else:
+                    cntr += 1
+
+        ref = list(model(u.i._ag.data))
+        self.runSim((len(u.i._ag.data) + 1) * int(CLK_PERIOD))
+
+        self.assertValSequenceEqual(u.o._ag.data, ref)
+
+    def test_PragmaInline_FilterZeros(self):
+        u = PragmaInline_FilterZeros()
+        self.compileSimAndStart(u, target_platform=VirtualHlsPlatform())
+        CLK_PERIOD = freq_to_period(u.clk.FREQ)
+        u.i._ag.data.extend([0, 0, 1, 0, 1, 1, 1, 0, 12, 0])
+
+        def model(data):
+            for d in data:
+                if d != 0:
+                    yield d
+
+        ref = list(model(u.i._ag.data))
+        self.runSim((len(u.i._ag.data) + 1) * int(CLK_PERIOD))
+
+        self.assertValSequenceEqual(u.o._ag.data, ref)
+
+    def test_PragmaInline_TwoInLoopLiveVars(self):
+        u = PragmaInline_TwoInLoopLiveVars()
+        self.compileSimAndStart(u, target_platform=VirtualHlsPlatform())
+        CLK_PERIOD = freq_to_period(u.clk.FREQ)
+        u.i._ag.data.extend([0, 0, 1, 0, 1, 129, 1, 1, 0, 12, 0])
+
+        def model(data):
+            it = iter(data)
+            try:
+                while True:
+                    cntr = 0
+                    v = next(it)
+                    while v < 128:
+                        cntr += v
+                        v = next(it)
+                    if v < 4:
+                        cntr += 1
+                    yield cntr
+            except StopIteration:
+                return
+        ref = list(model(u.i._ag.data))
+        self.runSim((len(u.i._ag.data) + 1) * int(CLK_PERIOD))
+
+        self.assertValSequenceEqual(u.o._ag.data, ref)
+
 
 if __name__ == "__main__":
     import unittest
-    from hwt.synthesizer.utils import to_rtl_str
-    from hwtHls.platform.platform import HlsDebugBundle
-    u = PragmaInline_NestedLoop()
-    print(to_rtl_str(u, target_platform=VirtualHlsPlatform(debugFilter=HlsDebugBundle.ALL_RELIABLE)))
+    #from hwt.synthesizer.utils import to_rtl_str
+    #from hwtHls.platform.platform import HlsDebugBundle
+    #u = PragmaInline_TwoInLoopLiveVars()
+    #print(to_rtl_str(u, target_platform=VirtualHlsPlatform(debugFilter=HlsDebugBundle.ALL_RELIABLE)))
 
     testLoader = unittest.TestLoader()
-    # suite = unittest.TestSuite([PyBytecodeInline_TC("test_PragmaInline_writeCntr2")])
+    #suite = unittest.TestSuite([PyBytecodeInline_TC("test_PragmaInline_TwoInLoopLiveVars")])
     suite = testLoader.loadTestsFromTestCase(PyBytecodeInline_TC)
     runner = unittest.TextTestRunner(verbosity=3)
     runner.run(suite)
