8c80066c5324a189d6eeb7686c817d8f9c43413d
Nic30 <nic30@seznam.cz>
2023-09-07 12:45:53 +0200

    codestyle

--- hwtHls/ssa/transformation/ssaPass.py
+++ hwtHls/ssa/transformation/ssaPass.py
@@ -2,5 +2,5 @@
 
 class SsaPass():
 
-    def apply(self, hls: "HlsScope", to_ssa: "HlsAstToSsa"):
+    def apply(self, hls: "HlsScope", toSsa: "HlsAstToSsa"):
         raise NotImplementedError("Should be implemented in child class", self)
