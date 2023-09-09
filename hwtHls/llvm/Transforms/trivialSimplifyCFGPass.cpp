8c9f8d445ed688067796da64558e1cf4f7a261b6
Nic30 <nic30@seznam.cz>
2023-09-09 14:20:45 +0200

    llvm: TrivialSimplifyCFGPass: fix unintentional discard of br metadata

--- hwtHls/llvm/Transforms/trivialSimplifyCFGPass.cpp
+++ hwtHls/llvm/Transforms/trivialSimplifyCFGPass.cpp
@@ -12,9 +12,20 @@ using namespace llvm;
 
 namespace hwtHls {
 
-bool tryRemoveSingleSuccessorSinglePredecessorBlock(BasicBlock *BB, llvm::SmallSetVector<BasicBlock*, 16>& WorkList) {
+void scavengeTerminatorMetadata(Instruction *br, Instruction *newBr) {
+	SmallVector<std::pair<unsigned, MDNode*>> MDs;
+	br->getAllMetadata(MDs);
+	for (const auto &md : MDs) {
+		assert(!newBr->hasMetadata(md.first));
+		newBr->setMetadata(md.first, md.second);
+	}
+}
+
+bool tryRemoveSingleSuccessorSinglePredecessorBlock(BasicBlock *BB,
+		llvm::SmallSetVector<BasicBlock*, 16> &WorkList) {
 	// Remove empty basic block if has single successor and predecessor and may be replaced by predecessor in successor PHIs
-	if (BB->begin() == BB->end() || &*BB->begin() == BB->getTerminator() || ++BB->begin() == BB->end()) {
+	if (BB->begin() == BB->end() || &*BB->begin() == BB->getTerminator()
+			|| ++BB->begin() == BB->end()) {
 		if (auto *SucBB = BB->getSingleSuccessor()) {
 			if (SucBB == BB) {
 				return false; // can not remove self loop
@@ -40,7 +51,8 @@ bool tryRemoveSingleSuccessorSinglePredecessorBlock(BasicBlock *BB, llvm::SmallS
 
 				// update PHIs
 				for (PHINode &SucPhi : SucBB->phis()) {
-					if (std::find(alreadyHasTheValueInPhis.begin(), alreadyHasTheValueInPhis.end(), &SucPhi)
+					if (std::find(alreadyHasTheValueInPhis.begin(),
+							alreadyHasTheValueInPhis.end(), &SucPhi)
 							!= alreadyHasTheValueInPhis.end())
 						SucPhi.removeIncomingValue(BB, false);
 				}
@@ -48,6 +60,8 @@ bool tryRemoveSingleSuccessorSinglePredecessorBlock(BasicBlock *BB, llvm::SmallS
 				// because there is a single predecessor
 				BB->replaceAllUsesWith(PredBB); // replace in other PHIs, which effectively disconnect this from predecessor
 				assert(BB->hasNPredecessors(0));
+				scavengeTerminatorMetadata(BB->getTerminator(),
+						SucBB->getTerminator());
 				BB->eraseFromParent();
 				//DeleteDeadBlock(BB); // this causes segfault as it expect for PHIs to have this block in operands
 				WorkList.insert(PredBB);
@@ -59,12 +73,14 @@ bool tryRemoveSingleSuccessorSinglePredecessorBlock(BasicBlock *BB, llvm::SmallS
 	return false;
 }
 
-bool trySimplifyTerminator(BasicBlock & BB, llvm::SmallSetVector<BasicBlock*, 16> & worklist) {
+bool trySimplifyTerminator(BasicBlock &BB,
+		llvm::SmallSetVector<BasicBlock*, 16> &worklist) {
 	if (auto br = dyn_cast<BranchInst>(BB.getTerminator())) {
-		if (br->isConditional() && br->getSuccessor(0)== br->getSuccessor(1)) {
+		if (br->isConditional() && br->getSuccessor(0) == br->getSuccessor(1)) {
 			// br c, bb0, bb0 -> br bb0
 			IRBuilder<> Builder(br);
-			Builder.CreateBr(br->getSuccessor(0));
+			auto *newBr = Builder.CreateBr(br->getSuccessor(0));
+			scavengeTerminatorMetadata(br, newBr);
 			br->eraseFromParent();
 			return true;
 		}
@@ -86,7 +102,7 @@ llvm::PreservedAnalyses TrivialSimplifyCFGPass::run(llvm::Function &F,
 	}
 
 	while (!WorkList.empty()) {
-		BasicBlock * BB = WorkList.pop_back_val();
+		BasicBlock *BB = WorkList.pop_back_val();
 		// attention trySimplifyTerminator is required because previous opt may generate conditiona br to same successor causes issues for rest of transformations like llvm::SimplifyCFG
 		if (trySimplifyTerminator(*BB, WorkList)) {
 			Changed = true;
