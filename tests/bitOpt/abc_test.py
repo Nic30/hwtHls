4a6a5099408b09f58fdb6a34e791e9d012a4f438
Nic30 <nic30@seznam.cz>
2024-01-27 09:32:03 +0100

    tests(AbcTC): test also longer patterns

--- tests/bitOpt/abc_test.py
+++ tests/bitOpt/abc_test.py
@@ -14,19 +14,24 @@ class AbcTC(unittest.TestCase):
         a = hwtNet.sig("a")
         b = hwtNet.sig("b")
         c = hwtNet.sig("c")
+        c1 = hwtNet.sig("c1")
         d = a ^ b & a & a ^ 1
         e = a._ternary(b, d)
 
         toAig = RtlNetlistToAbcAig()
-        f, net, aig = toAig.translate([a, b, c], [
+        f, net, aig = toAig.translate([a, b, c, c1], [
             d,  # 0
             e,  # 1
             a,  # 2
             a | b,  # 3
             ~a,  # 4
             a & ~a,  # 5
+            ~a & ~b, # 5b
             a | ~a,  # 6
             ~(a | b | c),  # 7
+            ~(a | b | c | c1),  # 7b
+            ~(a & b & c),  # 7c
+            ~(a & b & c & c1),  # 7d
             a._eq(1),  # 8
             a._eq(0),  # 9
             a != 1,  # 10
@@ -37,6 +42,7 @@ class AbcTC(unittest.TestCase):
             a & ~b,  # 15
             ~a & b,  # 16
             a & (~b | c),  # 17
+            (a & b) | (c & ~b), # 18
             ])
         net = abcCmd_resyn2(net)
         net = abcCmd_compress2(net)
@@ -44,14 +50,18 @@ class AbcTC(unittest.TestCase):
         res = toRtl.translate()
         f.DeleteAllNetworks()
         self.assertSequenceEqual(res, [
-            ~(a & ~b),  # 0
-            ~(a & ~b),  # 1
+            ~a | b,  # 0
+            ~a | b,  # 1
             a,  # 2
             a | b,  # 3
             ~a,  # 4
             BIT.from_py(0),  # 5
+            ~(a | b), # 5b
             BIT.from_py(1),  # 6
-            ~c & (~a & ~b),  # 7
+            ~(c | a | b),  # 7
+            ~(c1 | c | a | b),  # 7b
+            ~(c & a & b),  # 7c
+            ~(c1 & c & a & b ),  # 7d
             a,  # 8
             ~a,  # 9
             a ^ 1,  # 10
@@ -61,7 +71,8 @@ class AbcTC(unittest.TestCase):
             a | ~b | c,  # 14
             a & ~b,  # 15
             ~a & b,  # 16
-            a & ~(b & ~c),  # 17
+            a & (~b | c),  # 17
+            (a | ~b) & (b | c), # 18
             ])
 
 
