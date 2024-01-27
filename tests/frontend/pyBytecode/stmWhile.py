50a83ca7fdcea67ff5c198ced98479e7f714cd33
Nic30 <nic30@seznam.cz>
2024-01-27 10:14:12 +0100

    tests(StmWhile_sim_TC): more tests for shifts and slices in loop

--- tests/frontend/pyBytecode/stmWhile.py
+++ tests/frontend/pyBytecode/stmWhile.py
@@ -4,28 +4,42 @@
 from hwt.code import Concat
 from hwt.hdl.types.bits import Bits
 from hwt.hdl.types.defs import BIT
+from hwt.interfaces.hsStructIntf import HsStructIntf
 from hwt.interfaces.std import VectSignal, Signal, Handshaked, Clk
 from hwt.interfaces.utils import addClkRstn
+from hwt.synthesizer.param import Param
 from hwt.synthesizer.unit import Unit
 from hwtHls.frontend.pyBytecode import hlsBytecode
-from hwtHls.frontend.pyBytecode.markers import PyBytecodeInline,\
+from hwtHls.frontend.pyBytecode.markers import PyBytecodeInline, \
     PyBytecodeBlockLabel
 from hwtHls.frontend.pyBytecode.thread import HlsThreadFromPy
 from hwtHls.scope import HlsScope
 from hwtLib.types.ctypes import uint8_t
-
+from typing import List, Iterator, Union
+from hwt.hdl.types.bitsVal import BitsVal
 
 TRUE = BIT.from_py(1)
 
-class HlsPythonHwWhile0(Unit):
+
+class HlsPythonHwWhile0a(Unit):
 
     def _config(self) -> None:
-        Clk._declr(self)
+        self.CLK_FREQ = Param(Clk.DEFAULT_FREQ)
 
     def _declr(self):
         addClkRstn(self)
         self.i = Signal()  # rst
         self.o = VectSignal(8, signed=False)._m()
+
+    @staticmethod
+    def model(dataIn: Iterator[BitsVal], dataOut: List[int]):
+        i = uint8_t.from_py(0)
+        while True:  # recognized as HW loop because of type
+            i += 1
+            dataOut.append(int(i))
+            rst = next(dataIn)
+            if rst:
+                i = 0
 
     @hlsBytecode
     def mainThread(self, hls: HlsScope):
@@ -37,13 +51,13 @@ class HlsPythonHwWhile0(Unit):
                 i = 0
 
     def _impl(self):
-        hls = HlsScope(self, freq=int(100e6))
+        hls = HlsScope(self)
         mainThread = HlsThreadFromPy(hls, self.mainThread, hls)
         hls.addThread(mainThread)
         hls.compile()
 
 
-class HlsPythonHwWhile0b(HlsPythonHwWhile0):
+class HlsPythonHwWhile0b(HlsPythonHwWhile0a):
     """
     Test of directly nested HW while true loop
     """
@@ -51,6 +65,12 @@ class HlsPythonHwWhile0b(HlsPythonHwWhile0):
     def _declr(self):
         addClkRstn(self)
         self.o = VectSignal(8, signed=False)._m()
+
+    @staticmethod
+    def model(dataOut: List[int]):
+        while True:
+            dataOut.append(10)
+            yield
 
     @hlsBytecode
     def mainThread(self, hls: HlsScope):
@@ -60,7 +80,17 @@ class HlsPythonHwWhile0b(HlsPythonHwWhile0):
                 hls.write(i, self.o)
 
 
-class HlsPythonHwWhile0c(HlsPythonHwWhile0):
+class HlsPythonHwWhile0c(HlsPythonHwWhile0a):
+
+    @staticmethod
+    def model(dataIn: Iterator[BitsVal], dataOut: List[BitsVal]):
+        while True:
+            i = uint8_t.from_py(10)
+            while True:
+                i = i + 1
+                dataOut.append(i)
+                if next(dataIn):
+                    break
 
     @hlsBytecode
     def mainThread(self, hls: HlsScope):
@@ -73,11 +103,24 @@ class HlsPythonHwWhile0c(HlsPythonHwWhile0):
                     break
 
 
-class HlsPythonHwWhile1(HlsPythonHwWhile0):
+class HlsPythonHwWhile1(HlsPythonHwWhile0a):
+
+    @staticmethod
+    def model(dataIn: Iterator[BitsVal], dataOut: List[int]):
+        i = uint8_t.from_py(10)
+        while True:
+            while True:
+                dataOut.append(int(i))
+                i = i + 1
+                rst = next(dataIn)
+                if rst:
+                    break
+
+            i = 0
 
     @hlsBytecode
     def mainThread(self, hls: HlsScope):
-        i = uint8_t.from_py(0)
+        i = uint8_t.from_py(10)
         while TRUE:  # recognized as HW loop because of type
             while True:  # recognized as HW loop because of break condition
                 hls.write(i, self.o)
@@ -88,16 +131,38 @@ class HlsPythonHwWhile1(HlsPythonHwWhile0):
             i = 0
 
 
-class HlsPythonHwWhile2(HlsPythonHwWhile0):
+class HlsPythonHwWhile2(HlsPythonHwWhile0a):
+
+    def _config(self) -> None:
+        HlsPythonHwWhile0a._config(self)
+        self.CLK_FREQ = int(1e6)
 
     def _declr(self):
         addClkRstn(self)
-        self.o = VectSignal(8, signed=False)._m()
+        self.o = HsStructIntf()._m()
+        self.o.T = uint8_t
+        # self.o = VectSignal(8, signed=False)._m()
+
+    @staticmethod
+    def model(dataOut: List[Union[BitsVal, int]]):
+        i = uint8_t.from_py(0)
+        while True:  # recognized as HW loop because of type
+            if i <= 4:
+                dataOut.append(i)
+                yield
+            elif i._eq(10):
+                break
+            i += 1
+
+        while True:
+            dataOut.append(0)
+            yield
 
     @hlsBytecode
     def mainThread(self, hls: HlsScope):
         i = uint8_t.from_py(0)
         while TRUE:  # recognized as HW loop because of type
+            PyBytecodeBlockLabel("wh0")
             if i <= 4:
                 hls.write(i, self.o)
             elif i._eq(10):
@@ -105,6 +170,7 @@ class HlsPythonHwWhile2(HlsPythonHwWhile0):
             i += 1
 
         while TRUE:
+            PyBytecodeBlockLabel("wh1")
             hls.write(0, self.o)
 
 
@@ -116,6 +182,21 @@ class HlsPythonHwWhile3(HlsPythonHwWhile2):
         self.o = Handshaked()._m()
         for i in (self.i, self.o):
             i.DATA_WIDTH = 8
+
+    @staticmethod
+    def model(dataIn: Iterator[BitsVal], dataOut: List[Union[BitsVal, int]]):
+        while True:
+            while True:
+                r1 = next(dataIn)
+                if r1 != 1:
+                    r2 = next(dataIn)
+                    dataOut.append(r2)
+                    if r2 != 2:
+                        break
+                else:
+                    break
+
+            dataOut.append(99)
 
     @hlsBytecode
     def mainThread(self, hls: HlsScope):
@@ -142,6 +223,17 @@ class HlsPythonHwWhile4(HlsPythonHwWhile2):
         self.o = Handshaked()._m()
         self.i.DATA_WIDTH = 1
         self.o.DATA_WIDTH = 8
+
+    @staticmethod
+    def model(dataIn: Iterator[BitsVal], dataOut: List[BitsVal]):
+        while True:
+            data = Bits(8).from_py(None)
+            cntr = 8 - 1
+            while cntr >= 0:
+                d = next(dataIn)
+                data = Concat(d, data[8:1])  # shift-in data from left
+                cntr = cntr - 1
+            dataOut.append(data)
 
     @hlsBytecode
     def mainThread(self, hls: HlsScope):
@@ -195,11 +287,129 @@ class HlsPythonHwWhile6(HlsPythonHwWhile4):
                     hls.write(data, self.o)
 
 
+class MovingOneGen(Unit):
+
+    def _config(self) -> None:
+        self.DATA_WIDTH = Param(4)
+        self.FREQ = Param(int(100e6))
+
+    def _declr(self) -> None:
+        addClkRstn(self)
+        self.clk.FREQ = self.FREQ
+
+        self.o = HsStructIntf()._m()
+        self.o.T = Bits(self.DATA_WIDTH)
+
+    @staticmethod
+    def model(dataOut: List[int]):
+        t = Bits(4)
+        width = t.bit_length()
+        while True:
+            qMask = t.from_py(1 << (width - 1))
+            while qMask != 0:
+                dataOut.append(int(qMask))
+                yield
+                qMask >>= 1
+
+    @hlsBytecode
+    def mainThread(self, hls: HlsScope):
+        t = self.o.T
+        width = t.bit_length()
+        while BIT.from_py(1):
+            qMask = t.from_py(1 << (width - 1))
+            while qMask != 0:
+                hls.write(qMask, self.o)
+                qMask >>= 1
+
+    def _impl(self) -> None:
+        HlsPythonHwWhile0a._impl(self)
+
+
+class LoopCondBitSet(MovingOneGen):
+
+    def _declr(self) -> None:
+        MovingOneGen._declr(self)
+        self.i = HsStructIntf()
+        self.i.T = BIT
+
+    @staticmethod
+    def model(dataIn: Iterator[BitsVal], dataOut: List[int]):
+        t = Bits(4)
+        width = t.bit_length()
+        while True:
+            qMask = t.from_py(1 << (width - 1))
+            res = t.from_py(0)
+            while qMask != 0:
+                if next(dataIn):
+                    res = res | qMask
+                dataOut.append(int(res))
+                qMask >>= 1
+
+    @hlsBytecode
+    def mainThread(self, hls: HlsScope):
+        t = self.o.T
+        width = t.bit_length()
+        while BIT.from_py(1):
+            qMask = t.from_py(1 << (width - 1))
+            res = t.from_py(0)
+            while qMask != 0:
+                if hls.read(self.i).data:
+                    res |= qMask
+                hls.write(res, self.o)
+                qMask >>= 1
+
+
+class LoopZeroPadCompareShift(MovingOneGen):
+
+    def _declr(self) -> None:
+        MovingOneGen._declr(self)
+        self.i = HsStructIntf()
+        self.i.T = self.o.T
+
+    @staticmethod
+    def model(dataIn: Iterator[BitsVal], dataOut: List[int]):
+        t = Bits(4)
+        divisor = next(dataIn)
+        dividend = next(dataIn)
+        
+        width = t.bit_length()
+        zeroPad = Bits(width - 1).from_py(0)
+        divisorTmp = Concat(divisor, zeroPad)
+        i = 0
+        while True:
+            if divisorTmp <= Concat(zeroPad, dividend):
+                dividend -= divisorTmp[width:]
+
+            dataOut.append(int(dividend))
+            divisorTmp >>= 1
+            if i == width:
+                break
+            else:
+                i += 1 
+
+
+    @hlsBytecode
+    def mainThread(self, hls: HlsScope):
+        divisor = hls.read(self.i)
+        dividend = hls.read(self.i)
+        
+        t = self.o.T
+        width = t.bit_length()
+        zeroPad = Bits(width - 1).from_py(0)
+        divisorTmp = Concat(divisor, zeroPad)
+    
+        while BIT.from_py(1):
+            if divisorTmp <= Concat(zeroPad, dividend):
+                dividend -= divisorTmp[width:]
+            hls.write(dividend, self.o)
+            divisorTmp >>= 1
+
+
 class PragmaInline_HlsPythonHwWhile5(HlsPythonHwWhile5):
 
     @hlsBytecode
     def mainThread(self, hls:HlsScope):
-        while TRUE: # [fixme] afterPrefix does not add prefix correctly and generates new blocks without loop prefix for inlined blocks
+        while TRUE:  # [fixme] afterPrefix does not add prefix correctly and generates new blocks without loop prefix for inlined blocks
             PyBytecodeBlockLabel("LBeforeInline")
             PyBytecodeInline(HlsPythonHwWhile5.mainThread)(self, hls)
 
--- tests/frontend/pyBytecode/stmWhile_test.py
+++ tests/frontend/pyBytecode/stmWhile_test.py
@@ -10,14 +10,15 @@ from tests.baseSsaTest import BaseSsaTC
 from tests.frontend.pyBytecode.stmWhile import HlsPythonHwWhile0a, \
     HlsPythonHwWhile1, HlsPythonHwWhile2, HlsPythonHwWhile3, HlsPythonHwWhile4, \
     HlsPythonHwWhile5, HlsPythonHwWhile0b, HlsPythonHwWhile0c, \
-    PragmaInline_HlsPythonHwWhile5, HlsPythonHwWhile6
+    PragmaInline_HlsPythonHwWhile5, HlsPythonHwWhile6, MovingOneGen, \
+    LoopCondBitSet, LoopZeroPadCompareShift
 
 
 class StmWhile_ll_TC(BaseSsaTC):
     __FILE__ = __file__
 
-    def test_HlsPythonHwWhile0_ll(self):
-        self._test_ll(HlsPythonHwWhile0)
+    def test_HlsPythonHwWhile0a_ll(self):
+        self._test_ll(HlsPythonHwWhile0a)
 
     def test_HlsPythonHwWhile1_ll(self):
         self._test_ll(HlsPythonHwWhile1)
@@ -28,96 +29,49 @@ class StmWhile_ll_TC(BaseSsaTC):
 
 class StmWhile_sim_TC(BaseIrMirRtl_TC):
 
+    def test_HlsPythonHwWhile0a(self):
+        OUT_CNT = 16
+        dataIn = [BIT.from_py(self._rand.getrandbits(1)) for _ in range(OUT_CNT)]
+        self._test_OneInOneOut(HlsPythonHwWhile0a(), HlsPythonHwWhile0a.model, dataIn,
+                   OUT_CNT * 20, OUT_CNT * 20,
+                   OUT_CNT * 20, OUT_CNT + 2)
+
     def test_HlsPythonHwWhile0b(self):
-
-        def model(dataOut):
-            while True:
-                dataOut.append(10)
-                yield
-
         OUT_CNT = 8
-
-        self._testOneOut(HlsPythonHwWhile0b(), model, OUT_CNT,
+        self._testOneOut(HlsPythonHwWhile0b(), HlsPythonHwWhile0b.model, OUT_CNT,
                          OUT_CNT * 10, OUT_CNT * 10,
                          OUT_CNT * 10, OUT_CNT + 1)
 
     def test_HlsPythonHwWhile0c(self):
-
-        def model(dataIn, dataOut):
-            while True:
-                i = uint8_t.from_py(10)
-                while True:
-                    i += 1
-                    dataOut.append(i)
-                    if next(dataIn):
-                        break
-
         OUT_CNT = 16
         dataIn = [BIT.from_py(self._rand.getrandbits(1)) for _ in range(OUT_CNT)]
-        self._test_OneInOneOut(HlsPythonHwWhile0c(), model, dataIn,
+        self._test_OneInOneOut(HlsPythonHwWhile0c(), HlsPythonHwWhile0c.model, dataIn,
                    OUT_CNT * 20, OUT_CNT * 20,
                    OUT_CNT * 20, OUT_CNT + 2)
 
-    # def test_HlsPythonHwWhile0(self):
-    #    self._test(HlsPythonHwWhile0())
-    #
-    # def test_HlsPythonHwWhile1(self):
-    #    self._test(HlsPythonHwWhile1())
-    #
-    def test_HlsPythonHwWhile2(self):
-
-        def model(dataOut):
-            i = uint8_t.from_py(0)
-            while True:  # recognized as HW loop because of type
-                if i <= 4:
-                    dataOut.append(i)
-                    yield
-                elif i._eq(10):
-                    break
-                i += 1
-
-            while True:
-                dataOut.append(0)
-                yield
-
+    def test_HlsPythonHwWhile1(self):
         OUT_CNT = 16
-        self._testOneOut(HlsPythonHwWhile2(), model, OUT_CNT,
+        dataIn = [BIT.from_py(self._rand.getrandbits(1)) for _ in range(OUT_CNT)]
+        self._test_OneInOneOut(HlsPythonHwWhile1(), HlsPythonHwWhile1.model, dataIn,
+                         OUT_CNT * 10, OUT_CNT * 10,
+                         OUT_CNT * 10, OUT_CNT + 1 + 1,
+                         freq=int(50e6))
+
+    def test_HlsPythonHwWhile2(self):
+        OUT_CNT = 16
+        self._testOneOut(HlsPythonHwWhile2(), HlsPythonHwWhile2.model, OUT_CNT,
                          OUT_CNT * 20, OUT_CNT * 20,
-                         OUT_CNT * 20, OUT_CNT + 1)
+                         OUT_CNT * 20, OUT_CNT + 6 + 1,
+                         freq=int(100e6))
 
     def test_HlsPythonHwWhile3(self):
-
-        def model(dataIn, dataOut):
-            while True:
-                while True:
-                    r1 = next(dataIn)
-                    if r1 != 1:
-                        r2 = next(dataIn)
-                        dataOut.append(r2)
-                        if r2 != 2:
-                            break
-                    else:
-                        break
-
-                dataOut.append(99)
-
         IN_CNT = 32
         in_t = Bits(8)
         dataIn = [in_t.from_py(self._rand.getrandbits(2)) for _ in range(IN_CNT)]
-        self._test_OneInOneOut(HlsPythonHwWhile3(), model, dataIn)
+        self._test_OneInOneOut(HlsPythonHwWhile3(), HlsPythonHwWhile3.model,
+                               dataIn, wallTimeRtlClks=IN_CNT + 9 + 1)
 
     def test_HlsPythonHwWhile4(self, uCls=HlsPythonHwWhile4):
-
-        def model(dataIn, dataOut):
-            while True:
-                data = Bits(8).from_py(None)
-                cntr = 8 - 1
-                while cntr >= 0:
-                    d = next(dataIn)
-                    data = Concat(d, data[8:1])  # shift-in data from left
-                    cntr = cntr - 1
-                dataOut.append(data)
-
         IN_CNT = 32
         dataIn = [BIT.from_py(self._rand.getrandbits(1)) for _ in range(IN_CNT)]
         self._test_OneInOneOut(uCls(), uCls.model, dataIn)
@@ -128,15 +82,47 @@ class StmWhile_sim_TC(BaseIrMirRtl_TC):
     def test_HlsPythonHwWhile6(self):
         self.test_HlsPythonHwWhile4(uCls=HlsPythonHwWhile6)
 
+    def test_MovingOneGen(self):
+        OUT_CNT = 10
+        self._testOneOut(MovingOneGen(), MovingOneGen.model, OUT_CNT,
+                         OUT_CNT * 20, OUT_CNT * 20,
+                         OUT_CNT * 20, OUT_CNT + 1,
+                         freq=int(100e6))
+
+    def test_LoopCondBitSet(self):
+        IN_CNT = 12
+        dataIn = [BIT.from_py(self._rand.getrandbits(1)) for _ in range(IN_CNT)]
+        self._test_OneInOneOut(LoopCondBitSet(), LoopCondBitSet.model, dataIn)
+
+    def test_LoopZeroPadCompareShift(self):
+        u = LoopZeroPadCompareShift()
+        u.DATA_WIDTH = 4
+        t = Bits(u.DATA_WIDTH)
+        dataIn = [t.from_py(13), t.from_py(3)]
+        self._test_OneInOneOut(u, u.model, dataIn,
+                                wallTimeIr=64,
+                                wallTimeOptIr=64,
+                                wallTimeOptMir=6,
+                                )
+
     def test_PragmaInline_HlsPythonHwWhile5(self):
         self.test_HlsPythonHwWhile4(uCls=PragmaInline_HlsPythonHwWhile5)
 
 
 if __name__ == "__main__":
+    from hwt.synthesizer.utils import to_rtl_str
+    u = LoopZeroPadCompareShift()
+    u.DATA_WIDTH = 4
+    # u = HlsPythonHwWhile1()
+    u.CLK_FREQ = int(1e6)
+    print(to_rtl_str(u, target_platform=VirtualHlsPlatform(
+        debugFilter=HlsDebugBundle.ALL_RELIABLE.union(HlsDebugBundle.DBG_FRONTEND))))
+
     import unittest
 
     testLoader = unittest.TestLoader()
-    suite = unittest.TestSuite([StmWhile_sim_TC("test_HlsPythonHwWhile2")])
-    # suite = testLoader.loadTestsFromTestCase(StmWhile_sim_TC)
+    # suite = unittest.TestSuite([StmWhile_sim_TC("test_LoopZeroPadCompareShift")])
+    # suite = testLoader.loadTestsFromTestCase(StmWhile_ll_TC)
+    suite = testLoader.loadTestsFromTestCase(StmWhile_sim_TC)
     runner = unittest.TextTestRunner(verbosity=3)
     runner.run(suite)
