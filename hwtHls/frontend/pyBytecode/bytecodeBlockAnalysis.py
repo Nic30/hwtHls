72bcce99412e2fec8f4806d81c10b1c1d4f26191
Nic30 <nic30@seznam.cz>
2024-02-03 15:20:55 +0100

    fix(pyBytecode): support EXTENDED_ARG before FOR_ITER

--- hwtHls/frontend/pyBytecode/bytecodeBlockAnalysis.py
+++ hwtHls/frontend/pyBytecode/bytecodeBlockAnalysis.py
@@ -4,7 +4,7 @@ from typing import Tuple, Dict
 
 from hwtHls.frontend.pyBytecode.instructions import \
     FOR_ITER, RAISE_VARARGS, RERAISE, \
-    RETURN_VALUE, JUMPS_RELATIVE, JUMPS_CONDITIONAL_ANY 
+    RETURN_VALUE, JUMPS_RELATIVE, JUMPS_CONDITIONAL_ANY, EXTENDED_ARG
 
 
 def extractBytecodeBlocks(instructions: Tuple[Instruction, ...]) -> Tuple[Dict[int, Tuple[Instruction, ...]], DiGraph]:
@@ -12,7 +12,7 @@ def extractBytecodeBlocks(instructions: Tuple[Instruction, ...]) -> Tuple[Dict[i
     This function extract CFG graph from instruction list.
     """
     cfg = DiGraph()
-        
+
     curBlock = []
     blocks: Dict[int, Tuple[Instruction, ...]] = {}
     blocks[0] = curBlock
@@ -26,7 +26,7 @@ def extractBytecodeBlocks(instructions: Tuple[Instruction, ...]) -> Tuple[Dict[i
                 continue  # skip unreachable code
             else:
                 skipNonJumpTargets = False
-        
+
         isNotEntryPointAndJumpTarget = instr.is_jump_target and instr.offset != 0
         if isNotEntryPointAndJumpTarget or lastWasAbsoluteJump or lastWasConditionalJump:
             # create a new block
@@ -39,11 +39,11 @@ def extractBytecodeBlocks(instructions: Tuple[Instruction, ...]) -> Tuple[Dict[i
             blocks[instr.offset] = curBlock
             lastWasAbsoluteJump = False
             lastWasConditionalJump = False
-            
+
         else:
             # append to existing block
             curBlock.append(instr)
-        
+
         opc = instr.opcode
         if opc in (RETURN_VALUE, RERAISE, RAISE_VARARGS):
             lastWasAbsoluteJump = True
@@ -59,11 +59,12 @@ def extractBytecodeBlocks(instructions: Tuple[Instruction, ...]) -> Tuple[Dict[i
         elif opc in JUMPS_CONDITIONAL_ANY:
             if instr.opcode == FOR_ITER:
                 # :note: FOR_ITER is always jump target and conditional jump so this block always contains just this instr.
-                assert len(curBlock) == 1, curBlock
-                
+                assert len(curBlock) == 1 or (
+                    len(curBlock) == 2 and curBlock[0].opcode == EXTENDED_ARG), curBlock
+
             src = curBlock[0].offset
             cfg.add_edge(src, instr.argval)
-            #cfg.add_edge(src, instr.offset + 2)
+            # cfg.add_edge(src, instr.offset + 2)
             lastWasConditionalJump = True
 
     if not cfg.nodes:
