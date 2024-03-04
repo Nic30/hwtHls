292fdd99998ec63beb9210d71a635548de56d533
Nic30 <nic30@seznam.cz>
2024-03-04 22:42:11 +0100

    tests(BinToBcd_TC): add _test_no_comb_loops

--- tests/frontend/pyBytecode/binToBcd_test.py
+++ tests/frontend/pyBytecode/binToBcd_test.py
@@ -10,6 +10,7 @@ from hwtLib.logic.bcdToBin_test import bin_to_bcd
 from hwtLib.logic.binToBcd_test import BinToBcdTC as HwtLibBinToBcdTC
 from hwtSimApi.utils import freq_to_period
 from tests.frontend.pyBytecode.binToBcd import BinToBcd
+from tests.baseIrMirRtlTC import BaseIrMirRtl_TC
 
 
 class BinToBcd_TC(HwtLibBinToBcdTC):
@@ -17,12 +18,17 @@ class BinToBcd_TC(HwtLibBinToBcdTC):
     @classmethod
     def setUpClass(cls):
         cls.u = BinToBcd()
-        cls.u.DATA_WIDTH = 10
+        cls.u.DATA_WIDTH = 8
         cls.CLK_PERIOD = int(freq_to_period(cls.u.FREQ))
-        cls.compileSim(cls.u, target_platform=Artix7Medium(debugFilter={HlsDebugBundle.DBG_3_mir}))
+        cls.compileSim(cls.u, target_platform=Artix7Medium(debugFilter={HlsDebugBundle.DBG_3_mir, HlsDebugBundle.DBG_20_addSignalNamesToSync, HlsDebugBundle.DBG_20_addSignalNamesToData}))
+
+    def test_0to127(self):
+        BaseIrMirRtl_TC._test_no_comb_loops(self)
+        HwtLibBinToBcdTC.test_0to127(self)
 
     def test_MIR(self):
-
+        # :attention: MIR is loaded to file to test MIR loading, in other tests mir object should be used directly
+        # and dump to file is not required
         with open(Path(self.DEFAULT_LOG_DIR) / "BinToBcd.mainThread" / "03.mir.ll") as f:
             refData = [0, 1, 2, 3, 4, 5, 6, 7, 99, 127, 255]
             args = [iter(refData), []]
@@ -35,7 +41,7 @@ if __name__ == "__main__":
 
     u = BinToBcd()
     u.DATA_WIDTH = 10
-    print(to_rtl_str(u, target_platform=Artix7Medium(debugFilter=HlsDebugBundle.ALL_RELIABLE)))
+    print(to_rtl_str(u, target_platform=Artix7Medium(debugFilter=HlsDebugBundle.ALL_RELIABLE.union({HlsDebugBundle.DBG_20_addSignalNamesToSync, HlsDebugBundle.DBG_20_addSignalNamesToData}))))
 
     import unittest
     testLoader = unittest.TestLoader()
