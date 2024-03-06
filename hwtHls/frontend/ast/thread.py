5a4fac269276e9231496d08f38fa622ba75ddf99
Nic30 <nic30@seznam.cz>
2024-03-06 19:24:05 +0100

    refactor(frontend.ast.thread): use VldSyncedStructIntf from hwt

--- hwtHls/frontend/ast/thread.py
+++ hwtHls/frontend/ast/thread.py
@@ -1,14 +1,12 @@
 from typing import List, Tuple, Union
 
-from hwt.hdl.types.hdlType import HdlType
-from hwt.interfaces.std import Signal, VldSynced
 from hwt.interfaces.structIntf import HdlType_to_Interface
+from hwt.interfaces.vldSyncedStructIntf import VldSyncedStructIntf
 from hwt.pyUtils.arrayQuery import flatten
 from hwt.pyUtils.uniqList import UniqList
 from hwt.synthesizer.interface import Interface
 from hwt.synthesizer.interfaceLevel.unitImplHelpers import Interface_without_registration, \
     getInterfaceName
-from hwt.synthesizer.param import Param
 from hwt.synthesizer.rtlLevel.rtlSignal import RtlSignal
 from hwtHls.frontend.ast.astToSsa import AnyStm, HlsAstToSsa
 from hwtHls.frontend.ast.builder import HlsAstBuilder
@@ -23,7 +21,7 @@ class HlsThreadFromAst(HlsThread):
         super(HlsThreadFromAst, self).__init__(hls)
         self.code = code
         self.name = name
-    
+
     def getLabel(self) -> str:
         i = self.hls._threads.index(self)
         return f"t{i:d}_{self.name}"
@@ -47,35 +45,25 @@ class HlsThreadFromAst(HlsThread):
         self.toSsa = toSsa
 
 
-class VldSyncedStructIntf(VldSynced):
-
-    def _config(self):
-        self.T: HdlType = Param(None)
-    
-    def _declr(self):
-        assert self.T is not None
-        self._dtype = self.T
-        self.vld:Signal = Signal()._m()
-        self.data = HdlType_to_Interface().apply(self.T)._m()
-
-
 class HlsThreadForSharedVar(HlsThreadFromAst):
 
     def __init__(self, hls: "HlsScope", var: Union[RtlSignal, Interface]):
         super(HlsThreadForSharedVar, self).__init__(hls, None, f"sharedVarThread_{getInterfaceName(var)}")
         self.var = var
-        self._exports: List[Tuple[Union[RtlSignal, Interface], DIRECTION.IN]] = [] 
+        self._exports: List[Tuple[Union[RtlSignal, Interface], DIRECTION.IN]] = []
 
     def getReadPort(self):
         p = HdlType_to_Interface().apply(self.var._dtype)
-        Interface_without_registration(self.hls.parentUnit, p, f"{getInterfaceName(self._parent.parentUnit, self.var):s}_{len(self._exports):d}")
+        Interface_without_registration(self.hls.parentUnit, p,
+                                        f"{getInterfaceName(self._parent.parentUnit, self.var):s}_{len(self._exports):d}")
         self._exports.append((p, DIRECTION.OUT))
         return p
-    
+
     def getWritePort(self):
         p = VldSyncedStructIntf()
         p.T = self.var._dtype
-        Interface_without_registration(self.hls.parentUnit, p, f"{getInterfaceName(self._parent.parentUnit, self.var):s}_{len(self._exports):d}")
+        Interface_without_registration(self.hls.parentUnit, p,
+                                       f"{getInterfaceName(self._parent.parentUnit, self.var):s}_{len(self._exports):d}")
         self._exports.append((p, DIRECTION.IN))
         return p
 
