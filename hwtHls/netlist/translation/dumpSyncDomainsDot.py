2939dfa9c1fea49163cd58584c5c2a2827198a5b
Nic30 <nic30@seznam.cz>
2024-03-06 20:00:46 +0100

    feat(HlsNetlistPassDumpSyncDomainsDot): add addLegend opt

--- hwtHls/netlist/translation/dumpSyncDomainsDot.py
+++ hwtHls/netlist/translation/dumpSyncDomainsDot.py
@@ -17,27 +17,27 @@ class HwtHlsNetlistSyncDomainsToGraphwiz(HwtHlsNetlistToGraphwiz):
     Generate a Graphwiz (dot) diagram of sync domains extracted from the netlist.
     """
 
-    def __init__(self, name:str, nodes:List[HlsNetNode], expandAggregates:bool = False):
-        HwtHlsNetlistToGraphwiz.__init__(self, name, nodes, expandAggregates=expandAggregates)
+    def __init__(self, name:str, nodes:List[HlsNetNode], expandAggregates:bool=False, addLegend:bool=True):
+        HwtHlsNetlistToGraphwiz.__init__(self, name, nodes, expandAggregates=expandAggregates, addLegend=addLegend)
         self.syncGroupOfSyncNode: Dict[HlsNetNodeExplicitSync, SyncGroupLabel] = {}
         self.groupGraphNodes: Dict[SyncGroupLabel, pydot.Cluster] = {}
-    
+
     def _getGraph(self, n:HlsNetNode):
         try:
             syncGroup = self.syncGroupOfSyncNode[n]
         except KeyError:
             return self.graph
         return self.groupGraphNodes[syncGroup]
-        
+
     def construct(self, syncDomains: HlsNetlistAnalysisPassSyncDomains):
         g = self.graph
-        
+
         groupGraphNodes = self.groupGraphNodes
         groupsSorted = syncDomains.syncDomains
         syncGroupOfSyncNode = self.syncGroupOfSyncNode
         for syncGroup, nodes in groupsSorted:
-            groupLabel = ','.join(str(n._id) for n in syncGroup) 
-    
+            groupLabel = ','.join(str(n._id) for n in syncGroup)
+
             clusterNode = pydot.Cluster(f"n{self._getNewNodeId()}", label=f'"SyncGroup({html.escape(groupLabel)})"')
             g.add_subgraph(clusterNode)
             groupGraphNodes[syncGroup] = clusterNode
@@ -47,21 +47,22 @@ class HwtHlsNetlistSyncDomainsToGraphwiz(HwtHlsNetlistToGraphwiz):
                 syncGroupOfSyncNode[n] = syncGroup
 
         super(HwtHlsNetlistSyncDomainsToGraphwiz, self).construct()
-    
+
     def dumps(self):
         return self.graph.to_string()
 
 
 class HlsNetlistPassDumpSyncDomainsDot(HlsNetlistPass):
 
-    def __init__(self, outStreamGetter: OutputStreamGetter):
+    def __init__(self, outStreamGetter: OutputStreamGetter, addLegend:bool=True):
         self.outStreamGetter = outStreamGetter
+        self.addLegend = addLegend
 
     def apply(self, hls: "HlsScope", netlist: HlsNetlistCtx):
         name = netlist.label
         out, doClose = self.outStreamGetter(name)
         try:
-            toGraphwiz = HwtHlsNetlistSyncDomainsToGraphwiz(name, netlist.iterAllNodes())
+            toGraphwiz = HwtHlsNetlistSyncDomainsToGraphwiz(name, netlist.iterAllNodes(), addLegend=self.addLegend)
             syncDomains = netlist.getAnalysis(HlsNetlistAnalysisPassSyncDomains)
             toGraphwiz.construct(syncDomains)
             out.write(toGraphwiz.dumps())
