a1f97a89987d4fd842c50d8dbacfe1e1e60bb33e
Nic30 <nic30@seznam.cz>
2024-04-02 12:02:54 +0200

    docs

--- tests/floatingpoint/__init__.py
+++ tests/floatingpoint/__init__.py
@@ -1,4 +1,7 @@
 """
 Potentially useful:
 * https://github.com/IamFlea/AdderCircuitGenerator
+* https://github.com/xesscorp/Floating_Point_Library-JHU/blob/master/FloatPt.vhd
+* https://github.com/taneroksuz/fpu
+* https://github.com/hukenovs/fp23fftk
 """
