55ec20b81126494557e746536460ed6797115998
Nic30 <nic30@seznam.cz>
2024-04-02 12:17:15 +0200

    test(HlsNetlistReduceCmpInAndTC): update for signed operands

--- tests/hlsNetlist/netlistReduceCmpInAnd_test.py
+++ tests/hlsNetlist/netlistReduceCmpInAnd_test.py
@@ -27,14 +27,14 @@ class HlsNetlistReduceCmpInAndTC(unittest.TestCase):
         netlist._setBuilder(b)
         return netlist, b
 
-    def _r(self, netlist: HlsNetlistCtx):
-        r = HlsNetNodeRead(netlist, None, dtype=uint8_t)
+    def _r(self, netlist: HlsNetlistCtx, dtype=uint8_t):
+        r = HlsNetNodeRead(netlist, None, dtype=dtype)
         netlist.inputs.append(r)
         return r._outputs[0]
 
     def _w(self, src: HlsNetNodeOut):
         netlist = src.obj.netlist
-        w = HlsNetNodeWrite(netlist, None, None)
+        w = HlsNetNodeWrite(netlist, None)
         netlist.outputs.append(w)
         link_hls_nodes(src, w._inputs[0])
         return w
@@ -42,13 +42,13 @@ class HlsNetlistReduceCmpInAndTC(unittest.TestCase):
     def test_reduceEqTo0(self):
         netlist, b = self._createNetlist()
         r0 = self._r(netlist)
-        
+
         res = b.buildAnd(
             b.buildEq(r0, b.buildConstPy(uint8_t, 0)),
             b.buildEq(r0, b.buildConstPy(uint8_t, 1))
         )
         w = self._w(res)
-        
+
         netlistReduceCmpInAnd(res.obj, [], set())
         resOpt = w.dependsOn[0].obj
         self.assertIsInstance(resOpt, HlsNetNodeConst)
@@ -57,14 +57,14 @@ class HlsNetlistReduceCmpInAndTC(unittest.TestCase):
     def test_reduceNe(self):
         netlist, b = self._createNetlist()
         r0 = self._r(netlist)
-        
+
         eq = b.buildEq(r0, b.buildConstPy(uint8_t, 0))
         res = b.buildAnd(
             eq,
             b.buildNe(r0, b.buildConstPy(uint8_t, 1))
         )
         w = self._w(res)
-        
+
         netlistReduceCmpInAnd(res.obj, [], set())
         resOpt = w.dependsOn[0].obj
         self.assertIs(resOpt, eq.obj)
@@ -72,20 +72,20 @@ class HlsNetlistReduceCmpInAndTC(unittest.TestCase):
     def test_reduceRangesToEq(self):
         netlist, b = self._createNetlist()
         r0 = self._r(netlist)
-        
-        e0 = b.buildOp(AllOps.LE, BIT, r0, b.buildConstPy(uint8_t, 10))
-        e1 = b.buildOp(AllOps.GE, BIT, r0, b.buildConstPy(uint8_t, 10))
+
+        e0 = b.buildOp(AllOps.ULE, BIT, r0, b.buildConstPy(uint8_t, 10))
+        e1 = b.buildOp(AllOps.UGE, BIT, r0, b.buildConstPy(uint8_t, 10))
         res = b.buildAnd(
             e0,
             e1,
         )
         w = self._w(res)
-        
+
         netlistReduceCmpInAnd(res.obj, [], set())
         resOpt = w.dependsOn[0].obj
         self.assertIsInstance(resOpt, HlsNetNodeOperator)
         self.assertEqual(resOpt.operator, AllOps.EQ)
-        
+
         o0, o1 = resOpt.dependsOn
         self.assertIs(o0, r0)
 
@@ -95,32 +95,32 @@ class HlsNetlistReduceCmpInAndTC(unittest.TestCase):
     def test_reduceLtGtTo0(self):
         netlist, b = self._createNetlist()
         r0 = self._r(netlist)
-        
-        e0 = b.buildOp(AllOps.LT, BIT, r0, b.buildConstPy(uint8_t, 10))
-        e1 = b.buildOp(AllOps.GT, BIT, r0, b.buildConstPy(uint8_t, 10))
+
+        e0 = b.buildOp(AllOps.ULT, BIT, r0, b.buildConstPy(uint8_t, 10))
+        e1 = b.buildOp(AllOps.UGT, BIT, r0, b.buildConstPy(uint8_t, 10))
         res = b.buildAnd(
             e0,
             e1,
         )
         w = self._w(res)
-        
+
         netlistReduceCmpInAnd(res.obj, [], set())
         resOpt = w.dependsOn[0].obj
         self.assertIsInstance(resOpt, HlsNetNodeConst)
         self.assertEqual(int(resOpt.val), 0)
-    
+
     def test_reduceNeLtToLt(self):
         netlist, b = self._createNetlist()
         r0 = self._r(netlist)
-        
-        e0 = b.buildOp(AllOps.LT, BIT, r0, b.buildConstPy(uint8_t, 10))
+
+        e0 = b.buildOp(AllOps.ULT, BIT, r0, b.buildConstPy(uint8_t, 10))
         e1 = b.buildNe(r0, b.buildConstPy(uint8_t, 15))
         res = b.buildAnd(
             e0,
             e1,
         )
         w = self._w(res)
-        
+
         netlistReduceCmpInAnd(res.obj, [], set())
         resOpt = w.dependsOn[0]
         self.assertIs(resOpt, e0)
@@ -128,8 +128,8 @@ class HlsNetlistReduceCmpInAndTC(unittest.TestCase):
     def test_reduceNeLtToNe(self):
         netlist, b = self._createNetlist()
         r0 = self._r(netlist)
-        
-        e0 = b.buildOp(AllOps.LE, BIT, r0, b.buildConstPy(uint8_t, 255))
+
+        e0 = b.buildOp(AllOps.ULE, BIT, r0, b.buildConstPy(uint8_t, 255))
         c15 = b.buildConstPy(uint8_t, 15)
         e1 = b.buildNe(r0, c15)
         res = b.buildAnd(
@@ -137,11 +137,11 @@ class HlsNetlistReduceCmpInAndTC(unittest.TestCase):
             e1,
         )
         w = self._w(res)
-        
+
         netlistReduceCmpInAnd(res.obj, [], set())
         resOpt = w.dependsOn[0]
         self.assertIs(resOpt, b.buildNot(b.buildEq(r0, c15)))
-    
+
 
 if __name__ == '__main__':
     testLoader = unittest.TestLoader()
