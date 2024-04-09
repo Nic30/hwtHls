4594829753bd8eaff0c1456f76eca63d24ec5ff2
Nic30 <nic30@seznam.cz>
2024-04-09 14:00:24 +0200

    feat(netlistCmpNormalize): update for signed ops

--- hwtHls/netlist/transformation/simplifyExpr/cmpNormalize.py
+++ hwtHls/netlist/transformation/simplifyExpr/cmpNormalize.py
@@ -8,8 +8,8 @@ from hwtHls.netlist.nodes.ops import HlsNetNodeOperator
 from hwtHls.netlist.transformation.simplifyUtils import replaceOperatorNodeWith
 from hwtHls.netlist.nodes.const import HlsNetNodeConst
 
-_DENORMALIZED_CMP_OPS = {AllOps.NE, AllOps.GT, AllOps.GE}
-_NORMALIZED_CMP_OPS = {AllOps.EQ, AllOps.LT, AllOps.LE}
+_DENORMALIZED_CMP_OPS = {AllOps.NE, AllOps.UGT, AllOps.UGE, AllOps.SGT, AllOps.SGE}
+_NORMALIZED_CMP_OPS = {AllOps.EQ, AllOps.ULT, AllOps.ULE, AllOps.SLT, AllOps.SLE}
 
 
 def netlistCmpNormalize(n: HlsNetNodeOperator, worklist: UniqList[HlsNetNode], removed: Set[HlsNetNode]):
@@ -20,8 +20,8 @@ def netlistCmpNormalize(n: HlsNetNodeOperator, worklist: UniqList[HlsNetNode], r
         return False
 
     t = n._outputs[0]._dtype
-    if t.signed:
-        raise NotImplementedError(n, t)
+    assert not t.signed, ("Signed should not be used internally")
+    #raise NotImplementedError("UGT not correct")
     newO = None
     # :note: const can not be first operand because it denormalizes the expression
     if op is AllOps.NE:
@@ -29,23 +29,41 @@ def netlistCmpNormalize(n: HlsNetNodeOperator, worklist: UniqList[HlsNetNode], r
         eq = b.buildOp(AllOps.EQ, t, op0, op1)
         newO = b.buildNot(eq)
 
-    elif op is AllOps.GT:
+    elif op is AllOps.UGT:
         if isinstance(op1.obj, HlsNetNodeConst):
             # a > b -> ~(a <= b)
-            le = b.buildOp(AllOps.LE, t, op0, op1)
+            le = b.buildOp(AllOps.ULE, t, op0, op1)
             newO = b.buildNot(le)
         else:
             # a > b -> b < a
-            newO = b.buildOp(AllOps.LT, t, op1, op0)
+            newO = b.buildOp(AllOps.ULT, t, op1, op0)
 
-    elif op is AllOps.GE:
+    elif op is AllOps.SGT:
+        if isinstance(op1.obj, HlsNetNodeConst):
+            # a > b -> ~(a <= b)
+            le = b.buildOp(AllOps.SLE, t, op0, op1)
+            newO = b.buildNot(le)
+        else:
+            # a > b -> b < a
+            newO = b.buildOp(AllOps.SLT, t, op1, op0)
+
+    elif op is AllOps.UGE:
         if isinstance(op1.obj, HlsNetNodeConst):
             # a >= b -> ~(a < b)
-            lt = b.buildOp(AllOps.LT, t, op0, op1)
+            lt = b.buildOp(AllOps.ULT, t, op0, op1)
             newO = b.buildNot(lt)
         else:
             # a >=- b -> b <= a
-            newO = b.buildOp(AllOps.LE, t, op1, op0)
+            newO = b.buildOp(AllOps.ULE, t, op1, op0)
+
+    elif op is AllOps.SGE:
+        if isinstance(op1.obj, HlsNetNodeConst):
+            # a >= b -> ~(a < b)
+            lt = b.buildOp(AllOps.SLT, t, op0, op1)
+            newO = b.buildNot(lt)
+        else:
+            # a >=- b -> b <= a
+            newO = b.buildOp(AllOps.SLE, t, op1, op0)
 
     if newO is not None:
         replaceOperatorNodeWith(n, newO, worklist, removed)
