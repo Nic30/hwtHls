901b362b96cf0095d2e257c03bc6aced75420d36
Nic30 <nic30@seznam.cz>
2024-04-11 11:39:16 +0200

    feat(SimplifyCFG2Pass): reduce uniq values in trySwitchToSelectOrRomLoad

--- hwtHls/llvm/Transforms/SimplifyCFG2Pass/SimplifyCFG2Pass_SwitchToSelect.cpp
+++ hwtHls/llvm/Transforms/SimplifyCFG2Pass/SimplifyCFG2Pass_SwitchToSelect.cpp
@@ -1,6 +1,7 @@
 #include <hwtHls/llvm/Transforms/SimplifyCFG2Pass/SimplifyCFG2Pass_SwitchToSelect.h>
 
 #include <algorithm>
+#include <map>
 
 #include <llvm/ADT/SmallVector.h>
 #include <llvm/ADT/SetVector.h>
@@ -16,6 +17,113 @@ using namespace llvm;
 
 namespace hwtHls {
 
+struct PhiWithFewUnieqValues {
+	PHINode &phi;
+	Value *v0;
+	size_t v0UseCnt;
+	bool v0IsFromSwitchDefault;
+	Value *v1;
+	size_t v1UseCnt;
+	bool v1IsFromSwitchDefault;
+	PhiWithFewUnieqValues(PHINode &phi) :
+			phi(phi), v0(nullptr), v0UseCnt(0), v0IsFromSwitchDefault(false), v1(
+					nullptr), v1UseCnt(0), v1IsFromSwitchDefault(false) {
+	}
+	PhiWithFewUnieqValues(const PhiWithFewUnieqValues &other) = default;
+	/*
+	 * :returns: false if the phi has few unique input values else false
+	 * */
+	bool add_incoming_value(Value *v, bool isFromSwitchDefault) {
+		if (v == v0) {
+			v0UseCnt++;
+			v0IsFromSwitchDefault |= isFromSwitchDefault;
+		} else if (v == v1) {
+			v1UseCnt++;
+			v1IsFromSwitchDefault |= isFromSwitchDefault;
+		} else if (!v0) {
+			v0 = v;
+			v0UseCnt = 1;
+			v0IsFromSwitchDefault = isFromSwitchDefault;
+		} else if (!v1) {
+			v1 = v;
+			v1UseCnt = 1;
+			v1IsFromSwitchDefault = isFromSwitchDefault;
+		} else {
+			return false;
+		}
+		return true;
+	}
+	/*
+	 * Sort v0, v1 so v0 has the most uses
+	 * */
+	void normalize() {
+		if (v0UseCnt < v1UseCnt) {
+			std::swap(v0, v1);
+			std::swap(v0UseCnt, v1UseCnt);
+			std::swap(v0IsFromSwitchDefault, v1IsFromSwitchDefault);
+		}
+	}
+};
+
+Value * buildSelectForSwitchWith2UniqValues(PHINode & PHI,
+		const SmallVector<std::pair<BasicBlock*, Value*> >& Conditions,
+		IRBuilder<> & Builder,
+		Value * v0,
+		Value * v1) {
+	assert(v1);
+	assert(v0);
+	Value* v1Cond = nullptr;
+	for (auto &Cond : Conditions) {
+		auto _V = PHI.getIncomingValueForBlock(Cond.first);
+		if (_V == v1) {
+			if (!v1Cond) {
+				v1Cond = Cond.second;
+			} else {
+				v1Cond = Builder.CreateOr(v1Cond, Cond.second);
+			}
+		}
+	}
+	assert(v1Cond);
+
+	return Builder.CreateSelect(v1Cond, v1, v0);
+}
+
+Value* buildRomLoadFromPHI(SwitchInst *SI, Value *Cond, BasicBlock *BBTop,
+		BasicBlock *BBBottom, PHINode &PHI, IRBuilder<> &Builder) {
+	SmallVector<Constant*> romData;
+	for (size_t i = 0; i < (1ull << Cond->getType()->getIntegerBitWidth());
+			++i) {
+		romData.push_back(nullptr);
+	}
+	// fill in values from cases
+	for (auto C : SI->cases()) {
+		size_t i = C.getCaseValue()->getZExtValue();
+		assert(i < romData.size());
+		auto phiPred = C.getCaseSuccessor();
+		if (phiPred == BBBottom)
+			phiPred = BBTop;
+		auto _caseVal = dyn_cast<Constant>(
+				PHI.getIncomingValueForBlock(phiPred));
+		assert(_caseVal);
+		romData[i] = _caseVal;
+	}
+	// fill in value from default on places which were not filled previously
+	auto phiPred = SI->getDefaultDest();
+	if (phiPred == BBBottom)
+		phiPred = BBTop;
+	auto *defVal = dyn_cast<Constant>(PHI.getIncomingValueForBlock(phiPred));
+	for (auto &romD : romData) {
+		if (romD == nullptr)
+			romD = defVal;
+	}
+
+	auto romGep = CreateGlobalDataWithGEP(Builder, *BBTop->getModule(), Cond,
+			romData, "switch.phirom", "switch.phirom.index", "switch.phi.gep");
+
+	return Builder.CreateLoad(PHI.getType(), romGep, true, "switch.phirom.val");
+}
+
+
 void rewritePHIsAsSelectOrRomLoad(size_t MaxRomAddrWidth, llvm::SwitchInst *SI,
 		BasicBlock *BBBottom, BasicBlock *BBTop, IRBuilder<> &Builder) {
 	// rewrite PHIs in BBBottom as selects
@@ -25,7 +133,6 @@ void rewritePHIsAsSelectOrRomLoad(size_t MaxRomAddrWidth, llvm::SwitchInst *SI,
 	Value *Cond = SI->getCondition();
 	auto *DefDst = SI->getDefaultDest();
 	SmallVector<std::pair<BasicBlock*, Value*> > Conditions;
-	bool allPhisCanBeRom = false;
 	std::set<PHINode*> phisForRomExtraction;
 	size_t CondWidth = Cond->getType()->getIntegerBitWidth();
 	if (CondWidth <= MaxRomAddrWidth
@@ -33,7 +140,6 @@ void rewritePHIsAsSelectOrRomLoad(size_t MaxRomAddrWidth, llvm::SwitchInst *SI,
 		// if PHI has more than 2 operands and SI cases are covering value domain
 		// are covering value domain of SI condition sufficiently.
 		// It is beneficial to use load from ROM instead of tree of selects
-		allPhisCanBeRom = true;
 		for (PHINode &PHI : BBBottom->phis()) {
 			bool allValuesAreConst = true;
 			for (Use &V : PHI.incoming_values()) {
@@ -44,11 +150,34 @@ void rewritePHIsAsSelectOrRomLoad(size_t MaxRomAddrWidth, llvm::SwitchInst *SI,
 			}
 			if (allValuesAreConst) {
 				phisForRomExtraction.insert(&PHI);
-			} else {
-				allPhisCanBeRom = false;
 			}
 		}
 	}
+
+	std::map<PHINode*, PhiWithFewUnieqValues> phisWithFewUniqValues;
+	size_t phiCnt = 0;
+	for (PHINode &PHI : BBBottom->phis()) {
+		++phiCnt;
+		PhiWithFewUnieqValues phiMeta(PHI);
+		bool hasJustPhiUniqInputs = true;
+		for (auto BB : PHI.blocks()) {
+			auto *V = PHI.getIncomingValueForBlock(&*BB);
+			if (!phiMeta.add_incoming_value(V, &*BB == DefDst)) {
+				hasJustPhiUniqInputs = false;
+				break;
+			}
+		}
+		if (hasJustPhiUniqInputs) {
+			phiMeta.normalize();
+			if (!phiMeta.v1 || phiMeta.v1UseCnt == 1) {
+				// if it has only a single value of v1 is used only once and rest is v0
+				phisWithFewUniqValues.insert(std::make_pair(&PHI, phiMeta));
+			}
+		}
+
+	}
+
+	bool allPhisCanBeRom = phisForRomExtraction.size() == phiCnt;
 	if (!allPhisCanBeRom) {
 		if (DefDst) {
 			Conditions.push_back(
@@ -66,48 +195,61 @@ void rewritePHIsAsSelectOrRomLoad(size_t MaxRomAddrWidth, llvm::SwitchInst *SI,
 		bool extractAsRom = phisForRomExtraction.find(&PHI)
 				!= phisForRomExtraction.end();
 		if (extractAsRom) {
-			SmallVector<Constant*> romData;
-			for (size_t i = 0; i < (1ull << Cond->getType()->getIntegerBitWidth()); ++i) {
-				romData.push_back(nullptr);
-			}
-			// fill in values from cases
-			for (auto C : SI->cases()) {
-				size_t i = C.getCaseValue()->getZExtValue();
-				assert(i< romData.size());
-				auto phiPred = C.getCaseSuccessor();
-				if (phiPred == BBBottom)
-					phiPred = BBTop;
-				auto _caseVal = dyn_cast<Constant>(PHI.getIncomingValueForBlock(phiPred));
-				assert(_caseVal);
-				romData[i] = _caseVal;
-			}
-			// fill in value from default on places which were not filled previously
-			auto phiPred = SI->getDefaultDest();
-			if (phiPred == BBBottom)
-				phiPred = BBTop;
-			auto * defVal = dyn_cast<Constant>(PHI.getIncomingValueForBlock(phiPred));
-			for (auto & romD: romData) {
-				if (romD == nullptr)
-					romD = defVal;
-			}
-
-			auto romGep = CreateGlobalDataWithGEP(Builder, *BBTop->getModule(), Cond,
-					romData, "switch.phirom", "switch.phirom.index",
-					"switch.phi.gep");
-
-			V = Builder.CreateLoad(PHI.getType(), romGep, true, "switch.phirom.val");
+			V = buildRomLoadFromPHI(SI, Cond, BBTop, BBBottom, PHI, Builder);
 
 		} else {
 			assert(Conditions.size());
-			for (auto &Cond : Conditions) {
-				auto _V = PHI.getIncomingValueForBlock(Cond.first);
-				if (V == nullptr) {
-					V = _V;
+			auto hasFewValues = phisWithFewUniqValues.find(&PHI);
+			if (hasFewValues != phisWithFewUniqValues.end()) {
+				auto& fewValues = hasFewValues->second;
+				assert(fewValues.v0);
+				assert(fewValues.v0UseCnt);
+				if (!fewValues.v1) {
+					// most simple case: there is just one unique incoming value in phi
+					V = fewValues.v0;
+				} else if (fewValues.v1IsFromSwitchDefault) {
+					// the value with least uses comes from SwitchInst default block,
+					// we check if we can reduce it with just simple eq cmp or if we have
+					// to compare all switch cases because switch cases are to sparse
+					if (SI->getNumCases() == (1llu << CondWidth) - 1) { // - 1 because of default bb
+						SmallVector<size_t> casesSorted;
+						casesSorted.reserve(SI->case_end() - SI->case_begin());
+						for (auto C : SI->cases()) {
+							size_t i = C.getCaseValue()->getZExtValue();
+							casesSorted.push_back(i);
+						}
+						std::sort(casesSorted.begin(), casesSorted.end());
+						size_t CondCaseForDefDst = 0;
+						for (auto c: casesSorted) {
+							if (CondCaseForDefDst == c)
+								++CondCaseForDefDst;
+							else
+								break; // we found first empty slot in value sequence
+						}
+						auto c = Builder.CreateICmpEQ(Cond, Builder.getIntN(Cond->getType()->getIntegerBitWidth(), CondCaseForDefDst ));
+						V = Builder.CreateSelect(c, fewValues.v1, fewValues.v0);
+					} else {
+						// the cases in switch are too sparse, we have to create cmp for every case to resolve condition
+						// for default branch
+						V = buildSelectForSwitchWith2UniqValues(PHI, Conditions, Builder, fewValues.v1, fewValues.v0);
+					}
+
 				} else {
-					assert(
-							Cond.second
-									&& "nullptr is used only for default value");
-					V = Builder.CreateSelect(Cond.second, _V, V);
+					// construct condition for the value with least uses
+					V = buildSelectForSwitchWith2UniqValues(PHI, Conditions, Builder, fewValues.v0, fewValues.v1);
+				}
+			} else {
+				for (auto &Cond : Conditions) {
+					auto _V = PHI.getIncomingValueForBlock(Cond.first);
+					if (V == nullptr) {
+						V = _V;
+					} else {
+						assert(
+								Cond.second
+										&& "nullptr is used only for default value");
+						if (_V != V)
+							V = Builder.CreateSelect(Cond.second, _V, V);
+					}
 				}
 			}
 		}
