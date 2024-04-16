769bca642dff76f2dc4ea508f48c7792b16de439
Nic30 <nic30@seznam.cz>
2024-04-16 20:55:45 +0200

    feat(netlistReduceMux): merge parent and child mux if beneficial

--- hwtHls/netlist/transformation/simplifyExpr/simplifyMux.py
+++ hwtHls/netlist/transformation/simplifyExpr/simplifyMux.py
@@ -1,6 +1,8 @@
-from typing import Set, List
+from typing import Set, List, Generator, Tuple, Optional
 
 from hwt.hdl.operatorDefs import AllOps
+from hwt.hdl.types.bits import Bits
+from hwt.hdl.types.slice import HSlice
 from hwt.pyUtils.uniqList import UniqList
 from hwtHls.netlist.builder import HlsNetlistBuilder
 from hwtHls.netlist.nodes.const import HlsNetNodeConst
@@ -14,15 +16,117 @@ from hwtHls.netlist.transformation.simplifyUtils import replaceOperatorNodeWith,
 from pyMathBitPrecise.bit_utils import ValidityError
 
 
-def netlistReduceMux(n: HlsNetNodeMux, worklist: UniqList[HlsNetNode], removed: Set[HlsNetNode]):
-    if len(n._inputs) == 1:
-        # mux x = x
-        i: HlsNetNodeOut = n.dependsOn[0]
-        replaceOperatorNodeWith(n, i, worklist, removed)
-        return True
+def netlistReduceMuxToRom(builder: HlsNetlistBuilder, n: HlsNetNodeMux, worklist: UniqList[HlsNetNode], removed: Set[HlsNetNode]):
+    # try to format mux to a format where each condition is comparison with EQ operator
+    # so the mux behaves like switch-case statement id it is suitable for ROM extraction
+    cases = tuple(n._iterValueConditionDriverPairs())
+    if cases[-1][1] is not None:
+        return False
 
+    # if contains else it may be possible to swap last two cases if required
+    everyNonLastConditionIsEq = True
+    everyConditionIsEq = True
+    lastConditionIsNe = False
+    preLastcaseIndex = len(cases) - 2
+    for i, (v, c) in enumerate(cases):
+        if c is None:
+            break
+        if isinstance(c.obj, HlsNetNodeOperator):
+            op = c.obj.operator
+            if i == preLastcaseIndex:
+                lastConditionIsNe = op == AllOps.NE
+                everyConditionIsEq = everyNonLastConditionIsEq and op == AllOps.EQ
+            else:
+                everyNonLastConditionIsEq &= op == AllOps.EQ
+        else:
+            everyConditionIsEq = False
+            everyNonLastConditionIsEq = False
+            break
+
+    if everyNonLastConditionIsEq and lastConditionIsNe:
+        # flip last condition NE -> EQ and swap cases
+        origNe = cases[-2][1]
+        origNeArgs = origNe.obj.dependsOn
+        cIn = n._inputs[preLastcaseIndex * 2 + 1]
+        unlink_hls_nodes(origNe, cIn)
+        worklist.append(origNe.obj)
+        newEq = builder.buildEq(origNeArgs[0], origNeArgs[1])
+        link_hls_nodes(newEq, cIn)
+
+        v0In = n._inputs[preLastcaseIndex * 2]
+        v0 = n.dependsOn[preLastcaseIndex * 2]
+        v1In = n._inputs[preLastcaseIndex * 2 + 2]
+        v1 = n.dependsOn[preLastcaseIndex * 2 + 2]
+        unlink_hls_nodes(v0, v0In)
+        unlink_hls_nodes(v1, v1In)
+        link_hls_nodes(v0, v1In)
+        link_hls_nodes(v1, v0In)
+        everyConditionIsEq = True
+        lastConditionIsNe = False
+        cases = tuple(n._iterValueConditionDriverPairs())
+
+    if everyConditionIsEq:
+        # try extract ROM
+        romCompatible = True
+        romData = {}
+        index = None
+        for (v, c) in cases:
+            if c is not None:
+                if not isinstance(c.obj, HlsNetNodeOperator) or len(c.obj.dependsOn) != 2:
+                    romCompatible = False
+                    break
+
+                cOp0, cOp1 = c.obj.dependsOn
+                if index is None:
+                    index = cOp0
+
+                if cOp0 is not index:
+                    romCompatible = False
+                    break
+
+                if isinstance(cOp1.obj, HlsNetNodeConst):
+                    try:
+                        cOp1 = int(cOp1.obj.val)
+                    except ValidityError:
+                        raise AssertionError(n, "value specified for undefined index in ROM")
+                    if isinstance(v.obj, HlsNetNodeConst):
+                        romData[cOp1] = v.obj.val
+                    else:
+                        romCompatible = False
+                        break
+                else:
+                    romCompatible = False
+                    break
+            else:
+                if index is None:
+                    romCompatible = False
+                    break
+
+                itemCnt = 2 ** index._dtype.bit_length()
+                if len(romData) == itemCnt - 1 and itemCnt - 1 not in romData.keys():
+                    # if the else branch of the mux contains trully the last item of the ROM
+                    if isinstance(v.obj, HlsNetNodeConst):
+                        romData[itemCnt - 1] = v.obj.val
+                    else:
+                        romCompatible = False
+                        break
+                else:
+                    romCompatible = False
+                    break
+
+        if romCompatible:
+            assert index is not None
+            rom = builder.buildRom(romData, index)
+            replaceOperatorNodeWith(n, rom, worklist, removed)
+            return True
+
+    return False
+
+
+def netlistReduceMuxConstantConditionsAndChildMuxSink(n: HlsNetNodeMux, worklist: UniqList[HlsNetNode], removed: Set[HlsNetNode]):
     builder: HlsNetlistBuilder = n.netlist.builder
     # resolve constant conditions
+    newCondSet: Set[HlsNetNodeOut] = set()
     newOps: List[HlsNetNodeIn] = []
     newValSet: Set[HlsNetNodeIn] = set()
     for (v, c) in n._iterValueConditionDriverPairs():
@@ -32,10 +136,32 @@ def netlistReduceMux(n: HlsNetNodeMux, worklist: UniqList[HlsNetNode], removed: 
                 newValSet.add(v)
                 break
         else:
-            newOps.append(v)
-            newValSet.add(v)
-            if c is not None:
-                newOps.append(c)
+            childMux = v.obj
+            if isinstance(childMux, HlsNetNodeMux) and all(u.obj == n and u.in_i % 2 == 0
+                                                        for u in childMux.usedBy[v.out_i]):
+                # if it is used only by value operands of n
+                # inline child mux
+                for (v1, c1) in childMux._iterValueConditionDriverPairs():
+                    if c is not None:
+                        if c1 is None:
+                            c1 = c
+                        else:
+                            c1 = builder.buildAnd(c, c1)
+
+                    if c1 not in newCondSet:
+                        newOps.append(v1)
+                        newValSet.add(v1)
+
+                        if c1 is not None:
+                            newOps.append(c1)
+                            newCondSet.add(c1)
+
+            elif c not in newCondSet:
+                newOps.append(v)
+                newValSet.add(v)
+                if c is not None:
+                    newOps.append(c)
+                    newCondSet.add(c)
 
     singleVal = len(newValSet) == 1
     newOpsLen = len(newOps)
@@ -46,12 +172,31 @@ def netlistReduceMux(n: HlsNetNodeMux, worklist: UniqList[HlsNetNode], removed: 
             i: HlsNetNodeOut = newOps[0]
         else:
             i = builder.buildMux(n._outputs[0]._dtype, tuple(newOps))
+            if i.obj.name is None:
+                i.obj.name = n.name
+            worklist.append(i.obj)  # may have become ROM
 
         replaceOperatorNodeWith(n, i, worklist, removed)
         return True
 
+    return False
+
+
+def netlistReduceMux(n: HlsNetNodeMux, worklist: UniqList[HlsNetNode], removed: Set[HlsNetNode]):
+    inpCnt = len(n._inputs)
+    if inpCnt == 1:
+        # mux x = x
+        i: HlsNetNodeOut = n.dependsOn[0]
+        replaceOperatorNodeWith(n, i, worklist, removed)
+        return True
+
+    if netlistReduceMuxConstantConditionsAndChildMuxSink(n, worklist, removed):
+        return True
+
+    builder: HlsNetlistBuilder = n.netlist.builder
+
     # merge mux to only user which is mux if this is the case and it is possible
-    if len(n._inputs) % 2 == 1:
+    if inpCnt % 2 == 1:
         assert len(n._outputs) == 1, n
         if len(n.usedBy[0]) == 1:
             u: HlsNetNodeIn = n.usedBy[0][0]
@@ -65,10 +210,11 @@ def netlistReduceMux(n: HlsNetNodeMux, worklist: UniqList[HlsNetNode], removed: 
                     replaceOperatorNodeWith(u.obj, res, worklist, removed)
                     disconnectAllInputs(n, worklist)
                     removed.add(n)
+
                     return True
 
     # x ? x: v1 -> x | v1
-    if len(n._inputs) == 3:
+    if inpCnt == 3:
         v0, c, v1 = n.dependsOn
         if v0 is c:
             newO = builder.buildOr(c, v1)
@@ -76,110 +222,12 @@ def netlistReduceMux(n: HlsNetNodeMux, worklist: UniqList[HlsNetNode], removed: 
             return True
 
     # search large ROMs implemented as MUX
-    if len(n._inputs) >= 3:
-        # try to format mux to a format where each condition is comparison with EQ operator
-        # so the mux behaves like switch-case statement id it is suitable for ROM extraction
-        cases = tuple(n._iterValueConditionDriverPairs())
-        if cases[-1][1] is None:
-            # if contains else it may be possible to swap last two cases if required
-            everyNonLastConditionIsEq = True
-            everyConditionIsEq = True
-            lastConditionIsNe = False
-            preLastcaseIndex = len(cases) - 2
-            for i, (v, c) in enumerate(cases):
-                if c is None:
-                    break
-                if isinstance(c.obj, HlsNetNodeOperator):
-                    op = c.obj.operator
-                    if i == preLastcaseIndex:
-                        lastConditionIsNe = op == AllOps.NE
-                        everyConditionIsEq = everyNonLastConditionIsEq and op == AllOps.EQ
-                    else:
-                        everyNonLastConditionIsEq &= op == AllOps.EQ
-                else:
-                    everyConditionIsEq = False
-                    everyNonLastConditionIsEq = False
-                    break
+    if inpCnt >= 3:
+        if netlistReduceMuxToRom(builder, n, worklist, removed):
+            return True
 
-            if everyNonLastConditionIsEq and lastConditionIsNe:
-                # flip last condition NE -> EQ and swap cases
-                origNe = cases[-2][1]
-                origNeArgs = origNe.obj.dependsOn
-                cIn = n._inputs[preLastcaseIndex * 2 + 1]
-                unlink_hls_nodes(origNe, cIn)
-                worklist.append(origNe.obj)
-                newEq = builder.buildEq(origNeArgs[0], origNeArgs[1])
-                link_hls_nodes(newEq, cIn)
-
-                v0In = n._inputs[preLastcaseIndex * 2]
-                v0 = n.dependsOn[preLastcaseIndex * 2]
-                v1In = n._inputs[preLastcaseIndex * 2 + 2]
-                v1 = n.dependsOn[preLastcaseIndex * 2 + 2]
-                unlink_hls_nodes(v0, v0In)
-                unlink_hls_nodes(v1, v1In)
-                link_hls_nodes(v0, v1In)
-                link_hls_nodes(v1, v0In)
-                everyConditionIsEq = True
-                lastConditionIsNe = False
-                cases = tuple(n._iterValueConditionDriverPairs())
-
-            if everyConditionIsEq:
-                # try extract ROM
-                romCompatible = True
-                romData = {}
-                index = None
-                for (v, c) in cases:
-                    if c is not None:
-                        if not isinstance(c.obj, HlsNetNodeOperator) or len(c.obj.dependsOn) != 2:
-                            romCompatible = False
-                            break
-
-                        cOp0, cOp1 = c.obj.dependsOn
-                        if index is None:
-                            index = cOp0
-
-                        if cOp0 is not index:
-                            romCompatible = False
-                            break
-
-                        if isinstance(cOp1.obj, HlsNetNodeConst):
-                            try:
-                                cOp1 = int(cOp1.obj.val)
-                            except ValidityError:
-                                raise AssertionError(n, "value specified for undefined index in ROM")
-                            if isinstance(v.obj, HlsNetNodeConst):
-                                romData[cOp1] = v.obj.val
-                            else:
-                                romCompatible = False
-                                break
-                        else:
-                            romCompatible = False
-                            break
-                    else:
-                        if index is None:
-                            romCompatible = False
-                            break
-
-                        itemCnt = 2 ** index._dtype.bit_length()
-                        if len(romData) == itemCnt - 1 and itemCnt - 1 not in romData.keys():
-                            # if the else branch of the mux contains trully the last item of the ROM
-                            if isinstance(v.obj, HlsNetNodeConst):
-                                romData[itemCnt - 1] = v.obj.val
-                            else:
-                                romCompatible = False
-                                break
-                        else:
-                            romCompatible = False
-                            break
-
-                if romCompatible:
-                    assert index is not None
-                    rom = builder.buildRom(romData, index)
-                    replaceOperatorNodeWith(n, rom, worklist, removed)
-                    return True
-                
     # ~c ? v0: v1 -> c ? v1: v0 (supports arbitrary number of operands, swaps last two values if last condition is negated to remove negation of c)
-    if len(n._inputs) % 2 == 1 and len(n._inputs) >= 3:
+    if inpCnt % 2 == 1 and inpCnt >= 3:
         v0, c, v1 = n.dependsOn[-3:]
         if isinstance(c.obj, HlsNetNodeOperator) and c.obj.operator == AllOps.NOT:
             cIn = n._inputs[-2]
