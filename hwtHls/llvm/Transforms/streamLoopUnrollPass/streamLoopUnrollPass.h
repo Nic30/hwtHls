4e0a986b3653a3e850cf151a2ab924d3740f5299
Nic30 <nic30@seznam.cz>
2024-06-03 13:00:58 +0200

    docs

--- hwtHls/llvm/Transforms/streamLoopUnrollPass/streamLoopUnrollPass.h
+++ hwtHls/llvm/Transforms/streamLoopUnrollPass/streamLoopUnrollPass.h
@@ -6,6 +6,15 @@
 namespace hwtHls {
 
 /// Loop unroll for loops which are dependent on some stream IO
+//  This form of loop unroll also copies loop body N-times
+//  however this works more like predicated vectorization as each loop body represents
+//  a functional unit which processes one chunk of the data. Thus loop body represents a lane.
+//  Each lane is guarded by "if" which enables lane if stream offset is of value matching this lane dedication.
+//  If data chunk boundary is not aligned to stream word boundary the chunk processing is postponed
+//  to next iteration until the chunk is completed, the offset is then always aligned to the beginning of the word.
+//  :note: If this happens (chunk is crossing word boundary) the chunk size is too large
+//    or it does not matter that there is 1 iteration latency because complete chunk is requested.
+//    Thus this does not affect throughput or optimality of the result.
 class StreamLoopUnrollPass: public llvm::PassInfoMixin<StreamLoopUnrollPass> {
 public:
 	llvm::PreservedAnalyses run(llvm::Function &F,
