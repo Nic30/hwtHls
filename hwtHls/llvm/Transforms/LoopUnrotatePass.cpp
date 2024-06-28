f46807126144507d554260f1f198a041410c5314
Nic30 <nic30@seznam.cz>
2024-06-28 13:52:42 +0200

    refactor(LoopUnrotatePass): rm duplicit code

--- hwtHls/llvm/Transforms/LoopUnrotatePass.cpp
+++ hwtHls/llvm/Transforms/LoopUnrotatePass.cpp
@@ -23,6 +23,7 @@
 #include <llvm/Transforms/Utils/BasicBlockUtils.h>
 
 #include <hwtHls/llvm/Transforms/utils/dceWorklist.h>
+#include <hwtHls/llvm/Transforms/utils/loopMerging.h>
 #include <hwtHls/llvm/Transforms/utils/writeCFGToDotFile.h>
 
 using namespace llvm;
@@ -191,49 +192,6 @@ bool collectAssociatedPHIs(llvm::Loop &L, BasicBlock *Guard,
 	return true;
 }
 
-/*
- * :note: parent loop must contain all nodes from child loops, in order to recognize exit correctly
- *
- * */
-void mergeNestedLoops(LoopInfo &LI, ScalarEvolution *SE, LPMUpdater & LPMU, llvm::Loop *L1,
-		llvm::Loop *L0) {
-	// :note: based on llvm/lib/Transforms/Scalar/LoopFuse.cpp
-	std::string L1Name = std::string(L1->getName());
-	//// Anything ScalarEvolution may know about this loop or the PHI nodes
-	//// in its header will soon be invalidated. We should also invalidate
-	//// all outer loops because insertion and deletion of blocks that happens
-	//// during the rotation may violate invariants related to backedge taken
-	//// infos in them.
-	if (SE) {
-		SE->forgetLoop(L1);
-		// We may hoist some instructions out of loop. In case if they were cached
-		// as "loop variant" or "loop computable", these caches must be dropped.
-		// We also may fold basic blocks, so cached block dispositions also need
-		// to be dropped.
-		SE->forgetBlockAndLoopDispositions();
-	}
-
-	// Merge the loops. (L1 into L0)
-	SmallVector<BasicBlock*, 8> Blocks(L1->blocks());
-	for (BasicBlock *BB : Blocks) {
-		// :note: may contain if L1 is inside of L0
-		assert(L0->contains(BB) && "Must contain because L1 should be nested in L0");
-		//	L0->addBlockEntry(BB);
-		L1->removeBlockFromLoop(BB);
-		if (LI.getLoopFor(BB) != L1)
-			continue;
-		LI.changeLoopFor(BB, L0);
-	}
-	while (!L1->isInnermost()) {
-		const auto &ChildLoopIt = L1->begin();
-		Loop *ChildLoop = *ChildLoopIt;
-		L1->removeChildLoop(ChildLoopIt);
-		L0->addChildLoop(ChildLoop);
-	}
-	LPMU.markLoopAsDeleted(*L1, L1Name);
-	// Delete the now empty loop L1.
-	LI.erase(L1);
-}
 void moveBlockBetweenLoops(LoopInfo &LI, BasicBlock *BB, llvm::Loop *Lsrc,
 		llvm::Loop *Ldst) {
 	Ldst->addBlockEntry(BB);
@@ -576,7 +534,7 @@ void rewriteGuardedDoWhileToWhile(llvm::Loop *L, LoopInfo &LI,
 			// to be dropped.
 			SE->forgetBlockAndLoopDispositions();
 		}
-		LPMU.isLoopNestChanged();
+		LPMU.markLoopNestChanged(true);
 	}
 }
 
