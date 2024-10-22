8451706e5a5c6f206590edf51404963e52cf9c0a
Nic30 <nic30@seznam.cz>
2024-10-22 20:08:48 +0200

    test(bitOpt): divNonRestoring and its tests: refactor for code reuse

--- tests/bitOpt/divNonRestoring.py
+++ tests/bitOpt/divNonRestoring.py
@@ -10,41 +10,72 @@ from hwtHls.frontend.pyBytecode.pragmaPreproc import PyBytecodeBlockLabel, \
     PyBytecodeInline
 
 
-@hlsBytecode
-def divNonRestoring(dividend: RtlSignal, divisor: RtlSignal, isSigned: RtlSignal, unrollFactor: int):
+def _divCastToUnsigned(dividend: RtlSignal, divisor: RtlSignal, isSigned: RtlSignal):
     """
-    Non-restoring integer division, dividend/divisor = quotient + remainder
-
-    based on https://github.com/ultraembedded/riscv/blob/master/core/riscv/riscv_divider.v
+    An utility function which cast signed operands to unsigned before division
+    before division algorithm is for unsigned only.
+    It also computes invertQuotient and invertRemainder which
+    should be used to correct result if inputs were signed.
     """
-    assert dividend._dtype == divisor._dtype
-    t = dividend._dtype
-    width = t.bit_length()
-    assert unrollFactor > 0 and unrollFactor <= width
+    PyBytecodeBlockLabel("_divCastToUnsigned")
+    width = dividend._dtype.bit_length()
 
     invertQuotient = isSigned & (dividend[width - 1] != divisor[width - 1]) & (divisor != 0)
     invertRemainder = isSigned & dividend[width - 1]
 
-    if isSigned & (dividend[width - 1]):
+    if isSigned & dividend[width - 1]:
         dividend = -dividend
-    
-    if isSigned & (divisor[width - 1]):
+
+    if isSigned & divisor[width - 1]:
         divisor = -divisor
+
+    return dividend, divisor, invertQuotient, invertRemainder
+
+
+# version with ctlz https://github.com/ShaheerSajid/RISCV-Compliant-Divider/blob/main/src/divider.sv
+# 1/4 LUT, 1/2 FF https://github.com/yasinxyz/muldiv/blob/main/src/MULDIV/divider_32.v
+@hlsBytecode
+def divNonRestoring(dividend: RtlSignal, divisor: RtlSignal, isSigned: RtlSignal,
+                    loopPragmaGetter: Callable[[], _PyBytecodeLoopPragma]=lambda: None,
+                    dbgNoSplitSlices:bool=True):
+    """
+    Non-restoring integer division, dividend/divisor = quotient + remainder
+
+    :param dbgSplitSlices: debug option which controls SplitSlices pass
+        (which does not improve anything but triggers many other optimizations)
+
+    based on https://github.com/ultraembedded/riscv/blob/master/core/riscv/riscv_divider.v
+    https://en.wikipedia.org/wiki/Division_algorithm
+    """
+    assert dividend._dtype == divisor._dtype
+    t = dividend._dtype
+    width = t.bit_length()
+
+    dividend, divisor, invertQuotient, invertRemainder = PyBytecodeInline(_divCastToUnsigned)(dividend, divisor, isSigned)
 
     zeroPad = HBits(width - 1).from_py(0)
     quotient = t.from_py(0)
     divisorTmp = Concat(divisor, zeroPad)
     qMask = t.from_py(1 << (width - 1))
+    if dbgNoSplitSlices:
+        PyBytecodeNoSplitSlices(divisorTmp)
 
     while qMask != 0:
+        PyBytecodeBlockLabel("divNonRestoring.divLoop")
         if divisorTmp <= Concat(zeroPad, dividend):
             dividend -= divisorTmp[width:]
             quotient |= qMask
-    
+
+        if dbgNoSplitSlices:
+            PyBytecodeNoSplitSlices(qMask)
+            PyBytecodeNoSplitSlices(divisorTmp)
+            PyBytecodeNoSplitSlices(quotient)
+
         divisorTmp >>= 1
         qMask >>= 1
-        PyBytecodeLLVMLoopUnroll(unrollFactor > 1, unrollFactor)
+        loopPragmaGetter()
 
+    PyBytecodeBlockLabel("divNonRestoring.signFinalize")
     if invertQuotient:
         quotient = -quotient
 
@@ -52,4 +83,5 @@ def divNonRestoring(dividend: RtlSignal, divisor: RtlSignal, isSigned: RtlSignal
     if invertRemainder:
         remainder = -remainder
 
+    PyBytecodeBlockLabel("divNonRestoring.return")
     return (quotient, remainder)
--- tests/bitOpt/divNonRestoring_test.py
+++ tests/bitOpt/divNonRestoring_test.py
@@ -7,27 +7,30 @@ from hwt.hdl.types.defs import BIT
 from hwt.hdl.types.struct import HStruct
 from hwt.hwIOs.hwIOStruct import HwIOStructRdVld
 from hwt.hwIOs.utils import addClkRstn
-from hwt.simulator.simTestCase import SimTestCase
-from hwt.hwParam import HwParam
 from hwt.hwModule import HwModule
+from hwt.hwParam import HwParam
+from hwt.simulator.simTestCase import SimTestCase
 from hwtHls.frontend.pyBytecode import hlsBytecode
-from hwtHls.frontend.pyBytecode.markers import PyBytecodeInline, \
-    PyBytecodeSkipPass
+from hwtHls.frontend.pyBytecode.pragmaFunction import PyBytecodeSkipPass
+from hwtHls.frontend.pyBytecode.pragmaLoop import PyBytecodeLoopFlattenUsingIf, \
+    PyBytecodeLLVMLoopUnroll
+from hwtHls.frontend.pyBytecode.pragmaPreproc import PyBytecodeInline
 from hwtHls.frontend.pyBytecode.thread import HlsThreadFromPy
-from hwtHls.platform.platform import HlsDebugBundle
 from hwtHls.scope import HlsScope
 from hwtSimApi.utils import freq_to_period
 from pyMathBitPrecise.bit_utils import mask
 from tests.bitOpt.divNonRestoring import divNonRestoring
+from tests.frontend.pyBytecode.stmWhile import TRUE
 from tests.testLlvmIrAndMirPlatform import TestLlvmIrAndMirPlatform
 
 
-class DivNonRestoring(HwModule):
+class _TestDiv(HwModule):
 
     def hwConfig(self) -> None:
         self.DATA_WIDTH = HwParam(4)
         self.FREQ = HwParam(int(20e6))
         self.UNROLL_FACTOR = HwParam(1)
+        self.DIV_FN = HwParam(divNonRestoring)
         self.MAIN_FN_META = HwParam(None)
 
     def hwDeclr(self) -> None:
@@ -47,15 +50,27 @@ class DivNonRestoring(HwModule):
             (t, "remainder")
         )
 
+    def _getLoopMeta(self):
+        UNROLL_FACTOR = self.UNROLL_FACTOR
+        if UNROLL_FACTOR > 1:
+            return PyBytecodeLLVMLoopUnroll(True, UNROLL_FACTOR,
+                followup_unrolled=PyBytecodeLoopFlattenUsingIf())
+        else:
+            return PyBytecodeLoopFlattenUsingIf()
+
     @hlsBytecode
     def mainThread(self, hls: HlsScope):
+        PyBytecodeSkipPass(["hwtHls::SlicesToIndependentVariablesPass", "hwtHls::SelectPruningPass"])
+
         self.MAIN_FN_META
-        while BIT.from_py(1):
+        while TRUE:
             inp = hls.read(self.data_in)
-            res = PyBytecodeInline(divNonRestoring)(inp.dividend, inp.divisor, inp.signed, self.UNROLL_FACTOR)
+            DIV_FN = self.DIV_FN
+            res = PyBytecodeInline(DIV_FN)(inp.dividend, inp.divisor, inp.signed,
+                loopPragmaGetter=self._getLoopMeta)
             resTmp = self.data_out.T.from_py(None)
-            resTmp.quotient(res[0])
-            resTmp.remainder(res[1])
+            resTmp.quotient = res[0]
+            resTmp.remainder = res[1]
             hls.write(resTmp, self.data_out)
 
     def hwImpl(self) -> None:
@@ -70,20 +85,23 @@ class DivNonRestoring_TC(SimTestCase):
         [(1, 1, 0), (2, 2, 0), (4, 2, 0), ],  # (13, 3, 0), (3, 15, 0), (9, 3, 0)],  # dividend, divisor, isSigned
         [(1, 0), (1, 0), (2, 0), ],  # (4, 1), (0, 3), (3, 0)],  # quotient, remainder
     ]
+    HLS_DIV_FN = staticmethod(divNonRestoring)
 
     def test_div_py(self):
         T = HBits(4)
+        divFn = self.HLS_DIV_FN
         for (dividend, divisor, isSigned), (quotient, remainder) in zip(self.GOLDEN_DATA[0], self.GOLDEN_DATA[1]):
             _dividend = T.from_py(dividend)
             _divisor = T.from_py(divisor)
             _isSigned = BIT.from_py(isSigned)
-            _quotient, _remainder = divNonRestoring(_dividend, _divisor, _isSigned, 1)
+            _quotient, _remainder = divFn(_dividend, _divisor, _isSigned)
             self.assertValSequenceEqual([_quotient, _remainder], (quotient, remainder),
                                         msg=((dividend, "//", divisor, "signed?:", isSigned), (quotient, "rem:", remainder)))
 
     def test_div(self, MAIN_FN_META=None, runTestAfterEachPass=False):
-        dut = DivNonRestoring()
+        dut = _TestDiv()
         dut.DATA_WIDTH = 3
+        dut.DIV_FN = self.HLS_DIV_FN
         dut.MAIN_FN_META = MAIN_FN_META
 
         def prepareDataInFn():
@@ -110,7 +128,7 @@ class DivNonRestoring_TC(SimTestCase):
 
         self.compileSimAndStart(dut,
                                 target_platform=TestLlvmIrAndMirPlatform.forSimpleDataInDataOutHwModule(
-                                    prepareDataInFn, checkDataOutFn, None, #debugFilter=HlsDebugBundle.ALL_RELIABLE,
+                                    prepareDataInFn, checkDataOutFn, None,  # debugFilter=HlsDebugBundle.ALL_RELIABLE,
                                     noOptIrTest=TestLlvmIrAndMirPlatform.TEST_NO_OPT_IR,
                                     runTestAfterEachPass=runTestAfterEachPass
                                     ))
@@ -127,12 +145,15 @@ class DivNonRestoring_TC(SimTestCase):
 
 
 if __name__ == "__main__":
-    # from hwt.synth import to_rtl_str
-    # from hwtHls.platform.virtual import VirtualHlsPlatform
-    # # from hwtHls.platform.xilinx.artix7 import Artix7Fast
-    # m = DivNonRestoring()
-    # # m.DATA_WIDTH = 8
-    # print(to_rtl_str(m, target_platform=VirtualHlsPlatform(debugFilter=HlsDebugBundle.ALL_RELIABLE)))
+    from hwt.synth import to_rtl_str
+    from hwtHls.platform.virtual import VirtualHlsPlatform
+    from hwtHls.platform.platform import HlsDebugBundle
+    from hwtHls.platform.xilinx.artix7 import Artix7Fast
+    m = _TestDiv()
+    m.DATA_WIDTH = 32
+    m.UNROLL_FACTOR = 1
+    m.FREQ = int(1e6)
+    print(to_rtl_str(m, target_platform=Artix7Fast(debugFilter=HlsDebugBundle.ALL_RELIABLE)))
 
     import unittest
 
