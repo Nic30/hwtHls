3cee06ae1ef639fee57e46fed7a6fa1b0ba48928
Nic30 <nic30@seznam.cz>
2024-10-22 20:12:38 +0200

    test(SelectPruningPass_TC): add more cases

--- tests/llvmIr/selectPruningPass_test.py
+++ tests/llvmIr/selectPruningPass_test.py
@@ -97,12 +97,78 @@ class SelectPruningPass_TC(BaseLlvmIrTC):
         """
         self._test_ll(llvmIr0)
 
+    def test_nothingToPrune(self):
+        # originally PragmaInline_writeSaturatedCntr4.mainThread
+        llvmIr0 = """        
+        define void @test_nothingToPrune(ptr addrspace(1) %o) {
+            entry:
+              br label %body
+            
+            body:
+              %.phiConc = phi i8 [ 0, %entry ], [ %.selConc, %body ]
+              %0 = call i3 @hwtHls.bitRangeGet.i8.i4.i3.5(i8 %.phiConc, i4 5) #2
+              %1 = call i1 @hwtHls.bitRangeGet.i8.i4.i1.4(i8 %.phiConc, i4 4) #2
+              %2 = call i4 @hwtHls.bitRangeGet.i8.i4.i4.0(i8 %.phiConc, i4 0) #2
+              store volatile i8 %.phiConc, ptr addrspace(1) %o, align 1
+              %"5" = add i8 %.phiConc, 1
+              %3 = call i8 @hwtHls.bitConcat.i4.i1.i3(i4 %2, i1 true, i3 %0) #2
+              %.selConc = select i1 %1, i8 %3, i8 %"5"
+              br label %body
+        }
+        """
+        self._test_ll(llvmIr0)
+
+    def test_counterArray(self):
+        # originally CntrArray.mainThread
+        
+        # %o_addr = load volatile i2, ptr addrspace(3) %o_addr, align 1
+        # %o_addr_0 = icmp eq i2 %o_addr, 0
+        # %o_addr_1 = icmp eq i2 %o_addr, 1
+        # %o_addr_2 = icmp eq i2 %o_addr, -2
+        # %outSel0 = select i1 %o_addr_0, i16 %v0, i16 %v3
+        # %outSel1 = select i1 %o_addr_1, i16 %v1, i16 %outSel0
+        # %outSel2 = select i1 %o_addr_2, i16 %v2, i16 %outSel1
+        # store volatile i16 %outSel2, ptr addrspace(2) %o, align 2
+        
+        llvmIr0 = """
+        define void @test_counterArray(ptr addrspace(1) %i) {
+        entry:
+          br label %body
+        
+        body:
+          %v0 = phi i16 [ 0, %entry ], [ %v0.1, %body ]
+          %v1 = phi i16 [ 0, %entry ], [ %v1.1, %body ]
+          %v2 = phi i16 [ 0, %entry ], [ %v2.1, %body ]
+          %v3 = phi i16 [ 0, %entry ], [ %v3.1, %body ]
+        
+          %i_addr = load volatile i2, ptr addrspace(1) %i, align 1
+          %i_addr_0 = icmp eq i2 %i_addr, 0
+          %i_addr_1 = icmp eq i2 %i_addr, 1
+          %i_addr_2 = icmp eq i2 %i_addr, -2
+          %i_addr_3 = icmp eq i2 %i_addr, -1
+          %inSel0 = select i1 %i_addr_0, i16 %v0, i16 %v3
+          %inSel1 = select i1 %i_addr_1, i16 %v1, i16 %inSel0
+          %inSel2 = select i1 %i_addr_2, i16 %v2, i16 %inSel1
+          %v.incr = add i16 %inSel2, 1
+          
+          %v0.1 = select i1 %i_addr_0, i16 %v.incr, i16 %v0
+          %v1.1 = select i1 %i_addr_1, i16 %v.incr, i16 %v1
+          %v2.1 = select i1 %i_addr_2, i16 %v.incr, i16 %v2
+          %v3.1 = select i1 %i_addr_3, i16 %v.incr, i16 %v3
+          
+          br label %body
+        }
+        """
+
+        self._test_ll(llvmIr0)
+        
+
 
 if __name__ == "__main__":
     import unittest
     import sys
     testLoader = unittest.TestLoader()
-    # suite = unittest.TestSuite([SelectPruningPass_TC('test_sameAndWithCond')])
+    # suite = unittest.TestSuite([SelectPruningPass_TC('test_nothingToPrune')])
     suite = testLoader.loadTestsFromTestCase(SelectPruningPass_TC)
     runner = unittest.TextTestRunner(verbosity=3)
     sys.exit(not runner.run(suite).wasSuccessful())
