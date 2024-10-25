7ce819043fc1742d8288799e90572874df7a5a2b
Nic30 <nic30@seznam.cz>
2024-10-25 11:13:24 +0200

    feat(netlistNormalizeConstToRhs): support for scheduled netlist

--- hwtHls/netlist/transformation/simplifyExpr/normalizeConstToRhs.py
+++ hwtHls/netlist/transformation/simplifyExpr/normalizeConstToRhs.py
@@ -7,7 +7,6 @@ from hwtHls.netlist.nodes.node import HlsNetNode
 from hwtHls.netlist.nodes.ops import HlsNetNodeOperator
 from hwtHls.netlist.transformation.simplifyUtilsHierarchyAware import replaceOperatorNodeWith
 
-
 BINARY_OPS_WITH_SWAPABLE_OPERANDS = {*BITWISE_OPS, *COMPARE_OPS, *ALWAYS_COMMUTATIVE_OPS}
 
 
@@ -22,10 +21,19 @@ def netlistNormalizeConstToRhs(n: HlsNetNodeOperator, worklist: SetList[HlsNetNo
         else:
             op = CMP_OP_SWAP[op]
 
-        newN = b.buildOp(op, n.operatorSpecialization, n._outputs[0]._dtype, op1, op0)
-        assert newN is not n
-        replaceOperatorNodeWith(n, newN, worklist)
-        worklist.append(newN.obj)
+        replacement = b.buildOp(op, n.operatorSpecialization, n._outputs[0]._dtype, op1, op0)
+        newN: HlsNetNode = replacement.obj
+        assert newN is not n, n
+        if n.realization is not None and newN.realization is None:
+            newN.assignRealization(n.realization)
+            if newN.isMulticlock:
+                newN._setScheduleZeroTimeSingleClock(n.scheduledZero)
+            else:
+                newN._setScheduleZeroTimeSingleClock(n.scheduledZero)
+            n.parent._addNodeIntoScheduled(n.scheduledZero // n.netlist.normalizedClkPeriod, newN)
+
+        replaceOperatorNodeWith(n, replacement, worklist)
+        worklist.append(replacement.obj)
         return True
 
     return False
