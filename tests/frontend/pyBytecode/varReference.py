289fc83e8534061d0c8c78b2cb7256c047333dea
Nic30 <nic30@seznam.cz>
2024-10-25 13:48:30 +0200

    test: new VariableChain_TC, VarReference_TC

--- tests/frontend/pyBytecode/varReference.py
+++ tests/frontend/pyBytecode/varReference.py
@@ -1,8 +1,13 @@
-from hwt.hdl.types.defs import BIT
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from hwt.hdl.commonConstants import b1
 from hwtHls.frontend.pyBytecode import hlsBytecode
 from hwtHls.scope import HlsScope
 from hwtLib.types.ctypes import uint8_t
 from tests.frontend.pyBytecode.pragmaInline import PragmaInline_writeCntr1
+from hwt.simulator.simTestCase import SimTestCase
+from tests.frontend.pyBytecode.hwrange_test import HlsPythonHwrange_TC
 
 
 class CntrHolder():
@@ -19,10 +24,11 @@ class VarReference_writeCntr0(PragmaInline_writeCntr1):
         cntr.val = 0
         limit = 4
 
-        while BIT.from_py(1):
+        while b1:
             hls.write(cntr.val, self.o)
             if limit > 0:
                 cntr.val += 1
+
 
 class VarReference_writeCntr1(PragmaInline_writeCntr1):
 
@@ -32,16 +38,35 @@ class VarReference_writeCntr1(PragmaInline_writeCntr1):
         cntr.val = 0xff
         limit = 4
 
-        while BIT.from_py(1):
+        while b1:
             if limit > 0:
                 cntr.val += 1
             hls.write(cntr.val, self.o)
+
+
+class VarReference_TC(SimTestCase):
+
+    def test_VarReference_writeCntr0(self):
+        HlsPythonHwrange_TC.test_HlsPythonHwrange_fromInt0(self, VarReference_writeCntr0, list(range(10)))
+
+    def test_VarReference_writeCntr1(self):
+        HlsPythonHwrange_TC.test_HlsPythonHwrange_fromInt0(self, VarReference_writeCntr1, list(range(0, 10)))
 
 
 if __name__ == "__main__":
     from hwt.synth import to_rtl_str
     from hwtHls.platform.virtual import VirtualHlsPlatform
     from hwtHls.platform.platform import HlsDebugBundle
-    
+
     m = VarReference_writeCntr1()
     print(to_rtl_str(m, target_platform=VirtualHlsPlatform(debugFilter=HlsDebugBundle.ALL_RELIABLE)))
+
+    import unittest
+    testLoader = unittest.TestLoader()
+    # suite = unittest.TestSuite([
+    #    VarReference_TC('test_VarReference_writeCntr0'),
+    # ])
+    suite = testLoader.loadTestsFromTestCase(VarReference_TC)
+    runner = unittest.TextTestRunner(verbosity=3)
+    runner.run(suite)
+
--- tests/frontend/pyBytecode/variableChain.py
+++ tests/frontend/pyBytecode/variableChain.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from hwt.hdl.types.defs import BIT
+from hwt.hdl.commonConstants import b1
 from hwt.hwIOs.std import HwIOVectSignal
 from hwt.hwIOs.utils import addClkRstn
 from hwt.hwModule import HwModule
 from hwt.hwParam import HwParam
 from hwt.pyUtils.typingFuture import override
+from hwt.simulator.simTestCase import SimTestCase
 from hwtHls.frontend.pyBytecode import hlsBytecode
 from hwtHls.frontend.pyBytecode.thread import HlsThreadFromPy
 from hwtHls.scope import HlsScope
+from hwtSimApi.utils import freq_to_period
 
 
 class VariableChain(HwModule):
@@ -28,7 +30,7 @@ class VariableChain(HwModule):
     def mainThread(self, hls: HlsScope):
         # :note: all variables are supposed to be reduced out and just direct connection should remain
         path = [hls.var(f"i{i:d}", self.i._dtype) for i in range(self.LEN)]
-        while BIT.from_py(1):
+        while b1:
             for i, p in enumerate(path):
                 if i == 0:
                     prev = hls.read(self.i).data
@@ -45,10 +47,30 @@ class VariableChain(HwModule):
         hls.compile()
 
 
+class VariableChain_TC(SimTestCase):
+
+    def test_VariableChain(self, cls=VariableChain):
+        dut = VariableChain()
+        self.compileSimAndStart(dut, target_platform=VirtualHlsPlatform())
+        CLK_PERIOD = freq_to_period(dut.clk.FREQ)
+        dut.i._ag.data.extend(range(1, 9))
+        self.runSim((8 + 1) * int(CLK_PERIOD))
+
+        self.assertValSequenceEqual(dut.o._ag.data, list(range(1, 9)))
+
+
 if __name__ == "__main__":
     from hwt.synth import to_rtl_str
     from hwtHls.platform.virtual import VirtualHlsPlatform
     from hwtHls.platform.platform import HlsDebugBundle
-    
+
     m = VariableChain()
     print(to_rtl_str(m, target_platform=VirtualHlsPlatform(debugFilter=HlsDebugBundle.ALL_RELIABLE)))
+
+    import unittest
+
+    testLoader = unittest.TestLoader()
+    # suite = unittest.TestSuite([PyArrShift_TC("test_frameHeader")])
+    suite = testLoader.loadTestsFromTestCase(VariableChain_TC)
+    runner = unittest.TextTestRunner(verbosity=3)
+    runner.run(suite)
