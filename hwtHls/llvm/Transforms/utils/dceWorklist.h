391e3bba25eb482d09b6ee02ee334db17b4afee9
Nic30 <nic30@seznam.cz>
2025-04-29 17:06:14 +0200

    refactor(llvm.ir.utils): DceWorklist rm support for slice dict
    * In general it is not possible to update slice dict
      if many instructions are modified at once, because of PHINodes
      only a to implement this behavior llvm::InstructionWorklist
      should be used, and DceWorklist is a complementary thing
    * IRBuilder with a custom inserter (IRBuilderCallbackInserter)
      should be used to insert into slice dict and llvm::InstructionWorklist
      (instead of DceWorklist) should be used for instruction combiner
      algorithms which modify instructions and tracking slices
      for example :see: hwtHls::SlicesMergeCombiner, hwtHls::SlicesMergePass

--- hwtHls/llvm/Transforms/utils/dceWorklist.h
+++ hwtHls/llvm/Transforms/utils/dceWorklist.h
@@ -11,39 +11,21 @@
 namespace hwtHls {
 
 class DceWorklist {
-public:
-	// (bit vector, offset of slice) to slices of this vector beginning on this offset
-	// :note: we do not need reverse dictionary, because instruction can be analyzed by OffsetWidthValue::fromValue and
-	//        thus key for slice can be found
-	using SliceDict = std::map<std::pair<llvm::Value*, uint64_t>, std::vector<llvm::Instruction*>>;
-
 protected:
 	llvm::SmallSetVector<llvm::Instruction*, 16> WorkList; // main worklist containing instructions which are suspected to be dead
 	llvm::TargetLibraryInfo *TLI;
-	SliceDict *slices; // temporary dictionary to speed up lookup of bit vector slices
 	bool DCEInstruction(llvm::Instruction *I, llvm::BasicBlock::iterator &curI);
-	bool dbgAssertConsistencyAfterEachChange;
 public:
-	/*
-	 * :param slices: optional dictionary which is used to speedup bit vector slice lookup (it may be updated by this class)
-	 * */
-	DceWorklist(llvm::TargetLibraryInfo *TLI, SliceDict *slices,
-			bool dbgAssertConsistencyAfterEachChange = false) :
-			TLI(TLI), slices(slices), dbgAssertConsistencyAfterEachChange(
-					dbgAssertConsistencyAfterEachChange) {
+	DceWorklist(llvm::TargetLibraryInfo *TLI) :
+			TLI(TLI) {
 	}
-	SliceDict* getSliceDict();
 	bool empty() const;
 	void insert(llvm::Instruction &I);
 	bool tryRemoveIfDead(llvm::Instruction &I,
 			llvm::BasicBlock::iterator &curI);
 	bool runToCompletition(llvm::BasicBlock::iterator &curIt);
 	bool runToCompletition();
-	void erraseFromSlices(hwtHls::OffsetWidthValue sliceItem,
-			llvm::Instruction &I);
-	void updateSlicesBeforeReplace(llvm::Instruction &I,
-			llvm::Value &replacement);
-	void assertSlicesConsistency() const;
+	llvm::SmallSetVector<llvm::Instruction*, 16>& getWorkList();
 };
 
 }
