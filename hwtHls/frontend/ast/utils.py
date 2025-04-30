2d829e9dcf8d20654dfe1db6a3c0955a1e7816e4
Nic30 <nic30@seznam.cz>
2025-04-30 16:28:49 +0200

    feat(frontend): _getNativeInterfaceWordType support void

--- hwtHls/frontend/ast/utils.py
+++ hwtHls/frontend/ast/utils.py
@@ -25,15 +25,16 @@ ANY_SCALAR_INT_VALUE = Union[RtlSignal, HConst, HwIOSignal, Value]
 def _getNativeInterfaceWordType(i: HwIO) -> HdlType:
     if isinstance(i, (HwIODataRdVld, Axi_hs, HwIOStructRdVld, HwIORdVldSync)):
         w = i._bit_length() - 2
-        if w == 0:
-            return HVoidExternData
-        return HBits(w)
+
     elif isinstance(i, (HwIODataRd, HwIODataVld)):
         w = i._bit_length() - 1
-        if w == 0:
-            return HVoidExternData
-        return HBits(w)
+
     elif isinstance(i, (HwIOSignal, RtlSignal, HwIOStruct)):
         return i._dtype
     else:
         raise NotImplementedError(i)
+
+    if w == 0:
+        return HVoidExternData
+
+    return HBits(w)
