1157943caf13a03a97382d206d16c96fb83ebec2
Nic30 <nic30@seznam.cz>
2025-05-01 13:35:49 +0200

    test(BaseLlvmIrTC): support for streaio and fp intrinsics

--- tests/llvmIr/baseLlvmIrTC.py
+++ tests/llvmIr/baseLlvmIrTC.py
@@ -1,37 +1,106 @@
-from itertools import takewhile
+from itertools import takewhile, chain
 import os
 import re
 
 from hwtHls.llvm.llvmIr import LlvmCompilationBundle, SMDiagnostic, parseIR, Function, verifyModule
 from tests.baseSsaTest import BaseSsaTC
+from typing import List
 
-RE_HWTHLS_FN_CALL = re.compile(r'call (i[0-9]+) @hwtHls.((bitRangeGet)|(bitConcat))((\.i?[0-9]+)+)\(.*\)( #(\d+))')
+RE_HWTHLS_FN_CALL = re.compile(r'call (i[0-9]+|void|double) '  # return type
+                               r'@hwtHls.('
+                                   r'bitRangeGet|bitConcat|'
+                                   r'streamWrite|streamWriteStartOfFrame|streamWriteEndOfFrame|streamWrite\.masked|'
+                                   r'streamRead|streamReadStartOfFrame|streamReadEndOfFrame|'
+                                   r'fp\.castToHFloatTmp|'
+                                   r'fp\.castFromHFloatTmp'
+                               r')'  # fn name stem
+                               r'((\.(i?[0-9]+|p[0-9]+|isVoid|double))+)'  # '.' separated arg types in function names
+                               r'\(.*\)'  # args ignored
+                               r'( #(\d+))'  # attribute id after definition
+                               )
+
+
+def _formatPointerTypeShortAsNormal(ptrType: str):
+    "p2 ->  ptr addrspace(2)"
+    assert ptrType.startswith("p"), ptrType
+    return f"ptr addrspace({ptrType[1:]:s})"
+
+
+def _formatParamsForStreamFnDeclaration(argTy: List[str]):
+    return ", ".join(f"{t:s} %{i}" if i > 0 else _formatPointerTypeShortAsNormal(t) for i, t in enumerate(argTy))
 
 
 def generateAndAppendHwtHlsFunctionDeclarations(llvmIrStr:str):
     indent = "".join(takewhile(lambda x: str.isspace(x) and x != '\n', llvmIrStr))
     declarations = set()
+    hasStreamFns = False
+    hasFpFns = False
+    hfloatTmpConfigArgTypes = ["i1", "i8", "i8", "i1", "i1", "i1", "i1", "i1", "i1", "i8", "i8"]
     for fn in RE_HWTHLS_FN_CALL.findall(llvmIrStr):
         retTy = fn[0]
         fnName = fn[1]
-        _argTy = fn[4]
-        assert fn[7] == "2", (fn[7], "@hwtHls.(bitRangeGet)|(bitConcat) must have memory attribute #2 otherwise it will not be reduced correctly")
+        _argTy = fn[2]
         argTy = _argTy.split(".")
         assert argTy[0] == ""
         argTy = argTy[1:]
         if fnName == "bitRangeGet":
+            assert fn[6] == "2", (fn[6], "@hwtHls.bitRangeGet call must have memory attribute #2 otherwise it will not be reduced correctly")
             #  %ret = call i16 @hwtHls.bitRangeGet.i19.i6.i16.0(i19 %1, i6 0) #2
             assert len(argTy) == 2 + 1 + 1, (fn, argTy)
             assert argTy[-2] == retTy, ("wrong bitRangeGet return type", argTy[-2], "!=", retTy)
             declarations.add(f"{indent:s}declare {retTy:s} @hwtHls.bitRangeGet{_argTy:s}({argTy[0]:s} %0, {argTy[1]:s} %1) #1")
         elif fnName == "bitConcat":
+            assert fn[6] == "2", (fn[6], "@hwtHls.bitConcat call must have memory attribute #2 otherwise it will not be reduced correctly")
             # %ret = call i10 @hwtHls.bitConcat.i8.i1.i1(i8 %1, i1 %2, i1 %3) #2
-            args = ", ".join(f"{t:s} %{i}" for i, t in enumerate(argTy))
-            declarations.add(f"{indent:s}declare {retTy:s} @hwtHls.bitConcat{_argTy:s}({args:s}) #1")
+            params = ", ".join(f"{t:s} %{i}" for i, t in enumerate(argTy))
+            declarations.add(f"{indent:s}declare {retTy:s} @hwtHls.bitConcat{_argTy:s}({params:s}) #1")
+        elif fnName in {"streamWrite", "streamWriteStartOfFrame", "streamWriteEndOfFrame", "streamWrite.masked",
+                        "streamRead", "streamReadStartOfFrame", "streamReadEndOfFrame"}:
+            # call void @hwtHls.streamWriteStartOfFrame.p2(ptr addrspace(2) %tx) #4
+            # call void @hwtHls.streamWriteEndOfFrame.p2(ptr addrspace(2) %tx) #4
+            # call void @hwtHls.streamWrite.p2.i8.i1(ptr addrspace(2) %tx, i8 %data, i1 %eof) #4
+            # call void @hwtHls.streamWrite.masked.p2.i16.i2.i1(ptr addrspace(2) %tx, i16 %data, i2 %mask, i1 %eof) #4
+            # %0 = call i10 @hwtHls.streamRead.p2.i64.i10(ptr addrspace(2) %i, i64 8) #4
+            assert fn[6] == "4", (fn[6], "@hwtHls.", fnName, " call must have memory attribute #4")
+            hasStreamFns = True
+            if fnName == "streamRead":
+                assert argTy[-1] == retTy, (fn, argTy[-1], retTy)
+                argTy = argTy[:-1]
+            params = _formatParamsForStreamFnDeclaration(argTy)
+            # declare void @hwtHls.streamWrite.p2.isVoid(ptr addrspace(2), i8, i1) #0
+            declarations.add(f"{indent:s}declare {retTy:s} @hwtHls.{fnName:s}{_argTy:s}({params:s}) #3")
 
-    atts = (f"{indent:s}attributes #1 = {{ nofree nounwind speculatable willreturn }}\n"
-            f"{indent:s}attributes #2 = {{ memory(none) }}")
-    return "\n".join([llvmIrStr ] + sorted(declarations) + ([atts] if declarations else []))
+        elif fnName.startswith("fp."):
+            # declare double @hwtHls.fp.castToHFloatTmp.i5(i5, i1, i8, i8, i1, i1, i1, i1, i1, i1, i8, i8) #5
+            # declare i5 @hwtHls.fp.castFromHFloatTmp.i5(double, i1, i8, i8, i1, i1, i1, i1, i1, i1, i8, i8) #5
+            hasFpFns = True
+            if fnName == "fp.castToHFloatTmp":
+                assert retTy == "double", (retTy, fn)
+            elif fnName == "fp.castFromHFloatTmp":
+                assert argTy[0] == retTy, (argTy[0], fn)
+                argTy[0] = "double"
+            params = ", ".join(f"{t:s} %{i}" for i, t in enumerate(chain(argTy, hfloatTmpConfigArgTypes)))
+            declarations.add(f"{indent:s}declare {retTy:s} @hwtHls.{fnName:s}{_argTy:s}({params:s}) #5")
+
+    buff = [llvmIrStr]
+    buff.extend(sorted(declarations))
+    if declarations:
+        atts = (
+            f"{indent:s}attributes #1 = {{ nofree nounwind speculatable willreturn }}\n"  # for function itself
+            f"{indent:s}attributes #2 = {{ memory(none) }}"
+        )
+        buff.append(atts)
+    if  hasStreamFns:
+        attrs = (
+            f"{indent:s}attributes #3 = {{ nofree nounwind willreturn }}\n"
+            f"{indent:s}attributes #4 = {{ memory(argmem: readwrite) }}"
+        )
+        buff.append(attrs)
+    if hasFpFns:
+        buff.append(
+            f"{indent:s}attributes #5 = {{ nofree nounwind willreturn }}"
+        )
+    return "\n".join(buff)
 
 
 class BaseLlvmIrTC(BaseSsaTC):
