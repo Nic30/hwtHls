c9cec5c890a20703b45aa3fda268e3a9e225c2a8
Nic30 <nic30@seznam.cz>
2025-05-01 16:35:54 +0200

    fix(tests): update HlsDebugBundle references

--- tests/bitOpt/andShiftInLoop_test.py
+++ tests/bitOpt/andShiftInLoop_test.py
@@ -169,7 +169,7 @@ class AndShiftInLoop_TC(SimTestCase):
 
 if __name__ == "__main__":
     from hwt.synth import to_rtl_str
-    from hwtHls.platform.platform import HlsDebugBundle
+    from hwtHls.platform.debugBundle import HlsDebugBundle
     dut = AndShiftInLoop2()
     print(to_rtl_str(dut, target_platform=VirtualHlsPlatform(debugFilter=HlsDebugBundle.ALL_RELIABLE)))
 
--- tests/bitOpt/cmpReduction.py
+++ tests/bitOpt/cmpReduction.py
@@ -40,7 +40,7 @@ class RedundantCmpGT(HwModule):
 if __name__ == "__main__":
     from hwt.synth import to_rtl_str
     from hwtHls.platform.virtual import VirtualHlsPlatform
-    from hwtHls.platform.platform import HlsDebugBundle
+    from hwtHls.platform.debugBundle import HlsDebugBundle
 
     m = RedundantCmpGT()
     print(to_rtl_str(m, target_platform=VirtualHlsPlatform(debugFilter=HlsDebugBundle.ALL_RELIABLE)))
