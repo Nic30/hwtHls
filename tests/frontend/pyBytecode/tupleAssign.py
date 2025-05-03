1ffc0c7a423ae21819cf7225ba20758ff275da30
Nic30 <nic30@seznam.cz>
2025-05-03 14:35:40 +0200

    test: more tests for tuple assignment

--- tests/frontend/pyBytecode/tupleAssign.py
+++ tests/frontend/pyBytecode/tupleAssign.py
@@ -1,5 +1,8 @@
-from hwt.hdl.types.defs import BIT
-from hwt.hwIOs.std import HwIOVectSignal
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from hwt.hdl.commonConstants import b1
+from hwt.hwIOs.std import HwIOVectSignal, HwIOSignal
 from hwt.hwIOs.utils import addClkRstn
 from hwt.hwModule import HwModule
 from hwt.pyUtils.typingFuture import override
@@ -24,7 +27,7 @@ class HlsPythonTupleAssign(HwModule):
         i0 = uint8_t.from_py(0)
         i1 = uint8_t.from_py(1)
 
-        while BIT.from_py(1):
+        while b1:
             hls.write(i0, self.o0)
             hls.write(i1, self.o1)
             # i0, i1 = i0, i1
@@ -39,12 +42,47 @@ class HlsPythonTupleAssign(HwModule):
         hls.compile()
 
 
+class HlsPythonSwap(HwModule):
+
+    @override
+    def hwDeclr(self):
+        addClkRstn(self)
+        self.swap = HwIOSignal()
+        self.i0 = HwIOVectSignal(8)
+        self.i1 = HwIOVectSignal(8)
+        self.o0 = HwIOVectSignal(8)._m()
+        self.o1 = HwIOVectSignal(8)._m()
+
+    @hlsBytecode
+    def mainThread(self, hls: HlsScope):
+
+        while b1:
+            swap = hls.read(self.swap).data
+            i0 = hls.read(self.i0).data
+            i1 = hls.read(self.i1).data
+
+            # i0, i1 = i0, i1
+            if swap:
+                copy = PyBytecodePreprocHwCopy
+                i1, i0 = copy(i0), copy(i1)
+
+            hls.write(i0, self.o0)
+            hls.write(i1, self.o1)
+
+    @override
+    def hwImpl(self):
+        HlsPythonTupleAssign.hwImpl(self)
+
+
 class HlsPythonTupleAssign_TC(BaseSsaTC):
     __FILE__ = __file__
     TEST_BLOCK_SYNC = False
 
     def test_HlsPythonTupleAssign_ll(self):
         self._test_ll(HlsPythonTupleAssign)
+
+    def test_HlsPythonSwap_ll(self):
+        self._test_ll(HlsPythonSwap)
 
 
 if __name__ == "__main__":
@@ -58,8 +96,8 @@ if __name__ == "__main__":
     import unittest
 
     testLoader = unittest.TestLoader()
-    # suite = unittest.TestSuite([HlsPythonTupleAssign_TC("test_frameHeader")])
-    suite = testLoader.loadTestsFromTestCase(HlsPythonTupleAssign_TC)
+    suite = unittest.TestSuite([HlsPythonTupleAssign_TC("test_HlsPythonSwap_ll")])
+    # suite = testLoader.loadTestsFromTestCase(HlsPythonTupleAssign_TC)
     runner = unittest.TextTestRunner(verbosity=3)
     runner.run(suite)
 
