c317e9b8def26344cff697b38acd7f95748c28f3
Nic30 <nic30@seznam.cz>
2025-05-24 18:30:47 +0200

    feat(fixp): atan2 fn

---
+++ tests/math/componentGenerators/fatan2.py
@@ -0,0 +1,233 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from hwt.hdl.types.bits import HBits
+from hwt.hdl.types.bitsRtlSignal import HBitsRtlSignal
+from hwt.hdl.types.struct import HStruct
+from hwt.hwIOs.utils import addClkRstn
+from hwt.hwParam import HwParam
+from hwt.pyUtils.setList import SetList
+from hwt.pyUtils.typingFuture import override
+from hwt.serializer.mode import serializeParamsUniq
+from hwtHls.architecture.componentGenerator import ComponentGenerator
+from hwtHls.architecture.componentGeneratorUtils import \
+    ComponentGenerator_replaceHlsNetNodeOperatorWithHwModule
+from hwtHls.architecture.componentGenerators.baseALU1HwModule import _BaseALU1HwModule
+from hwtHls.frontend.pyBytecode import hlsBytecode
+from hwtHls.frontend.pyBytecode.pragmaPreproc import PyBytecodeInline
+from hwtHls.llvm.llvmIr import HFloatTmpConfig, HFloatTmpRounding, HFloatTmpSaturation
+from hwtHls.netlist.nodes.node import HlsNetNode
+from hwtHls.netlist.nodes.ops import HlsNetNodeOperator
+from hwtHls.platform.opRealizationMeta import OpRealizationMeta
+from tests.math.componentGenerators.fsincos import FixpSinCosCordic
+from tests.math.fixp.cordicAtan2 import CordicAtan2
+from tests.math.fixp.fixpTypes import HFixedPointQ
+
+# maybe useful:
+# * approx version https://doi.org/10.1109/UPCON56432.2022.9986456
+
+@serializeParamsUniq
+class FixpAtan2HypotCordic(FixpSinCosCordic):
+
+    @override
+    def hwConfig(self) -> None:
+        FixpSinCosCordic.hwConfig(self)
+        self.HAS_ATAN2 = HwParam(True)
+        self.HAS_HYPOT = HwParam(False)
+
+    @override
+    def hwDeclr(self) -> None:
+        addClkRstn(self)
+        T = self.T
+        assert T.rounding == HFloatTmpRounding.ROUND_FLOOR, (T.rounding, "rounding on input should be disabled (only output is rounded)")
+        assert T.saturation == HFloatTmpSaturation.SATURATE_NONE, T.rounding
+
+        t = HBits(T.bit_length())
+        outputFields = []
+        if self.HAS_ATAN2:
+            outputFields.append((t, "atan2"))
+        if self.HAS_HYPOT:
+            outputFields.append((t, "hypot"))
+        _BaseALU1HwModule._addDataInDataOut(self,
+                                             HStruct((t, "y"), (t, "x")),
+                                             HStruct(*outputFields))
+        if self.ITERATION_COUNT is None:
+            self.ITERATION_COUNT = self._getMaxIterationCount()
+        else:
+            assert self.ITERATION_COUNT <= self._getMaxIterationCount(), (
+                self, self.ITERATION_COUNT, self._getMaxIterationCount())
+        assert self.STAGES_IN_LUT == 0, self.STAGES_IN_LUT
+
+    @staticmethod
+    def getAtan2Function(cordic: CordicAtan2):
+        return cordic.atan2
+
+    @override
+    @hlsBytecode
+    def aluFn(self, _inp: HBitsRtlSignal):
+        T = self.T
+        cordic = CordicAtan2(self.ITERATION_COUNT, loopPragmaGetter=lambda: _BaseALU1HwModule._getLoopMeta(self))
+
+        inpY = _inp.y._reinterpret_cast(T)
+        inpX = _inp.x._reinterpret_cast(T)
+        res = PyBytecodeInline(self.getAtan2Function(cordic))(inpY, inpX)
+        resTmp = self._getTypeOfIo(self.data_out).from_py(None)
+        if self.HAS_ATAN2:
+            resTmp.atan2 = res[0]._auto_cast(T)._reinterpret_cast(resTmp.atan2._dtype)
+        if self.HAS_HYPOT:
+            resTmp.hypot = res[1]._auto_cast(T)._reinterpret_cast(resTmp.hypot._dtype)
+        return resTmp
+
+
+class ComponentGeneratorFATAN2HYPOT(ComponentGenerator):
+
+    def __init__(self, platform:"DefaultHlsPlatform", hasAtan2:bool, hasHypot:bool,
+                 genNamePrefix:str, moduleName:str,
+                 optThroughputVsArea=0.0,
+                 FIXP_HWMODULE_CLS=FixpAtan2HypotCordic):
+        ComponentGenerator.__init__(self, platform, genNamePrefix, moduleName)
+        # dataWidth -> scheduling
+        self.schedulingCache: dict[tuple[float, HFloatTmpConfig], tuple[OpRealizationMeta, int, int]]
+        self._hasAtan2 = hasAtan2
+        self._hasHypot = hasHypot
+        self.optThroughputVsArea = optThroughputVsArea
+        self.FIXP_HWMODULE_CLS = FIXP_HWMODULE_CLS
+        assert hasAtan2 or hasHypot
+
+    def _getConfiguredFixpHwModule(self,
+                                   realTimeClkPeriod:float,
+                                   ty:HFixedPointQ,
+                                   UNROLL_FACTOR:int,
+                                   realization:OpRealizationMeta):
+        hwModule = self.FIXP_HWMODULE_CLS()
+        hwModule.T = ty
+        hwModule.CLK_FREQ = int(1 / realTimeClkPeriod)
+        hwModule.UNROLL_FACTOR = UNROLL_FACTOR
+        hwModule.HAS_ATAN2 = self._hasAtan2
+        hwModule.HAS_HYPOT = self._hasHypot
+        if realization is not None:
+            hwModule._setIoChannelTypes(realization)
+
+        return hwModule
+
+    @override
+    def resolveRealizationOfNode(self, node:HlsNetNodeOperator) -> None:
+        assert len(node.dependsOn) == 2, node
+        return self.resolveRealizationForHlsNetlist(node.netlist, node.operatorSpecialization)
+
+    def resolveRealizationForHlsNetlist(self, netlist: "HlsNetlistCtx", cfg: HFloatTmpConfig) -> None:
+        cacheKey = (cfg, self.optThroughputVsArea)
+        try:
+            return self.schedulingCache[cacheKey][0]
+        except KeyError:
+            pass
+
+        if cfg.isInQFormat:
+            if cfg.hasIs0 or cfg.hasIs1 or cfg.hasIsInf or cfg.hasIsNaN:
+                raise NotImplementedError()
+
+            ty = HFixedPointQ.fromHFloatTmpConfig(cfg)
+            maxIterations = self.FIXP_HWMODULE_CLS._getMaxIterationCountForTy(ty)
+            if self.optThroughputVsArea == 0:
+                UNROLL_FACTOR = 1
+            elif self.optThroughputVsArea == 1.0:
+                UNROLL_FACTOR = maxIterations
+            else:
+                raise NotImplementedError()
+
+            # run compilation of IntDiv HwModule to resolve scheduling properties
+            hwModule = self._getConfiguredFixpHwModule(netlist.realTimeClkPeriod, ty, UNROLL_FACTOR, None)
+            _, r = self.resolveRealizationOfNode_compileToResolveScheduling(
+                netlist.parentHwModule, hwModule,
+                netlist.dbgSubmoduleBuidTracer, cacheKey, (UNROLL_FACTOR,))
+            return r
+        else:
+            raise NotImplementedError()
+
+    @override
+    def toHwtCompatibleOperatorAfterScheduling(self, node:"HlsNetNode", worklist: SetList[HlsNetNode]) -> bool:
+        freq = node.netlist.realTimeClkPeriod
+        cfg: HFloatTmpConfig = node.operatorSpecialization
+        if cfg.isInQFormat:
+            if cfg.hasIs0 or cfg.hasIs1 or cfg.hasIsInf or cfg.hasIsNaN:
+                raise NotImplementedError()
+
+            cacheKey = (cfg, self.optThroughputVsArea)
+            realization, UNROLL_FACTOR = self.schedulingCache[cacheKey]
+            hwModule = self._getConfiguredFixpHwModule(freq, HFixedPointQ.fromHFloatTmpConfig(cfg),
+                                                       UNROLL_FACTOR, realization)
+            if self._hasAtan2 and self._hasHypot:
+                outputsBitMap = None
+            elif self._hasAtan2:
+                outputsBitMap = None  # cos starts at the bit 0
+            elif self._hasHypot:
+                outputsBitMap = (cfg.getBitWidth(),)  # remainder starts after quotient
+            else:
+                raise AssertionError("sincos component must be configured as a cos or sin (or both)")
+
+            ComponentGenerator_replaceHlsNetNodeOperatorWithHwModule(
+                self,
+                node, hwModule, worklist, outputsBitMap=outputsBitMap)
+            return True
+
+        else:
+            raise NotImplementedError()
+
+        return True
+
+    @override
+    def toRtlForNode(self, node:"HlsNetNode", allocator:"ArchElement") -> None:
+        raise NotImplementedError("This should have been lowered before in toHwtCompatibleOperatorAfterScheduling", node)
+
+
+@serializeParamsUniq
+class FixpAtan2HypotCordicPi(FixpAtan2HypotCordic):
+
+    @staticmethod
+    def getAtan2Function(cordic: CordicAtan2):
+        return cordic.atan2pi
+
+
+class ComponentGeneratorFATAN2HYPOT_PI(ComponentGeneratorFATAN2HYPOT):
+
+    def __init__(self, platform:"DefaultHlsPlatform", hasAtan2:bool, hasHypot:bool,
+                 genNamePrefix:str, moduleName:str,
+                 optThroughputVsArea=0.0,
+                 optMaxStagesInLut=10,
+                 FIXP_HWMODULE_CLS=FixpAtan2HypotCordicPi):
+        super().__init__(platform, hasAtan2, hasHypot, genNamePrefix, moduleName,
+                         optThroughputVsArea=optThroughputVsArea,
+                         optMaxStagesInLut=optMaxStagesInLut,
+                         FIXP_HWMODULE_CLS=FIXP_HWMODULE_CLS)
+
+
+if __name__ == "__main__":
+    from hwt.synth import to_rtl_str
+    from hwtHls.platform.virtual import VirtualHlsPlatform
+    from hwtHls.platform.debugBundle import HlsDebugBundle, LLVM_CLI_COMMON_OPTS
+    from hwtHls.platform.xilinx.artix7 import Artix7Fast
+    from tests.math.componentGenerators.install import installFpComponentGenerators
+
+    m = FixpAtan2HypotCordic()
+    m.T = HFixedPointQ(4, 4, True, rounding=HFloatTmpRounding.ROUND_FLOOR, saturation=HFloatTmpSaturation.SATURATE_NONE)
+    m.UNROLL_FACTOR = m._getMaxIterationCount()
+    m.CLK_FREQ = int(100e6)
+    platform = Artix7Fast(debugFilter=HlsDebugBundle.ALL_RELIABLE,
+                          llvmCliArgs=[
+                              # LLVM_CLI_COMMON_OPTS.filterPrintFuncs(["FixpCosSinCordic.mainThread",]),
+                              # LLVM_CLI_COMMON_OPTS.DEBUG_PASS_MANAGER,
+                              # LLVM_CLI_COMMON_OPTS.DEBUG_PASS_ARGUMENTS,
+                              # LLVM_CLI_COMMON_OPTS.PRINT_BEFORE_ALL,
+                              # LLVM_CLI_COMMON_OPTS.PRINT_CHANGED,
+                          ]
+                          )
+    installFpComponentGenerators(platform)
+    print(to_rtl_str(m, target_platform=platform))
+
+    # import unittest
+
+    # testLoader = unittest.TestLoader()
+    # # suite = unittest.TestSuite([DivRestoring_TC('test_div_py')])
+    # suite = testLoader.loadTestsFromTestCase(SinCosCordicTC)
+    # runner = unittest.TextTestRunner(verbosity=3)
+    # runner.run(suite)
---
+++ tests/math/fixp/cordicAtan2.py
@@ -0,0 +1,174 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import math
+from typing import Union
+
+from hwt.hdl.types.bits import HBits
+from hwt.hdl.types.bitsConst import HBitsConst
+from hwt.hdl.types.bitsRtlSignal import HBitsRtlSignal
+from hwtHls.frontend.pyBytecode import hlsBytecode
+from hwtHls.frontend.pyBytecode.pragmaPreproc import PyBytecodeInline
+from tests.math.fixp.cordicHybridLut import ANY_FP_VALUE, Cordic, \
+    CORDIC_COORDINATE_MODE, CORDIC_MODE
+from tests.math.hFloatTmp.hFloatTmp import HFloatTmp
+
+
+class CordicAtan2(Cordic):
+    """
+    atan2 is a mathematical function which calculates a phase from polar coordinates
+    x represents real part and y imaginary part of the vecotor,
+    result of the atan2 is an agle, but register x of cordic implementation also computes
+    the length of the vector.
+
+    https://www.mathworks.com/matlabcentral/fileexchange/19316-fixed-point-atan2-using-cordic    
+    * https://github.com/l3VGV/cordic_atan2/blob/master/main.c
+    * alternative version with multiplication https://github.com/MartinStokroos/fastCORDIC/blob/master/src/cordic.cpp
+    * alternative aprox version with div https://github.com/l3VGV/fxpt_atan2/blob/master/main.c#L49
+    * alternative version with https://github.com/stefan-zobel/speedy-math/blob/master/src/main/java/math/fast/JafamaFastMath.java#L413
+    
+    :note: Seemingly simple variant described in following article does not work.
+        An Overview of the Atan2 Cordic Implementation in Digital Hardware,
+        Document: 94, www.signal-processing.net, Andreas Schwarzinger Date: November 16, 2019
+        http://signal-processing.net/PDF/Doc%2051%20-%20Atan2%20Cordic%20Algorithm.pdf
+    """
+
+    def __init__(self, ITERATION_COUNT:int, STAGES_IN_LUT:int=0, loopPragmaGetter=lambda:None):
+        if STAGES_IN_LUT:
+            raise NotImplementedError()
+        Cordic.__init__(self, ITERATION_COUNT, STAGES_IN_LUT=STAGES_IN_LUT, loopPragmaGetter=loopPragmaGetter)
+
+    @hlsBytecode
+    @staticmethod
+    def getQuadrant(x: ANY_FP_VALUE, y: ANY_FP_VALUE) -> Union[HBitsRtlSignal, HBitsConst]:
+        qudrantT = HBits(2)
+        t = qudrantT.from_py
+        quadrant = t(None)
+        if (x >= 0.) & (y >= 0.):
+            quadrant = t(0)
+        elif (x < 0.) & (y >= 0.):
+            quadrant = t(1)
+        elif (x < 0.) & (y < 0.):
+            quadrant = t(2)
+        elif (x >= 0.) & (y < 0.):
+            quadrant = t(3)
+        return quadrant
+
+    @hlsBytecode
+    @staticmethod
+    def mapToQuadrant0(x: ANY_FP_VALUE, y: ANY_FP_VALUE, quadrant: Union[HBitsRtlSignal, HBitsConst])\
+            -> tuple[ANY_FP_VALUE, ANY_FP_VALUE]:
+        xOut = x
+        yOut = y
+        if quadrant._eq(0):
+            pass
+        elif quadrant._eq(1):
+            xOut = y
+            yOut = -x
+        elif quadrant._eq(2):
+            xOut = -x
+            yOut = -y
+        else:
+            xOut = -y
+            yOut = x
+
+        return xOut, yOut
+
+    # def runCordicRotationAtan2(self, y: ANY_FP_VALUE, x: ANY_FP_VALUE) -> tuple[ANY_FP_VALUE, ANY_FP_VALUE]:
+    #    """
+    #    based on:
+    #    An Overview of the Atan2 Cordic Implementation in Digital Hardware,
+    #    Document: 94, www.signal-processing.net, Andreas Schwarzinger Date: November 16, 2019
+    #    http://signal-processing.net/PDF/Doc%2051%20-%20Atan2%20Cordic%20Algorithm.pdf
+    #     """
+    #    iterations = self.ITERATION_COUNT
+    #    _f = HFloatTmp.from_py
+    #    total_rotation = _f(0.0)
+    #    for _i in hwrange(iterations):
+    #        i = _i._zext(_i._dtype.bit_length() + 2)._cast_sign(True)
+    #        shift = _f(2.) ** -i
+    #        x_shifted = x * shift
+    #        y_shifted = y * shift
+    #        x_new = HFloatTmp.from_py(None)
+    #        y_new = HFloatTmp.from_py(None)
+    #
+    #        # yShiftedLeft = y * (HFloatTmp.from_py(2.) ** i)
+    #        # print("cmp", float(yShiftedLeft), float(x))
+    #        # print("cmp", int(i), float(y), float(x_shifted))
+    #        # if yShiftedLeft >= x:  # check if vector is large enough for this stage
+    #        if y >= x_shifted:
+    #            # print("runCordicRotationAtan2", _i)
+    #            x_new = x + y_shifted
+    #            y_new = y - x_shifted
+    #            atanDiff = math.atan(-HFloatTmp.from_py(2.) ** -i)
+    #            print("atanDiff:", int(i), (float(atanDiff) / math.pi) * 180, float(atanDiff))
+    #            total_rotation = -atanDiff
+    #        else:
+    #            x_new = x
+    #            y_new = y
+    #
+    #        x, y = x_new, y_new
+    #
+    #    # x == sqrt(x**2+y**2) == length(vec2(y, x)) = math.hypot(y, x)
+    #    return total_rotation
+
+    @hlsBytecode
+    @staticmethod
+    def undoQadrantRemap(total_rotation: ANY_FP_VALUE,
+                       quadrant: Union[HBitsRtlSignal, HBitsConst],
+                       isZero: Union[bool, HBitsRtlSignal, HBitsConst]) -> ANY_FP_VALUE:
+        """
+        Final normalization based on quadrant and total rotation
+        """
+        if isZero:
+            total_rotation = HFloatTmp.from_py(0.0)
+        elif quadrant._eq(0):
+            pass
+        elif quadrant._eq(1):
+            total_rotation += math.pi / 2
+        elif quadrant._eq(2):
+            total_rotation -= math.pi
+        else:
+            # quadrant == 3
+            total_rotation -= math.pi / 2
+
+        return total_rotation
+
+    @hlsBytecode
+    def atan2(self, _y: ANY_FP_VALUE, _x: ANY_FP_VALUE) -> tuple[ANY_FP_VALUE, ANY_FP_VALUE]:
+        """
+        atan2 function based on Cordic alogorithm
+        """
+        if isinstance(_y, float):
+            T_INTERNAL = None
+            y = _y
+            x = _x
+            isZero = (y == 0.) & (x == 0.)
+        else:
+            T_INTERNAL = _y._dtype
+            y = _y._auto_cast(HFloatTmp)
+            x = _x._auto_cast(HFloatTmp)
+            isZero = (y._eq(0.)) & (x._eq(0.))
+
+        inline = PyBytecodeInline
+        quadrant = inline(self.getQuadrant)(x, y)
+        x0, y0 = inline(self.mapToQuadrant0)(x, y, quadrant)
+        z0 = HFloatTmp.from_py(0)
+        _thetaROM = self.getThetaROM(CORDIC_COORDINATE_MODE.CIRCULAR)
+        if T_INTERNAL is None:
+            thetaROM = _thetaROM
+        else:
+            thetaROM = HFloatTmp[len(_thetaROM)].from_py(_thetaROM)
+        # https://doi.org/10.1049/el.2017.2090
+        # z0 = 0, x0 = x, y0 = y
+        # di = −sign(yi )
+        # x_{i+1} = x_i − d_i * y_i * 2**-i
+        # y_{i+1} = y_i + d_i * x_i * 2**-i
+        # z_{i+1} = z_i − d_i * atan(2**-i)
+        vec_length, _, atan2Tmp = inline(self.runCordicSteps)(
+            CORDIC_MODE.VECTORING,
+            CORDIC_COORDINATE_MODE.CIRCULAR,
+            x0, y0, z0, 0, thetaROM, loopPragmaGetter=self.loopPragmaGetter)
+        atan2 = inline(self.undoQadrantRemap)(atan2Tmp, quadrant, isZero)
+        return atan2, vec_length
+
