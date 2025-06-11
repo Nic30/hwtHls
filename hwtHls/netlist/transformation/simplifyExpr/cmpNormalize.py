c30c50ca5ba95edd54f92d4653ab06b39ebe97a9
Nic30 <nic30@seznam.cz>
2025-06-11 10:46:26 +0200

    refactor(netlistCmpNormalize): use HlsNetlistBuilderWithWorklist

--- hwtHls/netlist/transformation/simplifyExpr/cmpNormalize.py
+++ hwtHls/netlist/transformation/simplifyExpr/cmpNormalize.py
@@ -1,20 +1,18 @@
-from typing import Set
-
 from hwt.hdl.operatorDefs import HwtOps
 from hwt.pyUtils.setList import SetList
-from hwtHls.netlist.builder import HlsNetlistBuilder
+from hwtHls.netlist.builder import HlsNetlistBuilder, \
+    HlsNetlistBuilderWithWorklist
 from hwtHls.netlist.nodes.const import HlsNetNodeConst
 from hwtHls.netlist.nodes.node import HlsNetNode
 from hwtHls.netlist.nodes.ops import HlsNetNodeOperator
 from hwtHls.netlist.transformation.simplifyUtilsHierarchyAware import replaceOperatorNodeWith
-
 
 _DENORMALIZED_CMP_OPS = {HwtOps.NE, HwtOps.UGT, HwtOps.UGE, HwtOps.SGT, HwtOps.SGE}
 _NORMALIZED_CMP_OPS = {HwtOps.EQ, HwtOps.ULT, HwtOps.ULE, HwtOps.SLT, HwtOps.SLE}
 
 
 def netlistCmpNormalize(n: HlsNetNodeOperator, worklist: SetList[HlsNetNode]):
-    b: HlsNetlistBuilder = n.getHlsNetlistBuilder()
+    b: HlsNetlistBuilder = HlsNetlistBuilderWithWorklist(n.getHlsNetlistBuilder(), worklist)
     op = n.operator
     op0, op1 = n.dependsOn
     if op in _NORMALIZED_CMP_OPS:
@@ -22,20 +20,18 @@ def netlistCmpNormalize(n: HlsNetNodeOperator, worklist: SetList[HlsNetNode]):
 
     t = n._outputs[0]._dtype
     assert not t.signed, ("Signed should not be used internally")
-    #raise NotImplementedError("UGT not correct")
+    # raise NotImplementedError("UGT not correct")
     newO = None
     # :note: const can not be first operand because it denormalizes the expression
     if op is HwtOps.NE:
         # a != b -> ~(a == b)
         eq = b.buildOp(HwtOps.EQ, None, t, op0, op1)
-        worklist.append(eq.obj)
         newO = b.buildNot(eq)
 
     elif op is HwtOps.UGT:
         if isinstance(op1.obj, HlsNetNodeConst):
             # a > b -> ~(a <= b)
             le = b.buildOp(HwtOps.ULE, None, t, op0, op1)
-            worklist.append(le.obj)
             newO = b.buildNot(le)
         else:
             # a > b -> b < a
@@ -45,7 +41,6 @@ def netlistCmpNormalize(n: HlsNetNodeOperator, worklist: SetList[HlsNetNode]):
         if isinstance(op1.obj, HlsNetNodeConst):
             # a > b -> ~(a <= b)
             le = b.buildOp(HwtOps.SLE, None, t, op0, op1)
-            worklist.append(le.obj)
             newO = b.buildNot(le)
         else:
             # a > b -> b < a
@@ -55,7 +50,6 @@ def netlistCmpNormalize(n: HlsNetNodeOperator, worklist: SetList[HlsNetNode]):
         if isinstance(op1.obj, HlsNetNodeConst):
             # a >= b -> ~(a < b)
             lt = b.buildOp(HwtOps.ULT, None, t, op0, op1)
-            worklist.append(lt.obj)
             newO = b.buildNot(lt)
         else:
             # a >=- b -> b <= a
@@ -65,14 +59,12 @@ def netlistCmpNormalize(n: HlsNetNodeOperator, worklist: SetList[HlsNetNode]):
         if isinstance(op1.obj, HlsNetNodeConst):
             # a >= b -> ~(a < b)
             lt = b.buildOp(HwtOps.SLT, None, t, op0, op1)
-            worklist.append(lt.obj)
             newO = b.buildNot(lt)
         else:
             # a >=- b -> b <= a
             newO = b.buildOp(HwtOps.SLE, None, t, op1, op0)
 
     if newO is not None:
-        worklist.append(newO.obj)
         replaceOperatorNodeWith(n, newO, worklist)
         return True
 
