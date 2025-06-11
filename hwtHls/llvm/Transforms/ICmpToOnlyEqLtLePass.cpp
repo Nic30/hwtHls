fb46775efdeea3c1b33dd43570170d3455455f6a
Nic30 <nic30@seznam.cz>
2025-06-11 10:49:06 +0200

    feat(llvm.ir.ICmpToOnlyEqLtLePass): normal. range checks without add/sub

--- hwtHls/llvm/Transforms/ICmpToOnlyEqLtLePass.cpp
+++ hwtHls/llvm/Transforms/ICmpToOnlyEqLtLePass.cpp
@@ -3,94 +3,127 @@
 #include <llvm/Analysis/AliasAnalysis.h>
 #include <llvm/Analysis/BasicAliasAnalysis.h>
 #include <llvm/Analysis/GlobalsModRef.h>
+#include <llvm/Analysis/TargetFolder.h>
 #include <llvm/IR/IRBuilder.h>
+#include <llvm/IR/PatternMatch.h>
 #include <algorithm>
 #include <hwtHls/llvm/targets/intrinsic/bitrange.h>
 
 using namespace llvm;
+using namespace llvm::PatternMatch;
 
 namespace hwtHls {
+
+Value* ICmpToOnlyEqLtLePass::_tryRewriteRangeCheckTo2xCmp(
+		IRBuilder<TargetFolder> &Builder, ICmpInst &CMP) {
+	Value *LHS = CMP.getOperand(0);
+	Value *RHS = CMP.getOperand(1);
+	using Pred = ICmpInst::Predicate;
+	if (CMP.getPredicate() == Pred::ICMP_ULT) {
+		if (auto c1 = dyn_cast<ConstantInt>(RHS)) {
+			if (auto LHS_I = dyn_cast<Instruction>(LHS)) {
+				Value *x;
+				ConstantInt *c0;
+				if (match(LHS_I, m_Add(m_Value(x), m_ConstantInt(c0)))) {
+					// x + c0 < c1
+					// to
+					// (x < (c1 - c0)) & (x > c0)   is smaller after offset substract, and offset substract does not underflow
+					auto newO1 = c1->getValue() - c0->getValue();
+					auto cmp = Builder.CreateICmpULT(x,
+							ConstantInt::get(c0->getType(), newO1));
+					auto overflowCheck = Builder.CreateICmpUGT(x, c0);
+					return Builder.CreateAnd(cmp, overflowCheck);
+				}
+			}
+		}
+	}
+	return nullptr;
+}
 
 PreservedAnalyses ICmpToOnlyEqLtLePass::run(llvm::Function &F,
 		llvm::FunctionAnalysisManager &AM) {
 	std::vector<Instruction*> toRemove;
+	auto &DL = F.getParent()->getDataLayout();
+	IRBuilder<TargetFolder> Builder(F.getContext(), TargetFolder(DL));
 	for (BasicBlock &BB : F) {
 		for (Instruction &I : BB) {
 			if (auto *CMP = dyn_cast<ICmpInst>(&I)) {
-				auto Name = CMP->getName();
 				Value *LHS = CMP->getOperand(0);
 				Value *RHS = CMP->getOperand(1);
+
 				Value *replacement = nullptr;
-				using Pred = ICmpInst::Predicate;
-				switch (CMP->getPredicate()) {
-				case Pred::ICMP_EQ:
-				case Pred::ICMP_ULT:
-				case Pred::ICMP_ULE:
-				case Pred::ICMP_SLT:
-				case Pred::ICMP_SLE:
-					break;
-				case Pred::ICMP_NE: { // a != b -> !(a == b)
-					IRBuilder<> Builder(&I);
-					auto eq = Builder.CreateICmpEQ(LHS, RHS);
-					replacement = Builder.CreateNot(eq, Name);
-					break;
+				Builder.SetInsertPoint(&I);
+				if (rangeCmpWithoutAdd) {
+					replacement = _tryRewriteRangeCheckTo2xCmp(Builder, *CMP);
 				}
-				case Pred::ICMP_UGT: {
-					IRBuilder<> Builder(&I);
-					if (isa<ConstantInt>(RHS)) {
-						// a > b -> ~(a <= b)
-						auto le = Builder.CreateICmpULE(LHS, RHS);
-						replacement = Builder.CreateNot(le, Name);
-					} else {
-						// a > b -> b < a
-						replacement = Builder.CreateICmpULT(RHS, LHS);
+				if (!replacement) {
+					using Pred = ICmpInst::Predicate;
+					switch (CMP->getPredicate()) {
+					case Pred::ICMP_EQ:
+					case Pred::ICMP_ULT:
+					case Pred::ICMP_ULE:
+					case Pred::ICMP_SLT:
+					case Pred::ICMP_SLE:
+						break;
+					case Pred::ICMP_NE: { // a != b -> !(a == b)
+						auto eq = Builder.CreateICmpEQ(LHS, RHS);
+						replacement = Builder.CreateNot(eq);
+						break;
 					}
-					break;
-				}
-				case Pred::ICMP_UGE: {
-					IRBuilder<> Builder(&I);
-					if (isa<ConstantInt>(RHS)) {
-						// a >= b -> ~(a < b)
-						auto lt = Builder.CreateICmpULT(LHS, RHS);
-						replacement = Builder.CreateNot(lt, Name);
-					} else {
-						//  a >=- b -> b <= a
-						replacement = Builder.CreateICmpULE(RHS, LHS);
+					case Pred::ICMP_UGT: {
+						if (isa<ConstantInt>(RHS)) {
+							// a > b -> ~(a <= b)
+							auto le = Builder.CreateICmpULE(LHS, RHS);
+							replacement = Builder.CreateNot(le);
+						} else {
+							// a > b -> b < a
+							replacement = Builder.CreateICmpULT(RHS, LHS);
+						}
+						break;
 					}
-					break;
-				}
+					case Pred::ICMP_UGE: {
+						if (isa<ConstantInt>(RHS)) {
+							// a >= b -> ~(a < b)
+							auto lt = Builder.CreateICmpULT(LHS, RHS);
+							replacement = Builder.CreateNot(lt);
+						} else {
+							//  a >=- b -> b <= a
+							replacement = Builder.CreateICmpULE(RHS, LHS);
+						}
+						break;
+					}
 
-				case Pred::ICMP_SGT: {
-					IRBuilder<> Builder(&I);
-					if (isa<ConstantInt>(RHS)) {
-						// a > b -> ~(a <= b)
-						auto le = Builder.CreateICmpSLE(LHS, RHS);
-						replacement = Builder.CreateNot(le, Name);
-					} else {
-						// a > b -> b < a
-						replacement = Builder.CreateICmpSLT(RHS, LHS);
+					case Pred::ICMP_SGT: {
+						if (isa<ConstantInt>(RHS)) {
+							// a > b -> ~(a <= b)
+							auto le = Builder.CreateICmpSLE(LHS, RHS);
+							replacement = Builder.CreateNot(le);
+						} else {
+							// a > b -> b < a
+							replacement = Builder.CreateICmpSLT(RHS, LHS);
+						}
+						break;
 					}
-					break;
-				}
-				case Pred::ICMP_SGE: {
-					IRBuilder<> Builder(&I);
-					if (isa<ConstantInt>(RHS)) {
-						// a >= b -> ~(a < b)
-						auto lt = Builder.CreateICmpSLT(LHS, RHS);
-						replacement = Builder.CreateNot(lt, Name);
-					} else {
-						//  a >=- b -> b <= a
-						replacement = Builder.CreateICmpSLE(RHS, LHS);
+					case Pred::ICMP_SGE: {
+						if (isa<ConstantInt>(RHS)) {
+							// a >= b -> ~(a < b)
+							auto lt = Builder.CreateICmpSLT(LHS, RHS);
+							replacement = Builder.CreateNot(lt);
+						} else {
+							//  a >=- b -> b <= a
+							replacement = Builder.CreateICmpSLE(RHS, LHS);
+						}
+						break;
 					}
-					break;
-				}
 
-				default:
-					I.dump();
-					llvm_unreachable("NotImplemented");
+					default:
+						I.dump();
+						llvm_unreachable("NotImplemented");
+					}
 				}
 				if (replacement) {
 					CMP->replaceAllUsesWith(replacement);
+					CMP->takeName(replacement);
 					toRemove.push_back(CMP);
 				}
 			}
--- hwtHls/llvm/Transforms/ICmpToOnlyEqLtLePass.h
+++ hwtHls/llvm/Transforms/ICmpToOnlyEqLtLePass.h
@@ -2,6 +2,12 @@
 
 #include <llvm/IR/Function.h>
 #include <llvm/IR/PassManager.h>
+#include <llvm/IR/IRBuilder.h>
+
+namespace llvm {
+class TargetFolder;
+class ICmpInst;
+}
 
 namespace hwtHls {
 
@@ -9,7 +15,13 @@ namespace hwtHls {
  *  A pass to rewrite NE(!=), GT(>), GE(>=) to EQ(==), LT(<), LE(<=) with optional NOT(x XOR -1)
  */
 class ICmpToOnlyEqLtLePass: public llvm::PassInfoMixin<ICmpToOnlyEqLtLePass> {
+	bool rangeCmpWithoutAdd;
+	llvm::Value* _tryRewriteRangeCheckTo2xCmp(
+			llvm::IRBuilder<llvm::TargetFolder> &Builder, llvm::ICmpInst &CMP);
 public:
+	ICmpToOnlyEqLtLePass(bool rangeCmpWithoutAdd = true) :
+			rangeCmpWithoutAdd(rangeCmpWithoutAdd) {
+	}
 	llvm::PreservedAnalyses run(llvm::Function &F,
 			llvm::FunctionAnalysisManager &AM);
 };
