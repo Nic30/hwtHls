df3e100c852ef50af98b1de109e279c8321c28e4
Nic30 <nic30@seznam.cz>
2025-06-24 12:28:46 +0200

    feat(platform.VirtualHlsPlatform): use ComponentGeneratorMUL_HL by def.

--- hwtHls/platform/virtual.py
+++ hwtHls/platform/virtual.py
@@ -15,9 +15,11 @@ from hwtHls.architecture.componentGenerators.ext import ComponentGeneratorZExt, 
 from hwtHls.architecture.componentGenerators.fsh import ComponentGeneratorFshl, \
     ComponentGeneratorFshr
 from hwtHls.architecture.componentGenerators.icmpNeEq import ComponentGeneratorICMP_EQ_NE
+from hwtHls.architecture.componentGenerators.mul_hl import ComponentGeneratorMUL_HL
 from hwtHls.code import OP_ASHR, OP_SHL, OP_LSHR, OP_CTLZ, OP_CTPOP, OP_CTTZ, \
     OP_BITREVERSE, OP_FSHR, OP_FSHL, OP_ROL, OP_ROR
 from hwtHls.llvm.llvmIr import HFloatTmpConfig
+from hwtHls.netlist.extraOps import OP_MUL_HL
 from hwtHls.netlist.nodes.memoryAllocationMeta import MemoryAllocationMeta
 from hwtHls.platform.debugBundleTypes import LlvmCliArgTuple
 from hwtHls.platform.opRealizationMeta import OpRealizationMeta
@@ -177,6 +179,7 @@ class VirtualHlsPlatform(DefaultHlsPlatform):
         _componentGenerators[HwtOps.SEXT] = ComponentGeneratorSExt(self, genNamePrefix, "sext")
         _componentGenerators[HwtOps.EQ] = ComponentGeneratorICMP_EQ_NE(self, genNamePrefix, "eq", HwtOps.EQ)
         _componentGenerators[HwtOps.NE] = ComponentGeneratorICMP_EQ_NE(self, genNamePrefix, "ne", HwtOps.NE)
+        _componentGenerators[OP_MUL_HL] = ComponentGeneratorMUL_HL(self, genNamePrefix, "mul_hl")
         _componentGenerators[OP_CTLZ] = ComponentGeneratorBitcount(self, CountLeadingZeros, genNamePrefix, "ctlz")
         _componentGenerators[OP_CTTZ] = ComponentGeneratorBitcount(self, CountTrailingZeros, genNamePrefix, "cttz")
         _componentGenerators[OP_CTPOP] = ComponentGeneratorBitcount(self, Ctpop, genNamePrefix, "ctpop")
