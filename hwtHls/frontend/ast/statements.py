d24a358bfc5d85b10b15a2060162c63d45950ead
Nic30 <nic30@seznam.cz>
2025-06-24 11:00:09 +0200

    refactor(frontend): rm deprecated HlsScopeDebugCodeSerializer

--- hwtHls/frontend/ast/statements.py
+++ hwtHls/frontend/ast/statements.py
@@ -1,6 +1,3 @@
-from io import StringIO
-
-from hdlConvertorAst.translate.common.name_scope import NameScope
 from hwt.doc_markers import internal
 from hwt.hdl.statements.statement import HdlStatement
 
@@ -14,17 +11,4 @@ class HlsStm(HdlStatement):
     @internal
     def _get_rtl_context(self) -> 'RtlNetlist':
         return self.parent.parent._rtlCtx
-
-    def __repr__(self):
-        from hwtHls.frontend.ast.debugCodeSerializer import HlsScopeDebugCodeSerializer
-        name_scope = NameScope(None, "debug", False, debug=True)
-        to_hdl = HlsScopeDebugCodeSerializer.TO_HDL_AST(name_scope)
-        to_hdl.debug = True
-        hdl = to_hdl.as_hdl(self)
-        buff = StringIO()
-        # import sys
-        # buff = sys.stdout
-        ser = HlsScopeDebugCodeSerializer.TO_HDL(buff)
-        ser.visit_iHdlObj(hdl)
-        return buff.getvalue()
 
