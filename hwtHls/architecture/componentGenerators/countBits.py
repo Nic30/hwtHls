1c456c05d7ad99c67b1fd5245c47b02a735b7be5
Nic30 <nic30@seznam.cz>
2025-07-15 11:51:10 +0200

    refactor(arch.countBits): extract to hwtLib

--- hwtHls/architecture/componentGenerators/countBits.py
+++ hwtHls/architecture/componentGenerators/countBits.py
@@ -3,12 +3,8 @@
 
 from typing import Type
 
-from hwt.code import Concat
-from hwt.hdl.commonConstants import b0
 from hwt.hdl.types.bits import HBits
 from hwt.hdl.types.bitsRtlSignal import HBitsRtlSignal
-from hwt.mainBases import RtlSignalBase
-from hwt.math import isPow2, log2ceil
 from hwt.pyUtils.setList import SetList
 from hwt.pyUtils.typingFuture import override
 from hwt.serializer.mode import serializeParamsUniq
@@ -17,110 +13,10 @@ from hwtHls.architecture.componentGeneratorUtils import replaceHlsNetNodeOperato
 from hwtHls.architecture.componentGenerators.baseALU1HwModule import _BaseALU1HwModule
 from hwtHls.architecture.componentGenerators.ctpop import Ctpop
 from hwtHls.frontend.pyBytecode import hlsBytecode
-from hwtHls.frontend.pragmaPreproc import PyBytecodeInline
 from hwtHls.netlist.nodes.node import HlsNetNode
 from hwtHls.netlist.nodes.ops import HlsNetNodeOperator
 from hwtHls.platform.opRealizationMeta import OpRealizationMeta
-from pyMathBitPrecise.bit_utils import mask, next_power_of_2
-
-
-# https://electronics.stackexchange.com/questions/196914/verilog-synthesize-high-speed-leading-zero-count
-# https://content.sciendo.com/view/journals/jee/66/6/article-p329.xml?language=en
-# [1]. Nebojša Z. Milenković and Vladimir V. Stanković and Miljana Lj. Milić, “MODULAR DESIGN OF FAST LEADING ZEROS COUNTING CIRCUIT”, Journal of ELECTRICAL ENGINEERING, VOL. 66, NO. 6, 2015, 329–333
-# https://github.com/tomverbeure/math?tab=readme-ov-file#leading-zero-counter-lzc-and-leading-zero-anticipor-lza
-@hlsBytecode
-def _countLeadingRecurse(dataIn: RtlSignalBase[HBits], bitValToCount: int):
-    """
-    Construct a balanced tree for counter of leading 0/1
-
-    :attention: result is not final result, it is only for 0 to width-1 values
-
-    """
-    assert bitValToCount in (0, 1), bitValToCount
-    inWidth = dataIn._dtype.bit_length()
-    if inWidth == 2:
-        if bitValToCount == 0:
-            return ~dataIn[1]
-        else:
-            return dataIn[1]
-    else:
-        assert inWidth > 2, inWidth
-        assert inWidth % 2 == 0, inWidth
-        lhs = dataIn[:inWidth // 2]
-        rhs = dataIn[inWidth // 2:]
-        if bitValToCount == 0:
-            leftFull = lhs._eq(0)
-        else:
-            leftFull = lhs._eq(mask(lhs._dtype.bit_length()))
-
-        in_ = lhs._dtype.from_py(None)
-        if leftFull:
-            in_ = rhs
-        else:
-            in_ = lhs
-
-        halfCount = PyBytecodeInline(_countLeadingRecurse)(in_, bitValToCount)
-        return Concat(leftFull, halfCount)
-
-
-@hlsBytecode
-def _countTrailingRecurse(dataIn: RtlSignalBase[HBits], bitValToCount: int):
-    """
-    Version of :func:`~._countLeadingRecurse` which counts from the back of the vector (upper bits first)
-    """
-    assert bitValToCount in (0, 1), bitValToCount
-    inWidth = dataIn._dtype.bit_length()
-    if inWidth == 2:
-        if bitValToCount == 0:
-            return ~dataIn[0]
-        else:
-            return dataIn[0]
-    else:
-        assert inWidth > 2, inWidth
-        assert inWidth % 2 == 0, inWidth
-        lhs = dataIn[:inWidth // 2]
-        rhs = dataIn[inWidth // 2:]
-        if bitValToCount == 0:
-            leftFull = rhs._eq(0)
-        else:
-            leftFull = rhs._eq(mask(rhs._dtype.bit_length()))
-
-        in_ = rhs._dtype.from_py(None)
-        if leftFull:
-            in_ = lhs
-        else:
-            in_ = rhs
-
-        halfCount = PyBytecodeInline(_countTrailingRecurse)(in_, bitValToCount)
-        return Concat(leftFull, halfCount)
-
-
-@hlsBytecode
-def countBits(dataIn: RtlSignalBase[HBits], bitValToCount: int, leading: bool):
-    """
-    :param bitValToCount: parameter to switch between count of zeros and ones
-    :param leading: flag which switches between leading (from MSB side) and trailing (from LSB side) count
-    :returns: number of bits set to bitValToCount value
-    """
-    inWidth = dataIn._dtype.bit_length()
-    assert bitValToCount in (0, 1), bitValToCount
-    assert isinstance(leading, bool), leading
-
-    if bitValToCount == 0:
-        full = dataIn._eq(0)
-    else:
-        full = dataIn._eq(mask(inWidth))
-
-    countFn = _countLeadingRecurse if leading else _countTrailingRecurse
-    halfCount = PyBytecodeInline(countFn)(dataIn, bitValToCount)
-    dataOut = HBits(log2ceil(inWidth + 1)).from_py(None)
-    if full:
-        # all bits are of counted value -> return max value
-        dataOut = inWidth
-    else:
-        dataOut = Concat(b0, halfCount)
-
-    return dataOut
+from hwtLib.logic.countLeading import countBits
 
 
 @serializeParamsUniq
@@ -130,27 +26,9 @@ class CountLeadingZeros(_BaseALU1HwModule):
     def hwDeclr(self):
         Ctpop.hwDeclr(self)
 
-    def _trimToOutSize(self, isExactlyPow2: bool, result: RtlSignalBase) -> RtlSignalBase:
-        """
-        Trim result in the case that input had to be extended to work correctly with bit count algorithm
-        """
-        if isExactlyPow2:
-            return result
-        else:
-            return result._trunc(self._getTypeOfIo(self.data_out).bit_length())
-
     @hlsBytecode
     def aluFn(self, inp: HBitsRtlSignal):
-        DATA_WIDTH = self.T.bit_length()
-        isExactlyPow2 = isPow2(DATA_WIDTH)
-        if isExactlyPow2:
-            _i = inp
-        else:
-            nextPow2 = next_power_of_2(DATA_WIDTH, 64)
-            # add padding from LSB, 1 is neutral element
-            _i = Concat(inp, HBits(nextPow2 - DATA_WIDTH).getAllOnesValue())
-        res = PyBytecodeInline(countBits)(_i, 0, True)
-        return self._trimToOutSize(isExactlyPow2, res)
+        return countBits(inp, 0, True)
 
 
 @serializeParamsUniq
@@ -158,16 +36,7 @@ class CountTrailingZeros(CountLeadingZeros):
 
     @hlsBytecode
     def aluFn(self, inp: HBitsRtlSignal):
-        DATA_WIDTH = self.T.bit_length()
-        isExactlyPow2 = isPow2(DATA_WIDTH)
-        if isExactlyPow2:
-            _i = inp
-        else:
-            nextPow2 = next_power_of_2(DATA_WIDTH, 64)
-            # add padding from MSB, 1 is neutral element
-            _i = Concat(HBits(nextPow2 - DATA_WIDTH).getAllOnesValue(), inp)
-        res = PyBytecodeInline(countBits)(_i, 0, False)
-        return self._trimToOutSize(isExactlyPow2, res)
+        return countBits(inp, 0, False)
 
 
 @serializeParamsUniq
@@ -175,16 +44,7 @@ class CountLeadingOnes(CountLeadingZeros):
 
     @hlsBytecode
     def aluFn(self, inp: HBitsRtlSignal):
-        DATA_WIDTH = self.T.bit_length()
-        isExactlyPow2 = isPow2(DATA_WIDTH)
-        if isExactlyPow2:
-            _i = inp
-        else:
-            nextPow2 = next_power_of_2(DATA_WIDTH, 64)
-            # add padding from LSB, 0 is neutral element
-            _i = Concat(inp, HBits(nextPow2 - DATA_WIDTH).from_py(0))
-        res = PyBytecodeInline(countBits)(_i, 1, True)
-        return self._trimToOutSize(isExactlyPow2, res)
+        return countBits(inp, 1, True)
 
 
 @serializeParamsUniq
@@ -192,16 +52,7 @@ class CountTrailingOnes(CountLeadingZeros):
 
     @hlsBytecode
     def aluFn(self, inp: HBitsRtlSignal):
-        DATA_WIDTH = self.T.bit_length()
-        isExactlyPow2 = isPow2(DATA_WIDTH)
-        if isExactlyPow2:
-            _i = inp
-        else:
-            nextPow2 = next_power_of_2(DATA_WIDTH, 64)
-            # add padding from MSB, 0 is neutral element
-            _i = Concat(HBits(nextPow2 - DATA_WIDTH).from_py(0), inp)
-        res = PyBytecodeInline(countBits)(_i, 1, False)
-        return self._trimToOutSize(isExactlyPow2, res)
+        return countBits(inp, 1, False)
 
 
 class ComponentGeneratorBitcount(ComponentGenerator):
