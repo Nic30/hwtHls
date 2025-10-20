00696be8b9e735f96aa0b75782c2bb5fae82b134
Nic30 <nic30@seznam.cz>
2025-10-20 16:02:33 +0200

    docs

--- hwtHls/netlist/__init__.py
+++ hwtHls/netlist/__init__.py
@@ -9,4 +9,11 @@ The process of translation of LLVM MIR to HlsNetlist is described in :class:`fro
 The HlsNetlist is composed of node instances :mod:`hwtHls.netlist.nodes` which do have input/output ports
 which can be connected together (1:N).
 The connection information is stored in node properties and can be accessed from both directions (src->dst, dst->src).
+
+HlsNetlist in comparison with LLVM SelectionDAG 
+* nodes have timing info per operand with clock and subclok resolution, (vs number of cycles per instruction in SelectionDAG)
+* nodes are grouped into ArchElements and scheduling aggregates to limit the scope where the node can move in schedule, (instead of BasicBlocks/Functions)
+* nodes are translated to RTL instead to instruction
+* node classes may be defined by user 
+* port types may be user defined
 """
