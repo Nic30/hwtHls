c513e11df875c8d19472a0b20f790c12e2345e24
Nic30 <nic30@seznam.cz>
2025-10-22 19:04:23 +0200

    style

--- tests/io/amba/axi4Stream/axi4sCopyByteByByte_test.py
+++ tests/io/amba/axi4Stream/axi4sCopyByteByByte_test.py
@@ -40,7 +40,8 @@ class Axi4SPacketCopyByteByByteTC(BaseAxi4SPktInPktOutTC):
 
     def test_2B_to_1B(self):
         PKT_CNT = 6
-        self._test(2 * 8, 8, [self._rand.randint(1, 4) for _ in range(PKT_CNT)])
+        FRAME_LENGTHS = [self._rand.randint(1, 4) for _ in range(PKT_CNT)]
+        self._test(2 * 8, 8, FRAME_LENGTHS)
 
     def test_1B_to_2B(self):
         PKT_CNT = 6
@@ -50,28 +51,27 @@ class Axi4SPacketCopyByteByByteTC(BaseAxi4SPktInPktOutTC):
         PKT_CNT = 6
         self._test(3 * 8, 3 * 8, [self._rand.randint(1, 9) for _ in range(PKT_CNT)])
 
-    # [todo] mir does not produce any packets
-    # def test_4B(self):
-    #     PKT_CNT = 6
-    #     self._test(4 * 8, 4 * 8, [self._rand.randint(1, 12) for _ in range(PKT_CNT)])
+    def test_4B(self):
+        PKT_CNT = 6
+        self._test(4 * 8, 4 * 8, [self._rand.randint(1, 12) for _ in range(PKT_CNT)])
 
-    def _testUnrollForEx(self, WORD_BYTE_CNT:int, PKT_CNT=6):
+    def _testUnrollForRx(self, WORD_BYTE_CNT:int, PKT_CNT=6):
         self._test(WORD_BYTE_CNT * 8, WORD_BYTE_CNT * 8, [self._rand.randint(1, WORD_BYTE_CNT * 3) for _ in range(PKT_CNT)], UNROLL=PyBytecodeStreamLoopUnroll)
 
     def test_1B_PyBytecodeStreamLoopUnroll_forRx(self):
-        self._testUnrollForEx(1)
+        self._testUnrollForRx(1)
 
     def test_2B_PyBytecodeStreamLoopUnroll_forRx(self):
-        self._testUnrollForEx(2)
+        self._testUnrollForRx(2)
 
     def test_3B_PyBytecodeStreamLoopUnroll_forRx(self):
-        self._testUnrollForEx(3)
+        self._testUnrollForRx(3)
 
     def test_4B_PyBytecodeStreamLoopUnroll_forRx(self):
-        self._testUnrollForEx(4)
+        self._testUnrollForRx(4)
 
     def test_16B_PyBytecodeStreamLoopUnroll_forRx(self):
-        self._testUnrollForEx(16)
+        self._testUnrollForRx(16)
 
     # :todo: unroll before stream lowering otherwise code explodes
     @expectedFailure
@@ -94,11 +94,11 @@ if __name__ == "__main__":
     from hwtHls.platform.debugBundle import HlsDebugBundle, LLVM_CLI_COMMON_OPTS
     from hwtHls.platform.virtual import VirtualHlsPlatform
     m = Axi4SPacketCopyByteByByte()
-    m.UNROLL = PyBytecodeStreamLoopUnroll  # PyBytecodeLLVMLoopUnroll(True, 2)
+    # m.UNROLL = PyBytecodeStreamLoopUnroll  # PyBytecodeLLVMLoopUnroll(True, 2)
     # m.UNROLL = PyBytecodeLLVMLoopUnroll(True, 2)
-    # m.UNROLL = None
-    m.DATA_WIDTH = 2 * 8
-    m.OUT_DATA_WIDTH = 2 * 8
+    m.UNROLL = None
+    m.DATA_WIDTH = 4 * 8
+    m.OUT_DATA_WIDTH = 4 * 8
     m.CLK_FREQ = int(1e6)
     p = VirtualHlsPlatform(debugFilter={
       *HlsDebugBundle.ALL_RELIABLE,
@@ -110,11 +110,11 @@ if __name__ == "__main__":
         # LLVM_CLI_COMMON_OPTS.PRINT_AFTER_ALL
        ],
     )
-    # print(to_rtl_str(m, target_platform=p))
+    print(to_rtl_str(m, target_platform=p))
 
     import unittest
     testLoader = unittest.TestLoader()
-    # suite = unittest.TestSuite([Axi4SPacketCopyByteByByteTC("test_2B_unroll2")])
     suite = testLoader.loadTestsFromTestCase(Axi4SPacketCopyByteByByteTC)
+    # suite = unittest.TestSuite([Axi4SPacketCopyByteByByteTC("test_4B_unroll2")])
     runner = unittest.TextTestRunner(verbosity=3)
     runner.run(suite)
