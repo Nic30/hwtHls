489a4d9d7e89b0e80c26b1aaadd53596dce48f53
Nic30 <nic30@seznam.cz>
2025-10-22 18:50:08 +0200

    test(llvm.ir): HwtHlsSimplifyCFGPass_TC prune extracted tests

--- tests/llvmIr/HwtHlsSimplifyCFGPass_test.py
+++ tests/llvmIr/HwtHlsSimplifyCFGPass_test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from hwtHls.llvm.llvmIr import LlvmCompilationBundle, Function
+from hwtHls.llvm.llvmIr import LlvmCompilationBundle, Function, verifyFunction
 from tests.llvmIr.baseLlvmIrTC import BaseLlvmIrTC
 
 
@@ -9,7 +9,9 @@ class HwtHlsSimplifyCFGPass_TC(BaseLlvmIrTC):
     __FILE__ = __file__
 
     def _runTestOpt(self, llvm:LlvmCompilationBundle, *args, **kwargs) -> Function:
-        return llvm._testHwtHlsSimplifyCFGPass(*args, **kwargs)
+        F = llvm._testHwtHlsSimplifyCFGPass(*args, **kwargs)
+        assert not verifyFunction(F)
+        return F
 
     def test_loadMerge(self):
         llvmIr = """\
@@ -206,7 +208,7 @@ class HwtHlsSimplifyCFGPass_TC(BaseLlvmIrTC):
         
         bb1:
           %byte_cnt1.0 = phi i16 [ 0, %bb0 ], [ %5, %bb4 ]
-          %i_read1 = call i19 @hwtHls.streamRead.p2.i64.i19(ptr addrspace(2) %i, i64 16) #4
+          %i_read1 = load volatile i19, ptr addrspace(2) %i, align 1
           %0 = call i1 @hwtHls.bitRangeGet.i19.i6.i1.16(i19 %i_read1, i6 16) #2
           %1 = xor i1 %0, true
           %2 = call i1 @hwtHls.bitRangeGet.i19.i6.i1.17(i19 %i_read1, i6 17) #2
@@ -233,60 +235,210 @@ class HwtHlsSimplifyCFGPass_TC(BaseLlvmIrTC):
         """
         self._test_ll(llvmIr)
 
-    def test_phiToLogicalExp0(self):
+    def test_noSuboptimalLoopUnswitch(self):
+        # llvmIr = """\
+        # define void @test_noSuboptimalLoopUnswitch(ptr addrspace(1) %dataIn, ptr addrspace(2) %dataOut) {
+        # bb0:
+        #   br label %mainLoop
+        #
+        # mainLoop:                                         ; preds = %bb0, %_forwardPacket.ret
+        #   %isChild = phi i1 [ false, %bb0 ], [ %isChildInLatch, %_forwardPacket.ret ]
+        #   br i1 %isChild, label %copyLoop, label %mainLoop.split
+        #
+        # mainLoop.split:                                   ; preds = %mainLoop
+        #   %r0 = load volatile i17, ptr addrspace(1) %dataIn, align 4
+        #   %r0.last = call i1 @hwtHls.bitRangeGet.i17.i6.i1.16(i17 %r0, i6 16) #2
+        #   %0 = call i8 @hwtHls.bitRangeGet.i17.i6.i8.0(i17 %r0, i6 0) #2
+        #   %r1 = zext i8 %0 to i9
+        #   %r1.last = call i1 @hwtHls.bitRangeGet.i9.i5.i1.8(i9 %r1, i5 8) #2
+        #   %1 = call i17 @hwtHls.bitConcat.i8.i8.i1(i8 %0, i8 undef, i1 true) #2
+        #   %2 = select i1 %r1.last, i17 %1, i17 %r0
+        #   store volatile i17 %2, ptr addrspace(2) %dataOut, align 4
+        #   %merge = or i1 %r0.last, %r1.last
+        #   br i1 %merge, label %_forwardPacket.ret.oldLatch, label %copyLoop
+        #
+        # copyLoop:                                         ; preds = %mainLoop.split, %mainLoop
+        #   %r2 = load volatile i17, ptr addrspace(1) %dataIn, align 4
+        #   %r2.last = call i1 @hwtHls.bitRangeGet.i17.i6.i1.16(i17 %r2, i6 16) #2
+        #   %3 = call i8 @hwtHls.bitRangeGet.i17.i6.i8.0(i17 %r2, i6 0) #2
+        #   %r3 = zext i8 %3 to i9
+        #   %r3.last = call i1 @hwtHls.bitRangeGet.i9.i5.i1.8(i9 %r3, i5 8) #2
+        #   %4 = call i17 @hwtHls.bitConcat.i8.i8.i1(i8 %3, i8 undef, i1 true) #2
+        #   %5 = select i1 %r3.last, i17 %4, i17 %r2
+        #   store volatile i17 %5, ptr addrspace(2) %dataOut, align 4
+        #   %merge2 = or i1 %r2.last, %r3.last
+        #   br i1 %merge2, label %_forwardPacket.ret.oldLatch, label %_forwardPacket.ret
+        #
+        # _forwardPacket.ret.oldLatch:                      ; preds = %copyLoop, %mainLoop.split
+        #   br label %_forwardPacket.ret
+        #
+        # _forwardPacket.ret:                               ; preds = %copyLoop, %_forwardPacket.ret.oldLatch
+        #   %isChildInLatch = phi i1 [ true, %copyLoop ], [ false, %_forwardPacket.ret.oldLatch ]
+        #   br label %mainLoop
+        # }
+        # """
         llvmIr = """\
-        define void @test_phiToLogicalExp0(ptr addrspace(1) %cIn, ptr addrspace(2) %out) {
-        bb.entry:
-          br label %bb0.guard
+        define void @test_noSuboptimalLoopUnswitch(ptr addrspace(1) %dataIn, ptr addrspace(2) %dataOut) {
+        bb0:
+          br label %mainLoop
+        
+        mainLoop:                                         ; preds = %bb0, %_forwardPacket.ret
+          %isChild = phi i1 [ false, %bb0 ], [ %isChildInLatch, %_forwardPacket.ret ]
+          br i1 %isChild, label %copyLoop, label %mainLoop.split
+        
+        mainLoop.split:                                   ; preds = %mainLoop
+          %r0 = load volatile i17, ptr addrspace(1) %dataIn, align 4
+          %r0.last = call i1 @hwtHls.bitRangeGet.i17.i6.i1.16(i17 %r0, i6 16) #2
+          %0 = call i8 @hwtHls.bitRangeGet.i17.i6.i8.0(i17 %r0, i6 0) #2
+          %r1 = zext i8 %0 to i9
+          %r1.last = call i1 @hwtHls.bitRangeGet.i9.i5.i1.8(i9 %r1, i5 8) #2
+          %1 = call i17 @hwtHls.bitConcat.i8.i8.i1(i8 %0, i8 undef, i1 true) #2
+          %2 = select i1 %r1.last, i17 %1, i17 %r0
+          store volatile i17 %2, ptr addrspace(2) %dataOut, align 4
+          %merge = or i1 %r0.last, %r1.last
+          br i1 %merge, label %_forwardPacket.ret.oldLatch, label %_forwardPacket.ret.oldLatchToChild
+        
+        copyLoop:                                         ; preds = %mainLoop.split, %mainLoop
+          %r2 = load volatile i17, ptr addrspace(1) %dataIn, align 4
+          %r2.last = call i1 @hwtHls.bitRangeGet.i17.i6.i1.16(i17 %r2, i6 16) #2
+          %3 = call i8 @hwtHls.bitRangeGet.i17.i6.i8.0(i17 %r2, i6 0) #2
+          %r3 = zext i8 %3 to i9
+          %r3.last = call i1 @hwtHls.bitRangeGet.i9.i5.i1.8(i9 %r3, i5 8) #2
+          %4 = call i17 @hwtHls.bitConcat.i8.i8.i1(i8 %3, i8 undef, i1 true) #2
+          %5 = select i1 %r3.last, i17 %4, i17 %r2
+          store volatile i17 %5, ptr addrspace(2) %dataOut, align 4
+          %merge2 = or i1 %r2.last, %r3.last
+          br i1 %merge2, label %_forwardPacket.ret.oldLatch, label %_forwardPacket.ret
+        
+        _forwardPacket.ret.oldLatch:                      ; preds = %copyLoop, %mainLoop.split
+          br label %_forwardPacket.ret
+        
+        _forwardPacket.ret.oldLatchToChild:                      ; preds = %copyLoop, %mainLoop.split
+          br label %_forwardPacket.ret
+        
+        _forwardPacket.ret:                               ; preds = %copyLoop, %_forwardPacket.ret.oldLatchToChild ],, %_forwardPacket.ret.oldLatch
+          %isChildInLatch = phi i1 [ true, %copyLoop ], [ true, %_forwardPacket.ret.oldLatchToChild ], [ false, %_forwardPacket.ret.oldLatch ]
+          br label %mainLoop
+        }
+        """
+        self._test_ll(llvmIr)
 
-        bb0.guard:
-          %bb0.g.c = load volatile i1, ptr addrspace(1) %cIn, align 1
-          %bb0.e.c = load volatile i1, ptr addrspace(1) %cIn, align 1
-          %bb1.g.c = load volatile i1, ptr addrspace(1) %cIn, align 1
-          %bb1.e.c = load volatile i1, ptr addrspace(1) %cIn, align 1
-          %bb2.g.c = load volatile i1, ptr addrspace(1) %cIn, align 1
-          %bb2.e.c = load volatile i1, ptr addrspace(1) %cIn, align 1
-          br i1 %bb0.g.c, label %bb0.enabled, label %bb0.exit
+    def test_speculatePredecessor0(self):
+        # note: based on HlsPythonHwrange_fromInt2
+        llvmIr = """\
+        define void @test_speculatePredecessor0(ptr addrspace(1) %o) {
+        bb0:
+          br label %bb.loop1.head
+
+        bb.loop1.head:                                    ; preds = %bb0, %bb.loop0.latch
+          %i0 = phi i3 [ %2, %bb.loop0.latch ], [ 0, %bb0 ]
+          %i1 = call i2 @hwtHls.bitRangeGet.i3.i3.i2.0(i3 %i0, i3 0) #2
+          br label %bb.loop2.head
         
-        bb0.enabled:
-          br label %bb0.exit
+        bb.loop2.head:                                    ; preds = %bb.loop2.head, %bb.loop1.head
+          %i2 = phi i3 [ 0, %bb.loop1.head ], [ %1, %bb.loop2.head ]
+          %i3 = call i2 @hwtHls.bitRangeGet.i3.i3.i2.0(i3 %i2, i3 0) #2
+          %0 = call i8 @hwtHls.bitConcat.i2.i2.i4(i2 %i3, i2 %i1, i4 0) #2
+          store volatile i8 %0, ptr addrspace(1) %o, align 1
+          %1 = add i3 %i2, 1
+          %hwrange.continue4.not = icmp eq i3 %1, -4
+          %2 = add i3 %i0, 1
+          %hwrange.continue.not.le.le = icmp eq i3 %2, -4
+          br i1 %hwrange.continue4.not, label %bb.loop0.latch, label %bb.loop2.head
         
-        bb0.exit:
-          %bb0.en.0 = phi i1 [ true, %bb0.enabled ], [ false, %bb0.guard ]
-          br i1 %bb0.e.c, label %bb.preexit, label %bb1.guard
+        bb.loop0.latch:                                   ; preds = %bb.loop2.head
+          br label %bb.loop1.head
+        }
+        """
+        self._test_ll(llvmIr)
+
+    def test_speculatePredecessor1(self):
+        # :note: based on HlsPythonHwrange_fromInt2
+        # :note: test_speculatePredecessor0 in 1 additional loop
+        llvmIr = """\
+        define void @test_speculatePredecessor1(ptr addrspace(1) %o) {
+        bb0:
+          br label %bb.loop0.head
         
-        bb1.guard:
-          br i1 %bb1.g.c, label %bb1.enabled, label %bb1.exit
+        bb.loop0.head:                                    ; preds = %bb.loop0.latch, %bb0
+          br label %bb.loop1.head
         
-        bb1.enabled:
-          br label %bb1.exit
+        bb.loop1.head:                              ; preds = %bb.loop0.head, %bb.loop0.latch
+          %i0 = phi i3 [ 0, %bb.loop0.head ], [ %2, %bb.loop0.latch ]
+          %i1 = call i2 @hwtHls.bitRangeGet.i3.i3.i2.0(i3 %i0, i3 0) #2
+          br label %bb.loop2.head
         
-        bb1.exit:
-          %bb1.en.0 = phi i1 [ true, %bb1.enabled ], [ false, %bb1.guard ]
-          br i1 %bb1.e.c, label %bb.preexit, label %bb2.guard
+        bb.loop2.head:                        ; preds = %bb.loop2.head, %bb.loop1.head
+          %i2 = phi i3 [ 0, %bb.loop1.head ], [ %1, %bb.loop2.head ]
+          %i3 = call i2 @hwtHls.bitRangeGet.i3.i3.i2.0(i3 %i2, i3 0) #2
+          %0 = call i8 @hwtHls.bitConcat.i2.i2.i4(i2 %i3, i2 %i1, i4 0) #2
+          store volatile i8 %0, ptr addrspace(1) %o, align 1
+          %1 = add i3 %i2, 1
+          %hwrange.continue4.not = icmp eq i3 %1, -4
+          %2 = add i3 %i0, 1
+          %hwrange.continue.not.le.le = icmp eq i3 %2, -4
+          br i1 %hwrange.continue4.not, label %bb.loop0.latch, label %bb.loop2.head
         
-        bb2.guard:
-          br i1 %bb2.g.c, label %bb2.enabled, label %bb2.exit
+        bb.loop0.latch:                        ; preds = %bb.loop2.head
+          br i1 %hwrange.continue.not.le.le, label %bb.loop0.head, label %bb.loop1.head
+        }
+        """
+        self._test_ll(llvmIr)
+
+    def test_speculatePredecessor2(self):
+        # :note: based on HlsPythonHwrange_fromInt1_breakBefore
+        llvmIr = """\
+        define void @HlsPythonHwrange_fromInt1_breakBefore.mainThread(ptr addrspace(1) %o) {
+        bb0:
+          br label %bb.loop0.head
         
-        bb2.enabled:
-          br label %bb2.exit
+        bb.loop0.head:                              ; preds = %bb.loop0.latch, %bb0
+          %i0 = phi i4 [ 0, %bb0 ], [ %i0.be, %bb.loop0.latch ]
+          %0 = icmp eq i4 %i0, 4
+          %1 = zext i4 %i0 to i8
+          br i1 %0, label %bb2, label %bb1
         
-        bb2.exit:
-          %bb2.en.0 = phi i1 [ true, %bb2.enabled ], [ false, %bb2.guard ]
-          br i1 %bb2.e.c, label %bb.preexit, label %bb.exit
+        bb1:                              ; preds = %bb.loop0.head
+          store volatile i8 %1, ptr addrspace(1) %o, align 1
+          %2 = add nuw i4 %i0, 1
+          %hwrange.continue = icmp ne i4 %2, -8
+          br i1 %hwrange.continue, label %bb.loop0.latch, label %bb2
         
-        bb.preexit:
-          %bb1.en.1 = phi i1 [ false, %bb0.exit ], [ %bb1.en.0, %bb1.exit ], [ %bb1.en.0, %bb2.exit ]
-          %bb2.en.1 = phi i1 [ false, %bb0.exit ], [ false, %bb1.exit ], [ %bb2.en.0, %bb2.exit ]
+        bb2:                                   ; preds = %bb.loop0.head, %bb1
+          br label %bb.loop0.latch
+          
+        bb.loop0.latch:                     ; preds = %bb1, %bb2
+          %i0.be = phi i4 [ 0, %bb2 ], [ %2, %bb1 ]
+          br label %bb.loop0.head
+        }
+        """
+        self._test_ll(llvmIr)
+
+    def test_tryHoistFromCheapBlocksWithSwitchLikeCmpBr0(self):
+        # :note: the result should not change because bb.c0 is loop heaer
+        llvmIr = """\
+        define void @test_tryHoistFromCheapBlocksWithSwitchLikeCmpBr0(ptr addrspace(1) %o) {
+        bb0:
+          br label %bb.c0
+        
+        bb.c0:                                            ; preds = %bb.exit, %bb0
+          %i = phi i4 [ 0, %bb0 ], [ %i.be, %bb.exit ]
+          %0 = icmp eq i4 %i, 4
+          %1 = zext i4 %i to i8
+          br i1 %0, label %bb.def, label %bb.c1
+        
+        bb.c1:                                            ; preds = %bb.c0
+          store volatile i8 %1, ptr addrspace(1) %o, align 1
+          %2 = add nuw i4 %i, 1
+          %hwrange.continue10.not = icmp eq i4 %i, 7
+          br i1 %hwrange.continue10.not, label %bb.def, label %bb.exit
+        
+        bb.exit:                                          ; preds = %bb.def, %bb.c1
+          %i.be = phi i4 [ 0, %bb.def ], [ %2, %bb.c1 ]
+          br label %bb.c0
+        
+        bb.def:                                           ; preds = %bb.c1, %bb.c0
           br label %bb.exit
-        
-        bb.exit:
-          %bb1.en.2 = phi i1 [ %bb1.en.1, %bb.preexit ], [ %bb1.en.0, %bb2.exit ]
-          %bb2.en.2 = phi i1 [ %bb2.en.1, %bb.preexit ], [ %bb2.en.0, %bb2.exit ]
-          store volatile i1 %bb0.en.0, ptr addrspace(2) %out, align 1
-          store volatile i1 %bb1.en.2, ptr addrspace(2) %out, align 1
-          store volatile i1 %bb2.en.2, ptr addrspace(2) %out, align 1
-          ret void
         }
         """
         self._test_ll(llvmIr)
@@ -295,7 +447,7 @@ class HwtHlsSimplifyCFGPass_TC(BaseLlvmIrTC):
 if __name__ == "__main__":
     import unittest
     testLoader = unittest.TestLoader()
-    # suite = unittest.TestSuite([HwtHlsSimplifyCFGPass_TC('test_loadMerge')])
     suite = testLoader.loadTestsFromTestCase(HwtHlsSimplifyCFGPass_TC)
+    # suite = unittest.TestSuite([HwtHlsSimplifyCFGPass_TC('test_0')])
     runner = unittest.TextTestRunner(verbosity=3)
     runner.run(suite)
