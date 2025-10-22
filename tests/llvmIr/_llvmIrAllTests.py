e45925ff99387f56d97c42126f22a232309b1adf
Nic30 <nic30@seznam.cz>
2025-10-22 13:51:46 +0200

    test(llvm.ir): HwtHlsInstCombinePass_select_TC

--- tests/llvmIr/_llvmIrAllTests.py
+++ tests/llvmIr/_llvmIrAllTests.py
@@ -3,6 +3,7 @@
 
 from hwtLib.tests.all import unittestMain
 from tests.llvmIr.HwtHlsInstCombinePass_bitcountExtract_test import HwtHlsInstCombinePass_bitcountExtract_TC
+from tests.llvmIr.HwtHlsInstCombinePass_select_test import HwtHlsInstCombinePass_select_TC
 from tests.llvmIr.HwtHlsInstCombinePass_streamEoFThreading_test import HwtHlsInstCombinePass_streamEoFThreading_TC
 from tests.llvmIr.HwtHlsInstCombinePass_test import HwtHlsInstCombinePass_TC
 from tests.llvmIr.LoopRotationNormalizationPass_test import LoopRotationNormalizationPass_TC
@@ -30,6 +31,7 @@ llvmIr_TCs = [
     LlvmIrFunctionMutating_TC,
     SlicesToIndependentVariablesPass_TC,
     HwtHlsSimplifyCFGPass_TC,
+    HwtHlsInstCombinePass_select_TC,
     HwtHlsSimplifyCFGPass_phiToLogicalExp_TC,
     HwtHlsSimplifyCFGPass_streamWrite_TC,
     LoopRotationNormalizationPass_TC,
---
+++ tests/llvmIr/data/HwtHlsInstCombinePass_select_TC.tryReduceSelectInst_concatWithConst0.ll
@@ -0,0 +1,9 @@
+define void @tryReduceSelectInst_concatWithConst0(ptr addrspace(1) %dataIn, ptr addrspace(2) %condIn, ptr addrspace(3) %dataOut) {
+bb.0:
+  %c0 = load volatile i1, ptr addrspace(2) %condIn, align 1
+  %c1 = load volatile i1, ptr addrspace(2) %condIn, align 1
+  %v0 = call i2 @hwtHls.bitConcat.i1.i1(i1 true, i1 %c0) #1
+  %v1 = select i1 %c1, i2 0, i2 %v0
+  store volatile i2 %v1, ptr addrspace(3) %dataOut, align 2
+  ret void
+}
