b1caaf9a48c0acdd0f63afc0079425c4e6d87fed
Nic30 <nic30@seznam.cz>
2025-10-23 13:20:42 +0200

    test(crypto): add _cryptoAllTests

---
+++ tests/crypto/_cryptoAllTests.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from hwtLib.tests.all import unittestMain
+from tests.crypto.md5_test import Md5_TC
+from tests.testCaseUtils import testSuiteFromTCs
+
+
+crypto_TCs = [
+    Md5_TC,
+]
+
+if __name__ == '__main__':
+    unittestMain(testSuiteFromTCs(*crypto_TCs))
+
