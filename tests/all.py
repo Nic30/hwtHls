f32dd0e89f26443ab3c852ca7663861bc8bb0b24
Nic30 <nic30@seznam.cz>
2025-10-23 13:17:41 +0200

    test: update tests/all.py

--- tests/all.py
+++ tests/all.py
@@ -11,24 +11,24 @@ from tests.crypto.md5_test import Md5_TC
 from tests.frontend._frontendAllTests import frontend_TCs
 from tests.io._ioAllTests import io_TCs
 from tests.llvmIr._llvmIrAllTests import llvmIr_TCs
+from tests.llvmMir._llvmMirAllTests import llvmMir_TCs
+from tests.math._mathAllTests import llvmMath_TCs
 from tests.syntehesisChecks_test import HlsSynthesisChecksTC
 from tests.testCaseUtils import testSuiteFromTCs
-
 
 suite = testSuiteFromTCs(
     *bitOpt_TCs,
     *frontend_TCs,
-    *frontend_TCs,
     *io_TCs,
     *llvmIr_TCs,
     *llvmMir_TCs,
+    llvmMath_TCs,
     HlsSynthesisChecksTC,
-
     Md5_TC,
-
     HashTable_TC,
 )
 
 if __name__ == '__main__':
+    sys.setrecursionlimit(int(1e5))  # see bitOpt_TCs
     unittestMain(suite)
 
