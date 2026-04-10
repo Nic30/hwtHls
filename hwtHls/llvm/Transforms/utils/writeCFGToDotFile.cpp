35faa15a3591daf661c75bc8143b4d6461c9a352
Nic30 <nic30@seznam.cz>
2026-04-10 20:26:58 +0200

    feat(llvm.ir.writeCFGToDotFile): discardNewlyRequestedAnalysis option

--- hwtHls/llvm/Transforms/utils/writeCFGToDotFile.cpp
+++ hwtHls/llvm/Transforms/utils/writeCFGToDotFile.cpp
@@ -1,28 +1,66 @@
-#include <hwtHls/llvm/Transforms/utils/writeCFGToDotFile.h>
-#include <limits>
-#include <iostream>
 #include <fstream>
+#include <hwtHls/llvm/Transforms/utils/writeCFGToDotFile.h>
+#include <iostream>
+#include <limits>
 #include <llvm/Analysis/CFGPrinter.h>
+#include <llvm/Analysis/LoopInfo.h>
+#include <llvm/Analysis/PostDominators.h>
+#include <llvm/IR/Analysis.h>
+#include <llvm/IR/Dominators.h>
 #include <llvm/Support/GraphWriter.h>
 
-
 using namespace llvm;
+
 namespace hwtHls {
 
 // based on llvm/Analysis/CFGPrinter.cpp
 void writeCFGToDotFile(Function &F, const std::string &Filename,
-		llvm::FunctionAnalysisManager &FAM, bool debugMsgs, bool CFGOnly) {
-	auto *BFI = &FAM.getResult<BlockFrequencyAnalysis>(F);
-	auto *BPI = &FAM.getResult<BranchProbabilityAnalysis>(F);
-	//BlockFrequencyInfo *BFI = nullptr;
-	//BranchProbabilityInfo *BPI = nullptr;
+					   llvm::FunctionAnalysisManager &FAM, bool debugMsgs,
+					   bool CFGOnly, bool discardNewlyRequestedAnalysis) {
+	// requested by BlockFrequencyInfo, BranchProbabilityInfo
+	auto *LI = FAM.getCachedResult<LoopAnalysis>(F);
+	// requested by BranchProbabilityInfo
+	auto *DT = FAM.getCachedResult<DominatorTreeAnalysis>(F);
+	auto *PDT = FAM.getCachedResult<PostDominatorTreeAnalysis>(F);
+
+	auto *BFI = FAM.getCachedResult<BlockFrequencyAnalysis>(F);
+	auto *BPI = FAM.getCachedResult<BranchProbabilityAnalysis>(F);
+	bool hadBFI = BFI != nullptr;
+	bool hadBPI = BPI != nullptr;
+	if (!BFI) {
+		BFI = &FAM.getResult<BlockFrequencyAnalysis>(F);
+	}
+	if (!BPI) {
+		BPI = &FAM.getResult<BranchProbabilityAnalysis>(F);
+	}
 	writeCFGToDotFile(F, Filename, BFI, BPI, debugMsgs, CFGOnly);
+	if (discardNewlyRequestedAnalysis) {
+		auto PA = PreservedAnalyses::all();
+
+		if (!hadBPI) {
+			PA.abandon<BranchProbabilityAnalysis>();
+		}
+		if (hadBFI) {
+			PA.abandon<BlockFrequencyAnalysis>();
+		}
+		if (!LI) {
+			PA.abandon<LoopAnalysis>();
+		}
+		if (!PDT) {
+			PA.abandon<PostDominatorTreeAnalysis>();
+		}
+		if (!DT) {
+			PA.abandon<DominatorTreeAnalysis>();
+		}
+		FAM.invalidate(F, PA);
+	}
 }
 
 void writeCFGToDotFile(Function &F, const std::string &Filename,
-		BlockFrequencyInfo *BFI, BranchProbabilityInfo *BPI, bool debugMsgs,
-		bool CFGOnly) {
-	// for loop passes use flags of to provide these createFunctionToLoopPassAdaptor
+					   BlockFrequencyInfo *BFI, BranchProbabilityInfo *BPI,
+					   bool debugMsgs, bool CFGOnly) {
+	// for loop passes use flags of to provide these
+	// createFunctionToLoopPassAdaptor
 	assert(BFI && "required after llvm-16");
 	assert(BPI && "required after llvm-16");
 	if (debugMsgs)
@@ -30,7 +68,9 @@ void writeCFGToDotFile(Function &F, const std::string &Filename,
 
 	std::error_code EC;
 	raw_fd_ostream File(Filename, EC, sys::fs::OF_Text);
-	uint64_t MaxFreq = BFI != nullptr ? getMaxFreq(F, BFI) : (uint64_t)std::numeric_limits<uint64_t>::max;
+	uint64_t MaxFreq = BFI != nullptr
+						   ? getMaxFreq(F, BFI)
+						   : (uint64_t)std::numeric_limits<uint64_t>::max;
 	DOTFuncInfo CFGInfo(&F, BFI, BPI, MaxFreq);
 	CFGInfo.setHeatColors(false);
 	CFGInfo.setEdgeWeights(BFI && BPI);
@@ -46,4 +86,4 @@ void writeCFGToDotFile(Function &F, const std::string &Filename,
 		errs() << "\n";
 }
 
-}
+} // namespace hwtHls
--- hwtHls/llvm/Transforms/utils/writeCFGToDotFile.h
+++ hwtHls/llvm/Transforms/utils/writeCFGToDotFile.h
@@ -4,11 +4,14 @@
 
 namespace hwtHls {
 
+// :note: writeCFGToDotFile requires DT, PDT, LI, BFI, BPI to be not present or valid
 void writeCFGToDotFile(llvm::Function &F, const std::string &Filename,
 		llvm::BlockFrequencyInfo *BFI=nullptr, llvm::BranchProbabilityInfo *BPI=nullptr,
 		bool debugMsgs = false, bool CFGOnly = false);
+// :param discardNewlyRequestedAnalysis: useful when calling this during other transformation
+//       which modifies CFG and thus may invalidate requested analyses
 void writeCFGToDotFile(llvm::Function &F, const std::string &Filename,
 		llvm::FunctionAnalysisManager &FAM, bool debugMsgs = false,
-		bool CFGOnly = false);
+		bool CFGOnly = false,  bool discardNewlyRequestedAnalysis = true);
 
 }
