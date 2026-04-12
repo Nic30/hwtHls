b2301316c2d6d46fbd16fa5dfaf64ccd9f05c6b4
Nic30 <nic30@seznam.cz>
2026-04-12 17:31:26 +0200

    style: unused include rm

--- hwtHls/llvm/targets/Transforms/vregIfConversionPriv.h
+++ hwtHls/llvm/targets/Transforms/vregIfConversionPriv.h
@@ -25,7 +25,6 @@
 #include <llvm/Support/BranchProbability.h>
 #include <llvm/Support/CommandLine.h>
 #include <llvm/Support/raw_ostream.h>
-#include <algorithm>
 #include <functional>
 
 #include <hwtHls/llvm/targets/Analysis/VRegLiveins.h>
