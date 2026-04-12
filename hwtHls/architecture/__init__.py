2758594150e62eeab087d50a30de954ebc239cb8
Nic30 <nic30@seznam.cz>
2026-04-12 16:58:53 +0200

    docs

--- hwtHls/architecture/__init__.py
+++ hwtHls/architecture/__init__.py
@@ -1,19 +1,20 @@
 """
 This module contains code related to hardware architecture layer of this project.
 A hardware architecture is represented by netlist with only :class:`hwtHls.netlist.nodes.archElement.ArchElement` nodes at top level.
+
 :class:`hwtHls.netlist.nodes.archElement.ArchElement` aggregates other HlsNetNodes to a larger chunks
 and specifies synchronization scheme for nodes inside.
 
 :note: This module works with already generated architecture from netlist.
-    The initial netlist to architecture translation is described in :mode:`hwtHls.netlist`
+    The initial netlist to architecture translation is described in :py:mod:`hwtHls.netlist`
 
 There are several complicated things:
 * Architecture type and communication channel implementation significantly affect scheduling.
   * It is up to user to tune up initial parameters or to write a design space search loop. This module
    does not do rescheduling.
 * Nodes which are crossing clock boundaries and nodes with internal structure or custom synchronization type.
-  * The synchronization is allocated per clock cycle. The problem is that node may span more than 1 clock cycle.
-    (for example BRAM read, DSP48E1 block etc.) Often we can not simply cut nodes to pieces because of complex internal structure.
+  * The synchronization is allocated per clock window. The problem is that node may span more than 1 clock cycle.
+    (for example BRAM read, DSP48E1 block etc.) Offten we can not simply cut nodes to pieces because of complex internal structure.
     Parts of node may be in a different ArchElement. Various types of nodes with complex internal hierarchy and internal synchronization
     behavior are hard to analyze and to map into arbitrary parent circuit implemented by ArchElement instance.
   * Nodes are cut on clock window boundaries by :class:`hwtHls.netlist.transformation.multiClockNodeSplit.HlsNetlistPassMultiClockNodeSplit`
@@ -29,4 +30,7 @@ There are several complicated things:
   * During optimizations back pressure, ArchElement merging or rescheduling may result in some IO operations to be performed only together.
     The original IO order must be taken in account when implementing sync of ArchElement.
   * Flushing logic is generated in :class:`hwtHls.architecture.transformation.syncLowering.HlsArchPassSyncLowering`
+* Communication asynchronous to FSM state. It is offten the case that some registers in FSM must be modified
+  independently on FSM state. For example some loop state which is partly mapped to FSM may be restarted externaly from outside
+  of FSM or from state where it is modified trough IMMEDIATE channels. 
 """
