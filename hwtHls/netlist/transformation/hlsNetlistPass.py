df819c1532f18f51fd18fbd65fc6417d309a4733
Nic30 <nic30@seznam.cz>
2026-04-22 09:18:37 +0200

    style(HlsNetlist.HlsNetlistPass): rm PreservedAnalysisSet dep

--- hwtHls/netlist/transformation/hlsNetlistPass.py
+++ hwtHls/netlist/transformation/hlsNetlistPass.py
@@ -1,16 +1,16 @@
 from hwtHls.netlist.context import HlsNetlistCtx
-from hwtHls.preservedAnalysisSet import PreservedAnalysisSet
 
 
 class HlsNetlistPass():
 
     def runOnHlsNetlist(self, netlist: HlsNetlistCtx, *args, **kwargs):
+        #from hwtHls.preservedAnalysisSet import PreservedAnalysisSet
         for cb in netlist.callbacksBeforePass:
             cb(self.__class__, self, netlist)
         pa = self.runOnHlsNetlistImpl(netlist, *args, **kwargs)
         for cb in netlist.callbacksAfterPass:
             cb(self.__class__, self, netlist)
-        assert isinstance(pa, PreservedAnalysisSet), (self.__class__, "runOnHlsNetlistImpl should return PreservedAnalysisSet", pa)
+        # assert isinstance(pa, PreservedAnalysisSet), (self.__class__, "runOnHlsNetlistImpl should return PreservedAnalysisSet", pa)
         assert netlist.subNodes, ("Netlist was completly optimized out", self)
         netlist.invalidateAnalysisUsingPreservedAnalysisSet(pa)
 
