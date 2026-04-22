9ec26226870da69ed655002b8e34d1d348c85c8a
Nic30 <nic30@seznam.cz>
2026-04-22 09:52:46 +0200

    feat(HlsNetlist): cp HlsNetlistAnalysisPassRunScheduler dbg opts from p.
    * Initialize scheduler debug options from platform._debug.

--- hwtHls/netlist/analysis/schedule.py
+++ hwtHls/netlist/analysis/schedule.py
@@ -1,7 +1,32 @@
+from hwt.pyUtils.typingFuture import override
 from hwtHls.netlist.analysis.hlsNetlistAnalysisPass import HlsNetlistAnalysisPass
+from hwtHls.platform.fileUtils import outputFileGetter
 
 
 class HlsNetlistAnalysisPassRunScheduler(HlsNetlistAnalysisPass):
 
+    @override
     def runOnHlsNetlistImpl(self, netlist:"HlsNetlistCtx"):
-        netlist.scheduler.schedule()
+        from hwtHls.platform.debugBundle import HlsDebugBundle
+        DBG = netlist.platform._debug.isActivated
+        scheduler: "HlsScheduler" = netlist.scheduler
+        if DBG(HlsDebugBundle.DBG_4_0_hwscheduleTrace):
+            tracerFile, tracerFileDoClose = outputFileGetter(netlist.platform._debug.dir, HlsDebugBundle.DBG_4_0_hwscheduleTrace[1])(netlist.dbgSubdir)
+            scheduler._dbgTraceFile = tracerFile
+        else:
+            tracerFileDoClose = False
+
+        scheduler._dbgDumpAfterPhases = DBG(HlsDebugBundle.DBG_4_0_hwscheduleDumpAfterPhases)
+        scheduler._dbgCheckCycles = DBG(HlsDebugBundle.DBG_4_0_hwscheduleCheckCycles)
+        scheduler._dbgPrintPhaseBoundaries = DBG(HlsDebugBundle.DBG_4_0_hwschedulePrintPhaseBoundaries)
+
+        try:
+            netlist.scheduler.schedule()
+            netlist.flagIsScheduled = True
+        finally:
+            if tracerFileDoClose:
+                tracerFile.close()
+
+    @override
+    def invalidate(self, netlist: "HlsNetlistCtx"):
+        netlist.flagIsScheduled = False
