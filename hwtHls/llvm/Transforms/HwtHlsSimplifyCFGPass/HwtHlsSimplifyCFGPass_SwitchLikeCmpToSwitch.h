eef048b1de58931f5f84c76ee5a459cc754f38a7
Nic30 <nic30@seznam.cz>
2026-04-23 10:55:52 +0200

    docs

--- hwtHls/llvm/Transforms/HwtHlsSimplifyCFGPass/HwtHlsSimplifyCFGPass_SwitchLikeCmpToSwitch.h
+++ hwtHls/llvm/Transforms/HwtHlsSimplifyCFGPass/HwtHlsSimplifyCFGPass_SwitchLikeCmpToSwitch.h
@@ -7,12 +7,36 @@
 namespace hwtHls {
 
 /*
- * If branch in this block is conditional and is driven by some ICmp x, const check if child block has br driven also by some ICmp x, const
+ * If branch in this block is conditional and is driven by some ICmp x, const0 check if child block has br driven also by some ICmp x, const1
  * and check if successor block contains any and only cheap instructions.
  * Continue stacking blocks from successors while this condition is met. Once block group is found
  * hoist all (cheap) instructions to a block where search has started
  * :note: this is beneficial because it potentially allows for branches to be reduced to SwitchInst
- * :noteL SimplifyCFGOpt::simplifyCondBranch expect block to contain only branch to fold this to SwitchInst
+ * :note: SimplifyCFGOpt::simplifyCondBranch expect block to contain only branch to fold this to SwitchInst
+ *
+ * .. code-block:: llvm  
+ *     ; for pattern like
+ *     bb0:
+ *       br c==0, bbExit, bb1
+ *     bb1:
+ *       br c==1, bbExit, bb2
+ *    ...
+ *     bbn:
+ *       br c==n, bbExit, bbDefault
+ *
+ *     ; rewrite this to:
+ *     bb0.0:
+ *        switch c, default: bbDefault {
+ *           0: bb0.1
+ *           1: bb1
+ *           ...
+ *        }
+ *     bb0.1:
+ *        br bbExit
+ *     bb1:
+ *        br bbExit
+ *     ...
+ *
  * */
 bool tryHoistFromCheapBlocksWithSwitchLikeCmpBr(llvm::BranchInst *BI,
 		llvm::IRBuilder<> &Builder, llvm::DomTreeUpdater *DTU, bool & exprChanged);
