5a17cb65fa9c59776a8badac61d3c18677573a9a
Nic30 <nic30@seznam.cz>
2026-04-23 10:57:06 +0200

    style: auto format

--- hwtHls/llvm/Transforms/HwtHlsSimplifyCFGPass/HwtHlsSimplifyCFGPass_storeHoist.cpp
+++ hwtHls/llvm/Transforms/HwtHlsSimplifyCFGPass/HwtHlsSimplifyCFGPass_storeHoist.cpp
@@ -1,8 +1,8 @@
 #include <hwtHls/llvm/Transforms/HwtHlsSimplifyCFGPass/HwtHlsSimplifyCFGPass_storeHoist.h>
-#include <hwtHls/llvm/Transforms/HwtHlsSimplifyCFGPass/HwtHlsSimplifyCFG_priv.h>
 #include <hwtHls/llvm/Transforms/HwtHlsSimplifyCFGPass/HwtHlsSimplifyCFGUtils.h>
-#include <llvm/IR/Instructions.h>
+#include <hwtHls/llvm/Transforms/HwtHlsSimplifyCFGPass/HwtHlsSimplifyCFG_priv.h>
 #include <llvm/IR/Constants.h>
+#include <llvm/IR/Instructions.h>
 
 #include <hwtHls/llvm/intrinsic/metadataSideEffect.h>
 
@@ -10,9 +10,10 @@ using namespace llvm;
 
 namespace hwtHls {
 
+
 bool HwtHlsSimplifyCFGPass_storeHoist(llvm::BasicBlock &BB) {
-	SmallVector<BasicBlock*> unreachableBBs;
-	SmallVector<std::pair<BasicBlock*, StoreInst*>> toHoist;
+	SmallVector<BasicBlock *> unreachableBBs;
+	SmallVector<std::pair<BasicBlock *, StoreInst *>> toHoist;
 
 	StoreInst *representativeSt = nullptr;
 	for (auto *suc : successors(&BB)) {
@@ -52,7 +53,7 @@ bool HwtHlsSimplifyCFGPass_storeHoist(llvm::BasicBlock &BB) {
 		return false; // case for blocks without suc
 	assert(unreachableBBs.size() + toHoist.size() == succ_size(&BB));
 	auto BBTer = BB.getTerminator()->getIterator();
-	for (const auto& [suc, st] : toHoist) {
+	for (const auto &[suc, st] : toHoist) {
 		// hoist everything before st to BB
 		BB.splice(BBTer, suc, suc->begin(), st->getIterator());
 	}
@@ -60,13 +61,13 @@ bool HwtHlsSimplifyCFGPass_storeHoist(llvm::BasicBlock &BB) {
 	auto BBBr = dyn_cast<BranchInst>(&*BBTer);
 	auto BBSw = dyn_cast<SwitchInst>(&*BBTer);
 	Value *srcVal = nullptr;
-	for (const auto& [suc, st] : toHoist) {
+	for (const auto &[suc, st] : toHoist) {
 		auto v = st->getValueOperand();
 		if (srcVal) {
 			if (BBBr) {
-				assert(
-						BBBr->isConditional()
-								&& "If this br is not conditional there can not be multiple values");
+				assert(BBBr->isConditional() &&
+					   "If this br is not conditional there can not be "
+					   "multiple values");
 				auto c = BBBr->getCondition();
 				if (BBBr->getSuccessor(0) == suc) {
 					srcVal = SelectInst::Create(c, v, srcVal, "", BBTer);
@@ -76,15 +77,19 @@ bool HwtHlsSimplifyCFGPass_storeHoist(llvm::BasicBlock &BB) {
 
 			} else if (BBSw) {
 				if (BBSw->getDefaultDest() == suc) {
-					assert(!srcVal && "[todo] Expect default successor to be the first");
+					assert(!srcVal &&
+						   "[todo] Expect default successor to be the first");
 				}
-				Value* caseC = BBSw->getCondition();
+				Value *caseC = BBSw->getCondition();
 				bool sucFound = false;
-				for (auto case_: BBSw->cases()) {
+				for (auto case_ : BBSw->cases()) {
 					if (case_.getCaseSuccessor() == suc) {
-						assert(!srcVal && "[todo] Expect default successor to be the first");
-						Value * cv = case_.getCaseValue();
-						auto c = new ICmpInst(BBTer, CmpInst::Predicate::ICMP_EQ, caseC, cv, "");
+						assert(
+							!srcVal &&
+							"[todo] Expect default successor to be the first");
+						Value *cv = case_.getCaseValue();
+						auto c = new ICmpInst(
+							BBTer, CmpInst::Predicate::ICMP_EQ, caseC, cv, "");
 						srcVal = SelectInst::Create(c, v, srcVal, "", BBTer);
 						sucFound = true;
 						break;
@@ -92,8 +97,8 @@ bool HwtHlsSimplifyCFGPass_storeHoist(llvm::BasicBlock &BB) {
 				}
 				assert(sucFound);
 			} else {
-				llvm_unreachable(
-						"HwtHlsSimplifyCFGPassstoreHoist: NotImplemened: Unsupported terminator");
+				llvm_unreachable("HwtHlsSimplifyCFGPassstoreHoist: "
+								 "NotImplemened: Unsupported terminator");
 			}
 		} else {
 			srcVal = v;
@@ -101,7 +106,7 @@ bool HwtHlsSimplifyCFGPass_storeHoist(llvm::BasicBlock &BB) {
 	}
 	assert(srcVal);
 	auto isFirstSuc = true;
-	for (const auto& [suc, st] : toHoist) {
+	for (const auto &[suc, st] : toHoist) {
 		if (isFirstSuc) {
 			st->moveBefore(BBTer);
 			st->setOperand(0, srcVal);
