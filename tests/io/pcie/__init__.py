f52e393253d0b8a86756f9fe5b9838f07f08466f
Nic30 <nic30@seznam.cz>
2026-04-24 17:35:02 +0200

    docs

---
+++ tests/io/pcie/__init__.py
@@ -0,0 +1,13 @@
+# https://www.semisaga.com/2019/07/pcie-tlp-header-packet-formats-address.html
+# https://github.com/texane/vpcie/blob/master/doc/qemu/pciefw_qemu_doc
+# https://github.com/ChimeraTK/pciedummy-driver
+# https://www.linuxquestions.org/questions/linux-kernel-70/how-to-emulate-a-pci-device-in-linux-901554/
+# https://github.com/luizinhosuraty/pciemu
+# https://github.com/iAndrewChan/qemu-pci-emulator
+# https://github.com/twilfredo/qemu-spdm-emulation-guide
+# https://github.com/dgrafe/pci-mock
+# https://stackoverflow.com/questions/28315265/how-to-add-a-new-device-in-qemu-source-code
+
+# https://github.com/alexforencich/cocotbext-pcie
+# https://github.com/Xilinx/libsystemctlm-soc/blob/master/tests/rtl-bridges/pcie/refdesign-sim.md
+# https://github.com/Xilinx/libsystemctlm-soc/blob/master/docs/pcie-rtl-bridges/overview.md
