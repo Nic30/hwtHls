ce07dc1d55999f6f94f6d54a7ce8e21669a9d2d5
Nic30 <nic30@seznam.cz>
2026-05-05 00:49:44 +0200

    docs

---
+++ tests/math/shader/__init__.py
@@ -0,0 +1,7 @@
+"""
+Potentialy useful:
+https://jonathan2251.github.io/lbd/gpu.html
+https://github.com/vortexgpgpu/vortex
+https://ijctjournal.org/wp-content/uploads/2025/09/Emerging-Trends-and-Technologies-in-Graphics-Rendering-Pipeline.pdf
+
+"""
