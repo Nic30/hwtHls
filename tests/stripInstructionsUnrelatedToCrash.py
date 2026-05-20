8dfcf1d0f9de1e3808ff45523c431fae30570c00
Nic30 <nic30@seznam.cz>
2026-05-20 09:36:51 +0200

    feat(llmIrStripInstrucionsUnrelatedToCrash):nprocs, timeout option in py

--- tests/stripInstructionsUnrelatedToCrash.py
+++ tests/stripInstructionsUnrelatedToCrash.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
-from typing import Callable
+import multiprocessing
+from typing import Callable, Optional
 
 from hwtHls.llvm.llvmIr import LlvmCompilationBundle, SMDiagnostic, \
     parseIR, verifyModule
 from tests.llvmIr.baseLlvmIrTC import generateAndAppendHwtHlsFunctionDeclarations
 
 
-def llmIrStripInstrucionsUnrelatedToCrash(irStr: str, testFunction: Callable[[LlvmCompilationBundle], None]):
+def llmIrStripInstrucionsUnrelatedToCrash(irStr: str, testFunction: Callable[[LlvmCompilationBundle], None], nprocs=None,
+                                          logAfterChange=False, timeout:Optional[float]=None):
     """
     Attempt to remove as much instruction as possible while preserving crashing of the compiler.
     :note: Use this to obtain minimal example to trigger the bug.
@@ -24,8 +26,11 @@ def llmIrStripInstrucionsUnrelatedToCrash(irStr: str, testFunction: Callable[[Ll
         llvm.main = fns[0]
         # name = llvm.main.getName().str()
     if verifyModule(M):
-        raise AssertionError()
+        raise AssertionError("The input module itself is already broken (this test that the function breaks on working code)")
         llvm = LlvmCompilationBundle(scopeName, [])
-    llvm._testStripInstrucionUnrelatedToCrash(6, testFunction)
+
+    if nprocs is None:
+        nprocs = multiprocessing.cpu_count()
+    llvm._testStripInstrucionUnrelatedToCrash(nprocs, testFunction, logAfterChange=logAfterChange, timeout=timeout)
     return llvm
-    #print(str(llvm.main))
+    # print(str(llvm.main))
