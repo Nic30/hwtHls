4e1e9037d43accb43e3f57bb48df1e9ddf949555
Nic30 <nic30@seznam.cz>
2026-05-20 09:07:25 +0200

    fix(llvm.ir.mergeSetsBasedLivenessAnalysis): use ReversePostOrder for DJ

--- hwtHls/llvm/Analysis/mergeSetsBasedLivenessAnalysis/djGraph.cpp
+++ hwtHls/llvm/Analysis/mergeSetsBasedLivenessAnalysis/djGraph.cpp
@@ -1,4 +1,5 @@
 #include <hwtHls/llvm/Analysis/mergeSetsBasedLivenessAnalysis/djGraph.h>
+#include <llvm/ADT/PostOrderIterator.h>
 
 using namespace llvm;
 
@@ -6,23 +7,24 @@ namespace hwtHls {
 
 // based on https://github.com/ethanblake4/control_flow_graph/blob/main/lib/src/dj_graph.dart#L11
 DJGraph computeDJGraph(llvm::Function &F, llvm::DominatorTree &DT) {
+	ReversePostOrderTraversal<Function*> RPOT(&F); // loop headers must be visited before loop blocks
 	DJGraph djGraph;
-	for (BasicBlock &BB : F) {
-		djGraph.addNode(&BB);
+	for (BasicBlock *BB : RPOT) {
+		djGraph.addNode(BB);
 	}
-	for (BasicBlock &BB : F) {
-		auto idom = DT.getNode(&BB)->getIDom();
+	for (BasicBlock *BB : RPOT) {
+		auto idom = DT.getNode(BB)->getIDom();
 		if (idom) {
 			auto et = DJ_EDGE_TYPE::DJ_EDGE_TYPE_D;
-			djGraph.addEdge(idom->getBlock(), &BB, et);
+			djGraph.addEdge(idom->getBlock(), BB, et);
 		}
 	}
 
-	for (BasicBlock &BB : F) {
-		for (auto *successor : successors(&BB)) {
-			if (DT.getNode(successor)->getIDom()->getBlock() != &BB) {
+	for (BasicBlock *BB : RPOT) {
+		for (auto *successor : successors(BB)) {
+			if (DT.getNode(successor)->getIDom()->getBlock() != BB) {
 				auto et = DJ_EDGE_TYPE::DJ_EDGE_TYPE_J;
-				djGraph.addEdge(&BB, successor, et);
+				djGraph.addEdge(BB, successor, et);
 			}
 		}
 	}
